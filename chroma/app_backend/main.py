f66bf65922d040b6f21dc8646b0bc5c5ae7ba819
Jeffrey Huber <jeff@trychroma.com>
2022-06-06 17:29:59 -0700

    fix

--- chroma/app_backend/main.py
+++ chroma/app_backend/main.py
@@ -1,7 +1,11 @@
 import os
 from os.path import isfile, getsize
 from api import app, db
-from flask import render_template
+from flask import request, jsonify, render_template
+from ariadne.constants import PLAYGROUND_HTML
+from ariadne import load_schema_from_path, make_executable_schema, \
+    graphql_sync, snake_case_fallback_resolvers, ObjectType
+from api.queries import resolve_embeddings
 
 def isSQLite3(filename):
     print(os.getcwd())
@@ -22,6 +26,37 @@ if not isSQLite3('chroma.db'):
 else:
   print('DB in place')
 
+# setting up graphql "routes"
+query = ObjectType("Query")
+query.set_field("embeddings", resolve_embeddings)
+
+mutation = ObjectType("Mutation")
+
+type_defs = load_schema_from_path("schema.graphql")
+schema = make_executable_schema(
+    type_defs, query, mutation, snake_case_fallback_resolvers
+)
+
 @app.route("/")
 def my_index():
     return render_template("index.html", flask_token="Hello world")
+
+# graphql playground
+@app.route("/graphql", methods=["GET"])
+def graphql_playground():
+    return PLAYGROUND_HTML, 200
+
+# the one api endpoint for graphql
+@app.route("/graphql", methods=["POST"])
+def graphql_server():
+    data = request.get_json()
+
+    success, result = graphql_sync(
+        schema,
+        data,
+        context_value=request,
+        debug=app.debug
+    )
+
+    status_code = 200 if success else 400
+    return jsonify(result), status_code
