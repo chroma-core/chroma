024b096de1ab63e409bacea380951fbc2b2f7176
atroyn <anton.troynikov@gmail.com>
2022-06-09 10:28:37 -0700

    Wired in UMAP projection in app backend - GQL issue.

--- chroma/app_backend/api/queries.py
+++ chroma/app_backend/api/queries.py
@@ -1,21 +1,19 @@
 import json
 
+from matplotlib import projections
+
 from api import chroma_data_manager
+from utils import umap_project
 
 def resolve_datapoints(obj, info):
      raw_embeddings = chroma_data_manager.get_embeddings()
-     print(len(raw_embeddings["embeddings"]["embeddings"]))
+     vectors = [emb["data"] for emb in raw_embeddings["embeddings"]["embeddings"]]
+
+     projections = umap_project(vectors)
+
+     data = [[proj[0], proj[1], { "class": "forest", "type": "production","ml_model_version": "v2"}] for proj in projections]
 
      payload = {
-         'data': json.dumps([
-                        [
-                            -0.36499756446397,
-                            -0.9535619122565171,
-                            {
-                                "class": "forest",
-                                "type": "production",
-                                "ml_model_version": "v2"
-                            }
-                        ],])
+         'data': json.dumps(str(data))
      }
      return payload
---
+++ chroma/app_backend/utils.py
@@ -0,0 +1,8 @@
+import umap
+
+# Project high-dimensional vectors to 2-D via UMAP (https://umap-learn.readthedocs.io)
+def umap_project(vectors):
+    reducer = umap.UMAP()
+    reducer.fit(vectors)
+    projection = reducer.transform(vectors)
+    return projection
