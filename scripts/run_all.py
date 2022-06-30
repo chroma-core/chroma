60bb28b518994d078cd06d441b92565dcba111ae
Jeffrey Huber <jeff@trychroma.com>
2022-06-30 05:34:56 -0700

    remove import that blocked startup

--- scripts/run_all.py
+++ scripts/run_all.py
@@ -4,7 +4,7 @@ import subprocess
 import time
 
 from chroma.cli.multi_command import MultiCommand, SubCommand
-from shared_runners import app_subcommand, frontend_subcommand, celery_subcommand, rabbit_subcommand
+from shared_runners import app_subcommand, frontend_subcommand, celery_subcommand
 
 if __name__ == "__main__":
     # os.getcwd will give the directory wherever the CLI is called
@@ -18,9 +18,6 @@ if __name__ == "__main__":
 
     celery_subcommand = celery_subcommand(base_dir, multicommand)
     multicommand.append_threaded_command(celery_subcommand.name, celery_subcommand)
-
-    # rabbit_subcommand = rabbit_subcommand(base_dir, multicommand)
-    # multicommand.append_threaded_command(rabbit_subcommand.name, rabbit_subcommand)
 
     frontend_subcommand = frontend_subcommand(base_dir, multicommand)
     multicommand.append_threaded_command(frontend_subcommand.name, frontend_subcommand)
