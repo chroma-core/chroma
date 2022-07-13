4c966baa0f9af8c22e0bae80a07881ae35ac8428
Jeffrey Huber <jeff@trychroma.com>
2022-07-13 11:43:37 -0700

    run redis before celery for dev make command

--- scripts/run_dev.py
+++ scripts/run_dev.py
@@ -4,7 +4,7 @@ import subprocess
 import time
 
 from chroma.cli.multi_command import MultiCommand, SubCommand
-from shared_runners import app_subcommand, frontend_subcommand, celery_subcommand
+from shared_runners import app_subcommand, frontend_subcommand, celery_subcommand, redis_subcommand
 
 if __name__ == "__main__":
     # os.getcwd will give the directory wherever the CLI is called
@@ -15,6 +15,9 @@ if __name__ == "__main__":
 
     app_subcommand = app_subcommand(base_dir, multicommand)
     multicommand.append_threaded_command(app_subcommand.name, app_subcommand)
+
+    redis_subcommand = redis_subcommand(base_dir, multicommand)
+    multicommand.append_threaded_command(redis_subcommand.name, redis_subcommand)
 
     celery_subcommand = celery_subcommand(base_dir, multicommand)
     multicommand.append_threaded_command(celery_subcommand.name, celery_subcommand)
