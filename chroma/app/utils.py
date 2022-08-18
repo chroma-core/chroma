a7c664be9f47751192af0a16a80ad0b79f95d34f
Jeffrey Huber <jeff@trychroma.com>
2022-08-18 15:43:47 -0700

    move sqlite check into utils

---
+++ chroma/app/utils.py
@@ -0,0 +1,12 @@
+from os.path import getsize, isfile
+
+def isSQLite3(filename):
+    if not isfile(filename):
+        return False
+    if getsize(filename) < 100:  # SQLite database file header is 100 bytes
+        return False
+
+    with open(filename, "rb") as fd:
+        header = fd.read(100)
+
+    return header[:16].decode("utf-8") == "SQLite format 3\x00"
