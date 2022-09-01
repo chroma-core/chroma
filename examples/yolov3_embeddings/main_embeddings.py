09889ae40e5f7b665a8b2dd9e092014aada89686
atroyn <anton.troynikov@gmail.com>
2022-09-01 19:31:37 +0200

    Storing label assoc, score, and iou (for assoced items) in metadata.

--- examples/yolov3_embeddings/main_embeddings.py
+++ examples/yolov3_embeddings/main_embeddings.py
@@ -1,6 +1,4 @@
 from __future__ import annotations
-from curses import meta
-from importlib.metadata import metadata
 import uuid
 import json
 import random
@@ -11,6 +9,7 @@ import numpy as np
 import torch
 from torch.utils.data import DataLoader
 from torch.autograd import Variable
+from pytorchyolo.utils.utils import bbox_iou
 
 from pytorchyolo.models import load_model
 from pytorchyolo.utils.utils import load_classes, non_max_suppression, xywh2xyxy, rescale_boxes
@@ -21,7 +20,8 @@ from pytorchyolo.utils.parse_config import parse_data_config
 from chroma.sdk import chroma_manager
 from chroma.sdk.utils import nn
 
-str_options = ['New York', 'San Francisco', 'Atlanta', 'Miami', 'Dallas', 'Chicago', 'DC']
+str_options = ["New York", "San Francisco", "Atlanta", "Miami", "Dallas", "Chicago", "DC"]
+
 
 def reshape_detections(inputs, num_anchors, no):
     bs, _, ny, nx = inputs.shape  # x(bs,255,_,_) to x(bs,3,_,_,85)
@@ -38,13 +38,15 @@ def reshape_context(inputs):
 def to_annotations_dict(boxes, category_ids, category_names, uids, metadatas):
     annotations = []
 
-    for box, category_id, category_name, uid, metadata in zip(boxes, category_ids, category_names, uids, metadatas):
+    for box, category_id, category_name, uid, metadata in zip(
+        boxes, category_ids, category_names, uids, metadatas
+    ):
         annotation = {
             "id": uid,
             "bbox": box.tolist(),
             "category_id": str(category_id),
             "category_name": category_name,
-            "metadata": metadata
+            "metadata": metadata,
         }
         annotations.append(annotation)
 
@@ -85,20 +87,6 @@ def infer(model, device, data_loader, class_names, chroma_storage: chroma_manage
         labels = []
         metadata_list = []
         for i, output in enumerate(outputs):
-            # Process detections
-            det_boxes = rescale_boxes(output, imgs.shape[-1], img_sizes[i, :2])[:, :4]
-            det_boxes[:, 2:4] = det_boxes[:, 2:4] - det_boxes[:, 0:2]  # xyxy2xywh
-            det_category_ids = output[:, -1].numpy().astype(int)
-            det_category_names = class_names[det_category_ids]
-            det_uids = [str(uuid.uuid4()) for i in range(len(det_category_ids))]
-            metadatas = [{
-                'quality': random.randint(0, 100),
-                'location': str_options[random.randint(0, 6)],
-            } for i in range(len(det_category_ids))]
-            det_annotations = to_annotations_dict(
-                det_boxes, det_category_ids, det_category_names, det_uids, metadatas
-            )
-            detection_inferences.append(det_annotations)
 
             # Process targets
             target = targets[targets[:, 0] == i]
@@ -112,18 +100,83 @@ def infer(model, device, data_loader, class_names, chroma_storage: chroma_manage
             target_cat_ids = target[:, 1].numpy().astype(int)
             target_cat_names = class_names[target_cat_ids]
             target_uids = [str(uuid.uuid4()) for i in range(len(target_cat_ids))]
-            metadatas = [{
-                'quality': random.randint(0, 100),
-                'location': str_options[random.randint(0, 6)]
-            } for i in range(len(target_cat_ids))]
+            metadatas = [
+                {"quality": random.randint(0, 100), "location": str_options[random.randint(0, 6)]}
+                for i in range(len(target_cat_ids))
+            ]
             target_annotations = to_annotations_dict(
                 target_boxes, target_cat_ids, target_cat_names, target_uids, metadatas
             )
             labels.append(target_annotations)
-            metadata_list.append( {
-                'quality': random.randint(0, 100),
-                'location': str_options[random.randint(0, 6)]
-            })
+
+            # Process detections
+            det_boxes = rescale_boxes(output, imgs.shape[-1], img_sizes[i, :2])[:, :4]
+            det_boxes[:, 2:4] = det_boxes[:, 2:4] - det_boxes[:, 0:2]  # xyxy2xywh
+            det_category_ids = output[:, -1].numpy().astype(int)
+            det_category_names = class_names[det_category_ids]
+            det_uids = [str(uuid.uuid4()) for i in range(len(det_category_ids))]
+
+            # Associate detections with labels
+            # We do this without rescaling to the original image, in the shape of the input
+            assoc_iou_threshold = 0.5
+            target[:, 2:] = xywh2xyxy(target[:, 2:])
+            target[:, 2:] *= imgs.shape[-1]
+
+            annotations = target[:, 1:]
+            target_labels = annotations[:, 0] if len(annotations) else []
+            pred_boxes = output[:, :4]
+            pred_scores = output[:, 4]
+            pred_labels = output[:, -1]
+
+            assocs = {}
+            ious = {}
+            scores = {}
+            if len(annotations):
+                for pred_i, (pred_box, pred_label) in enumerate(zip(pred_boxes, pred_labels)):
+                    scores[pred_i] = pred_scores[pred_i].item()
+                    # Skip if we've found all the annotations
+                    if len(assocs) == len(annotations):
+                        continue
+                    # Reject class mismatch
+                    if pred_label not in target_labels:
+                        continue
+
+                    # Filter target_boxes by pred_label so that we only match against boxes of our own label
+                    filtered_target_position, filtered_targets = zip(
+                        *filter(
+                            lambda x: target_labels[x[0]] == pred_label, enumerate(target_boxes)
+                        )
+                    )
+
+                    # Find the best matching target for our predicted box
+                    iou, target_box_filtered_index = bbox_iou(
+                        pred_box.unsqueeze(0), torch.stack(filtered_targets)
+                    ).max(0)
+
+                    # Remap the index in the list of filtered targets for that label to the index in the list with all targets.
+                    target_label_index = filtered_target_position[target_box_filtered_index]
+                    if iou >= assoc_iou_threshold and target_label_index not in assocs.values():
+                        assocs[pred_i] = target_label_index
+                        ious[pred_i] = iou.item()
+
+            metadatas = [
+                {
+                    "quality": random.randint(0, 100),
+                    "location": str_options[random.randint(0, 6)],
+                    "label_id": target_uids[assocs[i]] if i in assocs.keys() else "",
+                    "iou": ious[i] if i in ious.keys() else "",
+                    "score": scores[i] if i in scores.keys() else "",
+                }
+                for i in range(len(det_category_ids))
+            ]
+            det_annotations = to_annotations_dict(
+                det_boxes, det_category_ids, det_category_names, det_uids, metadatas
+            )
+            detection_inferences.append(det_annotations)
+
+            metadata_list.append(
+                {"quality": random.randint(0, 100), "location": str_options[random.randint(0, 6)]}
+            )
 
         # raise Exception(str(labels))
         chroma_storage.set_inferences(detection_inferences)
@@ -131,7 +184,6 @@ def infer(model, device, data_loader, class_names, chroma_storage: chroma_manage
         chroma_storage.set_metadata(metadata_list)
 
         ctx_embedding = reshape_context(layer_outputs[context_layer_index]).detach().cpu().numpy()
-        
 
         detection_embeddings = (
             torch.cat(
@@ -167,9 +219,7 @@ def infer(model, device, data_loader, class_names, chroma_storage: chroma_manage
 
         annotated_ctx_embs = []
         for ctx in ctx_embedding:
-            annotated_ctx_emb = [
-                {"target": None, "data": ctx.tolist()}
-            ]
+            annotated_ctx_emb = [{"target": None, "data": ctx.tolist()}]
             annotated_ctx_embs.append(annotated_ctx_emb)
 
         chroma_storage.set_ctx_embeddings(annotated_ctx_embs)
@@ -198,8 +248,8 @@ def main():
     class_object = []
     i = 0
     for class_name in class_names:
-        class_object.append({'supercategory': '', 'id': i, 'name': class_name})
-        i = i+1
+        class_object.append({"supercategory": "", "id": i, "name": class_name})
+        i = i + 1
 
     # Set the device
     device_name = "cpu"
@@ -224,8 +274,11 @@ def main():
     model = load_model(model_path=model_path, device=device, weights_path=weights_path)
     model.eval()
 
-    with chroma_manager.ChromaSDK(project_name="YOLO-1k", dataset_name="TestYolo1k", categories=json.dumps(class_object)) as chroma_storage:
+    with chroma_manager.ChromaSDK(
+        project_name="YOLO-1k", dataset_name="TestYolo1k", categories=json.dumps(class_object)
+    ) as chroma_storage:
         infer(model, device, dataloader, class_names, chroma_storage=chroma_storage)
+
 
 if __name__ == "__main__":
     main()
