9f35776a7174fe5f8fb9ad0c283004013941f4a5
Jesse Montrose <jesse@ninth.org>
2022-10-07 13:50:18 -0700

    restore both runs

--- exp/db/run.sh
+++ exp/db/run.sh
@@ -3,3 +3,6 @@
 ./db_run.py --train_input=mnist_train.jsonl.bz2 --prod_input=mnist_test.jsonl.bz2 \
 	    --scratch=/tmp \
 	    --db=pythondb
+./db_run.py --train_input=mnist_train.jsonl.bz2 --prod_input=mnist_test.jsonl.bz2 \
+	    --scratch=/tmp \
+	    --db=sqlite
