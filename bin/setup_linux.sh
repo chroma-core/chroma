edc6a8c1f4c0ccd9ba68315c0f5027533423c77c
Jeffrey Huber <jeff@trychroma.com>
2022-11-07 16:02:22 -0800

    actually add default scope, change pull branch to this one

--- bin/setup_linux.sh
+++ bin/setup_linux.sh
@@ -28,7 +28,7 @@ git clone https://oauth2:github_pat_11AAGZWEA0i4gAuiLWSPPV_j72DZ4YurWwGV6wm0RHBy
 
 #checkout the right branch
 cd chroma
-git checkout jeff/clickhouse
+git checkout jeff/packaging
 
 # run docker
 cd chroma-server
