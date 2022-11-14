3c68167d1d1fdce185388c004663fc2dc362c462
Jeffrey Huber <jeff@trychroma.com>
2022-11-13 21:48:42 -0800

    couldnt quite get all this working the way i wanted

--- chroma-server/chroma_server/test/test_api.py
+++ chroma-server/chroma_server/test/test_api.py
@@ -8,36 +8,13 @@ from ..api import app
 def anyio_backend():
     return "asyncio"
 
-
 @pytest.mark.anyio
 async def test_root():
     async with AsyncClient(app=app, base_url="http://test") as ac:
         response = await ac.get("/api/v1")
     assert response.status_code == 200
-    # assert response is an integer
     assert isinstance(response.json()["nanosecond heartbeat"], int)
-    # assert (
-    #     abs(response.json()["nanosecond heartbeat"] - int(1000 * time.time_ns())) < 3_000_000_000
-    # )  # a billion nanoseconds = 3s
 
-
-# async def post_one_record(ac):
-#     return await ac.post(
-#         "/api/v1/add",
-#         json={
-#             "embedding_data": [1.02, 2.03, 3.03],
-#             "input_uri": "https://example.com",
-#             "dataset": "coco",
-#             "category_name": "person",
-#         },
-#     )
-
-# space_key: Union[str, list]
-# embedding_data: list
-# input_uri: Union[str, list]
-# dataset: Union[str, list] = None
-# custom_quality_score: Union[float, list] = None
-# category_name: Union[str, list] = None
 async def post_batch_records(ac):
     return await ac.post(
         "/api/v1/add",
@@ -49,7 +26,6 @@ async def post_batch_records(ac):
             "space_key": ["test_space", "test_space"],
         },
     )
-
 
 @pytest.mark.anyio
 async def test_add_to_db():
--- chroma-server/chroma_server/test/test_benchmarks.py
+++ chroma-server/chroma_server/test/test_benchmarks.py
@@ -6,20 +6,56 @@ def anyio_backend():
     return "asyncio"
 
 
-def something(duration=0.000001):
-    """
-    Function that needs some serious benchmarking.
-    """
-    time.sleep(duration)
-    # You may return anything you want, like the result of a computation
-    return 123
+# def something(duration=0.000001):
+#     """
+#     Function that needs some serious benchmarking.
+#     """
+#     time.sleep(duration*2)
+#     # You may return anything you want, like the result of a computation
+#     return 123
+
+# @pytest.mark.anyio
+# def test_my_stuff(benchmark):
+#     # benchmark something
+#     result = benchmark(something)
+
+#     # Extra code, to verify that the run completed correctly.
+#     # Sometimes you may want to check the result, fast functions
+#     # are no good if they return incorrect results :-)
+#     assert result == 123
+
+import pytest
+import time
+from httpx import AsyncClient
+from ..api import app
+
+
+@pytest.fixture
+def anyio_backend():
+    return "asyncio"
+
+    
 
 @pytest.mark.anyio
-def test_my_stuff(benchmark):
-    # benchmark something
-    result = benchmark(something)
+async def test_root():
+    async with AsyncClient(app=app, base_url="http://test") as ac:
+        response = await ac.get("/api/v1")
+    assert response.status_code == 200
+    assert isinstance(response.json()["nanosecond heartbeat"], int)
 
-    # Extra code, to verify that the run completed correctly.
-    # Sometimes you may want to check the result, fast functions
-    # are no good if they return incorrect results :-)
-    assert result == 123
+async def post_batch_records(ac):
+    return await ac.post(
+        "/api/v1/add",
+        json={
+            "embedding_data": [[1.1, 2.3, 3.2], [1.2, 2.24, 3.2]],
+            "input_uri": ["https://example.com", "https://example.com"],
+            "dataset": ["training", "training"],
+            "category_name": ["person", "person"],
+            "space_key": ["test_space", "test_space"],
+        },
+    )
+
+@pytest.mark.anyio
+async def benchmark_add_to_db(benchmark):
+    async with AsyncClient(app=app, base_url="http://test") as ac:
+        response = benchmark(await(post_batch_records(ac)))
