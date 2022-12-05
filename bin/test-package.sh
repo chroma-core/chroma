5812dd84651f160b2b87ded6a01e898e8a7d69e8
Luke VanderHart <luke@vanderhart.net>
2022-12-05 14:00:50 -0500

    check package before release

---
+++ bin/test-package.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Verify PIP tarball
+tarball=$(readlink -f $1)
+if [ -f "$tarball" ]; then
+    echo "Testing PIP package from tarball: $tarball"
+else
+    echo "Could not find PIP package: $tarball"
+fi
+
+# Create temporary project dir
+dir=$(mktemp -d)
+
+echo "Building python project dir at $dir ..."
+
+cd $dir
+
+python3 -m venv venv
+
+source venv/bin/activate
+
+pip install $tarball
+
+python -c "import chroma; api = chroma.get_api(); print(api.heartbeat())"
