910d7d392ebf7cd0dbe1ed23d77504c193cb3e14
Hammad Bashir <hammadb@berkeley.edu>
2023-01-11 17:12:14 -0800

    remove debugging

--- chroma/server/fastapi/__init__.py
+++ chroma/server/fastapi/__init__.py
@@ -96,7 +96,6 @@ class FastAPI(chroma.server.Server):
 
 
     def process(self, process: ProcessEmbedding):
-        print(process)
         self._api.process(process.model_space, process.training_dataset_name, process.unlabeled_dataset_name)
         return True
 
