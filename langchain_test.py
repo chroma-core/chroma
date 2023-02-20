d4e30c105536510260259d42a4154b35fa814cdc
atroyn <anton.troynikov@gmail.com>
2023-02-20 13:51:57 -0800

    More testing

--- langchain_test.py
+++ langchain_test.py
@@ -1,6 +1,6 @@
 # Let's ask God some questions
 from langchain.vectorstores import Chroma
-from langchain.chains import VectorDBQAWithSourcesChain
+from langchain.chains import VectorDBQA
 from langchain.embeddings import OpenAIEmbeddings
 from langchain import OpenAI
 
@@ -11,6 +11,6 @@ persist_directory = 'chroma'
 openai_api_key = open('openai.key', 'r').read()
 
 docsearch = Chroma(collection_name=collection_name, persist_directory=persist_directory, embedding_function=OpenAIEmbeddings(openai_api_key=openai_api_key))
-chain = VectorDBQAWithSourcesChain.from_chain_type(OpenAI(temperature=0, openai_api_key=openai_api_key), chain_type="stuff", vectorstore=docsearch)
+chain = VectorDBQA.from_chain_type(OpenAI(temperature=0, openai_api_key=openai_api_key), chain_type="stuff", vectorstore=docsearch)
 
-chain({"question": "What is the greatest good?"}, return_only_outputs=True)
+print(chain({"query": "What is the greatest good?"}, return_only_outputs=True))
