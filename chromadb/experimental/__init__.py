70703a09769a1baa89e93104a9a9322fb8504160
atroyn <anton.troynikov@gmail.com>
2023-02-22 14:38:58 -0800

    Pretty output

--- chromadb/experimental/__init__.py
+++ chromadb/experimental/__init__.py
@@ -6,4 +6,17 @@ from chromadb.experimental.ExperimentalClient import ExperimentalClient as EClie
 
 # A monkeypatch wrapper for the regular client. 
 def ExperimentalClient(*args, **kwargs):
+    
+    # Output a pretty colorful message to the user
+    experimental_message = f"""
+     ###### \033[38;5;220m Welcome to the Chroma experimental client! \033[0m #####
+     {chromadb._logo}
+
+We'd love to know what you're experimenting with! Join the Discord:
+
+               https://discord.gg/9WZAkTEEwC
+
+    """ 
+    print(experimental_message)
+
     return EClient(*args, **kwargs)
