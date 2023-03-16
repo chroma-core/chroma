fac4f39b334b803fbafb0ec280fb21412fdf43dc
laserbear <christianr.ng@gmail.com>
2023-03-15 19:42:15 -0700

    use requests Session object to keep requests imported

--- chromadb/utils/embedding_functions.py
+++ chromadb/utils/embedding_functions.py
@@ -62,14 +62,15 @@ class CohereEmbeddingFunction(EmbeddingFunction):
 class HuggingFaceEmbeddingFunction(EmbeddingFunction):
     def __init__(self, api_key: str, model_name: str = "sentence-transformers/all-MiniLM-L6-v2"):
         try:
-            import requests
+            import requests 
         except ImportError:
             raise ValueError(
                 "The requests python package is not installed. Please install it with `pip install requests`"
             )
-        self.api_url = f"https://api-inference.huggingface.co/pipeline/feature-extraction/{model_id}"
-        self._headers = {"Authorization": f"Bearer {hf_token}"}
+        self._api_url = f"https://api-inference.huggingface.co/pipeline/feature-extraction/{model_name}" 
+        self._session = requests.Session()
+        self._session.headers.update({"Authorization": f"Bearer {api_key}"})
 
     def __call__(self, texts: Documents) -> Embeddings:
-        # Call HuggingFace Embedding API for each document.
-        return requests.post(self._api_url, headers=self._headers, json={"inputs": texts, "options":{"wait_for_model":True}}).json()
+        # Call HuggingFace Embedding API for each document
+        return self._session.post(self._api_url, json={"inputs": texts, "options":{"wait_for_model":True}}).json()
