bc6fcc6a5484d4a9fbe9b7312247316a9e47184d
Luke VanderHart <luke@vanderhart.net>
2023-03-28 11:38:59 -0400

    refactor hnswlib class

--- chromadb/db/index/__init__.py
+++ chromadb/db/index/__init__.py
@@ -3,29 +3,21 @@ from abc import ABC, abstractmethod
 
 class Index(ABC):
     @abstractmethod
-    def __init__(self, settings):
+    def __init__(self, id, settings, metadata):
         pass
 
     @abstractmethod
-    def delete(self, collection_name):
+    def delete(self):
         pass
 
     @abstractmethod
-    def delete_from_index(self, collection_name, uuids):
+    def delete_from_index(self, ids):
         pass
 
     @abstractmethod
-    def reset(self):
+    def add(self, ids, embeddings, update=False):
         pass
 
     @abstractmethod
-    def run(self, collection_name, uuids, embeddings):
-        pass
-
-    @abstractmethod
-    def has_index(self, collection_name):
-        pass
-
-    @abstractmethod
-    def get_nearest_neighbors(self, collection_name, embedding, n_results, ids):
+    def get_nearest_neighbors(self, embedding, n_results, ids):
         pass
