de9c29ab1b71d921bca654b8f1e02498f2f42f8d
Jeffrey Huber <jeff@trychroma.com>
2023-03-30 11:33:58 -0700

    cleanup

--- clients/js/test/client.test.ts
+++ clients/js/test/client.test.ts
@@ -136,7 +136,6 @@ test('it should peek a collection', async () => {
     const results = await collection.peek(2)
     expect(results).toBeDefined()
     expect(results).toBeInstanceOf(Object)
-    console.log('results', results)
     expect(results.ids.length).toBe(2)
     expect(['test1', 'test2']).toEqual(expect.arrayContaining(results.ids));
 })
