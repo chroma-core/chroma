0efc0cefecc172037d9afd568428d375bb5e7fb2
Alw3ys <alw3ys@gmail.com>
2023-03-30 17:00:31 +0200

    remove usage of sudo

--- examples/deployments/google-cloud-compute/startup.sh
+++ examples/deployments/google-cloud-compute/startup.sh
@@ -46,7 +46,7 @@ services:
 EOF
 
 mkdir config
-sudo cat << EOF > config/backup_disk.xml
+cat << EOF > config/backup_disk.xml
 <clickhouse>
     <storage_configuration>
         <disks>
@@ -63,7 +63,7 @@ sudo cat << EOF > config/backup_disk.xml
 </clickhouse>
 EOF
 
-sudo cat << EOF > config/chroma_users.xml
+cat << EOF > config/chroma_users.xml
 <clickhouse>
     <profiles>
         <default>
