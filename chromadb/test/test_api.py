chroma-core/chroma

commit 5cef7f9aae239141ff96c256ae67027913c4d378

Support parameters for HNSW indexes using metadata (#245)
* refactor hnswlib class

* update index usage

* udpate index usage in duckdb.py

* clean up unused method

* bug fixes

* fix bugs

* fix validation ordering

* persistence test

* Added params persistence test

---------

Co-authored-by: atroyn <anton.troynikov@gmail.com>

chromadb/db/clickhouse.py
@@ -1,6 +1,6 @@
 from chromadb.api.types import Documents, Embeddings, IDs, Metadatas, Where, WhereDocument
 from chromadb.db import DB
-from chromadb.db.index.hnswlib import Hnswlib
+from chromadb.db.index.hnswlib import Hnswlib, delete_all_indexes
 from chromadb.errors import (
     NoDatapointsException,
     InvalidDimensionException,
@@ -51,7 +51,6 @@ class Clickhouse(DB):
     #
     def __init__(self, settings):
         self._conn = None
-        self._idx = Hnswlib(settings)
         self._settings = settings
 
     def _init_conn(self):
@@ -80,6 +79,25 @@ class Clickhouse(DB):
             {db_array_schema_to_clickhouse_schema(EMBEDDING_TABLE_SCHEMA)}
         ) ENGINE = MergeTree() ORDER BY collection_uuid"""
         )
+
+    index_cache = {}
+
+    def _index(self, collection_id):
+        """Retrieve an HNSW index instance for the given collection"""
+
+        if collection_id not in self.index_cache:
+            coll = self.get_collection_by_id(collection_id)
+            collection_metadata = coll[2]
+            index = Hnswlib(collection_id, self._settings, collection_metadata)
+            self.index_cache[collection_id] = index
+
+        return self.index_cache[collection_id]
+
+    def _delete_index(self, collection_id):
+        """Delete an index from the cache"""
+        index = self._index(collection_id)
+        index.delete()
+        del self.index_cache[collection_id]
 
     #
     #  UTILITY METHODS
@@ -156,6 +174,19 @@ class Clickhouse(DB):
         # json.loads the metadata
         return [[x[0], x[1], json.loads(x[2])] for x in res]
 
+    def get_collection_by_id(self, collection_uuid: str):
+        res = (
+            self._get_conn()
+            .query(
+                f"""
+         SELECT * FROM collections WHERE uuid = '{collection_uuid}'
+         """
+            )
+            .result_rows
+        )
+        # json.loads the metadata
+        return [[x[0], x[1], json.loads(x[2])] for x in res][0]
+
     def list_collections(self) -> Sequence:
         res = self._get_conn().query(f"""SELECT * FROM collections""").result_rows
         return [[x[0], x[1], json.loads(x[2])] for x in res]
@@ -195,7 +226,7 @@ class Clickhouse(DB):
          """
         )
 
-        self._idx.delete_index(collection_uuid)
+        self._delete_index(collection_uuid)
 
     #
     #  ITEM METHODS
@@ -271,9 +302,8 @@ class Clickhouse(DB):
 
         # Update the index
         if embeddings is not None:
-            update_uuids = [x[1] for x in existing_items]
-            self._idx.delete_from_index(collection_uuid, update_uuids)
-            self._idx.add_incremental(collection_uuid, update_uuids, embeddings)
+            index = self._index(collection_uuid)
+            index.add(ids, embeddings, update=True)
 
     def _get(self, where={}, columns: Optional[List] = None):
         select_columns = db_schema_to_keys() if columns is None else columns
@@ -437,7 +467,8 @@ class Clickhouse(DB):
 
         deleted_uuids = self._delete(where_str)
 
-        self._idx.delete_from_index(collection_uuid, deleted_uuids)
+        index = self._index(collection_uuid)
+        index.delete_from_index(deleted_uuids)
 
         return deleted_uuids
 
@@ -476,21 +507,6 @@ class Clickhouse(DB):
         if collection_name is not None:
             collection_uuid = self.get_collection_uuid_from_name(collection_name)
 
-        self._idx.load_if_not_loaded(collection_uuid)
-
-        idx_metadata = self._idx.get_metadata()
-        # Check query embeddings dimensionality
-        if idx_metadata["dimensionality"] != len(embeddings[0]):
-            raise InvalidDimensionException(
-                f"Query embeddings dimensionality {len(embeddings[0])} does not match index dimensionality {idx_metadata['dimensionality']}"
-            )
-
-        # Check number of requested results
-        if n_results > idx_metadata["elements"]:
-            raise NotEnoughElementsException(
-                f"Number of requested results {n_results} cannot be greater than number of elements in index {idx_metadata['elements']}"
-            )
-
         if len(where) != 0 or len(where_document) != 0:
             results = self.get(
                 collection_uuid=collection_uuid, where=where, where_document=where_document
@@ -504,9 +520,9 @@ class Clickhouse(DB):
                 )
         else:
             ids = None
-        uuids, distances = self._idx.get_nearest_neighbors(
-            collection_uuid, embeddings, n_results, ids
-        )
+
+        index = self._index(collection_uuid)
+        uuids, distances = index.get_nearest_neighbors(embeddings, n_results, ids)
 
         return uuids, distances
 
@@ -523,13 +539,16 @@ class Clickhouse(DB):
         uuids = [x[1] for x in get]
         embeddings = [x[2] for x in get]
 
-        self._idx.run(collection_uuid, uuids, embeddings)
+        index = self._index(collection_uuid)
+        index.add(uuids, embeddings)
 
     def add_incremental(self, collection_uuid, uuids, embeddings):
-        self._idx.add_incremental(collection_uuid, uuids, embeddings)
+        index = self._index(collection_uuid)
+        index.add(uuids, embeddings)
 
-    def has_index(self, collection_uuid: str):
-        return self._idx.has_index(collection_uuid)
+    def reset_indexes(self):
+        delete_all_indexes(self._settings)
+        self.index_cache = {}
 
     def reset(self):
         conn = self._get_conn()
@@ -538,8 +557,7 @@ class Clickhouse(DB):
         self._create_table_collections(conn)
         self._create_table_embeddings(conn)
 
-        self._idx.reset()
-        self._idx = Hnswlib(self._settings)
+        self.reset_indexes()
 
     def raw_sql(self, sql):
         return self._get_conn().query(sql).result_rows

chromadb/test/test_api.py
@@ -687,7 +687,7 @@ def test_metadata_validation_add(api_fixture, request):
 
     api.reset()
     collection = api.create_collection("test_metadata_validation")
-    with pytest.raises(ValueError, match='metadata'):
+    with pytest.raises(ValueError, match="metadata"):
         collection.add(**bad_metadata_records)
 
 
@@ -698,7 +698,7 @@ def test_metadata_validation_update(api_fixture, request):
     api.reset()
     collection = api.create_collection("test_metadata_validation")
     collection.add(**metadata_records)
-    with pytest.raises(ValueError, match='metadata'):
+    with pytest.raises(ValueError, match="metadata"):
         collection.update(ids=["id1"], metadatas={"value": {"nested": "5"}})
 
 
@@ -708,7 +708,7 @@ def test_where_validation_get(api_fixture, request):
 
     api.reset()
     collection = api.create_collection("test_where_validation")
-    with pytest.raises(ValueError, match='where'):
+    with pytest.raises(ValueError, match="where"):
         collection.get(where={"value": {"nested": "5"}})
 
 
@@ -718,7 +718,7 @@ def test_where_validation_query(api_fixture, request):
 
     api.reset()
     collection = api.create_collection("test_where_validation")
-    with pytest.raises(ValueError, match='where'):
+    with pytest.raises(ValueError, match="where"):
         collection.query(query_embeddings=[0, 0, 0], where={"value": {"nested": "5"}})
 
 
@@ -908,13 +908,13 @@ def test_query_document_valid_operators(api_fixture, request):
     api.reset()
     collection = api.create_collection("test_where_valid_operators")
     collection.add(**operator_records)
-    with pytest.raises(ValueError, match='where document'):
+    with pytest.raises(ValueError, match="where document"):
         collection.get(where_document={"$lt": {"$nested": 2}})
 
-    with pytest.raises(ValueError, match='where document'):
+    with pytest.raises(ValueError, match="where document"):
         collection.query(query_embeddings=[0, 0, 0], where_document={"$contains": 2})
 
-    with pytest.raises(ValueError, match='where document'):
+    with pytest.raises(ValueError, match="where document"):
         collection.get(where_document={"$contains": []})
 
     # Test invalid $and, $or
@@ -1177,10 +1177,10 @@ def test_get_include(api_fixture, request):
     assert items["embeddings"] == None
     assert items["ids"][0] == "id1"
 
-    with pytest.raises(ValueError, match='include'):
+    with pytest.raises(ValueError, match="include"):
         items = collection.get(include=["metadatas", "undefined"])
 
-    with pytest.raises(ValueError, match='include'):
+    with pytest.raises(ValueError, match="include"):
         items = collection.get(include=None)
 
 
@@ -1224,3 +1224,85 @@ def test_invalid_id(api_fixture, request):
     with pytest.raises(ValueError) as e:
         collection.delete(ids=["valid", 0])
     assert "ID" in str(e.value)
+
+
+@pytest.mark.parametrize("api_fixture", test_apis)
+def test_index_params(api_fixture, request):
+    api = request.getfixturevalue(api_fixture.__name__)
+
+    # first standard add
+    api.reset()
+    collection = api.create_collection(name="test_index_params")
+    collection.add(**records)
+    items = collection.query(
+        query_embeddings=[0.6, 1.12, 1.6],
+        n_results=1,
+    )
+    assert items["distances"][0][0] > 4
+
+    # cosine
+    api.reset()
+    collection = api.create_collection(
+        name="test_index_params",
+        metadata={"hnsw:space": "cosine", "hnsw:construction_ef": 20, "hnsw:M": 5},
+    )
+    collection.add(**records)
+    items = collection.query(
+        query_embeddings=[0.6, 1.12, 1.6],
+        n_results=1,
+    )
+    assert items["distances"][0][0] > 0
+    assert items["distances"][0][0] < 1
+
+    # ip
+    api.reset()
+    collection = api.create_collection(name="test_index_params", metadata={"hnsw:space": "ip"})
+    collection.add(**records)
+    items = collection.query(
+        query_embeddings=[0.6, 1.12, 1.6],
+        n_results=1,
+    )
+    assert items["distances"][0][0] < -5
+
+
+@pytest.mark.parametrize("api_fixture", test_apis)
+def test_invalid_index_params(api_fixture, request):
+
+    api = request.getfixturevalue(api_fixture.__name__)
+    api.reset()
+
+    with pytest.raises(Exception):
+        collection = api.create_collection(
+            name="test_index_params", metadata={"hnsw:foobar": "blarg"}
+        )
+        collection.add(**records)
+
+    with pytest.raises(Exception):
+        collection = api.create_collection(
+            name="test_index_params", metadata={"hnsw:space": "foobar"}
+        )
+        collection.add(**records)
+
+
+@pytest.mark.parametrize("api_fixture", [local_persist_api])
+def test_persist_index_loading_params(api_fixture, request):
+    api = request.getfixturevalue("local_persist_api")
+    api.reset()
+    collection = api.create_collection("test", metadata={"hnsw:space": "ip"})
+    collection.add(ids="id1", documents="hello")
+
+    api.persist()
+    del api
+
+    api2 = request.getfixturevalue("local_persist_api_cache_bust")
+    collection = api2.get_collection("test")
+
+    assert collection.metadata["hnsw:space"] == "ip"
+
+    nn = collection.query(
+        query_texts="hello",
+        n_results=1,
+        include=["embeddings", "documents", "metadatas", "distances"],
+    )
+    for key in nn.keys():
+        assert len(nn[key]) == 1

chromadb/db/__init__.py
@@ -116,9 +116,5 @@ class DB(ABC):
         pass
 
     @abstractmethod
-    def has_index(self, collection_name):
-        pass
-
-    @abstractmethod
     def persist(self):
         pass

chromadb/db/duckdb.py
@@ -1,6 +1,5 @@
 from chromadb.api.types import Documents, Embeddings, IDs, Metadatas
 from chromadb.db import DB
-from chromadb.db.index.hnswlib import Hnswlib
 from chromadb.db.clickhouse import (
     Clickhouse,
     db_array_schema_to_clickhouse_schema,
@@ -48,7 +47,6 @@ class DuckDB(Clickhouse):
         self._conn = duckdb.connect()
         self._create_table_collections()
         self._create_table_embeddings()
-        self._idx = Hnswlib(settings)
         self._settings = settings
 
         # https://duckdb.org/docs/extensions/overview
@@ -106,6 +104,10 @@ class DuckDB(Clickhouse):
         # json.loads the metadata
         return [[x[0], x[1], json.loads(x[2])] for x in res]
 
+    def get_collection_by_id(self, uuid: str) -> Sequence:
+        res = self._conn.execute(f"""SELECT * FROM collections WHERE uuid = ?""", [uuid]).fetchone()
+        return [res[0], res[1], json.loads(res[2])]
+
     def list_collections(self) -> Sequence:
         res = self._conn.execute(f"""SELECT * FROM collections""").fetchall()
         return [[x[0], x[1], json.loads(x[2])] for x in res]
@@ -115,7 +117,8 @@ class DuckDB(Clickhouse):
         self._conn.execute(
             f"""DELETE FROM embeddings WHERE collection_uuid = ?""", [collection_uuid]
         )
-        self._idx.delete_index(collection_uuid)
+
+        self._delete_index(collection_uuid)
         self._conn.execute(f"""DELETE FROM collections WHERE name = ?""", [name])
 
     def update_collection(
@@ -349,12 +352,11 @@ class DuckDB(Clickhouse):
         self._create_table_collections()
         self._create_table_embeddings()
 
-        self._idx.reset()
-        self._idx = Hnswlib(self._settings)
+        self.reset_indexes()
 
     def __del__(self):
         logger.info("Exiting: Cleaning up .chroma directory")
-        self._idx.reset()
+        self.reset_indexes()
 
     def persist(self):
         raise NotImplementedError(

chromadb/db/index/__init__.py
@@ -3,29 +3,21 @@ from abc import ABC, abstractmethod
 
 class Index(ABC):
     @abstractmethod
-    def __init__(self, settings):
+    def __init__(self, id, settings, metadata):
         pass
 
     @abstractmethod
-    def delete(self, collection_name):
+    def delete(self):
         pass
 
     @abstractmethod
-    def delete_from_index(self, collection_name, uuids):
+    def delete_from_index(self, ids):
         pass
 
     @abstractmethod
-    def reset(self):
+    def add(self, ids, embeddings, update=False):
         pass
 
     @abstractmethod
-    def run(self, collection_name, uuids, embeddings):
-        pass
-
-    @abstractmethod
-    def has_index(self, collection_name):
-        pass
-
-    @abstractmethod
-    def get_nearest_neighbors(self, collection_name, embedding, n_results, ids):
+    def get_nearest_neighbors(self, embedding, n_results, ids):
         pass

commit 85fe406eb7938feb1f8ba7706f41fa1bcd105672

Fix update bug (#293)
A bug was introduced in #245. We changed from passing uuids into the index to ids, which leads to breakage. This is a bugfix that fixes the functionality by passing in uuids, along with a test.

chromadb/db/clickhouse.py
@@ -299,8 +299,9 @@ class Clickhouse(DB):
 
         # Update the index
         if embeddings is not None:
+            update_uuids = [x[1] for x in existing_items]
             index = self._index(collection_uuid)
-            index.add(ids, embeddings, update=True)
+            index.add(update_uuids, embeddings, update=True)
 
     def _get(self, where={}, columns: Optional[List] = None):
         select_columns = db_schema_to_keys() if columns is None else columns

chromadb/test/test_api.py
@@ -1367,3 +1367,33 @@ def test_multiple_collections(api_fixture, request):
 
     assert results1["ids"][0][0] == ids1[0]
     assert results2["ids"][0][0] == ids2[0]
+
+
+@pytest.mark.parametrize("api_fixture", test_apis)
+def test_update_query(api_fixture, request):
+
+    api = request.getfixturevalue(api_fixture.__name__)
+    api.reset()
+    collection = api.create_collection("test_update_query")
+    collection.add(**records)
+
+    updated_records = {
+        "ids": [records["ids"][0]],
+        "embeddings": [[0.1, 0.2, 0.3]],
+        "documents": ["updated document"],
+        "metadatas": [{"foo": "bar"}],
+    }
+
+    collection.update(**updated_records)
+
+    # test query
+    results = collection.query(
+        query_embeddings=updated_records["embeddings"],
+        n_results=1,
+        include=["embeddings", "documents", "metadatas"],
+    )
+    assert len(results["ids"][0]) == 1
+    assert results["ids"][0][0] == updated_records["ids"][0]
+    assert results["documents"][0][0] == updated_records["documents"][0]
+    assert results["metadatas"][0][0]["foo"] == "bar"
+    assert results["embeddings"][0][0] == updated_records["embeddings"][0]