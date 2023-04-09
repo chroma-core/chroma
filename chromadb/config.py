8483106bd4656619faf456743a34c848c376bd86
shoggoth-coder <shoggoth-coder@gmail.com>
2023-04-09 05:25:56 -0700

    Organize settings in config.py

--- chromadb/config.py
+++ chromadb/config.py
@@ -4,24 +4,27 @@ from typing import List
 TELEMETRY_WHITELISTED_SETTINGS = ["chroma_db_impl", "chroma_api_impl", "chroma_server_ssl_enabled"]
 
 
-class Settings(BaseSettings):
-    environment: str = ""
-
+class DatabaseSettings(BaseSettings):
     chroma_db_impl: str = "duckdb"
-    chroma_api_impl: str = "local"
-
     clickhouse_host: str = None
     clickhouse_port: str = None
-
     persist_directory: str = ".chroma"
 
+
+class ServerSettings(BaseSettings):
     chroma_server_host: str = None
     chroma_server_http_port: str = None
     chroma_server_ssl_enabled: bool = False
     chroma_server_grpc_port: str = None
     chroma_server_cors_allow_origins: List[str] = []  # eg ["http://localhost:3000"]
 
+
+class Settings(BaseSettings):
+    environment: str = ""
+    chroma_api_impl: str = "local"
     anonymized_telemetry: bool = True
+    database: DatabaseSettings = DatabaseSettings()
+    server: ServerSettings = ServerSettings()
 
     def __getitem__(self, item):
         return getattr(self, item)
