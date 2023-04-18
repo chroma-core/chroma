6821ea66388bf4fabff2b9a98538be65ae2835f8
Matt Oakly <m@tthew.dev>
2023-04-18 12:59:45 +1200

    make api_key in OpenAIEmbeddingFunction optional (#320)
    * make api_key in OpenAIEmbeddingFunction optional
    
    * make optional api_key actually work
    
    * Early error
    
    ---------
    
    Co-authored-by: atroyn <anton.troynikov@gmail.com>

--- chromadb/utils/embedding_functions.py
+++ chromadb/utils/embedding_functions.py
@@ -1,4 +1,5 @@
 from chromadb.api.types import Documents, EmbeddingFunction, Embeddings
+from typing import Optional
 
 
 class SentenceTransformerEmbeddingFunction(EmbeddingFunction):
@@ -18,7 +19,7 @@ class SentenceTransformerEmbeddingFunction(EmbeddingFunction):
 
 
 class OpenAIEmbeddingFunction(EmbeddingFunction):
-    def __init__(self, api_key: str, model_name: str = "text-embedding-ada-002"):
+    def __init__(self, api_key: Optional[str] = None, model_name: str = "text-embedding-ada-002"):
         try:
             import openai
         except ImportError:
@@ -26,7 +27,14 @@ class OpenAIEmbeddingFunction(EmbeddingFunction):
                 "The openai python package is not installed. Please install it with `pip install openai`"
             )
 
-        openai.api_key = api_key
+        if api_key is not None:
+            openai.api_key = api_key
+        # If the api key is still not set, raise an error
+        elif openai.api_key is None:
+            raise ValueError(
+                "Please provide an OpenAI API key. You can get one at https://platform.openai.com/account/api-keys"
+            )
+
         self._client = openai.Embedding
         self._model_name = model_name
 
