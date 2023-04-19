43dd000cf2fb8459be7980d416b4cc5fec778ed1
hammadb <hammad@trychroma.com>
2023-04-19 12:57:45 -0700

    change to process

--- chromadb/test/property/test_cross_version_persist.py
+++ chromadb/test/property/test_cross_version_persist.py
@@ -8,8 +8,6 @@ from hypothesis import given
 import pytest
 import json
 from urllib import request
-import importlib
-from chromadb.config import Settings
 from chromadb.test.configurations import (
     persist_old_version_configurations,
 )
@@ -18,7 +16,11 @@ import chromadb.test.property.invariants as invariants
 from importlib.util import spec_from_file_location, module_from_spec
 from packaging import version as packaging_version
 import re
+import multiprocessing
+from chromadb import Client
+from chromadb.config import Settings
 
+MINIMUM_VERSION = "0.3.21"
 version_re = re.compile(r"^[0-9]+\.[0-9]+\.[0-9]+$")
 
 
@@ -30,7 +32,7 @@ def versions():
     # Older versions on pypi contain "devXYZ" suffixes
     versions = [v for v in versions if version_re.match(v)]
     versions.sort(key=packaging_version.Version)
-    return ["0.3.21", versions[-1]]
+    return [MINIMUM_VERSION, versions[-1]]
 
 
 test_old_versions = versions()
@@ -84,22 +86,16 @@ def switch_to_version(version):
     for n in old_modules:
         del sys.modules[n]
 
-    # Load the target version
-    new_module = None
-    if version == "current":
-        # Will import from current working directory
-        new_module = importlib.import_module(module_name)
-    else:
-        path = get_path_to_version_library(version)
-        spec = spec_from_file_location(module_name, path)
-        assert spec is not None and spec.loader is not None
-        module = module_from_spec(spec)
-        spec.loader.exec_module(module)
-        new_module = module
-        assert new_module.__version__ == version
-
-    sys.modules[module_name] = new_module
-    return new_module
+    # Load the target version and override the path to the installed version
+    path = get_path_to_version_library(version)
+    sys.path.insert(0, get_path_to_version_install(version))
+    spec = spec_from_file_location(module_name, path)
+    assert spec is not None and spec.loader is not None
+    module = module_from_spec(spec)
+    spec.loader.exec_module(module)
+    assert module.__version__ == version
+    sys.modules[module_name] = module
+    return module
 
 
 @pytest.fixture(
@@ -121,24 +117,9 @@ def version_settings(request) -> Generator[Tuple[str, Settings], None, None]:
         shutil.rmtree(data_path)
 
 
-@given(
-    collection_strategy=strategies.collections(),
-    embeddings_strategy=strategies.embedding_set(),
-)
-def test_cycle_versions(
-    version_settings: Settings,
-    collection_strategy: strategies.Collection,
-    embeddings_strategy: strategies.EmbeddingSet,
+def persist_generated_data_with_old_version(
+    version, settings, collection_strategy, embeddings_strategy
 ):
-    # Test backwards compatibility
-    # For the current version, ensure that we can load a collection from
-    # the previous versions
-    version, settings = version_settings
-
-    # Add data with an old version + check the invariants are preserved in that version
-    if version == "0.3.21":
-        # Old versions do not support upper case collection names
-        collection_strategy["name"] = collection_strategy["name"].lower()
     old_module = switch_to_version(version)
     api = old_module.Client(settings)
     api.reset()
@@ -146,24 +127,43 @@ def test_cycle_versions(
         **collection_strategy, embedding_function=lambda x: None
     )
     coll.add(**embeddings_strategy)
-
-    invariants.count(
-        api,
-        coll.name,
-        len(embeddings_strategy["ids"]),
-    )
-    invariants.metadatas_match(coll, embeddings_strategy)
-    invariants.documents_match(coll, embeddings_strategy)
-    invariants.ids_match(coll, embeddings_strategy)
-    invariants.ann_accuracy(coll, embeddings_strategy)
-
     api.persist()
     del api
 
+
+@given(
+    collection_strategy=strategies.collections(),
+    embeddings_strategy=strategies.embedding_set(),
+)
+def test_cycle_versions(
+    version_settings: Tuple[str, Settings],
+    collection_strategy: strategies.Collection,
+    embeddings_strategy: strategies.EmbeddingSet,
+):
+    # # Test backwards compatibility
+    # # For the current version, ensure that we can load a collection from
+    # # the previous versions
+    version, settings = version_settings
+
+    # Add data with an old version + check the invariants are preserved in that version
+    if version == MINIMUM_VERSION:
+        # Old versions do not support upper case collection names
+        collection_strategy["name"] = collection_strategy["name"].lower()
+
+    # Run the task in a separate process to avoid polluting the current process
+    # with the old version. Using spawn instead of fork to avoid sharing the
+    # current process memory which would cause the old version to be loaded
+    ctx = multiprocessing.get_context("spawn")
+    p = ctx.Process(
+        target=persist_generated_data_with_old_version,
+        args=(version, settings, collection_strategy, embeddings_strategy),
+    )
+    p.start()
+    p.join()
+
     # Switch to the current version (local working directory) and check the invariants
     # are preserved for the collection
-    current_module = switch_to_version("current")
-    api = current_module.Client(settings)
+    api = Client(settings)
     coll = api.get_collection(
         name=collection_strategy["name"], embedding_function=lambda x: None
     )
