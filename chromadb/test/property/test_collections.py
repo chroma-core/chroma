be1ee89b68e89931e200dc9f4e6659ac1b54b4c7
Luke VanderHart <luke@vanderhart.net>
2023-04-19 10:14:38 -0700

    add unit test to cover case removed from state machine

--- chromadb/test/property/test_collections.py
+++ chromadb/test/property/test_collections.py
@@ -132,3 +132,11 @@ def test_upsert_metadata_example(api):
     v1 = state.create_coll(coll={"name": "E40", "metadata": None})
     state.get_or_create_coll(coll={"name": "E40", "metadata": {"foo": "bar"}})
     state.teardown()
+
+
+def test_create_coll_with_none_metadata(api):
+    coll = {"name": "foo", "metadata": None}
+    api.reset()
+    c = api.get_or_create_collection(**coll)
+    assert c.name == coll["name"]
+    assert c.metadata == coll["metadata"]
