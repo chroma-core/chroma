6ca7baa61861d80f2dabe3f5bb46ffcd86b66200
rxy1212 <RyanDotStark@163.com>
2023-04-24 20:12:37 +0800

    Add Text2VecEmbeddingFunction
    Add Text2VecEmbeddingFunction for Chinese sentence embedding.

--- chromadb/utils/embedding_functions.py
+++ chromadb/utils/embedding_functions.py
@@ -18,6 +18,20 @@ class SentenceTransformerEmbeddingFunction(EmbeddingFunction):
         return self._model.encode(list(texts), convert_to_numpy=True).tolist()
 
 
+class Text2VecEmbeddingFunction(EmbeddingFunction):
+    def __init__(self, model_name: str = "shibing624/text2vec-base-chinese"):
+        try:
+            from text2vec import SentenceModel
+        except ImportError:
+            raise ValueError(
+                "The text2vec python package is not installed. Please install it with `pip install text2vec`"
+            )
+        self._model = SentenceModel(model_name_or_path=model_name)
+
+    def __call__(self, texts: Documents) -> Embeddings:
+        return self._model.encode(list(texts), convert_to_numpy=True).tolist()
+
+
 class OpenAIEmbeddingFunction(EmbeddingFunction):
     def __init__(self, api_key: Optional[str] = None, model_name: str = "text-embedding-ada-002"):
         try:
