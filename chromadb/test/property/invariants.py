08d4fc06ce95d5ee13ae8ed72d8aab50e3fbba76
hammadb <hammad@trychroma.com>
2023-05-05 18:39:38 -0700

    Add epsilon for norms in cosine per hnswli

--- chromadb/test/property/invariants.py
+++ chromadb/test/property/invariants.py
@@ -114,9 +114,13 @@ def no_duplicates(collection: Collection):
 
 
 # These match what the spec of hnswlib is
+# This epsilon is used to prevent division by zero and the value is the same
+# https://github.com/nmslib/hnswlib/blob/359b2ba87358224963986f709e593d799064ace6/python_bindings/bindings.cpp#L238
+NORM_EPS = 1e-30
 distance_functions = {
     "l2": lambda x, y: np.linalg.norm(x - y) ** 2,
-    "cosine": lambda x, y: 1 - np.dot(x, y) / (np.linalg.norm(x) * np.linalg.norm(y)),
+    "cosine": lambda x, y: 1
+    - np.dot(x, y) / ((np.linalg.norm(x) + NORM_EPS) * (np.linalg.norm(y) + NORM_EPS)),
     "ip": lambda x, y: 1 - np.dot(x, y),
 }
 
