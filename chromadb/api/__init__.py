8dcc746e034afb1b2fa11705def90d57e5537500
Luke VanderHart <luke@vanderhart.net>
2023-05-08 13:11:29 -0400

    remove pseudo-type-signatures from docstrings

--- chromadb/api/__init__.py
+++ chromadb/api/__init__.py
@@ -60,11 +60,11 @@ class API(ABC):
         """Creates a new collection in the database
 
         Args:
-            name (str): The name of the collection to create. The name must be unique.
-            metadata (Optional[Dict], optional): A dictionary of metadata to associate with the collection. Defaults to None.
-            get_or_create (bool, optional): If True, will return the collection if it already exists,
+            name  The name of the collection to create. The name must be unique.
+            metadata: A dictionary of metadata to associate with the collection. Defaults to None.
+            get_or_create: If True, will return the collection if it already exists,
                 and update the metadata (if applicable). Defaults to False.
-            embedding_function (Optional[Callable], optional): A function that takes documents and returns an embedding. Defaults to None.
+            embedding_function: A function that takes documents and returns an embedding. Defaults to None.
 
         Returns:
             dict: the created collection
@@ -80,7 +80,7 @@ class API(ABC):
         """Deletes a collection from the database
 
         Args:
-            name (str): The name of the collection to delete
+            name: The name of the collection to delete
         """
 
     @abstractmethod
@@ -91,10 +91,10 @@ class API(ABC):
            If the collection exists, but with different metadata, the metadata will be replaced.
 
         Args:
-            name (str): The name of the collection to create. The name must be unique.
-            metadata (Optional[Dict], optional): A dictionary of metadata to associate with the collection. Defaults to None.
+            name: The name of the collection to create. The name must be unique.
+            metadata: A dictionary of metadata to associate with the collection. Defaults to None.
         Returns:
-            dict: the created collection
+            the created collection
 
         """
         pass
@@ -108,8 +108,8 @@ class API(ABC):
         """Gets a collection from the database by either name or uuid
 
         Args:
-            name (Optional[str]): The name of the collection to get. Defaults to None.
-            embedding_function (Optional[Callable], optional): A function that takes documents and returns an embedding. Should be the same as the one used to create the collection. Defaults to None.
+            name: The name of the collection to get. Defaults to None.
+            embedding_function: A function that takes documents and returns an embedding. Should be the same as the one used to create the collection. Defaults to None.
 
         Returns:
             dict: the requested collection
@@ -126,9 +126,9 @@ class API(ABC):
         """Modify a collection in the database - can update the name and/or metadata
 
         Args:
-            current_name (str): The name of the collection to modify
-            new_name (Optional[str], optional): The new name of the collection. Defaults to None.
-            new_metadata (Optional[Dict], optional): The new metadata to associate with the collection. Defaults to None.
+            current_name: The name of the collection to modify
+            new_name: The new name of the collection. Defaults to None.
+            new_metadata: The new metadata to associate with the collection. Defaults to None.
         """
         pass
 
@@ -146,11 +146,11 @@ class API(ABC):
         ⚠️ It is recommended to use the more specific methods below when possible.
 
         Args:
-            collection_ids (UUID): The collection to add the embeddings to
-            embedding (Sequence[Sequence[float]]): The sequence of embeddings to add
-            metadata (Optional[Union[Dict, Sequence[Dict]]], optional): The metadata to associate with the embeddings. Defaults to None.
-            documents (Optional[Union[str, Sequence[str]]], optional): The documents to associate with the embeddings. Defaults to None.
-            ids (Optional[Union[str, Sequence[str]]], optional): The ids to associate with the embeddings. Defaults to None.
+            collection_ids: The collection to add the embeddings to
+            embedding: The sequence of embeddings to add
+            metadata: The metadata to associate with the embeddings. Defaults to None.
+            documents: The documents to associate with the embeddings. Defaults to None.
+            ids: The ids to associate with the embeddings. Defaults to None.
         """
         pass
 
@@ -167,8 +167,8 @@ class API(ABC):
         ⚠️ It is recommended to use the more specific methods below when possible.
 
         Args:
-            collection_id (UUID): The collection to add the embeddings to
-            embedding (Sequence[Sequence[float]]): The sequence of embeddings to add
+            collection_id: The collection to add the embeddings to
+            embedding: The sequence of embeddings to add
         """
         pass
 
@@ -186,12 +186,12 @@ class API(ABC):
         If an entry with the same id already exists, it will be updated, otherwise it will be added.
 
         Args:
-            collection_id (UUID): The collection to add the embeddings to
-            ids (Optional[Union[str, Sequence[str]]], optional): The ids to associate with the embeddings. Defaults to None.
-            embeddings (Sequence[Sequence[float]]): The sequence of embeddings to add
-            metadatas (Optional[Union[Dict, Sequence[Dict]]], optional): The metadata to associate with the embeddings. Defaults to None.
-            documents (Optional[Union[str, Sequence[str]]], optional): The documents to associate with the embeddings. Defaults to None.
-            increment_index (bool, optional): If True, will incrementally add to the ANN index of the collection. Defaults to True.
+            collection_id: The collection to add the embeddings to
+            ids: The ids to associate with the embeddings. Defaults to None.
+            embeddings: The sequence of embeddings to add
+            metadatas: The metadata to associate with the embeddings. Defaults to None.
+            documents: The documents to associate with the embeddings. Defaults to None.
+            increment_index: If True, will incrementally add to the ANN index of the collection. Defaults to True.
         """
         pass
 
@@ -200,7 +200,7 @@ class API(ABC):
         """Returns the number of embeddings in the database
 
         Args:
-            collection_id (UUID): The collection to count the embeddings in.
+            collection_id: The collection to count the embeddings in.
 
 
         Returns:
@@ -231,12 +231,12 @@ class API(ABC):
         ⚠️ This method should not be used directly.
 
         Args:
-            where (Optional[Dict[str, str]], optional): A dictionary of key-value pairs to filter the embeddings by. Defaults to {}.
-            sort (Optional[str], optional): The column to sort the embeddings by. Defaults to None.
-            limit (Optional[int], optional): The maximum number of embeddings to return. Defaults to None.
-            offset (Optional[int], optional): The number of embeddings to skip before returning. Defaults to None.
-            page (Optional[int], optional): The page number to return. Defaults to None.
-            page_size (Optional[int], optional): The number of embeddings to return per page. Defaults to None.
+            where: A dictionary of key-value pairs to filter the embeddings by. Defaults to {}.
+            sort: The column to sort the embeddings by. Defaults to None.
+            limit: The maximum number of embeddings to return. Defaults to None.
+            offset: The number of embeddings to skip before returning. Defaults to None.
+            page: The page number to return. Defaults to None.
+            page_size: The number of embeddings to return per page. Defaults to None.
 
         Returns:
             pd.DataFrame: A pandas dataframe containing the embeddings and metadata
@@ -256,7 +256,7 @@ class API(ABC):
         ⚠️ This method should not be used directly.
 
         Args:
-            where (Optional[Dict[str, str]], optional): A dictionary of key-value pairs to filter the embeddings by. Defaults to {}.
+            where: A dictionary of key-value pairs to filter the embeddings by. Defaults to {}.
         """
         pass
 
@@ -274,9 +274,9 @@ class API(ABC):
         ⚠️ This method should not be used directly.
 
         Args:
-            embedding (Sequence[float]): The embedding to find the nearest neighbors of
-            n_results (int, optional): The number of nearest neighbors to return. Defaults to 10.
-            where (Dict[str, str], optional): A dictionary of key-value pairs to filter the embeddings by. Defaults to {}.
+            embedding: The embedding to find the nearest neighbors of
+            n_results: The number of nearest neighbors to return. Defaults to 10.
+            where: A dictionary of key-value pairs to filter the embeddings by. Defaults to {}.
         """
         pass
 
@@ -288,7 +288,7 @@ class API(ABC):
             None
 
         Returns:
-            bool: True if the reset was successful
+            True if the reset was successful
         """
         pass
 
@@ -298,7 +298,7 @@ class API(ABC):
         ⚠️ This method should not be used directly.
 
         Args:
-            sql (str): The SQL query to run
+            sql: The SQL query to run
 
         Returns:
             pd.DataFrame: A pandas dataframe containing the results of the query
@@ -311,7 +311,7 @@ class API(ABC):
         ⚠️ This method should not be used directly.
 
         Args:
-            collection_name (Optional[str], optional): The collection to create the index for. Uses the client's collection if None. Defaults to None.
+            collection_name: The collection to create the index for. Uses the client's collection if None. Defaults to None.
 
         Returns:
             bool: True if the index was created successfully
