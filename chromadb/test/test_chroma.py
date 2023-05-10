1c08c9a62f12df1bb7b3af23a69a7cdb40b27e1f
Luke VanderHart <luke@vanderhart.net>
2023-05-10 10:55:42 -0400

    Fix system initialization checks

--- chromadb/test/test_chroma.py
+++ chromadb/test/test_chroma.py
@@ -8,11 +8,13 @@ import chromadb.config
 
 class GetDBTest(unittest.TestCase):
     @patch("chromadb.db.duckdb.DuckDB", autospec=True)
+    @patch.dict(chromadb.config._impls, {}, clear=True)
     def test_default_db(self, mock):
         chromadb.get_db(chromadb.config.Settings(persist_directory="./foo"))
         assert mock.called
 
     @patch("chromadb.db.duckdb.PersistentDuckDB", autospec=True)
+    @patch.dict(chromadb.config._impls, {}, clear=True)
     def test_persistent_duckdb(self, mock):
         chromadb.get_db(
             chromadb.config.Settings(
@@ -22,29 +24,37 @@ class GetDBTest(unittest.TestCase):
         assert mock.called
 
     @patch("chromadb.db.clickhouse.Clickhouse", autospec=True)
+    @patch.dict(chromadb.config._impls, {}, clear=True)
     def test_clickhouse(self, mock):
         chromadb.get_db(
             chromadb.config.Settings(
                 chroma_db_impl="clickhouse",
                 persist_directory="./foo",
                 clickhouse_host="foo",
-                clickhouse_port=666,
+                clickhouse_port="666",
             )
         )
         assert mock.called
 
 
 class GetAPITest(unittest.TestCase):
-    @patch("chromadb.db.duckdb.DuckDB", autospec=True)
     @patch("chromadb.api.local.LocalAPI", autospec=True)
     @patch.dict(os.environ, {}, clear=True)
-    def test_local(self, mock_api, mock_db):
+    @patch.dict(chromadb.config._impls, {}, clear=True)
+    def test_local(self, mock_api):
         chromadb.Client(chromadb.config.Settings(persist_directory="./foo"))
         assert mock_api.called
+
+    @patch("chromadb.db.duckdb.DuckDB", autospec=True)
+    @patch.dict(os.environ, {}, clear=True)
+    @patch.dict(chromadb.config._impls, {}, clear=True)
+    def test_local_db(self, mock_db):
+        chromadb.Client(chromadb.config.Settings(persist_directory="./foo"))
         assert mock_db.called
 
     @patch("chromadb.api.fastapi.FastAPI", autospec=True)
     @patch.dict(os.environ, {}, clear=True)
+    @patch.dict(chromadb.config._impls, {}, clear=True)
     def test_fastapi(self, mock):
         chromadb.Client(
             chromadb.config.Settings(
