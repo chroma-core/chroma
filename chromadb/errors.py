477578a3f3e358af4a7c3c3596f80c3ceac9e5b1
hammadb <hammad@trychroma.com>
2023-05-11 16:39:20 -0700

    Fix types

--- chromadb/errors.py
+++ chromadb/errors.py
@@ -2,62 +2,62 @@ from abc import abstractmethod
 
 
 class ChromaError(Exception):
-    def code(self):
+    def code(self) -> int:
         """Return an appropriate HTTP response code for this error"""
         return 400  # Bad Request
 
-    def message(self):
+    def message(self) -> str:
         return ", ".join(self.args)
 
     @classmethod
     @abstractmethod
-    def name(self):
+    def name(self) -> str:
         """Return the error name"""
         pass
 
 
 class NoDatapointsException(ChromaError):
     @classmethod
-    def name(cls):
+    def name(cls) -> str:
         return "NoDatapoints"
 
 
 class NoIndexException(ChromaError):
     @classmethod
-    def name(cls):
+    def name(cls) -> str:
         return "NoIndex"
 
 
 class InvalidDimensionException(ChromaError):
     @classmethod
-    def name(cls):
+    def name(cls) -> str:
         return "InvalidDimension"
 
 
 class NotEnoughElementsException(ChromaError):
     @classmethod
-    def name(cls):
+    def name(cls) -> str:
         return "NotEnoughElements"
 
 
 class IDAlreadyExistsError(ChromaError):
-    def code(self):
+    def code(self) -> int:
         return 409  # Conflict
 
     @classmethod
-    def name(cls):
+    def name(cls) -> str:
         return "IDAlreadyExists"
 
 
 class DuplicateIDError(ChromaError):
     @classmethod
-    def name(cls):
+    def name(cls) -> str:
         return "DuplicateID"
 
 
 class InvalidUUIDError(ChromaError):
     @classmethod
-    def name(cls):
+    def name(cls) -> str:
         return "InvalidUUID"
 
 
--- chromadb/server/fastapi/types.py
+++ chromadb/server/fastapi/types.py
@@ -1,73 +1,71 @@
 from pydantic import BaseModel
-from typing import List, Union
-from chromadb.api.types import Include
+from typing import Optional
+from chromadb.api.types import (
+    CollectionMetadata,
+    Documents,
+    Embeddings,
+    IDs,
+    Include,
+    Metadatas,
+    Where,
+    WhereDocument,
+)
 
 
-# type supports single and batch mode
-class AddEmbedding(BaseModel):
-    embeddings: List
-    metadatas: Union[List, dict] = None
-    documents: Union[str, List] = None
-    ids: Union[str, List] = None
+class AddEmbedding(BaseModel):  # type: ignore
+    embeddings: Optional[Embeddings] = None
+    metadatas: Optional[Metadatas] = None
+    documents: Optional[Documents] = None
+    ids: IDs
     increment_index: bool = True
 
 
-class UpdateEmbedding(BaseModel):
-    embeddings: List = None
-    metadatas: Union[List, dict] = None
-    documents: Union[str, List] = None
-    ids: Union[str, List] = None
+class UpdateEmbedding(BaseModel):  # type: ignore
+    embeddings: Optional[Embeddings] = None
+    metadatas: Optional[Metadatas] = None
+    documents: Optional[Documents] = None
+    ids: IDs
     increment_index: bool = True
 
 
-class QueryEmbedding(BaseModel):
-    where: dict = {}
-    where_document: dict = {}
-    query_embeddings: List
+class QueryEmbedding(BaseModel):  # type: ignore
+    where: Where = {}
+    where_document: WhereDocument = {}
+    query_embeddings: Embeddings
     n_results: int = 10
     include: Include = ["metadatas", "documents", "distances"]
 
 
-class ProcessEmbedding(BaseModel):
-    collection_name: str = None
-    training_dataset_name: str = None
-    unlabeled_dataset_name: str = None
-
-
-class GetEmbedding(BaseModel):
-    ids: List = None
-    where: dict = None
-    where_document: dict = None
-    sort: str = None
-    limit: int = None
-    offset: int = None
+class GetEmbedding(BaseModel):  # type: ignore
+    ids: Optional[IDs] = None
+    where: Optional[Where] = None
+    where_document: Optional[WhereDocument] = None
+    sort: Optional[str] = None
+    limit: Optional[int] = None
+    offset: Optional[int] = None
     include: Include = ["metadatas", "documents"]
 
 
-class CountEmbedding(BaseModel):
-    collection_name: str = None
+class RawSql(BaseModel):  # type: ignore
+    raw_sql: str
 
 
-class RawSql(BaseModel):
-    raw_sql: str = None
-
-
-class SpaceKeyInput(BaseModel):
+class SpaceKeyInput(BaseModel):  # type: ignore
     collection_name: str
 
 
-class DeleteEmbedding(BaseModel):
-    ids: List = None
-    where: dict = None
-    where_document: dict = None
+class DeleteEmbedding(BaseModel):  # type: ignore
+    ids: Optional[IDs] = None
+    where: Optional[Where] = None
+    where_document: Optional[WhereDocument] = None
 
 
-class CreateCollection(BaseModel):
+class CreateCollection(BaseModel):  # type: ignore
     name: str
-    metadata: dict = None
+    metadata: Optional[CollectionMetadata] = None
     get_or_create: bool = False
 
 
-class UpdateCollection(BaseModel):
-    new_name: str = None
-    new_metadata: dict = None
+class UpdateCollection(BaseModel):  # type: ignore
+    new_name: Optional[str] = None
+    new_metadata: Optional[CollectionMetadata] = None
