b1a966a992bf85e6baee6b5852849c732ccf295b
hammadb <hammad@trychroma.com>
2023-05-12 16:59:22 -0700

    Add test cleanup/teardown

--- chromadb/test/hnswlib/test_hnswlib.py
+++ chromadb/test/hnswlib/test_hnswlib.py
@@ -1,11 +1,25 @@
+import os
+import shutil
+import tempfile
+from typing import Generator
+
+import pytest
 from chromadb.db.index.hnswlib import Hnswlib
 from chromadb.config import Settings
 import uuid
 import numpy as np
 
 
-def test_count_tracking() -> None:
-    hnswlib = Hnswlib("test", Settings(), {})
+@pytest.fixture(scope="module")  # type: ignore
+def settings() -> Generator[Settings, None, None]:
+    save_path = tempfile.gettempdir() + "/tests/hnswlib/"
+    yield Settings(persist_directory=save_path)
+    if os.path.exists(save_path):
+        shutil.rmtree(save_path)
+
+
+def test_count_tracking(settings: Settings) -> None:
+    hnswlib = Hnswlib("test", settings, {})
     hnswlib._init_index(2)
     assert hnswlib._index_metadata["elements"] == 0
     idA, idB = uuid.uuid4(), uuid.uuid4()
