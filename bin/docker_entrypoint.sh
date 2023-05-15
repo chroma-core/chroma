ad33423ea05e81d82d04a5726b1c5dfb0ccacf0e
sdan <git@sdan.io>
2023-05-14 23:31:55 -0700

    Update docker_entrypoint.sh

--- bin/docker_entrypoint.sh
+++ bin/docker_entrypoint.sh
@@ -2,4 +2,4 @@
 
 echo "Rebuilding hnsw to ensure architecture compatibility"
 pip install --force-reinstall --no-cache-dir hnswlib
-uvicorn chromadb.app:app --workers 1 --host 0.0.0.0 --port 8000 --proxy-headers --log-config log_config.yml
+uvicorn chromadb.app:app --workers 4 --host 0.0.0.0 --port 8000 --proxy-headers --log-config log_config.yml
