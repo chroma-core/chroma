0101ab174901a06e1d6fb598e7b2c88fe7657933
hammadb <hammad@trychroma.com>
2023-05-15 12:54:02 -0700

    Release 0.3.23

--- chromadb/__init__.py
+++ chromadb/__init__.py
@@ -10,7 +10,7 @@ logger = logging.getLogger(__name__)
 
 __settings = chromadb.config.Settings()
 
-__version__ = "0.3.22"
+__version__ = "0.3.23"
 
 
 def configure(**kwargs: Dict[Any, Any]) -> None:
