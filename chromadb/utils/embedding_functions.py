b37125b9e8592372b63e716687dabba97c42c8a6
Josh XT <102809327+Josh-XT@users.noreply.github.com>
2023-05-19 18:07:41 -0400

    Add Google Vertex Embedding Function (#528)
    Add Google Vertex Embedding Function
    
    
    https://console.cloud.google.com/vertex-ai/publishers/google/model-garden/textembedding-gecko?project=noted-victory-383712
    
    ## Description of changes
    Added Google Vertex Embedding Function
    
    ## Test plan
    By providing the API key and project ID.
    
    ## Documentation Changes
    Has as much documentation as the others in that file.
    
    ---------
    
    Co-authored-by: Jeffrey Huber <jeff@trychroma.com>

--- chromadb/utils/embedding_functions.py
+++ chromadb/utils/embedding_functions.py
@@ -340,3 +340,28 @@ class GooglePalmEmbeddingFunction(EmbeddingFunction):
             ]
             for text in texts
         ]
+
+class GoogleVertexEmbeddingFunction(EmbeddingFunction):
+    # Follow API Quickstart for Google Vertex AI
+    # https://cloud.google.com/vertex-ai/docs/generative-ai/start/quickstarts/api-quickstart
+    # Information about the text embedding modules in Google Vertex AI
+    # https://cloud.google.com/vertex-ai/docs/generative-ai/embeddings/get-text-embeddings
+    def __init__(
+        self,
+        api_key: str,
+        model_name: str = "textembedding-gecko-001",
+        project_id: str = "cloud-large-language-models",
+        region: str = "us-central1",
+    ):
+        self._api_url = f"https://{region}-aiplatform.googleapis.com/v1/projects/{project_id}/locations/{region}/endpoints/{model_name}:predict"
+        self._session = requests.Session()
+        self._session.headers.update({"Authorization": f"Bearer {api_key}"})
+
+    def __call__(self, texts: Documents) -> Embeddings:
+        response = self._session.post(
+            self._api_url, json={"instances": [{"content": texts}]}
+        ).json()
+
+        if "predictions" in response:
+            return response["predictions"]
+        return {}
