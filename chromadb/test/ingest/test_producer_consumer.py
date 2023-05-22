5be2e6a06cf9288a3f4315d37bb99a18646cd27a
Luke VanderHart <luke@vanderhart.net>
2023-05-22 11:49:18 -0400

    refactor tests to rely on consumer/producer interface only

--- chromadb/test/ingest/test_producer_consumer.py
+++ chromadb/test/ingest/test_producer_consumer.py
@@ -10,9 +10,9 @@ from typing import (
     Iterator,
     Sequence,
     cast,
+    Tuple,
 )
-from chromadb.db.mixins.embeddings_queue import EmbeddingsQueue
-from chromadb.ingest import RejectedEmbeddingException
+from chromadb.ingest import Producer, Consumer, RejectedEmbeddingException
 from chromadb.db.impl.sqlite import SqliteDB
 from chromadb.types import (
     InsertEmbeddingRecord,
@@ -26,17 +26,20 @@ from chromadb.config import System, Settings
 from pytest import FixtureRequest, approx
 
 
-def sqlite() -> Generator[EmbeddingsQueue, None, None]:
-    """Fixture generator for sqlite DB"""
-    yield SqliteDB(System(Settings(sqlite_database=":memory:", allow_reset=True)))
+def sqlite() -> Generator[Tuple[Producer, Consumer], None, None]:
+    """Fixture generator for sqlite Producer + Consumer"""
+    db = SqliteDB(System(Settings(sqlite_database=":memory:", allow_reset=True)))
+    yield db, db
 
 
-def db_fixtures() -> List[Callable[[], Generator[EmbeddingsQueue, None, None]]]:
+def fixtures() -> List[Callable[[], Generator[Tuple[Producer, Consumer], None, None]]]:
     return [sqlite]
 
 
-@pytest.fixture(scope="module", params=db_fixtures())
-def db(request: FixtureRequest) -> Generator[EmbeddingsQueue, None, None]:
+@pytest.fixture(scope="module", params=fixtures())
+def producer_consumer(
+    request: FixtureRequest,
+) -> Generator[Tuple[Producer, Consumer], None, None]:
     yield next(request.param())
 
 
@@ -62,7 +65,7 @@ def sample_embeddings() -> Iterator[InsertEmbeddingRecord]:
     return (create_record(i) for i in count())
 
 
-class CapturingConsumer:
+class CapturingConsumeFn:
     embeddings: List[Union[EmbeddingRecord, EmbeddingDeleteRecord]]
 
     def __init__(self) -> None:
@@ -99,160 +102,174 @@ def assert_records_match(
 
 
 def test_backfill(
-    db: EmbeddingsQueue, sample_embeddings: Iterator[InsertEmbeddingRecord]
+    producer_consumer: Tuple[Producer, Consumer],
+    sample_embeddings: Iterator[InsertEmbeddingRecord],
 ) -> None:
-    db.reset()
+    producer, consumer = producer_consumer
+    producer.reset()
 
     embeddings = [next(sample_embeddings) for _ in range(3)]
 
-    db.create_topic("test_topic")
+    producer.create_topic("test_topic")
     for e in embeddings:
-        db.submit_embedding("test_topic", e)
+        producer.submit_embedding("test_topic", e)
 
-    consume_fn = CapturingConsumer()
-    db.subscribe("test_topic", consume_fn, start=db.min_seqid())
+    consume_fn = CapturingConsumeFn()
+    consumer.subscribe("test_topic", consume_fn, start=consumer.min_seqid())
 
     assert_records_match(embeddings, consume_fn.embeddings)
 
 
 def test_notifications(
-    db: EmbeddingsQueue, sample_embeddings: Iterator[InsertEmbeddingRecord]
+    producer_consumer: Tuple[Producer, Consumer],
+    sample_embeddings: Iterator[InsertEmbeddingRecord],
 ) -> None:
-    db.reset()
-    db.create_topic("test_topic")
+    producer, consumer = producer_consumer
+    producer.reset()
+    producer.create_topic("test_topic")
 
     embeddings: List[InsertEmbeddingRecord] = []
 
-    consume_fn = CapturingConsumer()
+    consume_fn = CapturingConsumeFn()
 
-    db.subscribe("test_topic", consume_fn, start=db.min_seqid())
+    consumer.subscribe("test_topic", consume_fn, start=consumer.min_seqid())
 
     for i in range(10):
         e = next(sample_embeddings)
         embeddings.append(e)
-        db.submit_embedding("test_topic", e)
+        producer.submit_embedding("test_topic", e)
         assert_records_match(embeddings, consume_fn.embeddings)
 
 
 def test_sync_failure(
-    db: EmbeddingsQueue, sample_embeddings: Iterator[InsertEmbeddingRecord]
+    producer_consumer: Tuple[Producer, Consumer],
+    sample_embeddings: Iterator[InsertEmbeddingRecord],
 ) -> None:
-    db.reset()
-    db.create_topic("test_topic")
+    producer, consumer = producer_consumer
+    producer.reset()
+    producer.create_topic("test_topic")
 
     def failing_consumer(
         embeddings: Sequence[Union[EmbeddingRecord, EmbeddingDeleteRecord]]
     ) -> None:
         raise RejectedEmbeddingException("test failure")
 
-    db.subscribe("test_topic", failing_consumer, start=db.min_seqid())
+    consumer.subscribe("test_topic", failing_consumer, start=consumer.min_seqid())
 
     e = next(sample_embeddings)
     with pytest.raises(Exception):
-        db.submit_embedding("test_topic", e, sync=True)
+        producer.submit_embedding("test_topic", e, sync=True)
 
-    second_consumer = CapturingConsumer()
-    db.subscribe("test_topic", second_consumer, start=db.min_seqid())
+    second_consumer = CapturingConsumeFn()
+    consumer.subscribe("test_topic", second_consumer, start=consumer.min_seqid())
     assert second_consumer.embeddings == []
 
 
 def test_async_failure(
-    db: EmbeddingsQueue, sample_embeddings: Iterator[InsertEmbeddingRecord]
+    producer_consumer: Tuple[Producer, Consumer],
+    sample_embeddings: Iterator[InsertEmbeddingRecord],
 ) -> None:
-    db.reset()
-    db.create_topic("test_topic")
+    producer, consumer = producer_consumer
+    producer.reset()
+    producer.create_topic("test_topic")
 
     def failing_consumer(
         embeddings: Sequence[Union[EmbeddingRecord, EmbeddingDeleteRecord]]
     ) -> None:
         raise RejectedEmbeddingException("test failure")
 
-    db.subscribe("test_topic", failing_consumer, start=db.min_seqid())
+    consumer.subscribe("test_topic", failing_consumer, start=consumer.min_seqid())
     e = next(sample_embeddings)
-    db.submit_embedding("test_topic", e, sync=False)
+    producer.submit_embedding("test_topic", e, sync=False)
 
-    second_consumer = CapturingConsumer()
-    db.subscribe("test_topic", second_consumer, start=db.min_seqid())
+    second_consumer = CapturingConsumeFn()
+    consumer.subscribe("test_topic", second_consumer, start=consumer.min_seqid())
     assert_records_match([e], second_consumer.embeddings)
 
 
 def test_multiple_topics(
-    db: EmbeddingsQueue, sample_embeddings: Iterator[InsertEmbeddingRecord]
+    producer_consumer: Tuple[Producer, Consumer],
+    sample_embeddings: Iterator[InsertEmbeddingRecord],
 ) -> None:
-    db.reset()
-    db.create_topic("test_topic_1")
-    db.create_topic("test_topic_2")
+    producer, consumer = producer_consumer
+    producer.reset()
+    producer.create_topic("test_topic_1")
+    producer.create_topic("test_topic_2")
 
     embeddings_1: List[InsertEmbeddingRecord] = []
     embeddings_2: List[InsertEmbeddingRecord] = []
 
-    consume_fn_1 = CapturingConsumer()
-    consume_fn_2 = CapturingConsumer()
+    consume_fn_1 = CapturingConsumeFn()
+    consume_fn_2 = CapturingConsumeFn()
 
-    db.subscribe("test_topic_1", consume_fn_1, start=db.min_seqid())
-    db.subscribe("test_topic_2", consume_fn_2, start=db.min_seqid())
+    consumer.subscribe("test_topic_1", consume_fn_1, start=consumer.min_seqid())
+    consumer.subscribe("test_topic_2", consume_fn_2, start=consumer.min_seqid())
 
     for i in range(10):
         e_1 = next(sample_embeddings)
         embeddings_1.append(e_1)
-        db.submit_embedding("test_topic_1", e_1)
+        producer.submit_embedding("test_topic_1", e_1)
         assert_records_match(embeddings_1, consume_fn_1.embeddings)
 
         e_2 = next(sample_embeddings)
         embeddings_2.append(e_2)
-        db.submit_embedding("test_topic_2", e_2)
+        producer.submit_embedding("test_topic_2", e_2)
         assert_records_match(embeddings_2, consume_fn_2.embeddings)
 
 
 def test_start_seq_id(
-    db: EmbeddingsQueue, sample_embeddings: Iterator[InsertEmbeddingRecord]
+    producer_consumer: Tuple[Producer, Consumer],
+    sample_embeddings: Iterator[InsertEmbeddingRecord],
 ) -> None:
-    db.reset()
-    db.create_topic("test_topic")
+    producer, consumer = producer_consumer
+    producer.reset()
+    producer.create_topic("test_topic")
 
-    consume_fn_1 = CapturingConsumer()
-    consume_fn_2 = CapturingConsumer()
+    consume_fn_1 = CapturingConsumeFn()
+    consume_fn_2 = CapturingConsumeFn()
 
-    db.subscribe("test_topic", consume_fn_1, start=db.min_seqid())
+    consumer.subscribe("test_topic", consume_fn_1, start=consumer.min_seqid())
 
     embeddings = []
     for _ in range(5):
         e = next(sample_embeddings)
         embeddings.append(e)
-        db.submit_embedding("test_topic", e)
+        producer.submit_embedding("test_topic", e)
 
     assert_records_match(embeddings, consume_fn_1.embeddings)
 
     start = consume_fn_1.embeddings[-1]["seq_id"]
-    db.subscribe("test_topic", consume_fn_2, start=start)
+    consumer.subscribe("test_topic", consume_fn_2, start=start)
     for _ in range(5):
         e = next(sample_embeddings)
         embeddings.append(e)
-        db.submit_embedding("test_topic", e)
+        producer.submit_embedding("test_topic", e)
 
     assert_records_match(embeddings[-5:], consume_fn_2.embeddings)
 
 
 def test_end_seq_id(
-    db: EmbeddingsQueue, sample_embeddings: Iterator[InsertEmbeddingRecord]
+    producer_consumer: Tuple[Producer, Consumer],
+    sample_embeddings: Iterator[InsertEmbeddingRecord],
 ) -> None:
-    db.reset()
-    db.create_topic("test_topic")
+    producer, consumer = producer_consumer
+    producer.reset()
+    producer.create_topic("test_topic")
 
-    consume_fn_1 = CapturingConsumer()
-    consume_fn_2 = CapturingConsumer()
+    consume_fn_1 = CapturingConsumeFn()
+    consume_fn_2 = CapturingConsumeFn()
 
-    db.subscribe("test_topic", consume_fn_1, start=db.min_seqid())
+    consumer.subscribe("test_topic", consume_fn_1, start=consumer.min_seqid())
 
     embeddings = []
     for _ in range(10):
         e = next(sample_embeddings)
         embeddings.append(e)
-        db.submit_embedding("test_topic", e)
+        producer.submit_embedding("test_topic", e)
 
     assert_records_match(embeddings, consume_fn_1.embeddings)
 
     end = consume_fn_1.embeddings[-5]["seq_id"]
-    db.subscribe("test_topic", consume_fn_2, start=db.min_seqid(), end=end)
+    consumer.subscribe("test_topic", consume_fn_2, start=consumer.min_seqid(), end=end)
 
     assert_records_match(embeddings[:6], consume_fn_2.embeddings)
