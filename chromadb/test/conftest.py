70470511b325ccccbf9d66949aca280c7e856c1a
hammadb <hammad@trychroma.com>
2023-05-31 17:45:13 -0700

    revert fixture

--- chromadb/test/conftest.py
+++ chromadb/test/conftest.py
@@ -35,7 +35,7 @@ def find_free_port() -> int:
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.bind(("", 0))
         s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    return s.getsockname()[1]  # type: ignore
+        return s.getsockname()[1]  # type: ignore
 
 
 def _run_server(port: int) -> None:
