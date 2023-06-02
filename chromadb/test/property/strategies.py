b4be6e2c617dc4667437e4a1921ff4e27a7607cf
atroyn <anton.troynikov@gmail.com>
2023-06-02 13:26:23 -0700

    Bound safe floats

--- chromadb/test/property/strategies.py
+++ chromadb/test/property/strategies.py
@@ -103,7 +103,11 @@ safe_integers = st.integers(
     min_value=-(2**31), max_value=2**31 - 1
 )  # TODO: handle longs
 safe_floats = st.floats(
-    allow_infinity=False, allow_nan=False, allow_subnormal=False
+    allow_infinity=False,
+    allow_nan=False,
+    allow_subnormal=False,
+    min_value=-1e6,
+    max_value=1e6,
 )  # TODO: handle infinity and NAN
 
 safe_values: List[SearchStrategy[Union[int, float, str]]] = [
