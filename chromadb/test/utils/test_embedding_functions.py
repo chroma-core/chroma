307896611d96648fee58a75a76319d2d8d8ba361
benakrab <mustapha.benbarka@mindimensions.fr>
2023-06-15 15:39:49 +0200

    add some unittests for SentenceTransformerEmbeddingFunction

---
+++ chromadb/test/utils/test_embedding_functions.py
@@ -0,0 +1,28 @@
+import logging
+from unittest import mock
+
+import pytest
+
+from chromadb.utils.embedding_functions import SentenceTransformerEmbeddingFunction
+
+logger = logging.getLogger(__name__)
+
+
+class TestSentenceTransformerEmbeddingFunction:
+    error_message_require_sentence_transformers = (
+        "The sentence_transformers python package is not installed."
+        " Please install it with `pip install sentence_transformers`"
+    )
+    good_model_name = "all-MiniLM-L6-v2"
+
+    def test__init__require_sentence_transformers(self) -> None:
+        with mock.patch.dict("sys.modules", sentence_transformers=None):
+            with pytest.raises(ValueError) as exc_info:
+                SentenceTransformerEmbeddingFunction(model_name=self.good_model_name)
+        assert self.error_message_require_sentence_transformers in str(exc_info.value)
+
+    def test__init__with_good_model_name(self) -> None:
+        sent_trans_embed_func = SentenceTransformerEmbeddingFunction(
+            model_name=self.good_model_name
+        )
+        assert sent_trans_embed_func.models.get(self.good_model_name) is not None
