e760831ea0441cce62bfcf9022363bdda36c19df
benakrab <mustapha.benbarka@mindimensions.fr>
2023-06-18 09:43:40 +0200

    tests for cohere embedding function

--- chromadb/test/utils/test_embedding_functions.py
+++ chromadb/test/utils/test_embedding_functions.py
@@ -6,6 +6,7 @@ from unittest import mock
 import pytest
 
 from chromadb.utils.embedding_functions import (
+    CohereEmbeddingFunction,
     OpenAIEmbeddingFunction,
     SentenceTransformerEmbeddingFunction,
     Text2VecEmbeddingFunction,
@@ -169,6 +170,54 @@ class TestOpenAIEmbeddingFunction:
                 openai_embed_func = OpenAIEmbeddingFunction(api_key=self.api_key)
                 assert callable(openai_embed_func)
                 embeddings = openai_embed_func(texts=self.documents)
+                assert len(embeddings) == len(self.documents)
+                for embedding in embeddings:
+                    assert len(embedding) == self.embedding_dim
+
+
+class TestCohereEmbeddingFunction:
+    error_message_require_cohere = (
+        "The cohere python package is not installed. "
+        "Please install it with `pip install cohere`"
+    )
+    api_key = os.getenv("COHERE_TOKEN", default="thisisanapikey")
+    documents = ["document 1", "document 2"]
+    embedding_dim = 96
+
+    cohere_api_response = {
+        "id": "7f37d160-945a-4aba-a3cd-3da4d3552eac",
+        "texts": ["document 1", "document 2"],
+        "embeddings": [
+            embedding_dim * [0.71386],
+            embedding_dim * [-0.40307],
+        ],
+        "meta": {"api_version": {"version": "1"}},
+    }
+
+    try:
+        import cohere
+    except ModuleNotFoundError:
+        pass
+
+    def test__init__require_cohere(self) -> None:
+        with mock.patch.dict("sys.modules", cohere=None):
+            with pytest.raises(ValueError) as exc_info:
+                CohereEmbeddingFunction(api_key=self.api_key)
+        assert self.error_message_require_cohere in str(exc_info.value)
+
+    # TODO: getting cohore embeddings can be achieved by :
+    #  `self._client.embed(texts=texts, model=self._model_name).embeddings` without adding a new loop.
+    #   I am using cohere 4.11.2.
+    #   May be embedding_functions.py needs some refactoring.
+    #   It will be very useful if someone can add a requirements file concerning :
+    #   openai, cohere, sentence_transformers, etc.
+    def test_callable_instances(self) -> None:
+        if "cohere" in sys.modules:
+            with mock.patch("cohere.Client.embed") as patched_call:
+                patched_call.return_value = self.cohere_api_response.get("embeddings")
+                cohere_embed_func = CohereEmbeddingFunction(api_key=self.api_key)
+                assert callable(cohere_embed_func)
+                embeddings = cohere_embed_func(texts=self.documents)
                 assert len(embeddings) == len(self.documents)
                 for embedding in embeddings:
                     assert len(embedding) == self.embedding_dim
