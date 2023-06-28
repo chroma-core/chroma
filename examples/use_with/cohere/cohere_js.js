73974046c4b7f12caf78c30f3aaa0f574f1cd4e8
Jeffrey Huber <jeff@trychroma.com>
2023-06-28 15:24:38 -0700

    cohere examples in js and python

--- examples/use_with/cohere/cohere_js.js
+++ examples/use_with/cohere/cohere_js.js
@@ -28,28 +28,53 @@ const cohere = require("cohere-ai");
 
 const main = async () => {
 
+  const COHERE_API_KEY = "COHERE_API_KEY";
+
   const client = new chroma.ChromaClient({ path: "http://localhost:8000" });
   await client.reset();
 
-  const cohereAIEmbedder = new chroma.CohereEmbeddingFunction({ cohere_api_key: "APIKEY" });
+  const cohereAIEmbedder = new chroma.CohereEmbeddingFunction({ cohere_api_key: COHERE_API_KEY });
 
   const collection = await client.createCollection({
     name: "cohere_js",
     embeddingFunction: cohereAIEmbedder
   });
 
-  console.log(await cohereAIEmbedder.generate(["I like apples", "I like bananas", "I like oranges"]))
+  await collection.add({
+    ids: ["1", "2", "3"],
+    documents: ["I like apples", "I like bananas", "I like oranges"],
+    metadatas: [{ "fruit": "apple" }, { "fruit": "banana" }, { "fruit": "orange" }],
+  });
 
-  // await collection.add({
-  //   ids: ["1", "2", "3"],
-  //   documents: ["I like apples", "I like bananas", "I like oranges"],
-  //   metadatas: [{ "fruit": "apple" }, { "fruit": "banana" }, { "fruit": "orange" }],
-  // });
+  console.log(await collection.query({
+    queryTexts: ["citrus"],
+    nResults: 1
+  }));
 
-  // console.log(await collection.query({
-  //   queryTexts: ["citrus"],
-  //   nResults: 1
-  // }));
+  // Multilingual Example
+
+  const cohereAIMulitlingualEmbedder = new chroma.CohereEmbeddingFunction({ cohere_api_key: COHERE_API_KEY, model: "multilingual-22-12" });
+
+  const collection_multilingual = await client.createCollection({
+    name: "cohere_js_multilingual",
+    embeddingFunction: cohereAIMulitlingualEmbedder
+  });
+
+  // # 나는 오렌지를 좋아한다 is "I like oranges" in Korean
+  multilingual_texts = [ 'Hello from Cohere!', 'مرحبًا من كوهير!',
+        'Hallo von Cohere!', 'Bonjour de Cohere!',
+        '¡Hola desde Cohere!', 'Olá do Cohere!',
+        'Ciao da Cohere!', '您好，来自 Cohere！',
+        'कोहेरे से नमस्ते!', '나는 오렌지를 좋아한다'  ]
+
+  let ids = Array.from({ length: multilingual_texts.length }, (_, i) => String(i));
+
+  await collection.add({
+      ids:ids,
+      documents:multilingual_texts
+})
+
+  console.log(await collection.query({queryTexts:["citrus"], nResults:1}))
 
 }
 
