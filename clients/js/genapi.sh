1ecdee6b1b64656e8c77e1cd0ebb66437d783b7c
Sebastian Sosa <1sebastian1sosa1@gmail.com>
2023-07-04 21:59:37 -0400

    reinstate genapi comments

--- clients/js/genapi.sh
+++ clients/js/genapi.sh
@@ -1,12 +1,15 @@
 #!/usr/bin/env sh
 
+# curl -s http://localhost:8000/openapi.json | jq > openapi.json
 curl -s http://localhost:8000/openapi.json | python -c "import sys, json; print(json.dumps(json.load(sys.stdin), indent=2))" > openapi.json
 
 if [[ "$OSTYPE" == "darwin"* ]]; then
+  # macOS
   sed -i '' 's/"schema": {}/"schema": {"type": "object"}/g' openapi.json
   sed -i '' 's/"items": {}/"items": { "type": "object" }/g' openapi.json
   sed -i '' -e 's/"title": "Collection Name"/"title": "Collection Name","type": "string"/g' openapi.json
 else
+  # Linux
   sed -i 's/"schema": {}/"schema": {"type": "object"}/g' openapi.json
   sed -i 's/"items": {}/"items": { "type": "object" }/g' openapi.json
   sed -i -e 's/"title": "Collection Name"/"title": "Collection Name","type": "string"/g' openapi.json
