7b0139f15c0a8db142d479c7e752cc715ab72701
hammadb <hammad@trychroma.com>
2023-07-13 10:03:46 -0700

    Just use a basic, first readers-writers problem

--- chromadb/utils/read_write_lock.py
+++ chromadb/utils/read_write_lock.py
@@ -4,40 +4,41 @@ from typing import Optional, Type
 
 
 class ReadWriteLock:
+    """A lock object that allows many simultaneous "read locks", but
+    only one "write lock." """
+
     def __init__(self) -> None:
         self._read_ready = threading.Condition(threading.RLock())
         self._readers = 0
-        self._writers = 0
 
     def acquire_read(self) -> None:
         """Acquire a read lock. Blocks only if a thread has
         acquired the write lock."""
         self._read_ready.acquire()
         try:
-            while self._writers > 0:
-                self._read_ready.wait()
             self._readers += 1
         finally:
             self._read_ready.release()
 
     def release_read(self) -> None:
+        """Release a read lock."""
         self._read_ready.acquire()
         try:
             self._readers -= 1
-            if self._readers == 0:
-                self._read_ready.notify_all()
+            if not self._readers:
+                self._read_ready.notifyAll()
         finally:
             self._read_ready.release()
 
     def acquire_write(self) -> None:
+        """Acquire a write lock. Blocks until there are no
+        acquired read or write locks."""
         self._read_ready.acquire()
-        self._writers += 1
         while self._readers > 0:
             self._read_ready.wait()
 
     def release_write(self) -> None:
-        self._writers -= 1
-        self._read_ready.notify_all()
+        """Release a write lock."""
         self._read_ready.release()
 
 
