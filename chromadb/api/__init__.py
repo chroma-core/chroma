b5065f4dd579b51691c9abd4b808f1b98e897839
Anton Troynikov <atroyn@users.noreply.github.com>
2023-07-14 09:55:13 -0700

    API ABC Docstrings (#803)
    ## Description of changes
    
    *Summarize the changes made by this PR.*
    - Updated and cleaned up all the Docstrings on the API Abstract Base
    Class
    - Removed all the shadowing docstrings from the concrete API
    implementations
    
    ## Test plan
    N/A - No functional change
    
    ## Documentation Changes
    This is a documentation change.

--- chromadb/api/__init__.py
+++ chromadb/api/__init__.py
@@ -23,27 +23,30 @@ import chromadb.utils.embedding_functions as ef
 class API(Component, ABC):
     @abstractmethod
     def heartbeat(self) -> int:
-        """Returns the current server time in nanoseconds to check if the server is alive
-
-        Args:
-            None
+        """Get the current time in nanoseconds since epoch.
+        Used to check if the server is alive.
 
         Returns:
-            int: The current server time in nanoseconds
+            int: The current time in nanoseconds since epoch
 
         """
         pass
 
+    #
+    # COLLECTION METHODS
+    #
+
     @abstractmethod
     def list_collections(self) -> Sequence[Collection]:
-        """Returns all collections in the database
-
-        Args:
-            None
-
+        """List all collections.
         Returns:
-            dict: A dictionary of collections
+            Sequence[Collection]: A list of collections
 
+        Examples:
+            ```python
+            client.list_collections()
+            # [collection(name="my_collection", metadata={})]
+            ```
         """
         pass
 
@@ -55,49 +58,29 @@ class API(Component, ABC):
         embedding_function: Optional[EmbeddingFunction] = ef.DefaultEmbeddingFunction(),
         get_or_create: bool = False,
     ) -> Collection:
-        """Creates a new collection in the database
-
+        """Create a new collection with the given name and metadata.
         Args:
-            name  The name of the collection to create. The name must be unique.
-            metadata: A dictionary of metadata to associate with the collection. Defaults to None.
-            embedding_function: A function that takes documents and returns an embedding. Defaults to None.
-            get_or_create: If True, will return the collection if it already exists,
-                and update the metadata (if applicable). Defaults to False.
+            name: The name of the collection to create.
+            metadata: Optional metadata to associate with the collection.
+            embedding_function: Optional function to use to embed documents.
+                                Uses the default embedding function if not provided.
+            get_or_create: If True, return the existing collection if it exists.
 
         Returns:
-            dict: the created collection
+            Collection: The newly created collection.
 
-        """
-        pass
+        Raises:
+            ValueError: If the collection already exists and get_or_create is False.
+            ValueError: If the collection name is invalid.
 
-    @abstractmethod
-    def delete_collection(
-        self,
-        name: str,
-    ) -> None:
-        """Deletes a collection from the database
+        Examples:
+            ```python
+            client.create_collection("my_collection")
+            # collection(name="my_collection", metadata={})
 
-        Args:
-            name: The name of the collection to delete
-        """
-
-    @abstractmethod
-    def get_or_create_collection(
-        self,
-        name: str,
-        metadata: Optional[CollectionMetadata] = None,
-        embedding_function: Optional[EmbeddingFunction] = ef.DefaultEmbeddingFunction(),
-    ) -> Collection:
-        """Calls create_collection with get_or_create=True.
-           If the collection exists, but with different metadata, the metadata will be replaced.
-
-        Args:
-            name: The name of the collection to create. The name must be unique.
-            metadata: A dictionary of metadata to associate with the collection. Defaults to None.
-            embedding_function: A function that takes documents and returns an embedding. Should be the same as the one used to create the collection. Defaults to None.
-        Returns:
-            the created collection
-
+            client.create_collection("my_collection", metadata={"foo": "bar"})
+            # collection(name="my_collection", metadata={"foo": "bar"})
+            ```
         """
         pass
 
@@ -107,15 +90,47 @@ class API(Component, ABC):
         name: str,
         embedding_function: Optional[EmbeddingFunction] = ef.DefaultEmbeddingFunction(),
     ) -> Collection:
-        """Gets a collection from the database by either name or uuid
-
+        """Get a collection with the given name.
         Args:
-            name: The name of the collection to get. Defaults to None.
-            embedding_function: A function that takes documents and returns an embedding. Should be the same as the one used to create the collection. Defaults to None.
+            name: The name of the collection to get
+            embedding_function: Optional function to use to embed documents.
+                                Uses the default embedding function if not provided.
 
         Returns:
-            dict: the requested collection
+            Collection: The collection
 
+        Raises:
+            ValueError: If the collection does not exist
+
+        Examples:
+            ```python
+            client.get_collection("my_collection")
+            # collection(name="my_collection", metadata={})
+            ```
+        """
+        pass
+
+    @abstractmethod
+    def get_or_create_collection(
+        self,
+        name: str,
+        metadata: Optional[CollectionMetadata] = None,
+        embedding_function: Optional[EmbeddingFunction] = ef.DefaultEmbeddingFunction(),
+    ) -> Collection:
+        """Get or create a collection with the given name and metadata.
+        Args:
+            name: The name of the collection to get or create
+            metadata: Optional metadata to associate with the collection
+            embedding_function: Optional function to use to embed documents
+
+        Returns:
+            The collection
+
+        Examples:
+            ```python
+            client.get_or_create_collection("my_collection")
+            # collection(name="my_collection", metadata={})
+            ```
         """
         pass
 
@@ -125,14 +140,39 @@ class API(Component, ABC):
         new_name: Optional[str] = None,
         new_metadata: Optional[CollectionMetadata] = None,
     ) -> None:
-        """Modify a collection in the database - can update the name and/or metadata
+        """[Internal] Modify a collection by UUID. Can update the name and/or metadata.
 
         Args:
-            current_name: The name of the collection to modify
-            new_name: The new name of the collection. Defaults to None.
-            new_metadata: The new metadata to associate with the collection. Defaults to None.
+            id: The internal UUID of the collection to modify.
+            new_name: The new name of the collection.
+                                If None, the existing name will remain. Defaults to None.
+            new_metadata: The new metadata to associate with the collection.
+                                      Defaults to None.
         """
         pass
+
+    @abstractmethod
+    def delete_collection(
+        self,
+        name: str,
+    ) -> None:
+        """Delete a collection with the given name.
+        Args:
+            name: The name of the collection to delete.
+
+        Raises:
+            ValueError: If the collection does not exist.
+
+        Examples:
+            ```python
+            client.delete_collection("my_collection")
+            ```
+        """
+        pass
+
+    #
+    # ITEM METHODS
+    #
 
     @abstractmethod
     def _add(
@@ -144,15 +184,18 @@ class API(Component, ABC):
         documents: Optional[Documents] = None,
         increment_index: bool = True,
     ) -> bool:
-        """Add embeddings to the data store. This is the most general way to add embeddings to the database.
-        ⚠️ It is recommended to use the more specific methods below when possible.
+        """[Internal] Add embeddings to a collection specified by UUID.
+        If (some) ids already exist, only the new embeddings will be added.
 
         Args:
-            collection_id: The collection to add the embeddings to
-            embedding: The sequence of embeddings to add
+            ids: The ids to associate with the embeddings.
+            collection_id: The UUID of the collection to add the embeddings to.
+            embedding: The sequence of embeddings to add.
             metadata: The metadata to associate with the embeddings. Defaults to None.
             documents: The documents to associate with the embeddings. Defaults to None.
-            ids: The ids to associate with the embeddings. Defaults to None.
+
+        Returns:
+            True if the embeddings were added successfully.
         """
         pass
 
@@ -165,12 +208,17 @@ class API(Component, ABC):
         metadatas: Optional[Metadatas] = None,
         documents: Optional[Documents] = None,
     ) -> bool:
-        """Add embeddings to the data store. This is the most general way to add embeddings to the database.
-        ⚠️ It is recommended to use the more specific methods below when possible.
+        """[Internal] Update entries in a collection specified by UUID.
 
         Args:
-            collection_id: The collection to add the embeddings to
-            embedding: The sequence of embeddings to add
+            collection_id: The UUID of the collection to update the embeddings in.
+            ids: The IDs of the entries to update.
+            embeddings: The sequence of embeddings to update. Defaults to None.
+            metadatas: The metadata to associate with the embeddings. Defaults to None.
+            documents: The documents to associate with the embeddings. Defaults to None.
+
+        Returns:
+            True if the embeddings were updated successfully.
         """
         pass
 
@@ -184,8 +232,9 @@ class API(Component, ABC):
         documents: Optional[Documents] = None,
         increment_index: bool = True,
     ) -> bool:
-        """Add or update entries in the embedding store.
-        If an entry with the same id already exists, it will be updated, otherwise it will be added.
+        """[Internal] Add or update entries in the a collection specified by UUID.
+        If an entry with the same id already exists, it will be updated,
+        otherwise it will be added.
 
         Args:
             collection_id: The collection to add the embeddings to
@@ -193,17 +242,17 @@ class API(Component, ABC):
             embeddings: The sequence of embeddings to add
             metadatas: The metadata to associate with the embeddings. Defaults to None.
             documents: The documents to associate with the embeddings. Defaults to None.
-            increment_index: If True, will incrementally add to the ANN index of the collection. Defaults to True.
+            increment_index: If True, will incrementally add to the ANN index.
+                                          Defaults to True.
         """
         pass
 
     @abstractmethod
     def _count(self, collection_id: UUID) -> int:
-        """Returns the number of embeddings in the database
+        """[Internal] Returns the number of entries in a collection specified by UUID.
 
         Args:
-            collection_id: The collection to count the embeddings in.
-
+            collection_id: The UUID of the collection to count the embeddings in.
 
         Returns:
             int: The number of embeddings in the collection
@@ -213,6 +262,17 @@ class API(Component, ABC):
 
     @abstractmethod
     def _peek(self, collection_id: UUID, n: int = 10) -> GetResult:
+        """[Internal] Returns the first n entries in a collection specified by UUID.
+
+        Args:
+            collection_id: The UUID of the collection to peek into.
+            n: The number of entries to peek. Defaults to 10.
+
+        Returns:
+            GetResult: The first n entries in the collection.
+
+        """
+
         pass
 
     @abstractmethod
@@ -229,19 +289,21 @@ class API(Component, ABC):
         where_document: Optional[WhereDocument] = {},
         include: Include = ["embeddings", "metadatas", "documents"],
     ) -> GetResult:
-        """Gets embeddings from the database. Supports filtering, sorting, and pagination.
-        ⚠️ This method should not be used directly.
+        """[Internal] Returns entries from a collection specified by UUID.
 
         Args:
-            where: A dictionary of key-value pairs to filter the embeddings by. Defaults to {}.
-            sort: The column to sort the embeddings by. Defaults to None.
-            limit: The maximum number of embeddings to return. Defaults to None.
-            offset: The number of embeddings to skip before returning. Defaults to None.
+            ids: The IDs of the entries to get. Defaults to None.
+            where: Conditional filtering on metadata. Defaults to {}.
+            sort: The column to sort the entries by. Defaults to None.
+            limit: The maximum number of entries to return. Defaults to None.
+            offset: The number of entries to skip before returning. Defaults to None.
             page: The page number to return. Defaults to None.
-            page_size: The number of embeddings to return per page. Defaults to None.
-
+            page_size: The number of entries to return per page. Defaults to None.
+            where_document: Conditional filtering on documents. Defaults to {}.
+            include: The fields to include in the response.
+                          Defaults to ["embeddings", "metadatas", "documents"].
         Returns:
-            pd.DataFrame: A pandas dataframe containing the embeddings and metadata
+            GetResult: The entries in the collection that match the query.
 
         """
         pass
@@ -254,14 +316,16 @@ class API(Component, ABC):
         where: Optional[Where] = {},
         where_document: Optional[WhereDocument] = {},
     ) -> IDs:
-        """Deletes embeddings from the database
-        ⚠️ This method should not be used directly.
+        """[Internal] Deletes entries from a collection specified by UUID.
 
         Args:
-            where: A dictionary of key-value pairs to filter the embeddings by. Defaults to {}.
+            collection_id: The UUID of the collection to delete the entries from.
+            ids: The IDs of the entries to delete. Defaults to None.
+            where: Conditional filtering on metadata. Defaults to {}.
+            where_document: Conditional filtering on documents. Defaults to {}.
 
         Returns:
-            List: The list of internal UUIDs of the deleted embeddings
+            IDs: The list of IDs of the entries that were deleted.
         """
         pass
 
@@ -275,31 +339,34 @@ class API(Component, ABC):
         where_document: WhereDocument = {},
         include: Include = ["embeddings", "metadatas", "documents", "distances"],
     ) -> QueryResult:
-        """Gets the nearest neighbors of a single embedding
-        ⚠️ This method should not be used directly.
+        """[Internal] Performs a nearest neighbors query on a collection specified by UUID.
 
         Args:
-            embedding: The embedding to find the nearest neighbors of
-            n_results: The number of nearest neighbors to return. Defaults to 10.
-            where: A dictionary of key-value pairs to filter the embeddings by. Defaults to {}.
+            collection_id: The UUID of the collection to query.
+            query_embeddings: The embeddings to use as the query.
+            n_results: The number of results to return. Defaults to 10.
+            where: Conditional filtering on metadata. Defaults to {}.
+            where_document: Conditional filtering on documents. Defaults to {}.
+            include: The fields to include in the response.
+                          Defaults to ["embeddings", "metadatas", "documents", "distances"].
+
+        Returns:
+            QueryResult: The results of the query.
         """
         pass
 
     @abstractmethod
     def reset(self) -> bool:
-        """Resets the database
-        ⚠️ This is destructive and will delete all data in the database.
-        Args:
-            None
+        """Resets the database. This will delete all collections and entries.
+
         Returns:
-            None
+            bool: True if the database was reset successfully.
         """
         pass
 
     @abstractmethod
     def raw_sql(self, sql: str) -> pd.DataFrame:
         """Runs a raw SQL query against the database
-        ⚠️ This method should not be used directly.
 
         Args:
             sql: The SQL query to run
@@ -312,10 +379,9 @@ class API(Component, ABC):
     @abstractmethod
     def create_index(self, collection_name: str) -> bool:
         """Creates an index for the given collection
-        ⚠️ This method should not be used directly.
 
         Args:
-            collection_name: The collection to create the index for. Uses the client's collection if None. Defaults to None.
+            collection_name: The collection to create the index for. Defaults to None.
 
         Returns:
             bool: True if the index was created successfully
@@ -325,7 +391,13 @@ class API(Component, ABC):
 
     @abstractmethod
     def persist(self) -> bool:
-        """Persist the database to disk"""
+        """Persist the database to disk
+
+        Returns:
+            bool: True if the database was persisted successfully
+
+        """
+
         pass
 
     @abstractmethod
