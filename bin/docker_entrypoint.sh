daa2ff64ca6c6a08134189a4a1a3322948f35eba
hammadb <hammad@trychroma.com>
2023-07-16 20:20:49 -0700

    Add persistence to docker

--- bin/docker_entrypoint.sh
+++ bin/docker_entrypoint.sh
@@ -2,4 +2,5 @@
 
 echo "Rebuilding hnsw to ensure architecture compatibility"
 pip install --force-reinstall --no-cache-dir chroma-hnswlib
+export IS_PERSISTENT=1
 uvicorn chromadb.app:app --workers 1 --host 0.0.0.0 --port 8000 --proxy-headers --log-config log_config.yml
