a29bcf71005ec9897443b1a7758accb7f27f15b8
atroyn <anton.troynikov@gmail.com>
2023-07-20 20:17:58 -0700

    New events

--- chromadb/telemetry/events.py
+++ chromadb/telemetry/events.py
@@ -14,10 +14,52 @@ class ServerStartEvent(TelemetryEvent):
 
 
 @dataclass
+class ClientCreateCollectionEvent(TelemetryEvent):
+    name: ClassVar[str] = "client_create_collection"
+    collection_uuid: str
+    embedding_function: str
+
+
+@dataclass
 class CollectionAddEvent(TelemetryEvent):
     name: ClassVar[str] = "collection_add"
     collection_uuid: str
     add_amount: int
+    with_embeddings: bool
+    with_metadatas: bool
+    with_documents: bool
+
+
+@dataclass
+class CollectionUpdateEvent(TelemetryEvent):
+    name: ClassVar[str] = "collection_update"
+    collection_uuid: str
+    update_amount: int
+    with_embeddings: bool
+    with_metadatas: bool
+    with_documents: bool
+
+
+@dataclass
+class CollectionQueryEvent(TelemetryEvent):
+    name: ClassVar[str] = "collection_query"
+    collection_uuid: str
+    with_metadata_filter: bool
+    with_document_filter: bool
+    query_with_embeddings: bool
+    query_size: int
+    n_neighbors: int
+    including: str
+
+
+@dataclass
+class CollectionGetEvent(TelemetryEvent):
+    name: ClassVar[str] = "collection_get"
+    collection_uuid: str
+    with_ids: bool
+    with_metadata_filter: bool
+    with_document_filter: bool
+    including: str
 
 
 @dataclass
