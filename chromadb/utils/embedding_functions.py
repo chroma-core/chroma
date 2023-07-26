4ddee076b48c6e628bdd6a00964af084c93a6f12
Minhhhh <mineskiroxor@gmail.com>
2023-07-26 21:51:08 +0800

    fix: update api request

--- chromadb/utils/embedding_functions.py
+++ chromadb/utils/embedding_functions.py
@@ -369,19 +369,23 @@ class GoogleVertexEmbeddingFunction(EmbeddingFunction):
     def __init__(
         self,
         api_key: str,
-        model_name: str = "textembedding-gecko-001",
+        model_name: str = "textembedding-gecko",
         project_id: str = "cloud-large-language-models",
         region: str = "us-central1",
     ):
-        self._api_url = f"https://{region}-aiplatform.googleapis.com/v1/projects/{project_id}/locations/{region}/endpoints/{model_name}:predict"
+        self._api_url = f"https://{region}-aiplatform.googleapis.com/v1/projects/{project_id}/locations/{region}/publishers/goole/models/{model_name}:predict"
         self._session = requests.Session()
         self._session.headers.update({"Authorization": f"Bearer {api_key}"})
 
     def __call__(self, texts: Documents) -> Embeddings:
-        response = self._session.post(
-            self._api_url, json={"instances": [{"content": texts}]}
-        ).json()
 
-        if "predictions" in response:
-            return response["predictions"]
-        return []
+        embeddings = []
+        for text in texts:
+            response = self._session.post(
+                self._api_url, json={"instances": [{"content": text}]}
+            ).json()
+
+            if "predictions" in response:
+                embeddings.append(response["predictions"]["embeddings"]["values"])
+
+        return embeddings
