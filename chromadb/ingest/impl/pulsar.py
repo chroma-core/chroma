1b708095d534d54553d28a0576d16b8336d48e77
hammadb <hammad@trychroma.com>
2023-08-01 10:56:01 -0700

    basic pulsar producer

--- chromadb/ingest/impl/pulsar.py
+++ chromadb/ingest/impl/pulsar.py
@@ -1,8 +1,7 @@
-from typing import Dict, Optional
+from typing import Dict
 from chromadb.config import System
-from chromadb.ingest import Consumer, ConsumerCallbackFn, Producer
+from chromadb.ingest import Producer
 from overrides import overrides, EnforceOverrides
-from uuid import UUID
 from chromadb.proto.convert import to_proto_submit
 import chromadb.proto.chroma_pb2 as proto
 from chromadb.types import SeqId, SubmitEmbeddingRecord
@@ -48,6 +47,7 @@ class PulsarProducer(Producer, EnforceOverrides):
         """Add an embedding record to the given topic. Returns the SeqID of the record."""
         producer = self._topic_to_producer[topic_name]
         proto_submit: proto.SubmitEmbeddingRecord = to_proto_submit(embedding)
+        # TODO: batch performance?
         msg_id: pulsar.MessageId = producer.send(proto_submit.SerializeToString())
         return pulsar_to_int(msg_id)
 
@@ -56,53 +56,6 @@ class PulsarProducer(Producer, EnforceOverrides):
             producer = self._client.create_producer(topic_name)
             self._topic_to_producer[topic_name] = producer
         return self._topic_to_producer[topic_name]
-
-
-class PulsarConsumer(Consumer, EnforceOverrides):
-    def __init__(self) -> None:
-        pass
-
-    @overrides
-    def subscribe(
-        self,
-        topic_name: str,
-        consume_fn: ConsumerCallbackFn,
-        start: Optional[SeqId] = None,
-        end: Optional[SeqId] = None,
-        id: Optional[UUID] = None,
-    ) -> UUID:
-        """Register a function that will be called to recieve embeddings for a given
-        topic. The given function may be called any number of times, with any number of
-        records, and may be called concurrently.
-
-        Only records between start (exclusive) and end (inclusive) SeqIDs will be
-        returned. If start is None, the first record returned will be the next record
-        generated, not including those generated before creating the subscription. If
-        end is None, the consumer will consume indefinitely, otherwise it will
-        automatically be unsubscribed when the end SeqID is reached.
-
-        If the function throws an exception, the function may be called again with the
-        same or different records.
-
-        Takes an optional UUID as a unique subscription ID. If no ID is provided, a new
-        ID will be generated and returned."""
-        pass
-
-    @overrides
-    def unsubscribe(self, subscription_id: UUID) -> None:
-        """Unregister a subscription. The consume function will no longer be invoked,
-        and resources associated with the subscription will be released."""
-        pass
-
-    @overrides
-    def min_seqid(self) -> SeqId:
-        """Return the minimum possible SeqID in this implementation."""
-        pass
-
-    @overrides
-    def max_seqid(self) -> SeqId:
-        """Return the maximum possible SeqID in this implementation."""
-        pass
 
 
 def _create_pulsar_connection_str(host: str, port: str) -> str:
