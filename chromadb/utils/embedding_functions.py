0cff79553a84a794483f72aeca97398157b89b21
Josh XT <josh@devxt.com>
2023-08-13 07:57:14 -0400

    Updates

--- chromadb/utils/embedding_functions.py
+++ chromadb/utils/embedding_functions.py
@@ -345,8 +345,9 @@ class ONNXMiniLM_L6_V2(EmbeddingFunction):
 
     def _download_model_if_not_exists(self) -> None:
         # Model is not downloaded yet
-        model_path = self.DOWNLOAD_PATH / self.EXTRACTED_FOLDER_NAME / "model.onnx"
-        if not os.path.exists(model_path):
+        if not os.path.exists(
+            self.DOWNLOAD_PATH / self.EXTRACTED_FOLDER_NAME / "model.onnx"
+        ):
             os.makedirs(self.DOWNLOAD_PATH, exist_ok=True)
             self._download(
                 self.MODEL_DOWNLOAD_URL, self.DOWNLOAD_PATH / self.ARCHIVE_FILENAME
