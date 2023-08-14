f4d55ac176679494a6cb09303b484c5a2ee786c5
Trayan Azarov <trayan.azarov@amikos.tech>
2023-08-15 02:14:38 +0300

    chore: Experimenting with pythonic approach to queries.

--- chromadb/utils/query_helper.py
+++ chromadb/utils/query_helper.py
@@ -3,19 +3,16 @@ from typing import List, Union, Literal, Any, Dict, cast
 from chromadb.types import (
     WhereOperator,
     LiteralValue,
-    WhereDocument as WhereDocumentType,
+    WhereDocument,
+    Where,
+    LogicalOperator,
 )
 
 
-class Where(object):
+class WhereFilters(Where):
     """
     A builder class for creating a where clause for a filter.
     """
-
-    query: Dict[str, Any]
-
-    def __init__(self) -> None:
-        self.query = {}
 
     def _add_condition(
         self,
@@ -26,9 +23,9 @@ class Where(object):
         """
         Add a condition to the filter query.
         """
-        if field not in self.query:
-            self.query[field] = {}
-        self.query[field][operator] = value
+        if field not in self.keys():
+            self[field] = {}
+        self[field][operator] = value
         return self
 
     def gt(self, field: str, value: LiteralValue) -> "Where":
@@ -67,69 +64,135 @@ class Where(object):
         """
         return self._add_condition(field, "$eq", value)
 
-    # def in_(self, field: str, values: List[LiteralValue]) -> "Where":
-    #     """
-    #     Add an in condition to the filter query.
-    #     """
-    #     return self._add_condition(field, "$in", values)
-
     def and_(self, *conditions: "Where") -> "Where":
         """
         Add an and condition to the filter query.
         """
-        if "$and" not in self.query:
-            self.query["$and"] = []
+        if "$and" not in self.keys():
+            self["$and"] = []
         for condition in conditions:
-            self.query["$and"].append(condition.query)
+            self["$and"].append(condition)
         return self
 
     def or_(self, *conditions: "Where") -> "Where":
         """
         Add an or condition to the filter query.
         """
-        if "$or" not in self.query:
-            self.query["$or"] = []
+        if "$or" not in self.keys():
+            self["$or"] = []
         for condition in conditions:
-            self.query["$or"].append(condition.query)
+            self["$or"].append(condition)
         return self
 
     def to_filter(self) -> Dict[str, Any]:
         """
         Return the filter query.
         """
-        return self.query
+        return self
 
 
-class WhereDocument(object):
+class WhereDocumentFilter(WhereDocument):
     """
     A builder class for creating a where clause for a filter.
     """
 
-    query: Dict[str, Any]
-
-    def __init__(self) -> None:
-        self.query = {}
-
-    def contains(self, value: str) -> "WhereDocument":
+    def contains(self, value: str) -> "WhereDocumentFilter":
         """
         Add a contains condition to the filter query.
         """
-        self.query["$contains"] = value
+        self["$contains"] = value
         return self
 
-    def and_(self, *conditions: "WhereDocument") -> "WhereDocument":
+    def and_(self, *conditions: "WhereDocumentFilter") -> "WhereDocumentFilter":
         """
         Add an and condition to the filter query.
         """
-        self.query["$and"] = [condition.query for condition in conditions]
+        self["$and"] = [condition for condition in conditions]
         return self
 
-    def or_(self, *conditions: "WhereDocument") -> "WhereDocument":
+    def or_(self, *conditions: "WhereDocumentFilter") -> "WhereDocumentFilter":
         """
         Add an or condition to the filter query.
         """
-        self.query["$or"] = [condition.query for condition in conditions]
+        self["$or"] = [condition for condition in conditions]
         return self
 
-    def to_filter(self) -> WhereDocumentType:
-        return cast(WhereDocumentType, self.query)
+
+class Filters(object):
+    """Global Filter class"""
+
+    @staticmethod
+    def where() -> "WhereFilters":
+        """
+        Return a new WhereFilters instance.
+        """
+        return WhereFilters()
+
+    @staticmethod
+    def where_document() -> "WhereDocumentFilter":
+        """
+        Return a new WhereDocumentFilter instance.
+        """
+        return WhereDocumentFilter()
+
+
+class AttrGroup(Where):
+    def __init__(
+        self,
+        lhs: Union[LiteralValue, "AttrGroup"],
+        operator: Union[WhereOperator, LogicalOperator, Literal["$not"]],
+        rhs: Union[List[LiteralValue], LiteralValue, "AttrGroup"],
+    ):
+        # self.lhs = lhs
+        # self.operator = operator
+        # self.rhs = rhs
+        print(f"addd: {lhs} {operator} {rhs}")
+        if operator in ["$and", "$or"]:
+            self[operator] = [lhs, rhs]
+        elif operator == "$in":
+            self[operator] = {lhs: rhs}
+        elif (
+            operator == "$not"
+            and isinstance(lhs, type(self))
+            and list(lhs.keys())[0] == "$in"
+        ):
+            print("-----")
+            self["$nin"] = lhs["$in"]
+        else:
+            self[lhs] = {operator: rhs}
+
+    def __and__(self, other: "AttrGroup") -> "AttrGroup":
+        print("dqwweqw")
+        return AttrGroup(self, "$and", other)
+
+    def __or__(self, other: "AttrGroup") -> "AttrGroup":
+        return AttrGroup(self, "$or", other)
+
+    def __invert__(self) -> "AttrGroup":
+        return AttrGroup(self, "$not", None)
+
+
+class Attr:
+    def __init__(self, name: str):
+        self.name = name
+
+    def __eq__(self, other: LiteralValue) -> AttrGroup:
+        print("EQ")
+        return AttrGroup(self.name, "$eq", other)
+
+    def __str__(self) -> str:
+        return self.name
+
+    def __and__(self, other):
+        print("dwqweqw")
+
+    def __rshift__(self, other):
+        return AttrGroup(self.name, "$in", other)
+
+
+if __name__ == "__main__":
+    print((Attr("a") == 1) | ((Attr("category") == 10) & (Attr("price") == 100)))
+    print((Attr("category") == 10) & (Attr("price") == 100))
+    print((Attr("category") == 10))
+    print((Attr("category") >> ["business", "sports"]))
+    print(~(Attr("category") >> ["business", "sports"]))
