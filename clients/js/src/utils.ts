26d40cbd8daef184d306b851b3f1eefdb0605c62
perzeuss <11357019+perzeuss@users.noreply.github.com>
2023-08-16 17:58:07 +0200

    refactor: rename util and rewrite jsdoc

--- clients/js/src/utils.ts
+++ clients/js/src/utils.ts
@@ -67,17 +67,17 @@ export async function handleSuccess(response: Response | string | Count200Respon
 
 /**
  * Dynamically imports a specified module, providing a workaround for browser environments.
- * This function is necessary due to the `"module": "commonjs"` setting in tsconfig.json,
- * which would ordinarily transpile dynamic imports into `require` statements.
- * This function ensures that the dynamic import remains intact for browser compatibility.
- * Refer to this TypeScript issue for further details:
- * {@link https://github.com/microsoft/TypeScript/issues/43329#issuecomment-1008361973}
+ * This function is necessary because we dynamically import optional dependencies
+ * which can cause issues with bundlers that detect the import and throw an error
+ * on build time when the dependency is not installed.
+ * Using this workaround, the dynamic import is only evaluated on runtime
+ * where we work with try-catch when importing optional dependencies.
  *
  * @param {string} moduleName - Specifies the module to import.
  * @returns {Promise<any>} Returns a Promise that resolves to the imported module.
  */
-export async function dynamicImportModule(moduleName: string) {
-    return Function(`return import("${moduleName}}")`)();
+export async function importOptionalModule(moduleName: string) {
+  return Function(`return import("${moduleName}}")`)();
 }
 
 /**
