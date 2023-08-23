b37eda9890db248f30a87b1644638a8bf89acb02
Jeffrey Huber <jeff@trychroma.com>
2023-08-23 16:58:29 -0700

    initial cli

---
+++ chromadb/cli.py
@@ -0,0 +1,80 @@
+import typer
+import uvicorn
+import os
+
+app = typer.Typer()
+
+_logo = """
+                \033[38;5;069m(((((((((    \033[38;5;203m(((((\033[38;5;220m####
+             \033[38;5;069m(((((((((((((\033[38;5;203m(((((((((\033[38;5;220m#########
+           \033[38;5;069m(((((((((((((\033[38;5;203m(((((((((((\033[38;5;220m###########
+         \033[38;5;069m((((((((((((((\033[38;5;203m((((((((((((\033[38;5;220m############
+        \033[38;5;069m(((((((((((((\033[38;5;203m((((((((((((((\033[38;5;220m#############
+        \033[38;5;069m(((((((((((((\033[38;5;203m((((((((((((((\033[38;5;220m#############
+         \033[38;5;069m((((((((((((\033[38;5;203m(((((((((((((\033[38;5;220m##############
+         \033[38;5;069m((((((((((((\033[38;5;203m((((((((((((\033[38;5;220m##############
+           \033[38;5;069m((((((((((\033[38;5;203m(((((((((((\033[38;5;220m#############
+             \033[38;5;069m((((((((\033[38;5;203m((((((((\033[38;5;220m##############
+                \033[38;5;069m(((((\033[38;5;203m((((    \033[38;5;220m#########\033[0m
+
+    """
+
+
+@app.command()  # type: ignore
+def run(
+    path: str = typer.Option(
+        "./chroma_data", help="The path to the file or directory."
+    ),
+    port: int = typer.Option(8000, help="The port to run the server on."),
+    test: bool = typer.Option(False, help="Test mode."),
+) -> None:
+    """Run a chroma server"""
+
+    print("\033[1m")  # Bold logo
+    print(_logo)
+    print("\033[1m")  # Bold
+    print("Running Chroma")
+    print("\033[0m")  # Reset
+
+    typer.echo(f"Saving data to: {path}")
+    typer.echo(f"Connect to chroma at: http://localhost:{port}\n\n")
+
+    # set ENV variable for PERSIST_DIRECTORY to path
+    os.environ["IS_PERSISTENT"] = "True"
+    os.environ["PERSIST_DIRECTORY"] = path
+
+    # get the path where chromadb is installed
+    chromadb_path = os.path.dirname(os.path.realpath(__file__))
+
+    config = {
+        "app": "chromadb.app:app",
+        "host": "0.0.0.0",
+        "port": port,
+        "workers": 1,
+        "log_config": f"{chromadb_path}/../log_config.yml",
+    }
+
+    if test:
+        return
+
+    uvicorn.run(**config)
+
+
+@app.command()  # type: ignore
+def help() -> None:
+    """Opens help url in your browser"""
+    import webbrowser
+
+    webbrowser.open("https://discord.gg/MMeYNTmh3x")
+
+
+@app.command()  # type: ignore
+def docs() -> None:
+    """Opens docs url in your browser"""
+    import webbrowser
+
+    webbrowser.open("https://docs.trychroma.com")
+
+
+if __name__ == "__main__":
+    app()
