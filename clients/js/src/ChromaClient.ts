f70abb8281064fa8b3f244447ac82f08f6edeb98
Trayan Azarov <trayan.azarov@amikos.tech>
2023-08-28 07:11:09 +0300

    feat: JS Client Support for Auth
    - Implemented auth config as options object to ChromaClient
    
    Refs: #1026

--- clients/js/src/ChromaClient.ts
+++ clients/js/src/ChromaClient.ts
@@ -1,9 +1,13 @@
 import { IEmbeddingFunction } from './embeddings/IEmbeddingFunction';
-import { Configuration, ApiApi as DefaultApi, Api } from "./generated";
+import { Configuration, ApiApi as DefaultApi } from "./generated";
 import { handleSuccess, handleError } from "./utils";
 import { Collection } from './Collection';
 import { CollectionMetadata, CollectionType, ConfigOptions } from './types';
-import {ClientAuthProtocolAdapter, IsomorphicFetchClientAuthProtocolAdapter} from "./auth";
+import {
+    AuthOptions,
+    ClientAuthProtocolAdapter,
+    IsomorphicFetchClientAuthProtocolAdapter
+} from "./auth";
 
 
 export class ChromaClient {
@@ -11,7 +15,7 @@ export class ChromaClient {
      * @ignore
      */
     private api: DefaultApi & ConfigOptions;
-    private apiAdapter: ClientAuthProtocolAdapter<any>;
+    private apiAdapter: ClientAuthProtocolAdapter<any>|undefined;
 
     /**
      * Creates a new ChromaClient instance.
@@ -29,20 +33,22 @@ export class ChromaClient {
     constructor({
         path,
         fetchOptions,
-        authProvider,
-        credentials
+        auth,
     }: {
         path?: string,
         fetchOptions?: RequestInit,
-        authProvider?: string,
-        credentials?: string
+        auth?: AuthOptions,
     } = {}) {
         if (path === undefined) path = "http://localhost:8000";
         const apiConfig: Configuration = new Configuration({
             basePath: path,
         });
-        this.apiAdapter= new IsomorphicFetchClientAuthProtocolAdapter(new DefaultApi(apiConfig),authProvider,credentials);
-        this.api = this.apiAdapter.getApi();
+        if (auth !== undefined) {
+            this.apiAdapter = new IsomorphicFetchClientAuthProtocolAdapter(new DefaultApi(apiConfig), auth);
+            this.api = this.apiAdapter.getApi();
+        } else {
+            this.api = new DefaultApi(apiConfig);
+        }
 
         this.api.options = fetchOptions ?? {};
     }
--- clients/js/test/initClientWithAuth.ts
+++ clients/js/test/initClientWithAuth.ts
@@ -2,6 +2,6 @@ import {ChromaClient} from "../src/ChromaClient";
 
 const PORT = process.env.PORT || "8000";
 const URL = "http://localhost:" + PORT;
-const chroma = new ChromaClient({path: URL, authProvider: "basic", credentials: "admin:admin"});
+const chroma = new ChromaClient({path: URL, auth: {provider: "basic", credentials: "admin:admin"}});
 
 export default chroma;
