a0b2a939358d9c74212d0df432a7939b687f188d
Trayan Azarov <trayan.azarov@amikos.tech>
2023-08-29 11:17:28 +0300

    fix: ONNX Providers
    - Rebase from #1054
    - Added an extra test to verify duplicates in preferred providers
    - Fixed failing test test_available_provider - added unique_by to the fixture
    
    Refs: #1006

--- chromadb/test/ef/test_default_ef.py
+++ chromadb/test/ef/test_default_ef.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Hashable
 
 import hypothesis.strategies as st
 import onnxruntime
@@ -8,19 +8,24 @@ from hypothesis import given
 from chromadb.utils.embedding_functions import ONNXMiniLM_L6_V2
 
 
+def unique_by(x: Hashable) -> Hashable:
+    return x
+
+
 @given(
     providers=st.lists(
         st.sampled_from(onnxruntime.get_all_providers()).filter(
             lambda x: x not in onnxruntime.get_available_providers()
         ),
+        unique_by=unique_by,
         min_size=1,
     )
 )
 def test_unavailable_provider_multiple(providers: List[str]) -> None:
-    print(providers)
     with pytest.raises(ValueError) as e:
         ef = ONNXMiniLM_L6_V2(preferred_providers=providers)
         ef(["test"])
+    assert "Preferred providers must be subset of available providers" in str(e.value)
 
 
 @given(
@@ -29,6 +34,7 @@ def test_unavailable_provider_multiple(providers: List[str]) -> None:
             lambda x: x in onnxruntime.get_available_providers()
         ),
         min_size=1,
+        unique_by=unique_by,
     )
 )
 def test_available_provider(providers: List[str]) -> None:
@@ -39,3 +45,18 @@ def test_available_provider(providers: List[str]) -> None:
 def test_warning_no_providers_supplied() -> None:
     ef = ONNXMiniLM_L6_V2()
     ef(["test"])
+
+
+@given(
+    providers=st.lists(
+        st.sampled_from(onnxruntime.get_all_providers()).filter(
+            lambda x: x in onnxruntime.get_available_providers()
+        ),
+        min_size=1,
+    ).filter(lambda x: len(x) > len(set(x)))
+)
+def test_provider_repeating(providers: List[str]) -> None:
+    with pytest.raises(ValueError) as e:
+        ef = ONNXMiniLM_L6_V2(preferred_providers=providers)
+        ef(["test"])
+    assert "Preferred providers must be unique" in str(e.value)
