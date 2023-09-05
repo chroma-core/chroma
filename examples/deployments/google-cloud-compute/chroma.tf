59d6ccc71d0bf39ec06bbc0357ec98abb2258a32
Trayan Azarov <trayan.azarov@amikos.tech>
2023-09-05 19:28:14 +0300

    feat: GCP template updates - WIP

--- examples/deployments/google-cloud-compute/chroma.tf
+++ examples/deployments/google-cloud-compute/chroma.tf
@@ -9,7 +9,7 @@ resource "google_compute_instance" "chroma1" {
   boot_disk {
     initialize_params {
       image = "debian-cloud/debian-11"
-      size  = 20
+      size  = var.chroma_instance_volume_size #size in GB
     }
   }
 
@@ -22,6 +22,22 @@ resource "google_compute_instance" "chroma1" {
   }
 
   metadata_startup_script = templatefile("${path.module}/startup.sh", { chroma_release = var.chroma_release })
+}
+
+resource "google_compute_disk" "chroma" {
+  name  = "test-disk"
+  type  = "pd-ssd"
+  zone  = "us-central1-a"
+  image = "debian-11-bullseye-v20220719"
+  labels = {
+    environment = "dev"
+  }
+  physical_block_size_bytes = 4096
+}
+
+resource "google_compute_attached_disk" "vm_attached_disk" {
+  disk     = google_compute_disk.chroma.id
+  instance = google_compute_instance.chroma1.self_link
 }
 
 resource "google_compute_firewall" "default" {
@@ -47,4 +63,15 @@ resource "google_compute_firewall" "default" {
 output "instance_public_ip" {
   description = "The public IP address of the instance."
   value       = google_compute_instance.chroma1.network_interface[0].access_config[0].nat_ip
+}
+
+output "chroma_auth_token" {
+  value = random_password.chroma_token.result
+  sensitive = true
+}
+
+
+output "chroma_auth_basic" {
+  value = "${local.basic_auth_credentials.username}:${local.basic_auth_credentials.password}"
+  sensitive = true
 }
