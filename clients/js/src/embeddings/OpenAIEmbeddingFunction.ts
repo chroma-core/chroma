9173ff5e306e7be6d22cff110ada6f2f1cb45a11
Julian Lagerberg García <juliandreas@fastmail.com>
2023-09-10 14:42:46 +0200

    make embeddings cjs and esm compatible

--- clients/js/src/embeddings/OpenAIEmbeddingFunction.ts
+++ clients/js/src/embeddings/OpenAIEmbeddingFunction.ts
@@ -1,125 +1,144 @@
-import {IEmbeddingFunction} from "./IEmbeddingFunction";
+import { importOptionalModule } from "../utils";
+import { IEmbeddingFunction } from "./IEmbeddingFunction";
 
 let OpenAIApi: any;
 let openAiVersion = null;
 let openAiMajorVersion = null;
 
 interface OpenAIAPI {
-    createEmbedding: (params: {
-        model: string;
-        input: string[];
-        user?: string;
-    }) => Promise<number[][]>;
+  createEmbedding: (params: {
+    model: string;
+    input: string[];
+    user?: string;
+  }) => Promise<number[][]>;
 }
 
 class OpenAIAPIv3 implements OpenAIAPI {
-    private readonly configuration: any;
-    private openai: any;
+  private readonly configuration: any;
+  private openai: any;
 
-    constructor(configuration: { organization: string, apiKey: string }) {
-        this.configuration = new OpenAIApi.Configuration({
-            organization: configuration.organization,
-            apiKey: configuration.apiKey,
-        });
-        this.openai = new OpenAIApi.OpenAIApi(this.configuration);
-    }
+  constructor(configuration: { organization: string; apiKey: string }) {
+    this.configuration = new OpenAIApi.Configuration({
+      organization: configuration.organization,
+      apiKey: configuration.apiKey,
+    });
+    this.openai = new OpenAIApi.OpenAIApi(this.configuration);
+  }
 
-    public async createEmbedding(params: {
-        model: string,
-        input: string[],
-        user?: string
-    }): Promise<number[][]> {
-        const embeddings: number[][] = [];
-        const response = await this.openai.createEmbedding({
-            model: params.model,
-            input: params.input,
-        }).catch((error: any) => {
-            throw error;
-        });
-        // @ts-ignore
-        const data = response.data["data"];
-        for (let i = 0; i < data.length; i += 1) {
-            embeddings.push(data[i]["embedding"]);
-        }
-        return embeddings
+  public async createEmbedding(params: {
+    model: string;
+    input: string[];
+    user?: string;
+  }): Promise<number[][]> {
+    const embeddings: number[][] = [];
+    const response = await this.openai
+      .createEmbedding({
+        model: params.model,
+        input: params.input,
+      })
+      .catch((error: any) => {
+        throw error;
+      });
+    const data = response.data["data"];
+    for (let i = 0; i < data.length; i += 1) {
+      embeddings.push(data[i]["embedding"]);
     }
+    return embeddings;
+  }
 }
 
 class OpenAIAPIv4 implements OpenAIAPI {
-    private readonly apiKey: any;
-    private openai: any;
+  private readonly apiKey: any;
+  private openai: any;
 
-    constructor(apiKey: any) {
-        this.apiKey = apiKey;
-        this.openai = new OpenAIApi({
-            apiKey: this.apiKey,
-        });
-    }
+  constructor(apiKey: any) {
+    this.apiKey = apiKey;
+    this.openai = new OpenAIApi({
+      apiKey: this.apiKey,
+    });
+  }
 
-    public async createEmbedding(params: {
-        model: string,
-        input: string[],
-        user?: string
-    }): Promise<number[][]> {
-        const embeddings: number[][] = [];
-        const response = await this.openai.embeddings.create(params);
-        const data = response["data"];
-        for (let i = 0; i < data.length; i += 1) {
-            embeddings.push(data[i]["embedding"]);
-        }
-        return embeddings
+  public async createEmbedding(params: {
+    model: string;
+    input: string[];
+    user?: string;
+  }): Promise<number[][]> {
+    const embeddings: number[][] = [];
+    const response = await this.openai.embeddings.create(params);
+    const data = response["data"];
+    for (let i = 0; i < data.length; i += 1) {
+      embeddings.push(data[i]["embedding"]);
     }
+    return embeddings;
+  }
+}
+
+async function loadOpenAIApi(): Promise<void> {
+  return importOptionalModule("openai").then((module) => {
+    OpenAIApi = module;
+  });
 }
 
 export class OpenAIEmbeddingFunction implements IEmbeddingFunction {
-    private api_key: string;
-    private org_id: string;
-    private model: string;
-    private openaiApi: OpenAIAPI;
+  private api_key: string;
+  private org_id: string;
+  private model: string;
+  private openaiApi: any;
+  private isInitialized: boolean;
 
-    constructor({openai_api_key, openai_model, openai_organization_id}: {
-        openai_api_key: string,
-        openai_model?: string,
-        openai_organization_id?: string
-    }) {
+  constructor({
+    openai_api_key,
+    openai_model,
+    openai_organization_id,
+  }: {
+    openai_api_key: string;
+    openai_model?: string;
+    openai_organization_id?: string;
+  }) {
+    this.api_key = openai_api_key;
+    this.org_id = openai_organization_id || "";
+    this.model = openai_model || "text-embedding-ada-002";
+    this.isInitialized = false;
+
+    loadOpenAIApi()
+      .then(() => {
+        let version = null;
         try {
-            // eslint-disable-next-line global-require,import/no-extraneous-dependencies
-            OpenAIApi = require("openai");
-            let version = null;
-            try {
-                const {VERSION} = require('openai/version');
-                version = VERSION;
-            } catch (e) {
-                version = "3.x";
-            }
-            openAiVersion = version.replace(/[^0-9.]/g, '');
-            openAiMajorVersion = openAiVersion.split('.')[0];
-        } catch (_a) {
-            // @ts-ignore
-            if (_a.code === 'MODULE_NOT_FOUND') {
-                throw new Error("Please install the openai package to use the OpenAIEmbeddingFunction, `npm install -S openai`");
-            }
-            throw _a; // Re-throw other errors
+          version = OpenAIApi.VERSION || "3.x";
+          this.isInitialized = true;
+        } catch (error) {
+          version = "3.x";
         }
-        this.api_key = openai_api_key;
-        this.org_id = openai_organization_id || "";
-        this.model = openai_model || "text-embedding-ada-002";
+
+        const openAiVersion = version.replace(/[^0-9.]/g, "");
+        const openAiMajorVersion = parseInt(openAiVersion.split(".")[0]);
+
         if (openAiMajorVersion > 3) {
-            this.openaiApi = new OpenAIAPIv4(this.api_key);
+          this.openaiApi = new OpenAIAPIv4(this.api_key);
         } else {
-            this.openaiApi = new OpenAIAPIv3({
-                organization: this.org_id,
-                apiKey: this.api_key,
-            });
+          this.openaiApi = new OpenAIAPIv3({
+            organization: this.org_id,
+            apiKey: this.api_key,
+          });
         }
+      })
+      .catch((error) => {
+        console.error("Could not load OpenAIApi:", error);
+      });
+  }
+
+  public async generate(texts: string[]): Promise<number[][]> {
+    if (!this.isInitialized) {
+      throw new Error("OpenAI API is not initialized.");
     }
 
-    public async generate(texts: string[]): Promise<number[][]> {
-        return await this.openaiApi.createEmbedding({
-            model: this.model,
-            input: texts,
-        }).catch((error: any) => {
-            throw error;
-        });
-    }
+    return await this.openaiApi
+      .createEmbedding({
+        model: this.model,
+        input: texts,
+      })
+      .catch((error: any) => {
+        throw error;
+      });
+  }
 }
