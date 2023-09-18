516d9117e1ba9e419046a7565bfaedb8825ba28d
Leonid Ganeline <leo.gan.57@gmail.com>
2023-09-18 10:45:34 -0700

    simplified ut-s (#1071)
    ## Description of changes
    
     - Improvements 
    	 - simplified ut-s
    - cleaned up a typing import
    
    ## Test plan
    
    - [+] Tests passed successfully locally with `pytest` for python, `yarn
    test` for js
    
    ## Documentation Changes
     N/A

--- chromadb/test/property/test_embeddings.py
+++ chromadb/test/property/test_embeddings.py
@@ -365,39 +365,41 @@ def test_escape_chars_in_ids(api: API) -> None:
     assert coll.count() == 0
 
 
-def test_delete_empty_fails(api: API):
+@pytest.mark.parametrize(
+    "kwargs",
+    [
+        {},
+        {"ids": []},
+        {"where": {}},
+        {"where_document": {}},
+        {"where_document": {}, "where": {}},
+    ],
+)
+def test_delete_empty_fails(api: API, kwargs: dict):
     api.reset()
     coll = api.create_collection(name="foo")
-
-    error_valid = (
-        lambda e: "You must provide either ids, where, or where_document to delete."
-        in e
-    )
-
     with pytest.raises(Exception) as e:
-        coll.delete()
-    assert error_valid(str(e))
+        coll.delete(**kwargs)
+    assert "You must provide either ids, where, or where_document to delete." in str(e)
 
-    with pytest.raises(Exception):
-        coll.delete(ids=[])
-    assert error_valid(str(e))
 
-    with pytest.raises(Exception):
-        coll.delete(where={})
-    assert error_valid(str(e))
-
-    with pytest.raises(Exception):
-        coll.delete(where_document={})
-    assert error_valid(str(e))
-
-    with pytest.raises(Exception):
-        coll.delete(where_document={}, where={})
-    assert error_valid(str(e))
-
+@pytest.mark.parametrize(
+    "kwargs",
+    [
+        {"ids": ["foo"]},
+        {"where": {"foo": "bar"}},
+        {"where_document": {"$contains": "bar"}},
+        {"ids": ["foo"], "where": {"foo": "bar"}},
+        {"ids": ["foo"], "where_document": {"$contains": "bar"}},
+        {
+            "ids": ["foo"],
+            "where": {"foo": "bar"},
+            "where_document": {"$contains": "bar"},
+        },
+    ],
+)
+def test_delete_success(api: API, kwargs: dict):
+    api.reset()
+    coll = api.create_collection(name="foo")
     # Should not raise
-    coll.delete(where_document={"$contains": "bar"})
-    coll.delete(where={"foo": "bar"})
-    coll.delete(ids=["foo"])
-    coll.delete(ids=["foo"], where={"foo": "bar"})
-    coll.delete(ids=["foo"], where_document={"$contains": "bar"})
-    coll.delete(ids=["foo"], where_document={"$contains": "bar"}, where={"foo": "bar"})
+    coll.delete(**kwargs)
