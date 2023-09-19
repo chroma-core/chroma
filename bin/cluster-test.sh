1d1ae475c95dd2a938b6ab7cdb785f4e4b953a8b
hammadb <hammad@trychroma.com>
2023-09-19 15:49:06 -0700

    Move wait

--- bin/cluster-test.sh
+++ bin/cluster-test.sh
@@ -8,7 +8,7 @@ function cleanup {
 
 trap cleanup EXIT
 
-docker-compose -f docker-compose.cluster.yml up -d pulsar --wait
+docker-compose -f docker-compose.cluster.yml up -d --wait pulsar
 
 export CHROMA_CLUSTER_TEST_ONLY=1
 
