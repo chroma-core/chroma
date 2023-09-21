d83469db946e426461afd909b32cead75d94d176
hammadb <hammad@trychroma.com>
2023-09-20 22:53:21 -0700

    Add tests xfail behavior in GH workflow

--- bin/cluster-test.sh
+++ bin/cluster-test.sh
@@ -3,12 +3,12 @@
 set -e
 
 function cleanup {
-  docker compose -f docker-compose.cluster.yml down --rmi local --volumes
+  docker compose -f docker-compose.cluster.test.yml down --rmi local --volumes
 }
 
 trap cleanup EXIT
 
-docker compose -f docker-compose.cluster.yml up -d --wait pulsar
+docker compose -f docker-compose.cluster.test.yml up -d --wait
 
 export CHROMA_CLUSTER_TEST_ONLY=1
 
