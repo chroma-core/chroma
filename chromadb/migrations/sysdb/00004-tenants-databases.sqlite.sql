a4509e53123292da2370e2a30cfeefc0a21dc8dd
hammadb <hammad@trychroma.com>
2023-10-23 15:13:21 -0700

    PR Comments - Migration idempotency and validate tenant/datatbase

--- chromadb/migrations/sysdb/00004-tenants-databases.sqlite.sql
+++ chromadb/migrations/sysdb/00004-tenants-databases.sqlite.sql
@@ -20,8 +20,8 @@ CREATE TABLE IF NOT EXISTS collections_tmp (
 );
 
 -- Create default tenant and database
-INSERT OR REPLACE INTO tenants (id) VALUES ('default'); -- The default tenant id is 'default' others are UUIDs
-INSERT OR REPLACE INTO databases (id, name, tenant_id) VALUES ('00000000-0000-0000-0000-000000000000', 'default', 'default');
+INSERT OR REPLACE INTO tenants (id) VALUES ('default_tenant'); -- The default tenant id is 'default_tenant' others are UUIDs
+INSERT OR REPLACE INTO databases (id, name, tenant_id) VALUES ('00000000-0000-0000-0000-000000000000', 'default_database', 'default_tenant');
 
 INSERT OR REPLACE INTO collections_tmp (id, name, topic, dimension, database_id)
     SELECT id, name, topic, dimension, '00000000-0000-0000-0000-000000000000' FROM collections;
