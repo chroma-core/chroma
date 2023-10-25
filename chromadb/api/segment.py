524f1cae9f614da0894a896ac25a08aac5294975
atroyn <anton.troynikov@gmail.com>
2023-10-24 20:12:40 -0700

    Update, upsert

--- chromadb/api/segment.py
+++ chromadb/api/segment.py
@@ -384,6 +384,7 @@ class SegmentAPI(ServerAPI):
             embeddings=embeddings,
             metadatas=metadatas,
             documents=documents,
+            uris=uris,
         ):
             self._validate_embedding_record(coll, r)
             records_to_submit.append(r)
@@ -425,6 +426,7 @@ class SegmentAPI(ServerAPI):
             embeddings=embeddings,
             metadatas=metadatas,
             documents=documents,
+            uris=uris,
         ):
             self._validate_embedding_record(coll, r)
             records_to_submit.append(r)
