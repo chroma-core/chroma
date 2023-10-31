5798b74274fb04fb279b36a409547a970a6c761a
atroyn <anton.troynikov@gmail.com>
2023-10-30 18:10:04 -0700

    Data loader types

--- chromadb/utils/data_loaders.py
+++ chromadb/utils/data_loaders.py
@@ -1,9 +1,10 @@
 import importlib
+from typing import Optional, Sequence, List
 import numpy as np
-from chromadb.api.types import URIs, Images, DataLoader
+from chromadb.api.types import URI, DataLoader, Image
 
 
-class ImageLoader(DataLoader[Images]):
+class ImageLoader(DataLoader[List[Optional[Image]]]):
     def __init__(self) -> None:
         try:
             self._PILImage = importlib.import_module("PIL.Image")
@@ -12,7 +13,7 @@ class ImageLoader(DataLoader[Images]):
                 "The PIL python package is not installed. Please install it with `pip install pillow`"
             )
 
-    def __call__(self, uris: URIs) -> Images:
+    def __call__(self, uris: Sequence[Optional[URI]]) -> List[Optional[Image]]:
         return [
             np.array(self._PILImage.open(uri)) if uri is not None else None
             for uri in uris
