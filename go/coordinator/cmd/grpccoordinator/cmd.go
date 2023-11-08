7d5b1794487a56f4963faab44247e96060d0a0f6
Liquan Pei <liquanpei@gmail.com>
2023-11-08 15:49:01 -0800

    [ENH] Add system-catalog-provider to simplify deployment (#1358)
    ## Description of changes
    
    *Summarize the changes made by this PR.*
     - Improvements & Bug fixes
    	 - This adds a system-catalog-provider config to simplify deployment
     - New functionality
    	 - ...
    
    ## Test plan
    *How are these changes tested?*
    
    - [ ] test_system.py
    
    ## Documentation Changes
    *Are all docstrings for user-facing APIs updated if required? Do we need
    to make documentation changes in the [docs
    repository](https://github.com/chroma-core/docs)?*

--- go/coordinator/cmd/grpccoordinator/cmd.go
+++ go/coordinator/cmd/grpccoordinator/cmd.go
@@ -23,6 +23,7 @@ var (
 
 func init() {
 	flag.GRPCAddr(Cmd, &conf.BindAddress)
+	Cmd.Flags().StringVar(&conf.SystemCatalogProvider, "system-catalog-provider", "memory", "System catalog provider")
 	Cmd.Flags().StringVar(&conf.Username, "username", "root", "MetaTable username")
 	Cmd.Flags().StringVar(&conf.Password, "password", "", "MetaTable password")
 	Cmd.Flags().StringVar(&conf.Address, "db-address", "127.0.0.1:3306", "MetaTable db address")
