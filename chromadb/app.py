f6deb2ee9a97642d05bd20ada2290ca14edb6731
hany-girgis <72340841+hany-girgis@users.noreply.github.com>
2023-11-09 21:56:04 +0200

    app.py

--- chromadb/app.py
+++ chromadb/app.py
@@ -2,6 +2,6 @@ import chromadb
 import chromadb.config
 from chromadb.server.fastapi import FastAPI
 
-settings = chromadb.config.Settings()
+settings = chromadb.config.Settings(persist_directory="/storage")
 server = FastAPI(settings)
 app = server.app()
