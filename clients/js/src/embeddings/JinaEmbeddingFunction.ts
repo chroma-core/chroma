907de09eebc3b39ddba7b5e9683d1b13df80cbd4
Joan Fontanals Martinez <joan.fontanals.martinez@jina.ai>
2023-11-17 09:11:09 +0100

    remove axios dependency
    Signed-off-by: Joan Fontanals Martinez <joan.fontanals.martinez@jina.ai>

--- clients/js/src/embeddings/JinaEmbeddingFunction.ts
+++ clients/js/src/embeddings/JinaEmbeddingFunction.ts
@@ -1,37 +1,46 @@
 import { IEmbeddingFunction } from "./IEmbeddingFunction";
-import axios, { AxiosInstance } from 'axios';
 
 export class JinaEmbeddingFunction implements IEmbeddingFunction {
-    private api_key: string;
-    private model: string;
+  private model_name: string;
+  private api_url: string;
+  private headers: { [key: string]: string };
 
-    constructor({ jinaai_api_key, model }: { jinaai_api_key: string, model?: string }) {
-        this.model = model || "jina-embeddings-v2-base-en";
-        this.api_url = "https://api.jina.ai/v1/embeddings";
-        this.session = axios.create({
-          headers: {
-            "Authorization": `Bearer ${jinaai_api_key}`,
-            "Accept-Encoding": "identity",
-          },
-        });
-    }
+  constructor({ jinaai_api_key, model_name }: { jinaai_api_key: string; model_name?: string }) {
+    this.model_name = model_name || 'jina-embeddings-v2-base-en';
+    this.api_url = 'https://api.jina.ai/v1/embeddings';
+    this.headers = {
+      Authorization: `Bearer ${jinaai_api_key}`,
+      'Accept-Encoding': 'identity',
+      'Content-Type': 'application/json',
+    };
+  }
 
-    public async generate(texts: string[]) {
-        try {
-          const response = await this.session.post(this.api_url, {input: texts, model: this.model_name});
+  public async generate(texts: string[]) {
+    try {
+      const response = await fetch(this.api_url, {
+        method: 'POST',
+        headers: this.headers,
+        body: JSON.stringify({
+          input: texts,
+          model: this.model_name,
+        }),
+      });
 
-          const data = response.data;
-          if (!data || !data.data) {
-            throw new Error(data.detail);
-          }
+      const data = (await response.json()) as { data: any[]; detail: string };
+      if (!data || !data.data) {
+        throw new Error(data.detail);
+      }
 
-          const embeddings: EmbeddingResult[] = data.data;
-          const sortedEmbeddings = embeddings.sort((a, b) => a.index - b.index);
+      const embeddings: any[] = data.data;
+      const sortedEmbeddings = embeddings.sort((a, b) => a.index - b.index);
 
-          return sortedEmbeddings.map((result) => result.embedding);
-        } catch (error) {
-          throw new Error(`Error calling Jina API: ${error.message}`);
-        }
+      return sortedEmbeddings.map((result) => result.embedding);
+    } catch (error) {
+      if (error instanceof Error) {
+        throw new Error(`Error calling Jina AI API: ${error.message}`);
+      } else {
+        throw new Error(`Error calling Jina AI API: ${error}`);
       }
     }
+  }
 }
