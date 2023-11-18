a60b53a472d167b52270ccda63e1834ac7b45e96
Joan Fontanals Martinez <joan.fontanals.martinez@jina.ai>
2023-11-18 10:59:25 +0100

    add to index

--- clients/js/src/index.ts
+++ clients/js/src/index.ts
@@ -3,4 +3,5 @@ export { Collection } from './Collection';
 export { IEmbeddingFunction } from './embeddings/IEmbeddingFunction';
 export { OpenAIEmbeddingFunction } from './embeddings/OpenAIEmbeddingFunction';
 export { CohereEmbeddingFunction } from './embeddings/CohereEmbeddingFunction';
+export { JinaEmbeddingFunction } from './embeddings/JinaEmbeddingFunction';
 export { IncludeEnum } from './types';
