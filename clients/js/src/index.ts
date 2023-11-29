0741cde31c2d0c3f0413d87fed8f271dc46317fc
Trayan Azarov <tazarov@users.noreply.github.com>
2023-11-29 20:53:09 +0200

    [ENH]: Embedding Function - Hugging Face Text Embedding Server (#1371)
    Refs: [Feature Request]: Hugging Face text embedding inference custom
    embedding #1367
    
    ## Description of changes
    
    *Summarize the changes made by this PR.*
     - New functionality
    	 - New Embedding Function for HF Text Embedding Server
    	 - Added sample docker compose to run things locally
    	 - Added example notebook
    
    ## Test plan
    *How are these changes tested?*
    
    - [x] Tests pass locally with `pytest` for python
    
    ## Documentation Changes
    TBD
    
    https://github.com/huggingface/text-embeddings-inference

--- clients/js/src/index.ts
+++ clients/js/src/index.ts
@@ -3,4 +3,5 @@ export { Collection } from './Collection';
 export { IEmbeddingFunction } from './embeddings/IEmbeddingFunction';
 export { OpenAIEmbeddingFunction } from './embeddings/OpenAIEmbeddingFunction';
 export { CohereEmbeddingFunction } from './embeddings/CohereEmbeddingFunction';
+export { HuggingFaceEmbeddingServerFunction } from './embeddings/HuggingFaceEmbeddingServerFunction';
 export { IncludeEnum } from './types';
