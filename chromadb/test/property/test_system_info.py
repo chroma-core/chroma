88e8765cafaf0a36fa555dbb92387d38397e50e0
Trayan Azarov <trayan.azarov@amikos.tech>
2023-12-07 19:14:59 +0200

    fix: System info tests.

--- chromadb/test/property/test_system_info.py
+++ chromadb/test/property/test_system_info.py
@@ -6,7 +6,7 @@ from typing import List, Dict
 from chromadb.api.fastapi import FastAPI
 from chromadb.api import ServerAPI as API
 from chromadb.api.types import SystemInfoFlags
-from hypothesis import given, strategies as st
+from hypothesis import given, strategies as st, settings
 
 dicts_list = [
     {"python_version": ["python_version"]},
@@ -14,11 +14,10 @@ dicts_list = [
     {"memory_info": ["memory_info"]},
     {"cpu_info": ["cpu_info"]},
     {"disk_info": ["disk_info"]},
-    {"network_info": ["network_info"]},
-    {"env_vars": ["env_vars"]},
 ]
 
 
+@settings(max_examples=10)
 @given(
     flags_dict=st.iterables(
         elements=st.sampled_from(dicts_list), min_size=1, max_size=len(dicts_list)
@@ -30,7 +29,10 @@ def test_flags(api_obs: API, flags_dict: List[Dict[str, List[str]]]) -> None:
     for di in flags_dict:
         _d_flags[list(di.keys())[0]] = True
         check_response_flags.extend(list(di.values())[0])
+    check_response_flags = set(check_response_flags)
     _sys_flags = SystemInfoFlags(**_d_flags)
+    if 'disk_info' in check_response_flags and not api_obs.get_settings().is_persistent:
+        check_response_flags.remove('disk_info')
     params = {field: getattr(_sys_flags, field) for field in _sys_flags._fields}
     if not isinstance(api_obs, FastAPI):
         _env = api_obs.env(system_info_flags=_sys_flags)
@@ -38,14 +40,6 @@ def test_flags(api_obs: API, flags_dict: List[Dict[str, List[str]]]) -> None:
         _text = json.dumps(_env)
         for flag in check_response_flags:
             assert flag in _text
-    elif api_obs.get_settings().chroma_server_env_endpoint_enabled:
-        resp = requests.get(f"{api_obs._api_url}/env", params=params)
-        assert resp.status_code == 200
-        _json = resp.json()
-        _text = resp.text
-        assert _json is not None
-        for flag in check_response_flags:
-            assert flag in _text
     else:
-        resp = requests.get(f"{api_obs._api_url}/env", params=params)
+        resp = requests.get(f"{api_obs._api_url}/api/v1//env", params=params)
         assert resp.status_code == 404
