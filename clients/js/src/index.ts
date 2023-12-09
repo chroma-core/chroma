91c737bd16992d2ac57dd929723620bbf05e2dfd
Shardul Kulkarni <shardulkulkarni95@gmail.com>
2023-12-09 19:23:22 +0530

    export type added in index js

--- clients/js/src/index.ts
+++ clients/js/src/index.ts
@@ -4,5 +4,5 @@ export { Collection } from './Collection';
 export { IEmbeddingFunction } from './embeddings/IEmbeddingFunction';
 export { OpenAIEmbeddingFunction } from './embeddings/OpenAIEmbeddingFunction';
 export { CohereEmbeddingFunction } from './embeddings/CohereEmbeddingFunction';
-export { IncludeEnum, GetParams } from './types';
 export { HuggingFaceEmbeddingServerFunction } from './embeddings/HuggingFaceEmbeddingServerFunction';
+export { IncludeEnum, GetParams, Embedding, Embeddings, Metadata, Metadatas, Document, Documents, ID, IDs, PositiveInteger, Where, WhereDocument, CollectionType, GetResponse, QueryResponse, AddResponse, CollectionMetadata, ConfigOptions } from './types';
