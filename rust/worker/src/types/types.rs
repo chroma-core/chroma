30d088074aad020b0164774348cfce5b0fefee68
hammadb <hammad@trychroma.com>
2023-12-12 20:28:15 -0800

    Cleanup

--- rust/worker/src/types/types.rs
+++ rust/worker/src/types/types.rs
@@ -2,8 +2,8 @@ use crate::errors::{ChromaError, ErrorCodes};
 use num_bigint::BigInt;
 use thiserror::Error;
 
-/// A macro for easily implementing match arms for a base error type
-/// so that other types can wrap it and still implement the ChromaError trait
+/// A macro for easily implementing match arms for a base error type with common errors.
+/// Other types can wrap it and still implement the ChromaError trait
 /// without boilerplate.
 macro_rules! impl_base_convert_error {
     ($err:ty, { $($variant:pat => $action:expr),* $(,)? }) => {
