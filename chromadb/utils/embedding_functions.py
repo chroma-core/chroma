08660f6020c71c575047ba39581f6a0cd37094ad
Mo Abbasi <aipythoner@gmail.com>
2024-01-17 01:58:51 +0330

    Add cache_folder for SentenceTransformerEmbeddingFunction

--- chromadb/utils/embedding_functions.py
+++ chromadb/utils/embedding_functions.py
@@ -60,6 +60,7 @@ class SentenceTransformerEmbeddingFunction(EmbeddingFunction[Documents]):
         model_name: str = "all-MiniLM-L6-v2",
         device: str = "cpu",
         normalize_embeddings: bool = False,
+        cache_folder:str | None = None,
     ):
         if model_name not in self.models:
             try:
@@ -68,7 +69,7 @@ class SentenceTransformerEmbeddingFunction(EmbeddingFunction[Documents]):
                 raise ValueError(
                     "The sentence_transformers python package is not installed. Please install it with `pip install sentence_transformers`"
                 )
-            self.models[model_name] = SentenceTransformer(model_name, device=device)
+            self.models[model_name] = SentenceTransformer(model_name, device=device, cache_folder=cache_folder)
         self._model = self.models[model_name]
         self._normalize_embeddings = normalize_embeddings
 
