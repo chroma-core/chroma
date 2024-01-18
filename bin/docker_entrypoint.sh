e40529b32e38059392a2fabf0801b8e0fa2c84cf
Mikhail Merkulov <Mihail.Merkulov@gmail.com>
2024-01-18 18:13:29 +0200

    Update docker_entrypoint.sh to be able to customize port

--- bin/docker_entrypoint.sh
+++ bin/docker_entrypoint.sh
@@ -2,4 +2,6 @@
 
 export IS_PERSISTENT=1
 export CHROMA_SERVER_NOFILE=65535
-exec uvicorn chromadb.app:app --workers 1 --host 0.0.0.0 --port 8000 --proxy-headers --log-config chromadb/log_config.yml --timeout-keep-alive 30
+port=$1
+port=${port:-"8000"}
+exec uvicorn chromadb.app:app --workers 1 --host 0.0.0.0 --port ${port} --proxy-headers --log-config chromadb/log_config.yml --timeout-keep-alive 30
