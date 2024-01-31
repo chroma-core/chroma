bd0780aca4f1c75f26090bddc4f092c16f9071b3
Mike Merkulov <mihail.merkulov@gmail.com>
2024-01-31 12:30:39 +0200

    Added support for docker CMD that starts with uvicorn with a warning that it should not be used.

--- bin/docker_entrypoint.sh
+++ bin/docker_entrypoint.sh
@@ -3,5 +3,13 @@ set -e
 
 export IS_PERSISTENT=1
 export CHROMA_SERVER_NOFILE=65535
-echo Starting server with args: $(eval echo "$@")
-exec uvicorn chromadb.app:app $(eval echo "$@")
+args="$@"
+
+if [[ $args =~ ^uvicorn.* ]]; then
+    echo "Starting server with args: $(eval echo "$args")"
+    echo -e "\033[31mWARNING: Please remove 'uvicorn chromadb.app:app' from your command line arguments. This is now handled by the entrypoint script."
+    exec $(eval echo "$args")
+else
+    echo "Starting 'uvicorn chromadb.app:app' with args: $(eval echo "$args")"
+    exec uvicorn chromadb.app:app $(eval echo "$args")
+fi
