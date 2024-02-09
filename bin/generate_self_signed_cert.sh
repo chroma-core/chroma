341323989e0fbf26a980a3dbd7ea42575e614c1a
Trayan Azarov <trayan.azarov@amikos.tech>
2024-02-09 13:34:58 +0200

    fix: Removing openssl check for GH runners.

--- bin/generate_self_signed_cert.sh
+++ bin/generate_self_signed_cert.sh
@@ -1,17 +1,5 @@
 #!/usr/bin/env sh
 
-if ! command -v openssl &> /dev/null
-then
-  if [ -f /etc/os-release ]; then
-    . /etc/os-release
-    if [ "$ID" == "ubuntu" ] || [ "$ID" == "debian" ]; then
-      sudo apt-get update && sudo apt-get install openssl -y
-    else
-      echo "openssl command not found. Please install it and try again."
-      exit
-    fi
-fi
-
 # check if the openssl.cnf file exists
 if [ ! -f chromadb/test/openssl.cnf ]; then
     echo "openssl.cnf file not found. Please run the script from the root of the project."
