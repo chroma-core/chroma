78d3e610679427f6f7332a3e99d162b3230098e6
Weili Gu <weili@trychroma.com>
2024-02-20 15:26:47 -0800

    push logs with error handling, need to add arrow

--- go/coordinator/internal/logservice/recordlog.go
+++ go/coordinator/internal/logservice/recordlog.go
@@ -2,6 +2,8 @@ package logservice
 
 import (
 	"context"
+	"github.com/apache/arrow/go/v12/arrow"
+	"github.com/apache/arrow/go/v12/arrow/memory"
 	"github.com/chroma/chroma-coordinator/internal/metastore/db/dao"
 	"github.com/chroma/chroma-coordinator/internal/metastore/db/dbmodel"
 	"github.com/pingcap/log"
@@ -10,8 +12,10 @@ import (
 var _ IRecordLog = (*RecordLog)(nil)
 
 type RecordLog struct {
-	ctx         context.Context
-	recordLogDb dbmodel.IRecordLogDb
+	ctx          context.Context
+	recordLogDb  dbmodel.IRecordLogDb
+	arrowPool    memory.Allocator
+	recordSchema *arrow.Schema
 }
 
 func NewLogService(ctx context.Context) (*RecordLog, error) {
@@ -24,6 +28,21 @@ func NewLogService(ctx context.Context) (*RecordLog, error) {
 
 func (s *RecordLog) Start() error {
 	log.Info("RecordLog start")
+	s.arrowPool = memory.NewGoAllocator()
+	s.recordSchema = arrow.NewSchema(
+		[]arrow.Field{
+			{Name: "id", Type: arrow.BinaryTypes.String},
+			{Name: "vector", Type: arrow.StructOf(
+				[]arrow.Field{
+					{Name: "dimension", Type: arrow.PrimitiveTypes.Int32},
+					{Name: "vector", Type: arrow.BinaryTypes.String},
+					{Name: "scalarEncoding", Type: arrow.PrimitiveTypes.Int32},
+				}...)},
+			{Name: "updateMetadata", Type: arrow.PrimitiveTypes.Float64},
+			{Name: "operation", Type: arrow.PrimitiveTypes.Float64},
+		},
+		nil,
+	)
 	return nil
 }
 
