02bc4671b185c93df66b85e9ce56b265d5e8269e
Ben Eggers <ben@beneggers.com>
2024-02-20 09:56:03 -0800

    once again

--- rust/worker/src/metadata/types.rs
+++ rust/worker/src/metadata/types.rs
@@ -2,7 +2,7 @@ use crate::blockstore::Blockfile;
 use crate::errors::{ChromaError, ErrorCodes};
 use thiserror::Error;
 
-use crate::blockstore::HashMapBlockfile;
+use crate::blockstore::{BlockfileKey, HashMapBlockfile, Key, Value};
 
 use async_trait::async_trait;
 use roaring::RoaringBitmap;
@@ -36,7 +36,7 @@ pub(crate) trait StringMetadataIndex {
     fn commit_transaction(&mut self) -> Result<(), Box<dyn ChromaError>>;
 
     // Must be in a transaction to put or delete.
-    fn put(&mut self, key: &str, value: &str, offset_id: usize) -> Result<(), Box<dyn ChromaError>>;
+    fn set(&mut self, key: &str, value: &str, offset_id: usize) -> Result<(), Box<dyn ChromaError>>;
     // Can delete anything -- if it's not in committed state the delete will be silently discarded.
     fn delete(&mut self, key: &str, value: &str, offset_id: usize) -> Result<(), Box<dyn ChromaError>>;
 
@@ -45,15 +45,13 @@ pub(crate) trait StringMetadataIndex {
 }
 
 struct InMemoryStringMetadataIndex {
-    path: String,
     blockfile: Box<dyn Blockfile>,
 }
 
 impl InMemoryStringMetadataIndex {
     pub fn new() -> Self {
         InMemoryStringMetadataIndex {
-            path: "in-memory".to_string(),
-            blockfile: Box::new(HashMapBlockfile::new()),
+            blockfile: Box::new(HashMapBlockfile::open(&"in-memory")).unwrap(),
         }
     }
 }
@@ -69,9 +67,11 @@ impl StringMetadataIndex for InMemoryStringMetadataIndex {
         Ok(())
     }
 
-    fn put(&mut self, key: &str, value: &str, offset_id: usize) -> Result<(), Box<dyn ChromaError>> {
-        let key = BlockfileKey::new(key, value);
-        return self.blockfile.set(key, offset_id)?;
+    fn set(&mut self, key: &str, value: &str, offset_id: usize) -> Result<(), Box<dyn ChromaError>> {
+        let prefix = key.to_string();
+        let key = Key::String(value.to_string());
+        let blockfilekey = BlockfileKey::new(prefix, key);
+        self.blockfile.set(blockfilekey, offset_id);
     }
 
     fn delete(&mut self, key: &str, value: &str, offset_id: usize) -> Result<(), Box<dyn ChromaError>> {
@@ -79,8 +79,10 @@ impl StringMetadataIndex for InMemoryStringMetadataIndex {
     }
 
     fn get(&self, key: &str, value: &str) -> Result<&RoaringBitmap, Box<dyn ChromaError>> {
-        let key = BlockfileKey::new(key, value);
-        Ok(self.blockfile.get(key)?)
+        let prefix = key.to_string();
+        let key = Key::String(value.to_string());
+        let blockfilekey = BlockfileKey::new(prefix, key);
+        self.blockfile.get(key);
     }
 }
 
@@ -90,7 +92,7 @@ mod test {
     #[test]
     fn test_in_memory_string_metadata_index_error_when_not_in_transaction() {
         let mut index = InMemoryStringMetadataIndex::new();
-        let result = index.put("key", "value", 1);
+        let result = index.set("key", "value", 1);
         assert_eq!(result.is_err(), true);
         let result = index.delete("key", "value", 1);
         assert_eq!(result.is_err(), true);
@@ -109,7 +111,7 @@ mod test {
     fn test_in_memory_string_metadata_index_put_get() {
         let mut index = InMemoryStringMetadataIndex::new();
         index.begin_transaction().unwrap();
-        index.put("key", "value", 1).unwrap();
+        index.set("key", "value", 1).unwrap();
         index.commit_transaction().unwrap();
         let bitmap = index.get("key", "value").unwrap();
         assert_eq!(bitmap.len(), 1);
@@ -120,7 +122,7 @@ mod test {
     fn test_in_memory_string_metadata_index_put_delete_get() {
         let mut index = InMemoryStringMetadataIndex::new();
         index.begin_transaction().unwrap();
-        index.put("key", "value", 1).unwrap();
+        index.set("key", "value", 1).unwrap();
         index.delete("key", "value", 1).unwrap();
         index.commit_transaction().unwrap();
         let bitmap = index.get("key", "value").unwrap();
@@ -131,9 +133,9 @@ mod test {
     fn test_in_memory_string_metadata_index_put_delete_put_get() {
         let mut index = InMemoryStringMetadataIndex::new();
         index.begin_transaction().unwrap();
-        index.put("key", "value", 1).unwrap();
+        index.set("key", "value", 1).unwrap();
         index.delete("key", "value", 1).unwrap();
-        index.put("key", "value", 1).unwrap();
+        index.set("key", "value", 1).unwrap();
         index.commit_transaction().unwrap();
         let bitmap = index.get("key", "value").unwrap();
         assert_eq!(bitmap.len(), 1);
@@ -144,8 +146,8 @@ mod test {
     fn test_in_memory_string_metadata_index_multiple_keys() {
         let mut index = InMemoryStringMetadataIndex::new();
         index.begin_transaction().unwrap();
-        index.put("key1", "value", 1).unwrap();
-        index.put("key2", "value", 2).unwrap();
+        index.set("key1", "value", 1).unwrap();
+        index.set("key2", "value", 2).unwrap();
         index.commit_transaction().unwrap();
         let bitmap = index.get("key1", "value").unwrap();
         assert_eq!(bitmap.len(), 1);
@@ -159,8 +161,8 @@ mod test {
     fn test_in_memory_string_metadata_index_multiple_values() {
         let mut index = InMemoryStringMetadataIndex::new();
         index.begin_transaction().unwrap();
-        index.put("key", "value1", 1).unwrap();
-        index.put("key", "value2", 2).unwrap();
+        index.set("key", "value1", 1).unwrap();
+        index.set("key", "value2", 2).unwrap();
         index.commit_transaction().unwrap();
         let bitmap = index.get("key", "value1").unwrap();
         assert_eq!(bitmap.len(), 1);
@@ -174,7 +176,7 @@ mod test {
     fn test_in_memory_string_metadata_index_delete_in_standalone_transaction() {
         let mut index = InMemoryStringMetadataIndex::new();
         index.begin_transaction().unwrap();
-        index.put("key", "value", 1).unwrap();
+        index.set("key", "value", 1).unwrap();
         index.commit_transaction().unwrap();
         index.begin_transaction().unwrap();
         index.delete("key", "value", 1).unwrap();
