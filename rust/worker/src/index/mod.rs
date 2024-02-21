1bf8ee4ab1f78e3c5071cd1baaa324dcf9ac2511
Ben Eggers <ben@beneggers.com>
2024-02-21 09:22:00 -0800

    Full-text document search.

--- rust/worker/src/index/mod.rs
+++ rust/worker/src/index/mod.rs
@@ -1,9 +1,11 @@
+mod document;
 mod hnsw;
 mod metadata;
 mod types;
 mod utils;
 
 // Re-export types
+pub(crate) use document::*;
 pub(crate) use hnsw::*;
 pub(crate) use metadata::*;
 pub(crate) use types::*;
