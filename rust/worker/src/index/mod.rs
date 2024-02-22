313cc361a98cc10b84b380383087d8d1e47711a3
Ben Eggers <ben@beneggers.com>
2024-02-21 16:30:01 -0800

    details

--- rust/worker/src/index/mod.rs
+++ rust/worker/src/index/mod.rs
@@ -1,19 +1,11 @@
-<<<<<<< HEAD
 mod fulltext;
-=======
-mod document;
->>>>>>> c2d607fb (Full-text document search.)
 mod hnsw;
 mod metadata;
 mod types;
 mod utils;
 
 // Re-export types
-<<<<<<< HEAD
 pub use fulltext::*;
-=======
-pub(crate) use document::*;
->>>>>>> c2d607fb (Full-text document search.)
 pub(crate) use hnsw::*;
 pub(crate) use metadata::*;
 pub(crate) use types::*;
