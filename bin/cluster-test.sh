8d02e59f8c23dd42e3ac4125dec3cdd5673e7f88
nicolas <nico@trychroma.com>
2024-02-21 23:16:20 -0800

    port forwards properly

--- bin/cluster-test.sh
+++ bin/cluster-test.sh
@@ -4,7 +4,7 @@ set -e
 export CHROMA_CLUSTER_TEST_ONLY=1
 export CHROMA_SERVER_HOST=localhost:8000
 export PULSAR_BROKER_URL=localhost
-export CHROMA_COORDINATOR_HOST=localhost
+export CHROMA_COORDINATOR_HOST=$(kubectl get svc coordinator -n chroma -o=jsonpath='{.spec.clusterIP}')
 export CHROMA_SERVER_GRPC_PORT="50051"
 
 echo "Chroma Server is running at port $CHROMA_SERVER_HOST"
