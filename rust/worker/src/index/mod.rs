1cea923319190e81af911d05fef1a881259ca630
Ben Eggers <ben@beneggers.com>
2024-02-21 16:30:01 -0800

    details

--- rust/worker/src/index/mod.rs
+++ rust/worker/src/index/mod.rs
@@ -1,11 +1,9 @@
-mod document;
 mod hnsw;
 mod metadata;
 mod types;
 mod utils;
 
 // Re-export types
-pub(crate) use document::*;
 pub(crate) use hnsw::*;
 pub(crate) use metadata::*;
 pub(crate) use types::*;
