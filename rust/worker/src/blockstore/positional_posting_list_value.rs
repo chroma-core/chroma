16fb4ea4c31e2a46cf418b864db791072eb95d7d
Ben Eggers <64657842+beggers@users.noreply.github.com>
2024-02-22 16:08:32 -0800

    [ENH] Make PositionalPostingListBuilder incremental (#1750)
    ## Description of changes
    
    *Summarize the changes made by this PR.*
     - Improvements & Bug fixes
    	 - ...
     - New functionality
    - PositionalPostingListBuilder now accepts incremental additions of
    positions for any doc ID. This makes it easier to use with the
    tokenizers lib
    
    ## Test plan
    *How are these changes tested?*
    
    - [ ] Tests pass locally with `pytest` for python, `yarn test` for js
    
    ## Documentation Changes
    *Are all docstrings for user-facing APIs updated if required? Do we need
    to make documentation changes in the [docs
    repository](https://github.com/chroma-core/docs)?*

--- rust/worker/src/blockstore/positional_posting_list_value.rs
+++ rust/worker/src/blockstore/positional_posting_list_value.rs
@@ -4,7 +4,7 @@ use arrow::{
 };
 use thiserror::Error;
 
-use std::collections::HashSet;
+use std::collections::{HashMap, HashSet};
 
 use crate::errors::{ChromaError, ErrorCodes};
 
@@ -12,22 +12,6 @@ use crate::errors::{ChromaError, ErrorCodes};
 pub(crate) struct PositionalPostingList {
     pub(crate) doc_ids: Int32Array,
     pub(crate) positions: ListArray,
-}
-
-pub(crate) struct PositionalPostingListBuilder {
-    doc_ids_builder: Int32Builder,
-    positions_builder: ListBuilder<Int32Builder>,
-    doc_id_set: HashSet<i32>,
-}
-
-impl PositionalPostingListBuilder {
-    pub(crate) fn new() -> Self {
-        PositionalPostingListBuilder {
-            doc_ids_builder: Int32Builder::new(),
-            positions_builder: ListBuilder::new(Int32Builder::new()),
-            doc_id_set: HashSet::new(),
-        }
-    }
 }
 
 impl PositionalPostingList {
@@ -53,39 +37,81 @@ impl PositionalPostingList {
 pub(crate) enum PositionalPostingListBuilderError {
     #[error("Doc ID already exists in the list")]
     DocIdAlreadyExists,
+    #[error("Doc ID does not exist in the list")]
+    DocIdDoesNotExist,
+    #[error("Incremental positions must be sorted")]
+    UnsortedPosition,
 }
 
 impl ChromaError for PositionalPostingListBuilderError {
     fn code(&self) -> ErrorCodes {
         match self {
             PositionalPostingListBuilderError::DocIdAlreadyExists => ErrorCodes::AlreadyExists,
+            PositionalPostingListBuilderError::DocIdDoesNotExist => ErrorCodes::InvalidArgument,
+            PositionalPostingListBuilderError::UnsortedPosition => ErrorCodes::InvalidArgument,
         }
     }
 }
 
+pub(crate) struct PositionalPostingListBuilder {
+    doc_ids: HashSet<i32>,
+    positions: HashMap<i32, Vec<i32>>,
+}
+
 impl PositionalPostingListBuilder {
+    pub(crate) fn new() -> Self {
+        PositionalPostingListBuilder {
+            doc_ids: HashSet::new(),
+            positions: HashMap::new(),
+        }
+    }
+
     pub(crate) fn add_doc_id_and_positions(
         &mut self,
         doc_id: i32,
         positions: Vec<i32>,
     ) -> Result<(), PositionalPostingListBuilderError> {
-        if self.doc_id_set.contains(&doc_id) {
+        if self.doc_ids.contains(&doc_id) {
             return Err(PositionalPostingListBuilderError::DocIdAlreadyExists);
         }
 
-        self.doc_ids_builder.append_value(doc_id);
-        let positions = positions
-            .into_iter()
-            .map(Some)
-            .collect::<Vec<Option<i32>>>();
-        self.positions_builder.append_value(positions);
-        self.doc_id_set.insert(doc_id);
+        self.doc_ids.insert(doc_id);
+        self.positions.insert(doc_id, positions);
+        Ok(())
+    }
+
+    pub(crate) fn add_positions_for_doc_id(
+        &mut self,
+        doc_id: i32,
+        positions: Vec<i32>,
+    ) -> Result<(), PositionalPostingListBuilderError> {
+        if !self.doc_ids.contains(&doc_id) {
+            return Err(PositionalPostingListBuilderError::DocIdDoesNotExist);
+        }
+
+        self.positions.get_mut(&doc_id).unwrap().extend(positions);
         Ok(())
     }
 
     pub(crate) fn build(&mut self) -> PositionalPostingList {
-        let doc_ids = self.doc_ids_builder.finish();
-        let positions = self.positions_builder.finish();
+        let mut doc_ids_builder = Int32Builder::new();
+        let mut positions_builder = ListBuilder::new(Int32Builder::new());
+
+        let mut doc_ids_vec: Vec<i32> = self.doc_ids.drain().collect();
+        doc_ids_vec.sort();
+        let doc_ids_slice = doc_ids_vec.as_slice();
+        doc_ids_builder.append_slice(doc_ids_slice);
+        let doc_ids = doc_ids_builder.finish();
+
+        for doc_id in doc_ids_slice.iter() {
+            // Get positions for the doc ID, sort them, put them into the positions_builder
+            let mut positions = self.positions.remove(doc_id).unwrap();
+            positions.sort();
+            let positions_as_some: Vec<Option<i32>> = positions.into_iter().map(Some).collect();
+            positions_builder.append_value(positions_as_some);
+        }
+        let positions = positions_builder.finish();
+
         PositionalPostingList {
             doc_ids: doc_ids,
             positions: positions,
@@ -98,12 +124,22 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_positional_posting_list() {
+    fn test_positional_posting_list_single_document() {
         let mut builder = PositionalPostingListBuilder::new();
+        let _res = builder.add_doc_id_and_positions(1, vec![1, 2, 3]);
+        let list = builder.build();
+        assert_eq!(list.get_doc_ids().values()[0], 1);
+        assert_eq!(
+            list.get_positions_for_doc_id(1).unwrap(),
+            Int32Array::from(vec![1, 2, 3])
+        );
+    }
 
+    #[test]
+    fn test_positional_posting_list_multiple_documents() {
+        let mut builder = PositionalPostingListBuilder::new();
         let _res = builder.add_doc_id_and_positions(1, vec![1, 2, 3]);
         let _res = builder.add_doc_id_and_positions(2, vec![4, 5, 6]);
-
         let list = builder.build();
         assert_eq!(list.get_doc_ids().values()[0], 1);
         assert_eq!(list.get_doc_ids().values()[1], 2);
@@ -115,8 +151,79 @@ mod tests {
             list.get_positions_for_doc_id(2).unwrap(),
             Int32Array::from(vec![4, 5, 6])
         );
+    }
 
-        let res = builder.add_doc_id_and_positions(1, vec![1, 2, 3]);
-        assert!(res.is_err());
+    #[test]
+    fn test_positional_posting_list_document_ids_sorted_after_build() {
+        let mut builder = PositionalPostingListBuilder::new();
+        let _res = builder.add_doc_id_and_positions(2, vec![4, 5, 6]);
+        let _res = builder.add_doc_id_and_positions(1, vec![1, 2, 3]);
+        let list = builder.build();
+        assert_eq!(list.get_doc_ids().values()[0], 1);
+        assert_eq!(list.get_doc_ids().values()[1], 2);
+        assert_eq!(
+            list.get_positions_for_doc_id(1).unwrap(),
+            Int32Array::from(vec![1, 2, 3])
+        );
+        assert_eq!(
+            list.get_positions_for_doc_id(2).unwrap(),
+            Int32Array::from(vec![4, 5, 6])
+        );
+    }
+
+    #[test]
+    fn test_positional_posting_list_all_positions_sorted_after_build() {
+        let mut builder = PositionalPostingListBuilder::new();
+        let _res = builder.add_doc_id_and_positions(1, vec![3, 2, 1]);
+        let list = builder.build();
+        assert_eq!(list.get_doc_ids().values()[0], 1);
+        assert_eq!(
+            list.get_positions_for_doc_id(1).unwrap(),
+            Int32Array::from(vec![1, 2, 3])
+        );
+    }
+
+    #[test]
+    fn test_positional_posting_list_incremental_build() {
+        let mut builder = PositionalPostingListBuilder::new();
+
+        let _res = builder.add_doc_id_and_positions(1, vec![1, 2, 3]);
+        let _res = builder.add_positions_for_doc_id(1, [4].into());
+        let _res = builder.add_positions_for_doc_id(1, [5].into());
+        let _res = builder.add_positions_for_doc_id(1, [6].into());
+        let _res = builder.add_doc_id_and_positions(2, vec![4, 5, 6]);
+        let _res = builder.add_positions_for_doc_id(2, [7].into());
+
+        let list = builder.build();
+        assert_eq!(list.get_doc_ids().values()[0], 1);
+        assert_eq!(list.get_doc_ids().values()[1], 2);
+        assert_eq!(
+            list.get_positions_for_doc_id(1).unwrap(),
+            Int32Array::from(vec![1, 2, 3, 4, 5, 6])
+        );
+    }
+
+    #[test]
+    fn test_all_positional_posting_list_behaviors_together() {
+        let mut builder = PositionalPostingListBuilder::new();
+
+        let _res = builder.add_doc_id_and_positions(1, vec![3, 2, 1]);
+        let _res = builder.add_positions_for_doc_id(1, [4].into());
+        let _res = builder.add_positions_for_doc_id(1, [6].into());
+        let _res = builder.add_positions_for_doc_id(1, [5].into());
+        let _res = builder.add_doc_id_and_positions(2, vec![5, 4, 6]);
+        let _res = builder.add_positions_for_doc_id(2, [7].into());
+
+        let list = builder.build();
+        assert_eq!(list.get_doc_ids().values()[0], 1);
+        assert_eq!(list.get_doc_ids().values()[1], 2);
+        assert_eq!(
+            list.get_positions_for_doc_id(1).unwrap(),
+            Int32Array::from(vec![1, 2, 3, 4, 5, 6])
+        );
+        assert_eq!(
+            list.get_positions_for_doc_id(2).unwrap(),
+            Int32Array::from(vec![4, 5, 6, 7])
+        );
     }
 }
