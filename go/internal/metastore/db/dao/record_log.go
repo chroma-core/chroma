000fbc10f9d3850ca3e72d4820e11e19a330c736
Weili Gu <weili@trychroma.com>
2024-02-26 13:45:36 -0800

    increase log id by collection

--- go/internal/metastore/db/dao/record_log.go
+++ go/internal/metastore/db/dao/record_log.go
@@ -1,6 +1,7 @@
 package dao
 
 import (
+	"errors"
 	"github.com/chroma/chroma-coordinator/internal/metastore/db/dbmodel"
 	"github.com/chroma/chroma-coordinator/internal/types"
 	"github.com/pingcap/log"
@@ -22,15 +23,26 @@ func (s *recordLogDb) PushLogs(collectionID types.UniqueID, recordsContent [][]b
 			zap.Int64("timestamp", timestamp),
 			zap.Int("count", len(recordsContent)))
 
+		var lastLog *dbmodel.RecordLog
+		err := tx.Select("id").Where("collection_id = ?", collectionIDStr).Last(&lastLog).Error
+		if err != nil && !errors.Is(err, gorm.ErrRecordNotFound) {
+			log.Error("Get last log id error", zap.Error(err))
+			tx.Rollback()
+			return err
+		}
+		var lastLogId = lastLog.ID
+		log.Info("PushLogs", zap.Int64("lastLogId", lastLogId))
+
 		var recordLogs []*dbmodel.RecordLog
 		for index := range recordsContent {
 			recordLogs = append(recordLogs, &dbmodel.RecordLog{
 				CollectionID: collectionIDStr,
+				ID:           lastLogId + int64(index) + 1,
 				Timestamp:    timestamp,
 				Record:       &recordsContent[index],
 			})
 		}
-		err := tx.CreateInBatches(recordLogs, len(recordLogs)).Error
+		err = tx.CreateInBatches(recordLogs, len(recordLogs)).Error
 		if err != nil {
 			log.Error("Batch insert error", zap.Error(err))
 			tx.Rollback()
@@ -53,7 +65,11 @@ func (s *recordLogDb) PullLogs(collectionID types.UniqueID, id int64, batchSize 
 		zap.Int("batch_size", batchSize))
 
 	var recordLogs []*dbmodel.RecordLog
-	s.db.Where("collection_id = ? AND id >= ?", collectionIDStr, id).Order("id").Limit(batchSize).Find(&recordLogs)
+	result := s.db.Where("collection_id = ? AND id >= ?", collectionIDStr, id).Order("id").Limit(batchSize).Find(&recordLogs)
+	if result.Error != nil && !errors.Is(result.Error, gorm.ErrRecordNotFound) {
+		log.Error("PullLogs error", zap.Error(result.Error))
+		return nil, result.Error
+	}
 	log.Info("PullLogs",
 		zap.String("collectionID", *collectionIDStr),
 		zap.Int64("ID", id),
