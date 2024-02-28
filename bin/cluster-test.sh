1df2647e2fb0a90f4188c6992885cd586b4a7289
Ben Eggers <ben@beneggers.com>
2024-02-27 18:01:14 -0800

    [CLN] Remove unused var

--- bin/cluster-test.sh
+++ bin/cluster-test.sh
@@ -6,9 +6,6 @@ export CHROMA_CLUSTER_TEST_ONLY=1
 export CHROMA_SERVER_HOST=localhost:8000
 export PULSAR_BROKER_URL=localhost
 export CHROMA_COORDINATOR_HOST=localhost
-export CHROMA_SERVER_GRPC_PORT="50051"
-
-
 
 echo "Chroma Server is running at port $CHROMA_SERVER_HOST"
 echo "Pulsar Broker is running at port $PULSAR_BROKER_URL"
