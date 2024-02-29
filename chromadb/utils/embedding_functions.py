43518a726185cb641732dfddb0f39da5838771e8
Dias Kalkamanov <diicellman@gmail.com>
2024-02-29 17:21:42 +0600

    added ollama embedding function

--- chromadb/utils/embedding_functions.py
+++ chromadb/utils/embedding_functions.py
@@ -815,6 +815,48 @@ class HuggingFaceEmbeddingServer(EmbeddingFunction[Documents]):
         )
 
 
+class OllamaEmbeddingFunction(EmbeddingFunction[Documents]):
+    """
+    This class is used to get embeddings for a list of texts using Ollama Python Library.
+    It requires a host url and a model name. The default model name is "nomic-embed-text".
+    """
+
+    def __init__(self, host: str, model_name: str = "nomic-embed-text"):
+        try:
+            import ollama
+        except ImportError:
+            raise ValueError(
+                "The ollama python package is not installed. Please install it with `pip install ollama`"
+            )
+
+        self._client = ollama.Client(host)
+        self._model_name = model_name
+
+    def __call__(self, input: Documents) -> Embeddings:
+        """
+        Get the embeddings for a list of texts.
+
+        Args:
+            input (Documents): A list of texts to get embeddings for.
+
+        Returns:
+            Embeddings: The embeddings for the texts.
+
+        Example:
+            >>> ollama = OllamaEmbeddingFunction(host="http://localhost:11434")
+            >>> texts = ["Hello, world!", "How are you?"]
+            >>> embeddings = ollama(texts)
+        """
+
+        embeddings = []
+        # Call Ollama Embedding API for each document.
+        for document in input:
+            embedding = self._client.embeddings(model=self._model_name, prompt=document)
+            embeddings.append(embedding["embedding"])
+
+        return embeddings
+
+
 # List of all classes in this module
 _classes = [
     name
