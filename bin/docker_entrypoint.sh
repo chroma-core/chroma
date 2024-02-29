dd38ac55160b26ff56dab84565c5f8fe46e41423
ammad-khalid <55195052+ammad-khalid@users.noreply.github.com>
2024-02-29 09:27:52 +0500

    Update docker_entrypoint.sh

--- bin/docker_entrypoint.sh
+++ bin/docker_entrypoint.sh
@@ -4,4 +4,4 @@ echo "Rebuilding hnsw to ensure architecture compatibility"
 pip install --force-reinstall --no-cache-dir chroma-hnswlib
 export IS_PERSISTENT=1
 export CHROMA_SERVER_NOFILE=65535
-uvicorn chromadb.app:app --workers 1 --host 0.0.0.0 --port 8000 --proxy-headers --log-config chromadb/log_config.yml --timeout-keep-alive 30
+uvicorn chromadb.app:app --workers 1 --host 0.0.0.0 --port 8000 --proxy-headers --timeout-keep-alive 30
