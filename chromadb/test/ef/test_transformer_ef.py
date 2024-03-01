84d8779fe7d2bbeb0655f112430f3651b4a72493
Trayan Azarov <trayan.azarov@amikos.tech>
2024-03-01 15:00:23 +0200

    fix: Added conditional skip if packages are not installed
    Refs: #1797

--- chromadb/test/ef/test_transformer_ef.py
+++ chromadb/test/ef/test_transformer_ef.py
@@ -1,12 +1,23 @@
+import importlib
+
+import pytest
+
 from chromadb.utils.embedding_functions import TransformerEmbeddingFunction
 
 
+@pytest.mark.skipif(
+    importlib.util.find_spec("transformers") is None and importlib.util.find_spec("torch") is None,
+    reason="test requires numba which is not installed"
+)
 def test_transformer_ef_default_mdoel():
     ef = TransformerEmbeddingFunction()
     embedding = ef(["text"])
     assert len(embedding[0]) == 384
 
-
+@pytest.mark.skipif(
+    importlib.util.find_spec("transformers") is None and importlib.util.find_spec("torch") is None,
+    reason="test requires numba which is not installed"
+)
 def test_transformer_ef_custom_model():
     ef = TransformerEmbeddingFunction(model_name="dbmdz/bert-base-turkish-cased")
     embedding = ef(["Merhaba dünya", "Bu bir test cümlesidir"])
