7f9644b1b2167e3f1b94ae35b2571721e4671f65
Trayan Azarov <trayan.azarov@amikos.tech>
2024-03-05 14:30:47 +0200

    chore: Exposing OllamaEmbeddingFunction at top level in the JS package

--- clients/js/src/index.ts
+++ clients/js/src/index.ts
@@ -11,6 +11,7 @@ export { DefaultEmbeddingFunction } from './embeddings/DefaultEmbeddingFunction'
 export { HuggingFaceEmbeddingServerFunction } from './embeddings/HuggingFaceEmbeddingServerFunction';
 export { JinaEmbeddingFunction } from './embeddings/JinaEmbeddingFunction';
 export { GoogleGenerativeAiEmbeddingFunction } from './embeddings/GoogleGeminiEmbeddingFunction';
+export { OllamaEmbeddingFunction } from './embeddings/OllamaEmbeddingFunction';
 
 export {
     IncludeEnum,
