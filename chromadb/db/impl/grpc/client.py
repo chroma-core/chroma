b7e8b62e6444a779193f0afb06bcb47fb4f25a26
nicolasgere <nicolas.gerelm@gmail.com>
2024-03-06 13:15:22 -0800

    [ENH]: add grpc client interceptor (#1818)
    ## Description of changes
    
    *Summarize the changes made by this PR.*
     - Improvements & Bug fixes
    	 - Add grpc client interceptor
    
    ## Test plan
    *How are these changes tested?*
    
    - Tested locally with distributed tracing

--- chromadb/db/impl/grpc/client.py
+++ chromadb/db/impl/grpc/client.py
@@ -27,6 +27,8 @@ from chromadb.proto.coordinator_pb2 import (
     UpdateSegmentRequest,
 )
 from chromadb.proto.coordinator_pb2_grpc import SysDBStub
+from chromadb.telemetry.opentelemetry import OpenTelemetryClient
+from chromadb.telemetry.opentelemetry.grpc import OtelInterceptor
 from chromadb.types import (
     Collection,
     Database,
@@ -64,6 +66,8 @@ class GrpcSysDB(SysDB):
         self._channel = grpc.insecure_channel(
             f"{self._coordinator_url}:{self._coordinator_port}"
         )
+        interceptors = [OtelInterceptor()]
+        self._channel = grpc.intercept_channel(self._channel, *interceptors)
         self._sys_db_stub = SysDBStub(self._channel)  # type: ignore
         return super().start()
 
