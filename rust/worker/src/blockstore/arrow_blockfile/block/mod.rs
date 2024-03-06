cbfd5637bb7fff955908c5be6aceaae3965b81cd
hammadb <hammad@trychroma.com>
2024-03-05 17:16:42 -0800

    [ENH] Add blockfile provider

--- rust/worker/src/blockstore/arrow_blockfile/block/mod.rs
+++
@@ -1,6 +0,0 @@
-pub(in crate::blockstore::arrow_blockfile) mod delta;
-mod iterator;
-mod types;
-
-// Re-export types at the module level
-pub(in crate::blockstore::arrow_blockfile) use types::*;
--- rust/worker/src/blockstore/mod.rs
+++ rust/worker/src/blockstore/mod.rs
@@ -1,4 +1,3 @@
-mod arrow_blockfile;
 mod positional_posting_list_value;
 mod types;
 
