a5bbbf2060dfb2ab79cb7b4c4465a1cd61d23ac5
Matthew Keller <matthew.ed.keller@gmail.com>
2024-03-06 10:17:41 -0500

    [ENH] reference api key from a .env file instead of the source code itself in the JS client node example.

--- clients/js/examples/node/app.js
+++ clients/js/examples/node/app.js
@@ -1,15 +1,24 @@
-var fs = require("fs");
-var path = require("path");
-
 var express = require("express");
 var chroma = require("chromadb");
+var dotenv = require("dotenv");
+
+dotenv.config();
+
+if (!process.env.GOOGLE_API_KEY) {
+  console.error(
+    "GOOGLE_API_KEY is not set. Please create a .env file and set it there."
+  );
+  process.exit(0);
+}
 
 var app = express();
 app.get("/", async (req, res) => {
   const cc = new chroma.ChromaClient({ path: "http://localhost:8000" });
   await cc.reset();
 
-  const google = new chroma.GoogleGenerativeAiEmbeddingFunction({ googleApiKey:"<APIKEY>" });
+  const google = new chroma.GoogleGenerativeAiEmbeddingFunction({
+    googleApiKey: process.env.GOOGLE_API_KEY,
+  });
 
   const collection = await cc.createCollection({
     name: "test-from-js",
@@ -18,16 +27,16 @@ app.get("/", async (req, res) => {
 
   await collection.add({
     ids: ["doc1", "doc2"],
-    documents: [
-      "doc1",
-      "doc2",
-    ]
+    documents: ["doc1", "doc2"],
   });
 
   let count = await collection.count();
   console.log("count", count);
 
-  const googleQuery = new chroma.GoogleGenerativeAiEmbeddingFunction({ googleApiKey:"<APIKEY>", taskType: 'RETRIEVAL_QUERY' });
+  const googleQuery = new chroma.GoogleGenerativeAiEmbeddingFunction({
+    googleApiKey: "<APIKEY>",
+    taskType: "RETRIEVAL_QUERY",
+  });
 
   const queryCollection = await cc.getCollection({
     name: "test-from-js",
@@ -36,16 +45,16 @@ app.get("/", async (req, res) => {
 
   const query = await collection.query({
     queryTexts: ["doc1"],
-    nResults: 1
+    nResults: 1,
   });
   console.log("query", query);
 
   console.log("COMPLETED");
 
   const collections = await cc.listCollections();
-  console.log('collections', collections)
+  console.log("collections", collections);
 
-  res.send('Hello World!');
+  res.send("Hello World!");
 });
 app.listen(3000, function () {
   console.log("Example app listening on port 3000!");
