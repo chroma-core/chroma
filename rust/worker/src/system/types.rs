637ea6c7fd18d580ba7cb2f7a6666fae8c3016cf
Liquan Pei <liquanpei@gmail.com>
2024-03-07 10:36:32 -0800

    add system scheduler

--- rust/worker/src/system/types.rs
+++ rust/worker/src/system/types.rs
@@ -1,5 +1,6 @@
 use super::scheduler::Scheduler;
 use async_trait::async_trait;
+use bytes::buf::Chain;
 use futures::Stream;
 use std::fmt::Debug;
 
@@ -151,8 +152,15 @@ mod tests {
         counter: Arc<AtomicUsize>,
     }
 
+    #[derive(Clone, Debug)]
+    struct ScheduleMessage {}
+
     impl TestComponent {
-        fn new(queue_size: usize, counter: Arc<AtomicUsize>) -> Self {
+        fn new(
+            queue_size: usize,
+            counter: Arc<AtomicUsize>,
+            scheduled_counter: Arc<AtomicUsize>,
+        ) -> Self {
             TestComponent {
                 queue_size,
                 counter,
@@ -183,7 +191,8 @@ mod tests {
     async fn it_can_work() {
         let mut system = System::new();
         let counter = Arc::new(AtomicUsize::new(0));
-        let component = TestComponent::new(10, counter.clone());
+        let scheduled_counter = Arc::new(AtomicUsize::new(0));
+        let component = TestComponent::new(10, counter.clone(), scheduled_counter.clone());
         let mut handle = system.start_component(component);
         handle.sender.send(1).await.unwrap();
         handle.sender.send(2).await.unwrap();
