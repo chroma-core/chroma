c9ecd2a403e308fd77d4744a7ea8a5365aab5afc
Trayan Azarov <trayan.azarov@amikos.tech>
2024-03-08 21:31:12 +0200

    fix: Removed hard-coded values from ollama ef test.
    - The test is now conditional on ollama server running and OLLAMA_SERVER_URL and OLLAMA_MODEL env vars being available

--- chromadb/test/ef/test_ollama_ef.py
+++ chromadb/test/ef/test_ollama_ef.py
@@ -1,3 +1,5 @@
+import os
+
 import pytest
 import requests
 from requests import HTTPError
@@ -7,15 +9,26 @@ from chromadb.utils.embedding_functions import OllamaEmbeddingFunction
 
 
 def test_ollama() -> None:
+    """
+    To set up the Ollama server, follow instructions at: https://github.com/ollama/ollama?tab=readme-ov-file
+    Export the OLLAMA_SERVER_URL and OLLAMA_MODEL environment variables.
+    """
+    if (
+        os.environ.get("OLLAMA_SERVER_URL") is None
+        or os.environ.get("OLLAMA_MODEL") is None
+    ):
+        pytest.skip(
+            "OLLAMA_SERVER_URL or OLLAMA_MODEL environment variable not set. Skipping test."
+        )
     try:
-        response = requests.get("http://localhost:11434/")
+        response = requests.get(os.environ.get("OLLAMA_SERVER_URL", ""))
         # If the response was successful, no Exception will be raised
         response.raise_for_status()
-    except (HTTPError, ConnectionError) as _:
+    except (HTTPError, ConnectionError):
         pytest.skip("Ollama server not running. Skipping test.")
     ef = OllamaEmbeddingFunction(
-        model_name="llama2",
-        url="http://localhost:11434/api/embeddings",
+        model_name=os.environ.get("OLLAMA_MODEL") or "llama2",
+        url=f"{os.environ.get('OLLAMA_SERVER_URL')}/embeddings",
     )
     embeddings = ef(["Here is an article about llamas...", "this is another article"])
     assert len(embeddings) == 2
