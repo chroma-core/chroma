3e373e533c4a67539e38956dc078789852089616
hammadb <hammad@trychroma.com>
2024-03-09 12:53:44 -0800

    Add segment interfaces

--- rust/worker/src/segment/mod.rs
+++ rust/worker/src/segment/mod.rs
@@ -2,6 +2,7 @@ pub(crate) mod config;
 mod distributed_hnsw_segment;
 mod segment_ingestor;
 mod segment_manager;
+mod types;
 
 pub(crate) use segment_ingestor::*;
 pub(crate) use segment_manager::*;
