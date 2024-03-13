97c7539bf1420f1293ab906eaaa8babe276ddd5b
Matthew Keller <matthew.ed.keller@gmail.com>
2024-03-13 19:58:05 -0400

    [CLN] Apply Prettier Config to JS Client Repo (#1831)
    ## Description of changes
    
    *Summarize the changes made by this PR.*
    
    Applies Prettier formatting to the JS Client Repo and add a presubmit
    rule to enforce that behavior.
    
    ## Test plan
    
    N/A just formatting changes.

--- clients/js/src/embeddings/HuggingFaceEmbeddingServerFunction.ts
+++ clients/js/src/embeddings/HuggingFaceEmbeddingServerFunction.ts
@@ -3,29 +3,28 @@ import { IEmbeddingFunction } from "./IEmbeddingFunction";
 let CohereAiApi: any;
 
 export class HuggingFaceEmbeddingServerFunction implements IEmbeddingFunction {
-    private url: string;
+  private url: string;
 
-    constructor({ url }: { url: string }) {
-        // we used to construct the client here, but we need to async import the types
-        // for the openai npm package, and the constructor can not be async
-        this.url = url;
+  constructor({ url }: { url: string }) {
+    // we used to construct the client here, but we need to async import the types
+    // for the openai npm package, and the constructor can not be async
+    this.url = url;
+  }
+
+  public async generate(texts: string[]) {
+    const response = await fetch(this.url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ inputs: texts }),
+    });
+
+    if (!response.ok) {
+      throw new Error(`Failed to generate embeddings: ${response.statusText}`);
     }
 
-    public async generate(texts: string[]) {
-        const response = await fetch(this.url, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({ 'inputs': texts })
-        });
-
-        if (!response.ok) {
-            throw new Error(`Failed to generate embeddings: ${response.statusText}`);
-        }
-
-        const data = await response.json();
-        return data;
-    }
-
+    const data = await response.json();
+    return data;
+  }
 }
--- clients/js/test/auth.basic.test.ts
+++ clients/js/test/auth.basic.test.ts
@@ -1,33 +1,33 @@
-import {expect, test} from "@jest/globals";
-import {chromaBasic} from "./initClientWithAuth";
+import { expect, test } from "@jest/globals";
+import { chromaBasic } from "./initClientWithAuth";
 import chromaNoAuth from "./initClient";
 import { ChromaClient } from "../src/ChromaClient";
 
 test("it should get the version without auth needed", async () => {
-    const version = await chromaNoAuth.version();
-    expect(version).toBeDefined();
-    expect(version).toMatch(/^[0-9]+\.[0-9]+\.[0-9]+$/);
+  const version = await chromaNoAuth.version();
+  expect(version).toBeDefined();
+  expect(version).toMatch(/^[0-9]+\.[0-9]+\.[0-9]+$/);
 });
 
 test("it should get the heartbeat without auth needed", async () => {
-    const heartbeat = await chromaNoAuth.heartbeat();
-    expect(heartbeat).toBeDefined();
-    expect(heartbeat).toBeGreaterThan(0);
+  const heartbeat = await chromaNoAuth.heartbeat();
+  expect(heartbeat).toBeDefined();
+  expect(heartbeat).toBeGreaterThan(0);
 });
 
 test("it should raise error when non authenticated", async () => {
-    await expect(chromaNoAuth.listCollections()).rejects.toMatchObject({
-        status: 401
-    });
+  await expect(chromaNoAuth.listCollections()).rejects.toMatchObject({
+    status: 401,
+  });
 });
 
-test('it should list collections', async () => {
-    await chromaBasic.reset()
-    let collections = await chromaBasic.listCollections()
-    expect(collections).toBeDefined()
-    expect(collections).toBeInstanceOf(Array)
-    expect(collections.length).toBe(0)
-    await chromaBasic.createCollection({name: "test"});
-    collections = await chromaBasic.listCollections()
-    expect(collections.length).toBe(1)
-})
+test("it should list collections", async () => {
+  await chromaBasic.reset();
+  let collections = await chromaBasic.listCollections();
+  expect(collections).toBeDefined();
+  expect(collections).toBeInstanceOf(Array);
+  expect(collections.length).toBe(0);
+  await chromaBasic.createCollection({ name: "test" });
+  collections = await chromaBasic.listCollections();
+  expect(collections.length).toBe(1);
+});
