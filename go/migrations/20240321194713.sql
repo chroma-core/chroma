ec6678a46917a90d246e495ed28792b2b783d3b9
Ben Eggers <ben@beneggers.com>
2024-03-21 12:58:08 -0700

    fix it

--- go/migrations/20240321194713.sql
+++ go/migrations/20240321194713.sql
@@ -1,7 +1,14 @@
-INSERT OR REPLACE INTO tenants (id) VALUES ('default_tenant'); -- The default tenant id is 'default_tenant' others are UUIDs
-INSERT OR REPLACE INTO databases (id, name, tenant_id) VALUES ('00000000-0000-0000-0000-000000000000', 'default_database', 'default_tenant');
+INSERT INTO
+  "public"."tenants" (id, last_compaction_time)
+VALUES
+  ('default_tenant', 0);
 
-INSERT OR REPLACE INTO collections_tmp (id, name, topic, dimension, database_id)
-    SELECT id, name, topic, dimension, '00000000-0000-0000-0000-000000000000' FROM collections;
-DROP TABLE collections;
-ALTER TABLE collections_tmp RENAME TO collections;
+-- The default tenant id is 'default_tenant' others are UUIDs
+INSERT INTO
+  databases (id, name, tenant_id)
+VALUES
+  (
+    '00000000-0000-0000-0000-000000000000',
+    'default_database',
+    'default_tenant'
+  );
