1d77f99edc43ade4d3490a4638b5e5d802050303
Trayan Azarov <tazarov@users.noreply.github.com>
2024-03-28 19:59:01 +0200

    [ENH]: Ollama embedding function (#1813)
    ## Description of changes
    
    *Summarize the changes made by this PR.*
     - New functionality
    	 - New Ollama embedding function (Python and JS)
    	 - Example of how to run Ollama with the embedding function
    
    ## Test plan
    *How are these changes tested?*
    
    - [x] Tests pass locally with `pytest` for python, `yarn test` for js
    
    ## Documentation Changes
    
    chroma-core/docs#222

--- chromadb/utils/embedding_functions.py
+++ chromadb/utils/embedding_functions.py
@@ -61,7 +61,7 @@ class SentenceTransformerEmbeddingFunction(EmbeddingFunction[Documents]):
         model_name: str = "all-MiniLM-L6-v2",
         device: str = "cpu",
         normalize_embeddings: bool = False,
-        **kwargs: Any
+        **kwargs: Any,
     ):
         """Initialize SentenceTransformerEmbeddingFunction.
 
@@ -78,7 +78,9 @@ class SentenceTransformerEmbeddingFunction(EmbeddingFunction[Documents]):
                 raise ValueError(
                     "The sentence_transformers python package is not installed. Please install it with `pip install sentence_transformers`"
                 )
-            self.models[model_name] = SentenceTransformer(model_name, device=device, **kwargs)
+            self.models[model_name] = SentenceTransformer(
+                model_name, device=device, **kwargs
+            )
         self._model = self.models[model_name]
         self._normalize_embeddings = normalize_embeddings
 
@@ -825,6 +827,62 @@ class HuggingFaceEmbeddingServer(EmbeddingFunction[Documents]):
         # Call HuggingFace Embedding Server API for each document
         return cast(
             Embeddings, self._session.post(self._api_url, json={"inputs": input}).json()
+        )
+
+
+class OllamaEmbeddingFunction(EmbeddingFunction[Documents]):
+    """
+    This class is used to generate embeddings for a list of texts using the Ollama Embedding API (https://github.com/ollama/ollama/blob/main/docs/api.md#generate-embeddings).
+    """
+
+    def __init__(self, url: str, model_name: str) -> None:
+        """
+        Initialize the Ollama Embedding Function.
+
+        Args:
+            url (str): The URL of the Ollama Server.
+            model_name (str): The name of the model to use for text embeddings. E.g. "nomic-embed-text" (see https://ollama.com/library for available models).
+        """
+        try:
+            import requests
+        except ImportError:
+            raise ValueError(
+                "The requests python package is not installed. Please install it with `pip install requests`"
+            )
+        self._api_url = f"{url}"
+        self._model_name = model_name
+        self._session = requests.Session()
+
+    def __call__(self, input: Documents) -> Embeddings:
+        """
+        Get the embeddings for a list of texts.
+
+        Args:
+            input (Documents): A list of texts to get embeddings for.
+
+        Returns:
+            Embeddings: The embeddings for the texts.
+
+        Example:
+            >>> ollama_ef = OllamaEmbeddingFunction(url="http://localhost:11434/api/embeddings", model_name="nomic-embed-text")
+            >>> texts = ["Hello, world!", "How are you?"]
+            >>> embeddings = ollama_ef(texts)
+        """
+        # Call Ollama Server API for each document
+        texts = input if isinstance(input, list) else [input]
+        embeddings = [
+            self._session.post(
+                self._api_url, json={"model": self._model_name, "prompt": text}
+            ).json()
+            for text in texts
+        ]
+        return cast(
+            Embeddings,
+            [
+                embedding["embedding"]
+                for embedding in embeddings
+                if "embedding" in embedding
+            ],
         )
 
 
