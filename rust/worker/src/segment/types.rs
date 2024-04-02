16f73f47c274aac94545f7b3ab59b4d19cf97d51
hammadb <hammad@trychroma.com>
2024-04-02 15:35:43 -0700

    wip

--- rust/worker/src/segment/types.rs
+++ rust/worker/src/segment/types.rs
@@ -1,12 +1,19 @@
+use crate::execution::data::data_chunk::Chunk;
 use crate::types::LogRecord;
+
+struct MaterializedLogRecord<'a> {
+    segment_offset_id: u32,
+    log_record: &'a LogRecord,
+}
 
 trait SegmentWriter {
     fn begin_transaction(&self);
-    fn write_records(&self, records: Vec<Box<LogRecord>>, offset_ids: Vec<u32>);
+    fn apply_materialized_log_chunk(&self, records: Chunk<MaterializedLogRecord>);
+    fn apply_log_chunk(&self, records: Chunk<LogRecord>);
     fn commit_transaction(&self);
     fn rollback_transaction(&self);
 }
 
-trait OffsetIdAssigner: SegmentWriter {
-    fn assign_offset_ids(&self, records: Vec<Box<LogRecord>>) -> Vec<u32>;
+trait LogMaterializer: SegmentWriter {
+    fn materialize(&self, records: Chunk<LogRecord>) -> Chunk<MaterializedLogRecord>;
 }
