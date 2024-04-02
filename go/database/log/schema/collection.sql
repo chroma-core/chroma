1d29b32bae2a6370d9bf5abebf6bc49f10dc754d
nicolasgere <nicolas.gerelm@gmail.com>
2024-04-02 15:33:39 -0700

    add comment

--- go/database/log/schema/collection.sql
+++ go/database/log/schema/collection.sql
@@ -4,3 +4,5 @@ CREATE TABLE collection (
                         record_enumeration_offset_position bigint NOT NULL
                         );
 
+-- The `record_compaction_offset_position` column indicates the offset position of the latest compaction.
+-- The `record_enenumeration_offset_position` column denotes the incremental offset for the most recent record in a collection.
