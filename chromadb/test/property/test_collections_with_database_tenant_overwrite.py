a406e26fc9b858f97a782e4e02f8d03a521f88f6
Ben Eggers <ben@beneggers.com>
2024-04-04 11:02:24 -0700

    ignore auth files, subclass tenant db test and use a fixture

---
+++ chromadb/test/property/test_collections_with_database_tenant_overwrite.py
@@ -0,0 +1,68 @@
+from hypothesis import given, settings
+from overrides import override
+from starlette.datastructures import Headers
+from typing import Any, Dict, List, Optional
+
+from hypothesis.stateful import (
+    Bundle,
+    rule,
+    initialize,
+    multiple,
+    run_state_machine_as_test,
+    MultipleResults,
+)
+import hypothesis.strategies as st
+import logging
+import pytest
+import string
+
+from chromadb.auth import (
+    ServerAuthenticationProvider,
+    UserIdentity
+)
+from chromadb.api import ServerAPI
+from chromadb.api.client import Client
+from chromadb.config import DEFAULT_DATABASE, DEFAULT_TENANT, System
+from chromadb.test.conftest import (
+  fastapi_fixture_admin_and_singleton_tenant_db_user
+)
+from chromadb.test.property.test_collections_with_database_tenant import (
+  TenantDatabaseCollectionStateMachine,
+)
+
+
+# This test reuses the state machines from test_collections.py and
+# test_collections_with_database_tenant.py. However instead of always using the
+# test's default api and admin_api (which have full permissions over all
+# tenants and dbs), we sometimes use api clients which only have access to
+# a single tenant and database. When this happens, we expect all requests
+# to be routed to that tenant and database regardless of which tenant and
+# database the request specifies.
+
+
+class SingletonTenantDatabaseCollectionStateMachine(
+    TenantDatabaseCollectionStateMachine
+):
+    def __init__(self, client: Client) -> None:
+        super().__init__(client)
+
+    @initialize()
+    def initialize(self) -> None:
+        super().initialize()
+
+
+def test_collections_with_tenant_database_overwrite(
+    caplog: pytest.LogCaptureFixture,
+) -> None:
+    caplog.set_level(logging.ERROR)
+
+    api_fixture = fastapi_fixture_admin_and_singleton_tenant_db_user()
+    sys: System = next(api_fixture)
+    sys.reset_state()
+    api = sys.instance(ServerAPI)
+    api.heartbeat()
+    client = Client.from_system(sys)
+
+    run_state_machine_as_test(
+        lambda: SingletonTenantDatabaseCollectionStateMachine(client)
+    )  # type: ignore
