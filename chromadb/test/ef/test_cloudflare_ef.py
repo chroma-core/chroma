c42287255ab75f714b1b4733ad8d58585303f394
Trayan Azarov <trayan.azarov@amikos.tech>
2024-04-08 09:15:34 +0300

    chore: Test cleanup

--- chromadb/test/ef/test_cloudflare_ef.py
+++ chromadb/test/ef/test_cloudflare_ef.py
@@ -5,9 +5,11 @@ import pytest
 from chromadb.utils.embedding_functions import CloudflareWorkersAIEmbeddingFunction
 
 
+@pytest.mark.skipif(
+    "CF_API_TOKEN" not in os.environ,
+    reason="CF_API_TOKEN and CF_ACCOUNT_ID not set, skipping test.",
+)
 def test_cf_ef_token_and_account() -> None:
-    if "CF_API_TOKEN" not in os.environ or "CF_ACCOUNT_ID" not in os.environ:
-        pytest.skip("CF_API_TOKEN and CF_ACCOUNT_ID not set")
     ef = CloudflareWorkersAIEmbeddingFunction(
         api_token=os.environ.get("CF_API_TOKEN", ""),
         account_id=os.environ.get("CF_ACCOUNT_ID"),
@@ -18,9 +20,11 @@ def test_cf_ef_token_and_account() -> None:
     assert len(embeddings[0]) > 0
 
 
+@pytest.mark.skipif(
+    "CF_API_TOKEN" not in os.environ,
+    reason="CF_API_TOKEN and CF_ACCOUNT_ID not set, skipping test.",
+)
 def test_cf_ef_gateway() -> None:
-    if "CF_API_TOKEN" not in os.environ or "CF_GATEWAY_ENDPOINT" not in os.environ:
-        pytest.skip("CF_API_TOKEN and CF_GATEWAY_ENDPOINT not set")
     ef = CloudflareWorkersAIEmbeddingFunction(
         api_token=os.environ.get("CF_API_TOKEN", ""),
         gateway_url=os.environ.get("CF_GATEWAY_ENDPOINT"),
@@ -31,27 +35,32 @@ def test_cf_ef_gateway() -> None:
     assert len(embeddings[0]) > 0
 
 
+@pytest.mark.skipif(
+    "CF_API_TOKEN" not in os.environ,
+    reason="CF_API_TOKEN and CF_ACCOUNT_ID not set, skipping test.",
+)
 def test_cf_ef_large_batch() -> None:
-    if "CF_API_TOKEN" not in os.environ:
-        pytest.skip("CF_API_TOKEN not set, not going to test Cloudflare EF.")
-
     ef = CloudflareWorkersAIEmbeddingFunction(api_token="dummy", account_id="dummy")
     with pytest.raises(ValueError, match="Batch too large"):
         ef(["test doc"] * 101)
 
 
+@pytest.mark.skipif(
+    "CF_API_TOKEN" not in os.environ,
+    reason="CF_API_TOKEN and CF_ACCOUNT_ID not set, skipping test.",
+)
 def test_cf_ef_missing_account_or_gateway() -> None:
-    if "CF_API_TOKEN" not in os.environ:
-        pytest.skip("CF_API_TOKEN not set, not going to test Cloudflare EF.")
     with pytest.raises(
         ValueError, match="Please provide either an account_id or a gateway_url"
     ):
         CloudflareWorkersAIEmbeddingFunction(api_token="dummy")
 
 
+@pytest.mark.skipif(
+    "CF_API_TOKEN" not in os.environ,
+    reason="CF_API_TOKEN and CF_ACCOUNT_ID not set, skipping test.",
+)
 def test_cf_ef_with_account_or_gateway() -> None:
-    if "CF_API_TOKEN" not in os.environ:
-        pytest.skip("CF_API_TOKEN not set, not going to test Cloudflare EF.")
     with pytest.raises(
         ValueError,
         match="Please provide either an account_id or a gateway_url, not both",
