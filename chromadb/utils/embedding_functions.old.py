2632601490144c761a0333a1821beac8e70767b6
David Reguera <birrick@gmail.com>
2024-04-13 12:08:33 +0200

    1965 - Move over `Text2VecEmbeddingFunction`

--- chromadb/utils/embedding_functions.old.py
+++ chromadb/utils/embedding_functions.old.py
@@ -39,22 +39,6 @@ except ImportError:
 logger = logging.getLogger(__name__)
 
 
-class Text2VecEmbeddingFunction(EmbeddingFunction[Documents]):
-    def __init__(self, model_name: str = "shibing624/text2vec-base-chinese"):
-        try:
-            from text2vec import SentenceModel
-        except ImportError:
-            raise ValueError(
-                "The text2vec python package is not installed. Please install it with `pip install text2vec`"
-            )
-        self._model = SentenceModel(model_name_or_path=model_name)
-
-    def __call__(self, input: Documents) -> Embeddings:
-        return cast(
-            Embeddings, self._model.encode(list(input), convert_to_numpy=True).tolist()
-        )  # noqa E501
-
-
 def DefaultEmbeddingFunction() -> Optional[EmbeddingFunction[Documents]]:
     if is_thin_client:
         return None
