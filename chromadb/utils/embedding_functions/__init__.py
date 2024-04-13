0196264d24866e3fbe4458688e9224f9f61715dd
David Reguera <birrick@gmail.com>
2024-04-13 12:04:30 +0200

    1965 - Move over `OpenAIEmbeddingFunction`

--- chromadb/utils/embedding_functions/__init__.py
+++ chromadb/utils/embedding_functions/__init__.py
@@ -9,3 +9,4 @@ from chromadb.utils.embedding_functions.jina_embedding_function import JinaEmbed
 from chromadb.utils.embedding_functions.ollama_embedding_function import OllamaEmbeddingFunction
 from chromadb.utils.embedding_functions.onnx_mini_lm_l6_v2 import ONNXMiniLM_L6_V2, _verify_sha256
 from chromadb.utils.embedding_functions.open_clip_embedding_function import OpenCLIPEmbeddingFunction
+from chromadb.utils.embedding_functions.openai_embedding_function import OpenAIEmbeddingFunction
