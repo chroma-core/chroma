8f08d60ebdfbe8aa7f6ef37c03a6ca1ffceb6a0e
David Reguera <birrick@gmail.com>
2024-04-20 12:12:52 +0200

    1965 - Lint onnx embedding function

--- chromadb/utils/embedding_functions/onnx_mini_lm_l6_v2.py
+++ chromadb/utils/embedding_functions/onnx_mini_lm_l6_v2.py
@@ -1,30 +1,22 @@
 import hashlib
 import logging
-from functools import cached_property
-
-from tenacity import stop_after_attempt, wait_random, retry, retry_if_exception
-
-from chromadb.api.types import (
-    Documents,
-    EmbeddingFunction,
-    Embeddings,
-)
-
-from pathlib import Path
 import os
 import tarfile
-import requests
-from typing import TYPE_CHECKING, List, Optional, cast
+from functools import cached_property
+from pathlib import Path
+from typing import List, Optional, cast
+
 import numpy as np
 import numpy.typing as npt
-import importlib
+import requests
+from onnxruntime import InferenceSession, get_available_providers, SessionOptions
+from tenacity import retry, retry_if_exception, stop_after_attempt, wait_random
+from tokenizers import Tokenizer
+from tqdm import tqdm
+
+from chromadb.api.types import Documents, EmbeddingFunction, Embeddings
 
 logger = logging.getLogger(__name__)
-
-
-if TYPE_CHECKING:
-    from onnxruntime import InferenceSession
-    from tokenizers import Tokenizer
 
 
 def _verify_sha256(fname: str, expected_sha256: str) -> bool:
@@ -35,6 +27,7 @@ def _verify_sha256(fname: str, expected_sha256: str) -> bool:
             sha256_hash.update(byte_block)
 
     return sha256_hash.hexdigest() == expected_sha256
+
 
 # In order to remove dependencies on sentence-transformers, which in turn depends on
 # pytorch and sentence-piece we have created a default ONNX embedding function that
@@ -67,40 +60,27 @@ class ONNXMiniLM_L6_V2(EmbeddingFunction[Documents]):
         ):
             raise ValueError("Preferred providers must be unique")
         self._preferred_providers = preferred_providers
-        try:
-            # Equivalent to import onnxruntime
-            self.ort = importlib.import_module("onnxruntime")
-        except ImportError:
-            raise ValueError(
-                "The onnxruntime python package is not installed. Please install it with `pip install onnxruntime`"
-            )
-        try:
-            # Equivalent to from tokenizers import Tokenizer
-            self.Tokenizer = importlib.import_module("tokenizers").Tokenizer
-        except ImportError:
-            raise ValueError(
-                "The tokenizers python package is not installed. Please install it with `pip install tokenizers`"
-            )
-        try:
-            # Equivalent to from tqdm import tqdm
-            self.tqdm = importlib.import_module("tqdm").tqdm
-        except ImportError:
-            raise ValueError(
-                "The tqdm python package is not installed. Please install it with `pip install tqdm`"
-            )
 
     # Borrowed from https://gist.github.com/yanqd0/c13ed29e29432e3cf3e7c38467f42f51
     # Download with tqdm to preserve the sentence-transformers experience
-    @retry(
+    @retry(  # type: ignore
         reraise=True,
         stop=stop_after_attempt(3),
         wait=wait_random(min=1, max=3),
         retry=retry_if_exception(lambda e: "does not match expected SHA256" in str(e)),
     )
     def _download(self, url: str, fname: str, chunk_size: int = 1024) -> None:
+        """
+        Download the onnx model from the URL and save it to the file path.
+
+        About ignored types:
+        tenacity.retry decorator is a bit convoluted when it comes to type annotations
+        which makes mypy unhappy. If some smart folk knows how to fix this in an
+        elegant way, please do so.
+        """
         resp = requests.get(url, stream=True)
         total = int(resp.headers.get("content-length", 0))
-        with open(fname, "wb") as file, self.tqdm(
+        with open(fname, "wb") as file, tqdm(
             desc=str(fname),
             total=total,
             unit="iB",
@@ -119,15 +99,14 @@ class ONNXMiniLM_L6_V2(EmbeddingFunction[Documents]):
 
     # Use pytorches default epsilon for division by zero
     # https://pytorch.org/docs/stable/generated/torch.nn.functional.normalize.html
-    def _normalize(self, v: npt.NDArray) -> npt.NDArray:
+    def _normalize(self, v: npt.NDArray[np.float32]) -> npt.NDArray[np.float32]:
         norm = np.linalg.norm(v, axis=1)
         norm[norm == 0] = 1e-12
-        return cast(npt.NDArray, v / norm[:, np.newaxis])
+        return cast(npt.NDArray[np.float32], v / norm[:, np.newaxis])
 
-    def _forward(self, documents: List[str], batch_size: int = 32) -> npt.NDArray:
-        # We need to cast to the correct type because the type checker doesn't know that init_model_and_tokenizer will set the values
-        self.tokenizer = cast(self.Tokenizer, self.tokenizer)
-        self.model = cast(self.ort.InferenceSession, self.model)
+    def _forward(
+        self, documents: List[str], batch_size: int = 32
+    ) -> npt.NDArray[np.float32]:
         all_embeddings = []
         for i in range(0, len(documents), batch_size):
             batch = documents[i : i + batch_size]
@@ -156,8 +135,8 @@ class ONNXMiniLM_L6_V2(EmbeddingFunction[Documents]):
         return np.concatenate(all_embeddings)
 
     @cached_property
-    def tokenizer(self) -> "Tokenizer":
-        tokenizer = self.Tokenizer.from_file(
+    def tokenizer(self) -> Tokenizer:
+        tokenizer = Tokenizer.from_file(
             os.path.join(
                 self.DOWNLOAD_PATH, self.EXTRACTED_FOLDER_NAME, "tokenizer.json"
             )
@@ -169,26 +148,26 @@ class ONNXMiniLM_L6_V2(EmbeddingFunction[Documents]):
         return tokenizer
 
     @cached_property
-    def model(self) -> "InferenceSession":
+    def model(self) -> InferenceSession:
         if self._preferred_providers is None or len(self._preferred_providers) == 0:
-            if len(self.ort.get_available_providers()) > 0:
+            if len(get_available_providers()) > 0:
                 logger.debug(
                     f"WARNING: No ONNX providers provided, defaulting to available providers: "
-                    f"{self.ort.get_available_providers()}"
+                    f"{get_available_providers()}"
                 )
-            self._preferred_providers = self.ort.get_available_providers()
+            self._preferred_providers = get_available_providers()
         elif not set(self._preferred_providers).issubset(
-            set(self.ort.get_available_providers())
+            set(get_available_providers())
         ):
             raise ValueError(
-                f"Preferred providers must be subset of available providers: {self.ort.get_available_providers()}"
+                f"Preferred providers must be subset of available providers: {get_available_providers()}"
             )
 
         # Suppress onnxruntime warnings. This produces logspew, mainly when onnx tries to use CoreML, which doesn't fit this model.
-        so = self.ort.SessionOptions()
+        so = SessionOptions()
         so.log_severity_level = 3
 
-        return self.ort.InferenceSession(
+        return InferenceSession(
             os.path.join(self.DOWNLOAD_PATH, self.EXTRACTED_FOLDER_NAME, "model.onnx"),
             # Since 1.9 onnyx runtime requires providers to be specified when there are multiple available - https://onnxruntime.ai/docs/api/python/api_summary.html
             # This is probably not ideal but will improve DX as no exceptions will be raised in multi-provider envs
