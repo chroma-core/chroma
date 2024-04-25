475f500f14da3d42bbd15dccdd7c23f902d1e9ab
nicolasgere <nicolas.gerelm@gmail.com>
2024-04-25 16:13:57 -0700

    enable purging with leader election api k8s

--- go/cmd/logservice/main.go
+++ go/cmd/logservice/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"github.com/chroma-core/chroma/go/pkg/log/configuration"
+	"github.com/chroma-core/chroma/go/pkg/log/purging"
 	"github.com/chroma-core/chroma/go/pkg/log/repository"
 	"github.com/chroma-core/chroma/go/pkg/log/server"
 	"github.com/chroma-core/chroma/go/pkg/proto/logservicepb"
@@ -16,6 +17,88 @@ import (
 	"google.golang.org/grpc"
 	"net"
 )
+
+//func runPurging(lg *repository.LogRepository) (err error) {
+//	defer func() {
+//		if err != nil {
+//			log.Error("failed to run purging", zap.Error(err))
+//
+//		}
+//	}()
+//	log.Info("starting purging")
+//	ctx := context.Background()
+//	podName, _ := os.LookupEnv("POD_NAME")
+//	fmt.Println(podName)
+//	namespace, _ := os.LookupEnv("POD_NAMESPACE")
+//	fmt.Println(namespace)
+//	lockName := "log-purging-lock"
+//	config, err := rest.InClusterConfig()
+//	if err != nil {
+//		return err
+//	}
+//	var client *clientset.Clientset
+//	client, err = clientset.NewForConfig(config)
+//	if err != nil {
+//		return err
+//	}
+//	lock := &resourcelock.LeaseLock{
+//		LeaseMeta: metav1.ObjectMeta{
+//			Name:      lockName,
+//			Namespace: namespace,
+//		},
+//		Client: client.CoordinationV1(),
+//		LockConfig: resourcelock.ResourceLockConfig{
+//			Identity: podName,
+//		},
+//	}
+//	var l *leaderelection.LeaderElector
+//	l, err = leaderelection.NewLeaderElector(leaderelection.LeaderElectionConfig{
+//		Lock:            lock,
+//		ReleaseOnCancel: true,
+//		LeaseDuration:   15 * time.Second,
+//		RenewDeadline:   10 * time.Second,
+//		RetryPeriod:     2 * time.Second,
+//
+//		Callbacks: leaderelection.LeaderCallbacks{
+//			OnStartedLeading: func(ctx context.Context) {
+//				log.Info("started leading")
+//				ticker := time.NewTicker(10 * time.Second)
+//				log.Info("Start loop for purging")
+//				for {
+//					select {
+//					case <-ctx.Done():
+//						return
+//
+//					case <-ticker.C:
+//						{
+//							log.Info("checking leader status")
+//							if l.IsLeader() {
+//								log.Info("leader is active")
+//								err = lg.PurgeRecords(ctx)
+//								if err != nil {
+//									log.Error("failed to purge records", zap.Error(err))
+//									continue
+//								}
+//								log.Info("purged records")
+//							} else {
+//								log.Info("leader is inactive")
+//							}
+//						}
+//					}
+//				}
+//			},
+//			OnStoppedLeading: func() {
+//				log.Info("stop leading")
+//			},
+//		},
+//	})
+//	if err != nil {
+//		return
+//	}
+//	l.Run(ctx)
+//
+//	return
+//}
 
 func main() {
 	ctx := context.Background()
@@ -49,6 +132,7 @@ func main() {
 	s := grpc.NewServer(grpc.UnaryInterceptor(otel.ServerGrpcInterceptor))
 	logservicepb.RegisterLogServiceServer(s, server)
 	log.Info("log service started", zap.String("address", listener.Addr().String()))
+	go purging.RunPurging(ctx, lr)
 	if err := s.Serve(listener); err != nil {
 		log.Fatal("failed to serve", zap.Error(err))
 	}
