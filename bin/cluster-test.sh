0a3af05803f1c32200be7dba67bf415fc4d32738
hammadb <hammad@trychroma.com>
2024-04-29 23:30:35 -0700

    remove unused forwards

--- bin/cluster-test.sh
+++ bin/cluster-test.sh
@@ -9,8 +9,4 @@ export CHROMA_COORDINATOR_HOST=localhost
 echo "Chroma Server is running at port $CHROMA_SERVER_HOST"
 echo "Chroma Coordinator is running at port $CHROMA_COORDINATOR_HOST"
 
-kubectl -n chroma port-forward svc/sysdb-lb 50051:50051 &
-kubectl -n chroma port-forward svc/logservice-lb 50052:50051 &
-kubectl -n chroma port-forward svc/frontend-service 8000:8000 &
-
 "$@"
