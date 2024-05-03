d11cd49942f7929b7ab8ec3d0f9cf0314f7c3f22
Aki Ariga <chezou+github@gmail.com>
2024-05-02 22:04:43 -0700

    Apply prettier

--- clients/js/src/embeddings/AmazonBedrockEmbeddingFunction.ts
+++ clients/js/src/embeddings/AmazonBedrockEmbeddingFunction.ts
@@ -8,7 +8,7 @@ export class AmazonBedrockEmbeddingFunction implements IEmbeddingFunction {
   private amazonBedrockApi?: any;
   private invokeCommand?: any;
 
-  constructor({ config, model }: { config: any; model?: string}) {
+  constructor({ config, model }: { config: any; model?: string }) {
     this.configuration = config;
     if (!this.configuration.region) {
       this.configuration.region = "us-east-1";
@@ -22,14 +22,14 @@ export class AmazonBedrockEmbeddingFunction implements IEmbeddingFunction {
       // eslint-disable-next-line global-require,import/no-extraneous-dependencies
       const { amazonBedrock } = await AmazonBedrockEmbeddingFunction.import();
       amazonBedrockApi = new amazonBedrock.BedrockRuntimeClient(
-        this.configuration
+        this.configuration,
       );
       this.invokeCommand = amazonBedrock.InvokeModelCommand;
     } catch (_a) {
       // @ts-ignore
       if (_a.code === "MODULE_NOT_FOUND") {
         throw new Error(
-          "Please install the @aws-sdk/client-bedrock-runtime package to use the AmazonBedrockEmbeddingFunction, `npm install -S @aws-sdk/client-bedrock-runtime`"
+          "Please install the @aws-sdk/client-bedrock-runtime package to use the AmazonBedrockEmbeddingFunction, `npm install -S @aws-sdk/client-bedrock-runtime`",
         );
       }
       throw _a; // Re-throw other errors
@@ -41,19 +41,20 @@ export class AmazonBedrockEmbeddingFunction implements IEmbeddingFunction {
     await this.loadClient();
     const td = new TextDecoder();
     const embeddings: number[][] = [];
-    return Promise.all(texts.map(async text => {
-      const input = {
-        modelId: this.model,
-        contentType: "application/json",
-        accept: "application/json",
-        body: JSON.stringify({ inputText: text }),
-      }
-      const command = new this.invokeCommand(input);
-      const response = await this.amazonBedrockApi.send(command);
-      const parsedBody = JSON.parse(td.decode(response.body));
-      return parsedBody.embedding;
-    }
-    ));
+    return Promise.all(
+      texts.map(async (text) => {
+        const input = {
+          modelId: this.model,
+          contentType: "application/json",
+          accept: "application/json",
+          body: JSON.stringify({ inputText: text }),
+        };
+        const command = new this.invokeCommand(input);
+        const response = await this.amazonBedrockApi.send(command);
+        const parsedBody = JSON.parse(td.decode(response.body));
+        return parsedBody.embedding;
+      }),
+    );
   }
 
   /** @ignore */
@@ -63,11 +64,11 @@ export class AmazonBedrockEmbeddingFunction implements IEmbeddingFunction {
   }> {
     try {
       // @ts-ignore
-      const amazonBedrock  = await import("@aws-sdk/client-bedrock-runtime");
+      const amazonBedrock = await import("@aws-sdk/client-bedrock-runtime");
       return { amazonBedrock };
     } catch (_a) {
       throw new Error(
-        "Please install the @aws-sdk/client-bedrock-runtime package to use the AmazonBedrockEmbeddingFunction, `npm install -S @aws-sdk/client-bedrock-runtime`"
+        "Please install the @aws-sdk/client-bedrock-runtime package to use the AmazonBedrockEmbeddingFunction, `npm install -S @aws-sdk/client-bedrock-runtime`",
       );
     }
   }
--- clients/js/test/embeddings/aws.bedrock.test.ts
+++ clients/js/test/embeddings/aws.bedrock.test.ts
@@ -1,32 +1,35 @@
-import { expect, test } from '@jest/globals';
-import chroma from '../initClient'
-import { DOCUMENTS, IDS } from '../data';
+import { expect, test } from "@jest/globals";
+import chroma from "../initClient";
+import { DOCUMENTS, IDS } from "../data";
 import { IncludeEnum } from "../../src/types";
 import { AmazonBedrockEmbeddingFunction } from "../../src/embeddings/AmazonBedrockEmbeddingFunction";
 
 if (!process.env.AWS_ACCESS_KEY_ID || !process.env.AWS_SECRET_ACCESS_KEY) {
-  test.skip("it should add Amazon Bedrock embeddings", async () => {
-  });
+  test.skip("it should add Amazon Bedrock embeddings", async () => {});
 } else {
   test("it should add Amazon Bedrock embeddings", async () => {
     await chroma.reset();
-    const embedder = new AmazonBedrockEmbeddingFunction({ config: {
-      credentials: {
-        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-        sessionToken: process.env.AWS_SESSION_TOKEN,
+    const embedder = new AmazonBedrockEmbeddingFunction({
+      config: {
+        credentials: {
+          accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+          secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+          sessionToken: process.env.AWS_SESSION_TOKEN,
+        },
+        region: "us-east-1",
       },
-      region: "us-east-1",
-    }})
-    const collection = await chroma.createCollection({ name: "test", embeddingFunction: embedder });
+    });
+    const collection = await chroma.createCollection({
+      name: "test",
+      embeddingFunction: embedder,
+    });
     const embeddings = await embedder.generate(DOCUMENTS);
     await collection.add({ ids: IDS, embeddings: embeddings });
     const count = await collection.count();
     expect(count).toBe(3);
     var res = await collection.get({
-      ids: IDS, include: [
-        IncludeEnum.Embeddings,
-      ]
+      ids: IDS,
+      include: [IncludeEnum.Embeddings],
     });
     expect(res.embeddings).toEqual(embeddings); // reverse because of the order of the ids
   });
