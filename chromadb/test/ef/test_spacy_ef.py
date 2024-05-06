1a17390ccdb7fef812c74346354e0bd17fd50f2f
Vishnu Nandakumar <38393302+Vishnunkumar@users.noreply.github.com>
2024-05-06 05:58:44 +0000

    tests: refactored test cases as per comments.

--- chromadb/test/ef/test_spacy_ef.py
+++ chromadb/test/ef/test_spacy_ef.py
@@ -1,9 +1,11 @@
 import pytest
+import numpy
 from chromadb.utils.embedding_functions import SpacyEmbeddingFunction
 
 input_list = ["great work by the guy", "Super man is that guy"]
 model_name = "en_core_web_md"
 unknown_model = "unknown_model"
+spacy = pytest.importorskip("spacy", reason="spacy not installed")
 
 
 def test_spacyembeddingfunction_isnotnone_wheninputisnotnone():
@@ -13,14 +15,18 @@ def test_spacyembeddingfunction_isnotnone_wheninputisnotnone():
 
 def test_spacyembddingfunction_throwserror_whenmodel_notfound():
     with pytest.raises(ValueError,
-                       match="""spacy models are not downloaded yet, please download them using `spacy download model_name`, Please checkout
-                for the list of models from: https://spacy.io/usage/models. By default the module will load en_core_web_lg
-                model as it optimizes accuracy and has embeddings in-built, please download and load with `en_core_web_md` 
-                if you want to priortize efficiency over accuracy, the same logic applies for models from other languages also.
-                language_web_core_sm and language_web_core_trf doesn't have pre-trained embeddings."""):
+                       match=r"""spacy models are not downloaded yet, please download them using `spacy download model_name`, Please checkout
+                for the list of models from: https://spacy.io/usage/models."""):
         SpacyEmbeddingFunction(unknown_model)
 
 
 def test_spacyembddingfunction_isembedding_wheninput_islist():
     spacy_emb_fn = SpacyEmbeddingFunction(model_name)
     assert type(spacy_emb_fn(input_list)) is list
+
+
+def test_spacyembeddingfunction_returnslistoflistsofloats():
+    spacy_emb_fn = SpacyEmbeddingFunction(model_name)
+    expected_output = spacy_emb_fn(input_list)
+    assert type(expected_output[0]) is list
+    assert type(expected_output[0][0]) is numpy.float64
