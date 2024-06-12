c94b7f80ccdee01ebc56dbdc7c34a9bfb7e6fac4
hammadb <hammad@trychroma.com>
2024-06-12 09:27:20 -0700

    wip

--- rust/worker/src/compactor/config.rs
+++ rust/worker/src/compactor/config.rs
@@ -5,4 +5,5 @@ pub(crate) struct CompactorConfig {
     pub(crate) compaction_manager_queue_size: usize,
     pub(crate) max_concurrent_jobs: usize,
     pub(crate) compaction_interval_sec: u64,
+    pub(crate) min_compaction_size: usize,
 }
