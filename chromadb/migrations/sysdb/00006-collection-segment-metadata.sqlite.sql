18dfd242a15373d347c4c58c6db677df8f2c358c
Sanket Kedia <sanket@trychroma.com>
2024-06-13 09:54:35 -0700

    Fix migration

--- chromadb/migrations/sysdb/00006-collection-segment-metadata.sqlite.sql
+++ chromadb/migrations/sysdb/00006-collection-segment-metadata.sqlite.sql
@@ -2,5 +2,5 @@
 -- involve creating a new table and copying the data over. It is over kill with adding
 -- a boolean type column. The application write to the table needs to ensure the data
 -- integrity.
-ALTER TABLE collection_metadata ADD COLUMN bool_value INTEGER
-ALTER TABLE segment_metadata ADD COLUMN bool_value INTEGER
+ALTER TABLE collection_metadata ADD COLUMN bool_value INTEGER;
+ALTER TABLE segment_metadata ADD COLUMN bool_value INTEGER;
