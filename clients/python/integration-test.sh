022393a9fa9679bf8a57024d8cfab8003aaf58fc
Max Isom <hi@maxisom.me>
2024-06-14 10:43:02 -0700

    Combine thin client test jobs

--- clients/python/integration-test.sh
+++ clients/python/integration-test.sh
@@ -18,7 +18,7 @@ trap cleanup EXIT
 docker compose -f docker-compose.test.yml up --build -d
 
 export CHROMA_INTEGRATION_TEST_ONLY=1
-export CHROMA_API_IMPL="${CHROMA_API_IMPL:-chromadb.api.fastapi.FastAPI}"
+export CHROMA_API_IMPL="chromadb.api.fastapi.FastAPI"
 export CHROMA_SERVER_HOST=localhost
 export CHROMA_SERVER_HTTP_PORT=8000
 export CHROMA_SERVER_NOFILE=65535
@@ -28,4 +28,9 @@ echo testing: python -m pytest 'chromadb/test/property/' --ignore-glob 'chromadb
 # Copy the thin client flag script in place, uvicorn takes a while to startup inside docker
 sleep 5
 cp "$is_thin_client_py" "$is_thin_client_target"
+python -m pytest 'chromadb/test/property/' --ignore-glob 'chromadb/test/property/*persist.py' --ignore 'chromadb/test/property/test_collections_with_database_tenant_overwrite.py'
+
+# Test async client
+export CHROMA_API_IMPL="chromadb.api.async_fastapi.AsyncFastAPI"
+
 python -m pytest 'chromadb/test/property/' --ignore-glob 'chromadb/test/property/*persist.py' --ignore 'chromadb/test/property/test_collections_with_database_tenant_overwrite.py'
