6cffafefcaf55c1f102b1a4200bd49b2242cce30
Trayan Azarov <trayan.azarov@amikos.tech>
2024-06-21 15:59:39 +0200

    test: Rebase + adding test

--- chromadb/utils/embedding_functions/cohere_embedding_function.py
+++ chromadb/utils/embedding_functions/cohere_embedding_function.py
@@ -1,4 +1,5 @@
 import logging
+from typing import Any
 
 from chromadb.api.types import Documents, EmbeddingFunction, Embeddings
 
@@ -17,11 +18,22 @@ class CohereEmbeddingFunction(EmbeddingFunction[Documents]):
         self._client = cohere.Client(api_key)
         self._model_name = model_name
 
+    @staticmethod
+    def _convert_v5_embeddings(embeddings_response: Any) -> Embeddings:
+        try:
+            # works with v5 - does't work with typed returned e.g. int/uint etc.
+            from cohere.types.embed_response import (  # noqa: F401
+                EmbedResponse_EmbeddingsFloats,
+            )
+
+            return embeddings_response.embeddings
+        except ImportError:
+            # work with v4
+            return [embeddings for embeddings in embeddings_response]
+
     def __call__(self, input: Documents) -> Embeddings:
         # Call Cohere Embedding API for each document.
-        return [
-            embeddings
-            for embeddings in self._client.embed(
-                texts=input, model=self._model_name, input_type="search_document"
-            )
-        ]
+        embeddings_response = self._client.embed(
+            texts=input, model=self._model_name, input_type="search_document"
+        )
+        return self._convert_v5_embeddings(embeddings_response)
