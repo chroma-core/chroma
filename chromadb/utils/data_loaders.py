b1e60fcc42cdce97e9a5ad4cb801515391c0f597
AN <shriarunnanda@gmail.com>
2024-06-25 21:07:35 +0530

    restored class ImageLoader to the original definition. Added new class ImageURLoader to work with remote URLs

--- chromadb/utils/data_loaders.py
+++ chromadb/utils/data_loaders.py
@@ -18,17 +18,38 @@ class ImageLoader(DataLoader[List[Optional[Image]]]):
                 "The PIL python package is not installed. Please install it with `pip install pillow`"
             )
 
-    #    def _load_image(self, uri: Optional[URI]) -> Optional[Image]:
-    #        return np.array(self._PILImage.open(uri)) if uri is not None else None
-
     def _load_image(self, uri: Optional[URI]) -> Optional[Image]:
-        if uri is not None:
-            if uri[0:4] == "http":
-                return np.asarray(self._PILImage.open(BytesIO(httpx.get(uri).content)))
-            else:
-                return np.array(self._PILImage.open(uri))
-        else:
-            return None
+        return np.array(self._PILImage.open(uri)) if uri is not None else None
+
+    def __call__(self, uris: Sequence[Optional[URI]]) -> List[Optional[Image]]:
+        with ThreadPoolExecutor(max_workers=self._max_workers) as executor:
+            return list(executor.map(self._load_image, uris))
+
+
+class ImageURLoader(DataLoader[List[Optional[Image]]]):
+    def __init__(
+        self,
+        max_workers: int = multiprocessing.cpu_count(),
+        username: Optional[str] = None,
+        password: Optional[str] = None,
+    ) -> None:
+        try:
+            self._PILImage = importlib.import_module("PIL.Image")
+            self._max_workers = max_workers
+        except ImportError:
+            raise ValueError(
+                "The PIL python package is not installed. Please install it with `pip install pillow`"
+            )
+        self._username = username
+        self._password = password
+
+    def _load_image(self, url: Optional[URI]) -> Optional[Image]:
+        auth = httpx.DigestAuth(self._username, self._password)
+        return (
+            np.asarray(self._PILImage.open(BytesIO(httpx.get(url, auth=auth).content)))
+            if url is not None
+            else None
+        )
 
     def __call__(self, uris: Sequence[Optional[URI]]) -> List[Optional[Image]]:
         with ThreadPoolExecutor(max_workers=self._max_workers) as executor:
