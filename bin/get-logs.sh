7487c07e1ec984a2636123af622263d3d531da46
Sanket Kedia <sanket@trychroma.com>
2024-07-05 18:38:21 -0700

    permission denied debug

--- bin/get-logs.sh
+++ bin/get-logs.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
+set -e
 
 # Hardcoded namespace
 NAMESPACE=chroma
+echo "Namespace: $NAMESPACE"
 
 # Check if the test-name is provided as an argument
 if [ -z "$1" ]; then
@@ -13,13 +15,17 @@ TEST_FILE_PATH=$1
 
 # Extract the test file name from the path and remove the ".py" extension
 TEST_NAME=$(basename "$TEST_FILE_PATH" .py)
+echo "Test name: $TEST_NAME"
 
 # Create a directory with the test name to store logs
 LOGS_DIR="./logs/${TEST_NAME}"
+echo "Logs directory: $LOGS_DIR"
 mkdir -p "$LOGS_DIR"
+echo "Created logs dir: $LOGS_DIR"
 
 # Get the list of all pods in the namespace
 PODS=$(kubectl get pods -n $NAMESPACE -o jsonpath='{.items[*].metadata.name}')
+echo "Got all the pods: $PODS"
 
 # Iterate over each pod and get the logs
 for POD in $PODS; do
@@ -35,4 +41,4 @@ zip -r "${TEST_NAME}_logs.zip" "$LOGS_DIR"
 echo "Logs have been zipped to ${TEST_NAME}_logs.zip"
 
 # Output the path to the zip file
-echo "$(pwd)/${TEST_NAME}_logs.zip"
+echo "logs_zip_path=$(pwd)/${TEST_NAME}_logs.zip" >> $GITHUB_OUTPUT
