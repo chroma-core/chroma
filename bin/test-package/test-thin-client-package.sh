chroma-core/chroma

commit f132a89990cf122bdf2639288fac3d1b3d9bf42c

[BUG]: Thin client imports (#2466)
## Description of changes

Closes #2459

*Summarize the changes made by this PR.*
 - Improvements & Bug fixes
- Fixed imports for onnxruntime and auth module to avoid import failures
for the thin client

## Test plan
*How are these changes tested?*

- [x] Tests pass locally with `pytest` for python, `yarn test` for js,
`cargo test` for rust

## Documentation Changes

.github/workflows/release-chromadb.yml
@@ -163,6 +163,8 @@ jobs:
           python-version: '3.12'
       - name: Build Client
         run: ./clients/python/build_python_thin_client.sh
+      - name: Test Client Package
+        run: bin/test-package.sh dist/*.tar.gz
       - name: Install setuptools_scm
         run: python -m pip install setuptools_scm
       - name: Publish to Test PyPI

chromadb/auth/__init__.py
@@ -11,7 +11,6 @@ from typing import (
     TypeVar,
 )
 from dataclasses import dataclass
-from starlette.datastructures import Headers
 
 from pydantic import SecretStr
 
@@ -88,7 +87,7 @@ class ServerAuthenticationProvider(Component):
         )
 
     @abstractmethod
-    def authenticate_or_raise(self, headers: Headers) -> UserIdentity:
+    def authenticate_or_raise(self, headers: Dict[str, str]) -> UserIdentity:
         pass
 
     def ignore_operation(self, verb: str, path: str) -> bool:

chromadb/auth/basic_authn/__init__.py
@@ -23,8 +23,10 @@ from chromadb.telemetry.opentelemetry import (
     OpenTelemetryGranularity,
     trace_method,
 )
-from starlette.datastructures import Headers
+
+
 from typing import Dict
+
 
 logger = logging.getLogger(__name__)
 
@@ -100,11 +102,11 @@ class BasicAuthenticationServerProvider(ServerAuthenticationProvider):
         "BasicAuthenticationServerProvider.authenticate", OpenTelemetryGranularity.ALL
     )
     @override
-    def authenticate_or_raise(self, headers: Headers) -> UserIdentity:
+    def authenticate_or_raise(self, headers: Dict[str, str]) -> UserIdentity:
         try:
-            if AUTHORIZATION_HEADER not in headers:
+            if AUTHORIZATION_HEADER.lower() not in headers.keys():
                 raise AuthError(AUTHORIZATION_HEADER + " header not found")
-            _auth_header = headers[AUTHORIZATION_HEADER]
+            _auth_header = headers[AUTHORIZATION_HEADER.lower()]
             _auth_header = re.sub(r"^Basic ", "", _auth_header)
             _auth_header = _auth_header.strip()
 

chromadb/auth/token_authn/__init__.py
@@ -5,10 +5,9 @@ import string
 import time
 import traceback
 from enum import Enum
-from starlette.datastructures import Headers
 from typing import cast, Dict, List, Optional, TypedDict, TypeVar
 
-from fastapi import HTTPException
+
 from overrides import override
 from pydantic import SecretStr
 import yaml
@@ -191,13 +190,13 @@ class TokenAuthenticationServerProvider(ServerAuthenticationProvider):
         "TokenAuthenticationServerProvider.authenticate", OpenTelemetryGranularity.ALL
     )
     @override
-    def authenticate_or_raise(self, headers: Headers) -> UserIdentity:
+    def authenticate_or_raise(self, headers: Dict[str, str]) -> UserIdentity:
         try:
-            if self._token_transport_header.value not in headers:
+            if self._token_transport_header.value.lower() not in headers.keys():
                 raise AuthError(
                     f"Authorization header '{self._token_transport_header.value}' not found"
                 )
-            token = headers[self._token_transport_header.value]
+            token = headers[self._token_transport_header.value.lower()]
             if self._token_transport_header == TokenTransportHeader.AUTHORIZATION:
                 if not token.startswith("Bearer "):
                     raise AuthError("Bearer not found in Authorization header")
@@ -232,4 +231,6 @@ class TokenAuthenticationServerProvider(ServerAuthenticationProvider):
         time.sleep(
             random.uniform(0.001, 0.005)
         )  # add some jitter to avoid timing attacks
+        from fastapi import HTTPException
+
         raise HTTPException(status_code=403, detail="Forbidden")

chromadb/server/fastapi/__init__.py
@@ -341,7 +341,7 @@ class FastAPI(Server):
         if not self.authn_provider:
             return (tenant, database)
 
-        user_identity = self.authn_provider.authenticate_or_raise(headers)
+        user_identity = self.authn_provider.authenticate_or_raise(dict(headers))
 
         (
             new_tenant,

chromadb/test/auth/test_base_class_behavior.py
@@ -2,7 +2,6 @@ import hypothesis.strategies as st
 
 from hypothesis import given, settings
 from overrides import override
-from starlette.datastructures import Headers
 from typing import Dict, List, Tuple
 
 from chromadb.api import ServerAPI
@@ -17,7 +16,7 @@ class DummyServerAuthenticationProvider(ServerAuthenticationProvider):
     """
 
     @override
-    def authenticate_or_raise(self, headers: Headers) -> UserIdentity:
+    def authenticate_or_raise(self, headers: Dict[str, str]) -> UserIdentity:
         return UserIdentity(user_id="test_user")
 
 

chromadb/utils/embedding_functions/onnx_mini_lm_l6_v2.py
@@ -10,9 +10,7 @@ from typing import List, Optional, cast
 import numpy as np
 import numpy.typing as npt
 import httpx
-from onnxruntime import InferenceSession, get_available_providers, SessionOptions
 from tenacity import retry, retry_if_exception, stop_after_attempt, wait_random
-from tokenizers import Tokenizer
 
 from chromadb.api.types import Documents, EmbeddingFunction, Embeddings
 
@@ -156,8 +154,8 @@ class ONNXMiniLM_L6_V2(EmbeddingFunction[Documents]):
         return np.concatenate(all_embeddings)
 
     @cached_property
-    def tokenizer(self) -> Tokenizer:
-        tokenizer = Tokenizer.from_file(
+    def tokenizer(self) -> "Tokenizer":  # noqa F821
+        tokenizer = self.Tokenizer.from_file(
             os.path.join(
                 self.DOWNLOAD_PATH, self.EXTRACTED_FOLDER_NAME, "tokenizer.json"
             )
@@ -169,26 +167,26 @@ class ONNXMiniLM_L6_V2(EmbeddingFunction[Documents]):
         return tokenizer
 
     @cached_property
-    def model(self) -> InferenceSession:
+    def model(self) -> "InferenceSession":  # noqa F821
         if self._preferred_providers is None or len(self._preferred_providers) == 0:
-            if len(get_available_providers()) > 0:
+            if len(self.ort.get_available_providers()) > 0:
                 logger.debug(
                     f"WARNING: No ONNX providers provided, defaulting to available providers: "
-                    f"{get_available_providers()}"
+                    f"{self.ort.get_available_providers()}"
                 )
-            self._preferred_providers = get_available_providers()
+            self._preferred_providers = self.ort.get_available_providers()
         elif not set(self._preferred_providers).issubset(
-            set(get_available_providers())
+            set(self.ort.get_available_providers())
         ):
             raise ValueError(
-                f"Preferred providers must be subset of available providers: {get_available_providers()}"
+                f"Preferred providers must be subset of available providers: {self.ort.get_available_providers()}"
             )
 
         # Suppress onnxruntime warnings. This produces logspew, mainly when onnx tries to use CoreML, which doesn't fit this model.
-        so = SessionOptions()
+        so = self.ort.SessionOptions()
         so.log_severity_level = 3
 
-        return InferenceSession(
+        return self.ort.InferenceSession(
             os.path.join(self.DOWNLOAD_PATH, self.EXTRACTED_FOLDER_NAME, "model.onnx"),
             # Since 1.9 onnyx runtime requires providers to be specified when there are multiple available - https://onnxruntime.ai/docs/api/python/api_summary.html
             # This is probably not ideal but will improve DX as no exceptions will be raised in multi-provider envs

commit 8b546be5e0da72cb10382ed84347dfa3aad12c4f

[BUG][TST] Fix thin client package test (#2497)
## Description of changes

*Summarize the changes made by this PR.*
 - Improvements & Bug fixes
	 - Fix thin client package bug introduced by #2466 by @tazarov 
- Do this as opposed to #2490 - as it is cleaner and more explicit.
Making it less brittle to changes.
 - New functionality
	 - ..

## Test plan
*How are these changes tested?*

- [ ] Tests pass locally with `pytest` for python, `yarn test` for js,
`cargo test` for rust

## Documentation Changes
*Are all docstrings for user-facing APIs updated if required? Do we need
to make documentation changes in the [docs
repository](https://github.com/chroma-core/docs)?*

.github/workflows/release-chromadb.yml
@@ -126,7 +126,7 @@ jobs:
       - name: Build Client
         run: python -m build
       - name: Test Client Package
-        run: bin/test-package.sh dist/*.tar.gz
+        run: bin/test-package/test-package.sh dist/*.tar.gz
       - name: Upload as artifact
         uses: actions/upload-artifact@v4
         with:
@@ -164,7 +164,7 @@ jobs:
       - name: Build Client
         run: ./clients/python/build_python_thin_client.sh
       - name: Test Client Package
-        run: bin/test-package.sh dist/*.tar.gz
+        run: bin/test-package/test-thin-client-package.sh dist/*.tar.gz
       - name: Install setuptools_scm
         run: python -m pip install setuptools_scm
       - name: Publish to Test PyPI

bin/test-package/install.sh
@@ -1,0 +1,24 @@
+#!/bin/bash
+
+pip_install_from_tarball() {
+    local tarball=$(readlink -f $1)
+    if [ -f "$tarball" ]; then
+        echo "Testing PIP package from tarball: $tarball"
+    else
+        echo "Could not find PIP package: $tarball"
+        return 1
+    fi
+
+    # Create temporary project dir
+    local dir=$(mktemp -d)
+
+    echo "Building python project dir at $dir ..."
+
+    cd $dir
+
+    python3 -m venv venv
+
+    source venv/bin/activate
+
+    pip install $tarball
+}

bin/test-package.sh
@@ -1,24 +1,0 @@
-#!/bin/bash
-
-# Verify PIP tarball
-tarball=$(readlink -f $1)
-if [ -f "$tarball" ]; then
-    echo "Testing PIP package from tarball: $tarball"
-else
-    echo "Could not find PIP package: $tarball"
-fi
-
-# Create temporary project dir
-dir=$(mktemp -d)
-
-echo "Building python project dir at $dir ..."
-
-cd $dir
-
-python3 -m venv venv
-
-source venv/bin/activate
-
-pip install $tarball
-
-python -c "import chromadb; api = chromadb.Client(); print(api.heartbeat())"

bin/test-package/test-package.sh
@@ -1,0 +1,8 @@
+#!/bin/bash
+# Sanity check to ensure package is installed correctly
+
+source ./install.sh
+
+pip_install_from_tarball $1
+
+python -c "import chromadb; api = chromadb.Client(); print(api.heartbeat())"

bin/test-package/test-thin-client-package.sh
@@ -1,0 +1,8 @@
+#!/bin/bash
+# Sanity check to ensure package is installed correctly
+
+source ./install.sh
+
+pip_install_from_tarball $1
+
+python -c "import chromadb; print(chromadb.__version__)"