2e5bff46ae90d96f177294ea6ac53dbccf43c689
Liquan Pei <liquanpei@gmail.com>
2024-07-16 00:45:40 -0700

    [ENH] Introduce stream abstraction and enable concurrency test for blockfile (#2454)
    ## Description of changes
    
    *Summarize the changes made by this PR.*
     - Improvements & Bug fixes
    	 - The tokio filesystem API will trigger panic if not invoked in the tokio runtime. However, the shuttle concurrency test has its runtime and can not properly run if tokio filesystem APIs are called. 
    	 - To fix the above issue, this PR introduces the stream abstraction to support sync and async local filesystem APIs. 
     - New functionality
    	 - ...
    
    ## Test plan
    *How are these changes tested?*
    
    - [ ] Tests pass locally with `pytest` for python, `yarn test` for js, `cargo test` for rust
    
    ## Documentation Changes
    *Are all docstrings for user-facing APIs updated if required? Do we need to make documentation changes in the [docs repository](https://github.com/chroma-core/docs)?*

--- rust/worker/src/blockstore/arrow/concurrency_test.rs
+++ rust/worker/src/blockstore/arrow/concurrency_test.rs
@@ -2,88 +2,82 @@
 mod tests {
     use crate::{
         blockstore::arrow::{config::TEST_MAX_BLOCK_SIZE_BYTES, provider::ArrowBlockfileProvider},
-        storage::{local::LocalStorage, Storage},
+        cache::{
+            cache::Cache,
+            config::{CacheConfig, UnboundedCacheConfig},
+        },
+        storage::{sync_local::SyncLocalStorage, Storage},
     };
     use rand::Rng;
     use shuttle::{future, thread};
 
     #[test]
     fn test_blockfile_shuttle() {
-        // shuttle::check_random(
-        //     || {
-        //         let tmp_dir = tempfile::tempdir().unwrap();
-        //         let storage = Storage::Local(LocalStorage::new(tmp_dir.path().to_str().unwrap()));
-        //         let blockfile_provider = ArrowBlockfileProvider::new(storage);
-        //         let writer = blockfile_provider.create::<&str, u32>().unwrap();
-        //         let id = writer.id();
-        //         // Generate N datapoints and then have T threads write them to the blockfile
-        //         let range_min = 10;
-        //         let range_max = 10000;
-        //         let n = shuttle::rand::thread_rng().gen_range(range_min..range_max);
-        //         // Make the max threads the number of cores * 2
-        //         let max_threads = num_cpus::get() * 2;
-        //         let t = shuttle::rand::thread_rng().gen_range(2..max_threads);
-        //         let mut join_handles = Vec::with_capacity(t);
-        //         for i in 0..t {
-        //             let range_start = i * n / t;
-        //             let range_end = (i + 1) * n / t;
-        //             let writer = writer.clone();
-        //             let handle = thread::spawn(move || {
-        //                 println!("Thread {} writing keys {} to {}", i, range_start, range_end);
-        //                 for j in range_start..range_end {
-        //                     let key_string = format!("key{}", j);
-        //                     future::block_on(async {
-        //                         writer
-        //                             .set::<&str, u32>("", key_string.as_str(), j as u32)
-        //                             .await
-        //                             .unwrap_or_else(|e| {
-        //                                 println!(
-        //                                     "Expect key to be set successfully, but got error: {:?}",
-        //                                     e
-        //                                 )
-        //                             });
-        //                     });
-        //                 }
-        //             });
-        //             join_handles.push(handle);
-        //         }
+        shuttle::check_random(
+            || {
+                let tmp_dir = tempfile::tempdir().unwrap();
+                let storage =
+                    Storage::SyncLocal(SyncLocalStorage::new(tmp_dir.path().to_str().unwrap()));
+                let block_cache = Cache::new(&CacheConfig::Unbounded(UnboundedCacheConfig {}));
+                let sparse_index_cache =
+                    Cache::new(&CacheConfig::Unbounded(UnboundedCacheConfig {}));
+                let blockfile_provider = ArrowBlockfileProvider::new(
+                    storage,
+                    TEST_MAX_BLOCK_SIZE_BYTES,
+                    block_cache,
+                    sparse_index_cache,
+                );
+                let writer = blockfile_provider.create::<&str, u32>().unwrap();
+                let id = writer.id();
+                // Generate N datapoints and then have T threads write them to the blockfile
+                let range_min = 10;
+                let range_max = 10000;
+                let n = shuttle::rand::thread_rng().gen_range(range_min..range_max);
+                // Make the max threads the number of cores * 2
+                let max_threads = num_cpus::get() * 2;
+                let t = shuttle::rand::thread_rng().gen_range(2..max_threads);
+                let mut join_handles = Vec::with_capacity(t);
+                for i in 0..t {
+                    let range_start = i * n / t;
+                    let range_end = (i + 1) * n / t;
+                    let writer = writer.clone();
+                    let handle = thread::spawn(move || {
+                        for j in range_start..range_end {
+                            let key_string = format!("key{}", j);
+                            future::block_on(async {
+                                writer
+                                    .set::<&str, u32>("", key_string.as_str(), j as u32)
+                                    .await
+                                    .unwrap();
+                            });
+                        }
+                    });
+                    join_handles.push(handle);
+                }
 
-        //         for handle in join_handles {
-        //             handle.join().unwrap();
-        //         }
+                for handle in join_handles {
+                    handle.join().unwrap();
+                }
 
-        //         // commit the writer
-        //         future::block_on(async {
-        //             let flusher = writer.commit::<&str, u32>().unwrap();
-        //             flusher.flush::<&str, u32>().await.unwrap();
-        //         });
+                // commit the writer
+                future::block_on(async {
+                    let flusher = writer.commit::<&str, u32>().unwrap();
+                    flusher.flush::<&str, u32>().await.unwrap();
+                });
 
-        //         let reader = future::block_on(async {
-        //             blockfile_provider.open::<&str, u32>(&id).await.unwrap()
-        //         });
-        //         // Read the data back
-        //         for i in 0..n {
-        //             let key_string = format!("key{}", i);
-        //             println!("Reading key {}", key_string);
-        //             future::block_on(async {
-        //                 match reader.get("", key_string.as_str()).await {
-        //                     Ok(value) => {
-        //                         // value.expect("Expect key to exist and there to be no error");
-        //                         assert_eq!(value, i as u32);
-        //                     }
-        //                     Err(e) => {
-        //                         println!(
-        //                             "Expect key to exist and there to be no error, but got error: {:?}",
-        //                             e
-        //                         )
-        //                     }
-        //                 }
-        //             });
-        //             // let value = value.expect("Expect key to exist and there to be no error");
-        //             // assert_eq!(value, i as u32);
-        //         }
-        //     },
-        //     100,
-        // );
+                let reader = future::block_on(async {
+                    blockfile_provider.open::<&str, u32>(&id).await.unwrap()
+                });
+                // Read the data back
+                for i in 0..n {
+                    let key_string = format!("key{}", i);
+                    let value =
+                        future::block_on(async { reader.get("", key_string.as_str()).await });
+                    let value = value.expect("Expect key to exist and there to be no error");
+                    assert_eq!(value, i as u32);
+                }
+            },
+            100,
+        );
     }
 }
