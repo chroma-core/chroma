2995835c6e64c2ccca9d2ae99760654707271207
Max Isom <hi@maxisom.me>
2024-07-17 10:42:08 -0700

    Migrate previous bad configs

--- chromadb/db/mixins/sysdb.py
+++ chromadb/db/mixins/sysdb.py
@@ -1,3 +1,4 @@
+import json
 from typing import Optional, Sequence, Any, Tuple, cast, Dict, Union, Set
 from uuid import UUID
 from overrides import override
@@ -435,8 +436,8 @@ class SqlSysDB(SqlDB, SysDB):
                 metadata = self._metadata_from_rows(rows)
                 dimension = int(rows[0][3]) if rows[0][3] else None
                 if rows[0][2] is not None:
-                    configuration = CollectionConfigurationInternal.from_json_str(
-                        rows[0][2]
+                    configuration = self._load_config_from_json_str_and_migrate(
+                        str(collection_id), rows[0][2]
                     )
                 else:
                     # 07/2024: This is a legacy case where we don't have a collection
@@ -763,6 +764,51 @@ class SqlSysDB(SqlDB, SysDB):
         sql, params = get_sql(q, self.parameter_format())
         if sql:
             cur.execute(sql, params)
+
+    def _load_config_from_json_str_and_migrate(
+        self, collection_id: str, json_str: str
+    ) -> CollectionConfigurationInternal:
+        try:
+            config_json = json.loads(json_str)
+        except json.JSONDecodeError:
+            raise ValueError(
+                f"Unable to decode configuration from JSON string: {json_str}"
+            )
+
+        # 07/17/2024: the initial migration from the legacy metadata-based config to the new sysdb-based config had a bug where the batch_size and sync_threshold were swapped. Along with this migration, a validator was added to HNSWConfigurationInternal to ensure that batch_size <= sync_threshold.
+        hnsw_configuration = config_json.get("hnsw_configuration")
+        if hnsw_configuration:
+            batch_size = hnsw_configuration.get("batch_size")
+            sync_threshold = hnsw_configuration.get("sync_threshold")
+
+            if batch_size and sync_threshold and batch_size > sync_threshold:
+                config_json["hnsw_configuration"][
+                    "sync_threshold"
+                ] = HNSWConfigurationInternal.definitions[
+                    "sync_threshold"
+                ].default_value
+                config_json["hnsw_configuration"][
+                    "batch_size"
+                ] = HNSWConfigurationInternal.definitions["batch_size"].default_value
+                configuration = CollectionConfigurationInternal.from_json(config_json)
+
+                collections_t = Table("collections")
+                q = (
+                    self.querybuilder()
+                    .update(collections_t)
+                    .set(
+                        collections_t.config_json_str,
+                        ParameterValue(configuration.to_json_str()),
+                    )
+                    .where(collections_t.id == ParameterValue(collection_id))
+                )
+                sql, params = get_sql(q, self.parameter_format())
+                with self.tx() as cur:
+                    cur.execute(sql, params)
+
+                return configuration
+
+        return CollectionConfigurationInternal.from_json_str(json_str)
 
     def _insert_config_from_legacy_params(
         self, collection_id: Any, metadata: Optional[Metadata]
