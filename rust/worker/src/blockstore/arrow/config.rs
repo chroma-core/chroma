561e6f59bfd4603fb6cd06ec4fddedadccc81973
Liquan Pei <liquanpei@gmail.com>
2024-07-18 11:09:30 -0700

    [CLN] Move max_block_size_bytes to block_manager_config (#2535)
    ## Description of changes
    
    *Summarize the changes made by this PR.*
     - Improvements & Bug fixes
    - Refactored the configuration key `max_block_size_bytes` from
    `ArrowBlockfileProviderConfig` to `BlockManagerConfig`.
     - New functionality
    	 - ...
    
    ## Test plan
    *How are these changes tested?*
    
    - [x] Tests pass locally with `pytest` for python, `yarn test` for js,
    `cargo test` for rust
    
    ## Documentation Changes
    *Are all docstrings for user-facing APIs updated if required? Do we need
    to make documentation changes in the [docs
    repository](https://github.com/chroma-core/docs)?*

--- rust/worker/src/blockstore/arrow/config.rs
+++ rust/worker/src/blockstore/arrow/config.rs
@@ -7,19 +7,14 @@ pub(crate) const TEST_MAX_BLOCK_SIZE_BYTES: usize = 16384;
 
 #[derive(Deserialize, Debug, Clone)]
 pub(crate) struct ArrowBlockfileProviderConfig {
-    // Note: This provider has two dependent components that
-    // are both internal to the arrow blockfile provider.
-    // The BlockManager and the SparseIndexManager.
-    // We could have a BlockManagerConfig and a SparseIndexManagerConfig
-    // but the only configuration that is needed is the max_block_size_bytes
-    // so for now we just hoid this configuration in the ArrowBlockfileProviderConfig.
-    pub(crate) max_block_size_bytes: usize,
+    // pub(crate) max_block_size_bytes: usize,
     pub(crate) block_manager_config: BlockManagerConfig,
     pub(crate) sparse_index_manager_config: SparseIndexManagerConfig,
 }
 
 #[derive(Deserialize, Debug, Clone)]
 pub(crate) struct BlockManagerConfig {
+    pub(crate) max_block_size_bytes: usize,
     pub(crate) block_cache_config: CacheConfig,
 }
 
