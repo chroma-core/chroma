c43d38c6f6277a2687de5ff12c99fee35a1e6ee9
Max Isom <hi@maxisom.me>
2024-07-24 10:24:45 -0700

    Include trace ID in error message for unknown exceptions

--- chromadb/api/base_http_client.py
+++ chromadb/api/base_http_client.py
@@ -98,4 +98,9 @@ class BaseHTTPClient:
         try:
             resp.raise_for_status()
         except httpx.HTTPStatusError:
+            trace_id = resp.headers.get("chroma-trace-id")
+            if trace_id:
+                raise Exception(
+                    f"Chroma request failed: {resp.text}. Trace ID: {trace_id}."
+                )
             raise (Exception(resp.text))
