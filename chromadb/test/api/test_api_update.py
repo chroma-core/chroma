a088632abaa7a16d70505b9a4a69748c77d1cd88
Spike Lu <spike@trychroma.com>
2024-09-12 13:52:37 -0700

    remove tests covered by the property testing

--- chromadb/test/api/test_api_update.py
+++ chromadb/test/api/test_api_update.py
@@ -1,31 +1,11 @@
 import pytest
-from typing import List, cast
-
 from chromadb.errors import InvalidCollectionException
 from chromadb.api import ClientAPI
-from chromadb.api.types import IncludeEnum
 from chromadb.test.api.utils import (
-    vector_approx_equal,
-    records,
     minimal_records,
     bad_dimensionality_records,
     metadata_records,
 )
-
-
-def test_metadata_update_get_int_float(client: ClientAPI) -> None:
-    client.reset()
-    collection = client.create_collection("test_int")
-    collection.add(**metadata_records)  # type: ignore[arg-type]
-
-    collection.update(
-        ids=["id1"],
-        metadatas=[{"int_value": 2, "string_value": "two", "float_value": 2.002}],
-    )
-    items = collection.get(ids=["id1"])
-    assert (items["metadatas"])[0]["int_value"] == 2  # type: ignore[index]
-    assert (items["metadatas"])[0]["string_value"] == "two"  # type: ignore[index]
-    assert (items["metadatas"])[0]["float_value"] == 2.002  # type: ignore[index]
 
 
 def test_metadata_validation_update(client: ClientAPI) -> None:
@@ -34,35 +14,6 @@ def test_metadata_validation_update(client: ClientAPI) -> None:
     collection.add(**metadata_records)  # type: ignore[arg-type]
     with pytest.raises(ValueError, match="metadata"):
         collection.update(ids=["id1"], metadatas={"value": {"nested": "5"}})  # type: ignore[dict-item]
-
-
-def test_update_query(client: ClientAPI) -> None:
-    client.reset()
-    collection = client.create_collection("test_update_query")
-    collection.add(**records)  # type: ignore[arg-type]
-
-    updated_records = {
-        "ids": [records["ids"][0]],  # type: ignore[index]
-        "embeddings": [[0.1, 0.2, 0.3]],
-        "documents": ["updated document"],
-        "metadatas": [{"foo": "bar"}],
-    }
-
-    collection.update(**updated_records)
-
-    # test query
-    results = collection.query(
-        query_embeddings=updated_records["embeddings"],
-        n_results=1,
-        include=cast(List[IncludeEnum], ["embeddings", "documents", "metadatas"]),
-    )
-    assert len(results["ids"][0]) == 1
-    assert results["ids"][0][0] == updated_records["ids"][0]
-    assert (results["documents"])[0][0] == updated_records["documents"][0]  # type: ignore[index]
-    assert (results["metadatas"])[0][0]["foo"] == "bar"  # type: ignore[index]
-    assert vector_approx_equal(
-        (results["embeddings"])[0][0], updated_records["embeddings"][0]  # type: ignore[index]
-    )
 
 
 # test to make sure update error on invalid embeddings input
