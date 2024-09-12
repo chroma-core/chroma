3ae64f0a0062081a07397dc4273cef3989a5309c
Max Isom <hi@maxisom.me>
2024-09-12 14:59:30 -0700

    Fix port forwarding

--- bin/cluster-test.sh
+++ bin/cluster-test.sh
@@ -9,13 +9,4 @@ export CHROMA_COORDINATOR_HOST=localhost
 echo "Chroma Server is running at port $CHROMA_SERVER_HOST"
 echo "Chroma Coordinator is running at port $CHROMA_COORDINATOR_HOST"
 
-# Despite the fact that tilt forwards the ports
-# We need the ports forwarded since tilt ci will
-# not keep them forwarded
-# https://github.com/tilt-dev/tilt/issues/5964
-kubectl -n chroma port-forward svc/sysdb 50051:50051 &
-kubectl -n chroma port-forward svc/logservice 50052:50051 &
-kubectl -n chroma port-forward svc/query-service 50053:50051 &
-kubectl -n chroma port-forward svc/frontend-service 8000:8000 &
-
 "$@"
