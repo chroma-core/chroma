83686944db73fa2b13e0b51f1467dbc9366082f5
Max Isom <hi@maxisom.me>
2024-09-17 14:44:12 -0700

    Cleanup, add test

--- rust/benchmark-datasets/src/types.rs
+++ rust/benchmark-datasets/src/types.rs
@@ -2,21 +2,23 @@ use anyhow::Result;
 use rand::seq::SliceRandom;
 use serde::{Deserialize, Serialize};
 use std::{collections::HashMap, future::Future, path::PathBuf};
-use tantivy::{collector::TopDocs, doc, query::QueryParser, schema::*, Index, ReloadPolicy};
+use tantivy::{
+    collector::TopDocs,
+    doc,
+    query::QueryParser,
+    schema::{Schema, STORED, TEXT},
+    Index, ReloadPolicy,
+};
 use tokio::io::AsyncWriteExt;
 use tokio_stream::{Stream, StreamExt};
 
-use crate::util::get_or_populate_cached_dataset;
-
-fn get_git_checked_in_dataset_dir() -> PathBuf {
-    PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("datasets")
-}
+use crate::util::{get_dir_for_persistent_dataset_files, get_or_populate_cached_dataset_file};
 
 trait TantivyIndexable {
     fn build_tantivy_index(&mut self) -> impl Future<Output = Result<(Index, Schema)>>;
 }
 
-impl<T: Stream<Item = BenchmarkDatasetDocument> + Unpin> TantivyIndexable for T {
+impl<T: Stream<Item = Document> + Unpin> TantivyIndexable for T {
     async fn build_tantivy_index(&mut self) -> Result<(Index, Schema)> {
         let mut schema_builder = Schema::builder();
         schema_builder.add_text_field("content", TEXT | STORED);
@@ -36,26 +38,20 @@ impl<T: Stream<Item = BenchmarkDatasetDocument> + Unpin> TantivyIndexable for T 
 }
 
 #[derive(Debug, Serialize, Deserialize)]
-pub enum BenchmarkDatasets {
-    Wikipedia,
-    SciDocs,
-    MsMarcoQueries,
-}
-
-#[derive(Debug, Serialize, Deserialize)]
-pub struct FrozenQueryset {
+pub struct FrozenQuerySubset {
     queries: Vec<String>,
-    against_dataset: BenchmarkDatasets,
+    queries_from_dataset: String,
+    dataset_queries_tested_against: String,
     min_results_per_query: usize,
 }
 
 #[derive(Debug, Clone)]
-pub struct BenchmarkDatasetDocument {
+pub struct Document {
     pub content: String,
     pub metadata: HashMap<String, String>,
 }
 
-pub trait BenchmarkDataset
+pub trait DocumentDataset
 where
     Self: Sized,
 {
@@ -63,31 +59,37 @@ where
     fn get_name(&self) -> &'static str;
     fn create_documents_stream(
         &self,
-    ) -> impl Future<Output = Result<impl Stream<Item = Result<BenchmarkDatasetDocument>>>> + Send;
+    ) -> impl Future<Output = Result<impl Stream<Item = Result<Document>>>> + Send;
 }
 
-pub trait QueryDataset: BenchmarkDataset
+/// A query dataset is a specialized `DocumentDataset` where the documents are queries.
+pub trait QueryDataset: DocumentDataset
 where
     Self: Sized,
 {
-    fn get_or_create_frozen_subset(
+    /// Returns a subset of queries from the dataset that have at least `min_results_per_query` results in the `corpus_dataset`.
+    /// The subset will contain at most `max_num_of_queries` queries.
+    ///
+    /// Because constructing this subset can be expensive (and different subsets may lead to different downstream test results), by default the constructed subset is stored in the `dataset_files/` directory in the root of this crate.
+    fn get_or_create_frozen_query_subset(
         &self,
-        corpus_dataset: &impl BenchmarkDataset,
+        corpus_dataset: &impl DocumentDataset,
         min_results_per_query: usize,
         max_num_of_queries: usize,
-    ) -> impl Future<Output = Result<FrozenQueryset>> {
+        cache_dir: Option<PathBuf>,
+    ) -> impl Future<Output = Result<FrozenQuerySubset>> {
         async move {
             let file_name = format!(
-                "frozen_queryset_{}_{}_{}.bin",
+                "frozen_query_subset_{}_{}_{}.bin",
                 corpus_dataset.get_name(),
                 min_results_per_query,
                 max_num_of_queries
             );
 
-            let file = get_or_populate_cached_dataset(
+            let file = get_or_populate_cached_dataset_file(
                 self.get_name(),
                 &file_name,
-                Some(get_git_checked_in_dataset_dir()),
+                Some(cache_dir.unwrap_or(get_dir_for_persistent_dataset_files())),
                 |mut file| async move {
                     let (corpus_index, corpus_index_schema) = futures::StreamExt::boxed_local(
                         corpus_dataset.create_documents_stream().await?.filter_map(
@@ -122,9 +124,10 @@ where
                         vec![corpus_index_schema.get_field("content")?],
                     );
 
-                    let mut frozen_queryset = FrozenQueryset {
+                    let mut frozen_query_subset = FrozenQuerySubset {
                         queries: Vec::new(),
-                        against_dataset: BenchmarkDatasets::Wikipedia, // todo
+                        queries_from_dataset: self.get_name().to_string(),
+                        dataset_queries_tested_against: corpus_dataset.get_name().to_string(),
                         min_results_per_query,
                     };
 
@@ -132,16 +135,16 @@ where
                         let query = query_parser.parse_query(query_text)?;
                         let top_docs =
                             searcher.search(&query, &TopDocs::with_limit(min_results_per_query))?;
-                        if top_docs.len() >= 2 {
-                            frozen_queryset.queries.push(query_text.to_string());
+                        if top_docs.len() >= min_results_per_query {
+                            frozen_query_subset.queries.push(query_text.to_string());
                         }
 
-                        if frozen_queryset.queries.len() == max_num_of_queries {
+                        if frozen_query_subset.queries.len() == max_num_of_queries {
                             break;
                         }
                     }
 
-                    let serialized = bincode::serialize(&frozen_queryset)?;
+                    let serialized = bincode::serialize(&frozen_query_subset)?;
                     file.write_all(&serialized).await?;
 
                     Ok(())
@@ -156,45 +159,77 @@ where
     }
 }
 
-// pub trait StreamReservoirSample {
-//     type Item;
-//     fn reservoir_sample(self, n: usize) -> impl Future<Output = Vec<Self::Item>>;
-// }
+#[cfg(test)]
+mod tests {
+    use async_tempfile::TempDir;
 
-// impl<T> StreamReservoirSample for T
-// where
-//     T: Stream + Unpin,
-// {
-//     type Item = T::Item;
+    use super::*;
 
-//     fn reservoir_sample(self, n: usize) -> impl Future<Output = Vec<Self::Item>> {
-//         async move {
-//             let mut rng = rand::thread_rng();
-//             let mut stream = self;
-//             let mut reservoir = Vec::with_capacity(n);
+    struct TestDataset {
+        documents: Vec<Document>,
+    }
 
-//             let random_index = Uniform::new(0, n);
+    impl DocumentDataset for TestDataset {
+        fn init() -> impl Future<Output = Result<Self>> + Send {
+            async move { Ok(TestDataset { documents: vec![] }) }
+        }
 
-//             let mut w: f64 = (rng.gen::<f64>().ln() / n as f64).exp();
-//             let mut next_sample_i = n;
+        fn get_name(&self) -> &'static str {
+            "test"
+        }
 
-//             let mut i = 0;
-//             while let Some(doc) = stream.next().await {
-//                 if i < n {
-//                     reservoir.push(doc);
-//                 } else {
-//                     if i == next_sample_i {
-//                         next_sample_i +=
-//                             (rng.gen::<f64>().ln() / (1.0 - w).ln()).floor() as usize + 1;
-//                         w *= (rng.gen::<f64>().ln() / n as f64).exp();
-//                         reservoir[random_index.sample(&mut rng)] = doc;
-//                     }
-//                 }
+        fn create_documents_stream(
+            &self,
+        ) -> impl Future<Output = Result<impl Stream<Item = Result<Document>>>> + Send {
+            async move {
+                let documents = self.documents.clone();
+                Ok(futures::stream::iter(documents.into_iter().map(Ok)))
+            }
+        }
+    }
 
-//                 i += 1;
-//             }
+    impl QueryDataset for TestDataset {}
 
-//             reservoir
-//         }
-//     }
-// }
+    #[tokio::test]
+    async fn test_frozen_query_subset() {
+        let mut test_dataset = TestDataset::init().await.unwrap();
+        test_dataset.documents = vec!["foo 0", "foo 1", "foo 3", "bar 0", "bar 2"]
+            .iter()
+            .map(|&content| Document {
+                content: content.to_string(),
+                metadata: HashMap::new(),
+            })
+            .collect();
+
+        let mut test_query_dataset = TestDataset::init().await.unwrap();
+        test_query_dataset.documents = vec!["foo", "bar", "baz"]
+            .iter()
+            .map(|&content| Document {
+                content: content.to_string(),
+                metadata: HashMap::new(),
+            })
+            .collect();
+
+        let temp_dir = TempDir::new().await.unwrap();
+
+        let frozen_query_subset = test_query_dataset
+            .get_or_create_frozen_query_subset(&test_dataset, 1, 100, Some(temp_dir.to_path_buf()))
+            .await
+            .unwrap();
+
+        // There are no documents with "baz" in the dataset
+        assert_eq!(frozen_query_subset.queries.len(), 2);
+        assert!(frozen_query_subset.queries.contains(&"foo".to_string()));
+        assert!(frozen_query_subset.queries.contains(&"bar".to_string()));
+
+        // Require at least 3 results per query
+        let frozen_query_subset = test_query_dataset
+            .get_or_create_frozen_query_subset(&test_dataset, 3, 100, Some(temp_dir.to_path_buf()))
+            .await
+            .unwrap();
+
+        // "foo" is the only query with at least 3 results
+        assert_eq!(frozen_query_subset.queries.len(), 1);
+        assert!(frozen_query_subset.queries.contains(&"foo".to_string()));
+    }
+}
