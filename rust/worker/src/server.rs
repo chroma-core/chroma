57a4840216fbb486f4e13207a2a4cd7410cd6d98
Sicheng Pan <sicheng@trychroma.com>
2024-09-17 09:33:34 -0700

    Remove redundant imports

--- rust/worker/src/server.rs
+++ rust/worker/src/server.rs
@@ -20,9 +20,7 @@ use chroma_types::chroma_proto::{
     GetVectorsRequest, GetVectorsResponse, QueryVectorsRequest, QueryVectorsResponse,
 };
 use chroma_types::{MetadataValue, ScalarEncoding};
-use kube::core::request;
 use std::collections::HashMap;
-use std::hash::Hash;
 use tokio::signal::unix::{signal, SignalKind};
 use tonic::{transport::Server, Request, Response, Status};
 use tracing::{trace_span, Instrument};
