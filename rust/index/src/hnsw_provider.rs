3e97018fa6698a83e40e84d3f1ab064eea894167
Robert Escriva <robert@trychroma.com>
2024-09-26 10:11:11 -0700

    [CHORE] cleanup Debug of HnswIndexProvider (#2864)
    ## Description of changes
    
    *Summarize the changes made by this PR.*
     - Small refactor.
    
    ## Test plan
    *How are these changes tested?*
    
    - [ ]  `cargo check` locally; ci remotely
    
    ## Documentation Changes
    *Are all docstrings for user-facing APIs updated if required? Do we need
    to make documentation changes in the [docs
    repository](https://github.com/chroma-core/docs)?*
    
    No

--- rust/index/src/hnsw_provider.rs
+++ rust/index/src/hnsw_provider.rs
@@ -71,11 +71,9 @@ impl Cacheable for HnswIndexRef {
 
 impl Debug for HnswIndexProvider {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(
-            f,
-            "HnswIndexProvider {{ temporary_storage_path: {:?} }}",
-            self.temporary_storage_path,
-        )
+        f.debug_struct("HnswIndexProvider")
+            .field("temporary_storage_path", &self.temporary_storage_path)
+            .finish_non_exhaustive()
     }
 }
 
