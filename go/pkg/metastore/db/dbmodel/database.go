05d691460a7ddd5c74605e6bf1646917e5b909dc
rohitcpbot <rohitcp15@gmail.com>
2024-10-04 02:59:49 +0530

    Changes to schema for go sysdb
    Schema changes related to Issue#2884 and
    few other necessary constraints.

--- go/pkg/metastore/db/dbmodel/database.go
+++ go/pkg/metastore/db/dbmodel/database.go
@@ -8,8 +8,8 @@ import (
 
 type Database struct {
 	ID        string          `gorm:"id;primaryKey;unique"`
-	Name      string          `gorm:"name;type:varchar(128);not_null;uniqueIndex:idx_tenantid_name"`
-	TenantID  string          `gorm:"tenant_id;type:varchar(128);not_null;uniqueIndex:idx_tenantid_name"`
+	Name      string          `gorm:"name;type:varchar(128);not null;uniqueIndex:idx_tenantid_name"`
+	TenantID  string          `gorm:"tenant_id;type:varchar(128);not null;uniqueIndex:idx_tenantid_name"`
 	Ts        types.Timestamp `gorm:"ts;type:bigint;default:0"`
 	IsDeleted bool            `gorm:"is_deleted;type:bool;default:false"`
 	CreatedAt time.Time       `gorm:"created_at;type:timestamp;not null;default:current_timestamp"`
