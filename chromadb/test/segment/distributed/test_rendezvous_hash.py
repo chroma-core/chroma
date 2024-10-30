60f5f94b76e7595ee6f74e28b6dfe882ac09b893
Hammad Bashir <HammadB@users.noreply.github.com>
2024-10-29 18:53:48 -0700

    [ENH] Sometimes simulation disagrees with math (#3030)
    ## Description of changes
    
    *Summarize the changes made by this PR.*
     - Improvements & Bug fixes
    - One can view the assignment as a binomial distribution. We can take
    the std dev of the binomial to validate the null hypothesis - that our
    data is a binomial distribution.
    - As @Sicheng-Pan pointed out
    (https://github.com/chroma-core/chroma/pull/2969#discussion_r1821694613)
    the previous tolerance to the multi-assign test should have failed. It
    did not. Sometimes math disagrees with simulation. We were still in two
    sigma with a tolerance of 75, now the tolerance is 150.
     - New functionality
    	 - None
    
    ## Test plan
    *How are these changes tested?*
    - [x] Tests pass locally with `pytest` for python, `yarn test` for js,
    `cargo test` for rust
    
    ## Documentation Changes
    *Are all docstrings for user-facing APIs updated if required? Do we need
    to make documentation changes in the [docs
    repository](https://github.com/chroma-core/docs)?*

--- chromadb/test/segment/distributed/test_rendezvous_hash.py
+++ chromadb/test/segment/distributed/test_rendezvous_hash.py
@@ -1,4 +1,5 @@
 from chromadb.utils.rendezvous_hash import assign, murmur3hasher
+from math import sqrt
 
 
 def test_rendezvous_hash() -> None:
@@ -14,12 +15,19 @@ def test_rendezvous_hash() -> None:
 
 def test_even_distribution() -> None:
     member_count = 10
-    tolerance = 25
+    num_keys = 1000
     nodes = [str(i) for i in range(member_count)]
+
+    expected = num_keys / len(nodes)
+    # Std deviation of a binomial distribution is sqrt(n * p * (1 - p))
+    # where n is the number of trials, and p is the probability of success
+    stddev = sqrt(num_keys * (1 / len(nodes)) * (1 - 1 / len(nodes)))
+    # https://en.wikipedia.org/wiki/68%E2%80%9395%E2%80%9399.7_rule
+    # For a 99.7% confidence interval
+    tolerance = 3 * stddev
 
     # Test if keys are evenly distributed across nodes
     key_distribution = {node: 0 for node in nodes}
-    num_keys = 1000
     for i in range(num_keys):
         key = f"key_{i}"
         node = assign(key, nodes, murmur3hasher, 1)[0]
@@ -27,18 +35,21 @@ def test_even_distribution() -> None:
 
     # Check if keys are somewhat evenly distributed
     for node in nodes:
-        assert abs(key_distribution[node] - num_keys / len(nodes)) < tolerance
+        assert abs(key_distribution[node] - expected) < tolerance
 
 
 def test_multi_assign_even_distribution() -> None:
     member_count = 10
-    tolerance = 75
+    num_keys = 10000
+    replication = 3
     nodes = [str(i) for i in range(member_count)]
+    expected = num_keys / len(nodes) * replication
+
+    stddev = sqrt(num_keys * replication * (1 / len(nodes)) * (1 - 1 / len(nodes)))
+    tolerance = 3 * stddev
 
     # Test if keys are evenly distributed across nodes
     key_distribution = {node: 0 for node in nodes}
-    num_keys = 10000
-    replication = 3
     for i in range(num_keys):
         key = f"key_{i}"
         nodes_assigned = assign(key, nodes, murmur3hasher, replication)
@@ -50,5 +61,4 @@ def test_multi_assign_even_distribution() -> None:
     # Check if keys are somewhat evenly distributed
     for node in nodes:
         # 3k keys expected for each node (10000 keys / 10 nodes * 3 replication)
-        expected = num_keys / len(nodes) * replication
         assert abs(key_distribution[node] - expected) < tolerance
