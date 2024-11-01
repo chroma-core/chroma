fc54f702f3244d67283e241b614bbbd8d01c18d3
Hammad Bashir <HammadB@users.noreply.github.com>
2024-11-01 10:39:41 -0700

    [ENH] Add fastapi middleware for http.disconnect (#3038)
    ## Description of changes
    
    *Summarize the changes made by this PR.*
     - Improvements & Bug fixes
    - This change adds the ASGI http.disconnect tag to the root span so that
    we can filter out disconnects from errors
     - New functionality
    	 - ...
    
    ## Test plan
    *How are these changes tested?*
    I manually ran a disconnect script and checked the events were tagged
    appropriately. I ensured requests with no disconnect were not tagged.
    - [x] Tests pass locally with `pytest` for python, `yarn test` for js,
    `cargo test` for rust
    
    ## Documentation Changes
    None

--- chromadb/telemetry/opentelemetry/fastapi.py
+++ chromadb/telemetry/opentelemetry/fastapi.py
@@ -1,10 +1,43 @@
-from typing import List, Optional
+from typing import Any, Awaitable, Callable, Dict, List, Optional
 from fastapi import FastAPI
 from opentelemetry.instrumentation.fastapi import FastAPIInstrumentor
+from opentelemetry import trace
 
 
 def instrument_fastapi(app: FastAPI, excluded_urls: Optional[List[str]] = None) -> None:
     """Instrument FastAPI to emit OpenTelemetry spans."""
+
+    # FastAPI calls middleware in reverse order, we want our filter disconnect
+    # middleware to be called first to ensure that we have a chance to mark
+    # spans as disconnected before they are ended by the instrumentation
+    # middleware.
+    app.add_middleware(FilterDisconnectMiddleware)
     FastAPIInstrumentor.instrument_app(
         app, excluded_urls=",".join(excluded_urls) if excluded_urls else None
     )
+
+
+class FilterDisconnectMiddleware:
+    def __init__(self, app: Any):
+        self.app = app
+
+    async def __call__(
+        self,
+        scope: Dict[str, Any],
+        receive: Callable[[], Awaitable[Dict[str, Any]]],
+        send: Callable[[Dict[str, Any]], Awaitable[None]],
+    ) -> None:
+        if scope["type"] != "http":
+            await self.app(scope, receive, send)
+            return
+
+        request_span = trace.get_current_span()
+
+        async def process() -> Dict[str, Any]:
+            message = await receive()
+            if message["type"] == "http.disconnect":
+                request_span.set_attribute("http.disconnect", True)
+
+            return message
+
+        await self.app(scope, process, send)
