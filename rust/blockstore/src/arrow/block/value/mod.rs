f0e02f5b6aabaac8bab49417cffda4b6e005f363
Max Isom <hi@maxisom.me>
2024-11-04 13:18:57 -0800

    PR comments

--- rust/blockstore/src/arrow/block/value/mod.rs
+++ rust/blockstore/src/arrow/block/value/mod.rs
@@ -1,5 +1,5 @@
 pub(super) mod data_record_value;
-pub(super) mod int32array_value;
 pub(super) mod roaring_bitmap_value;
 pub(super) mod str_value;
 pub(super) mod u32_value;
+pub(super) mod uint32array_value;
