9f854de4ed914bcd2e9171301e89ac561e3361ed
Max Isom <hi@maxisom.me>
2024-11-04 16:36:18 -0800

    Cleanup

--- rust/index/src/fulltext/types.rs
+++ rust/index/src/fulltext/types.rs
@@ -1,3 +1,4 @@
+use super::util::TokenInstance;
 use chroma_blockstore::{BlockfileFlusher, BlockfileReader, BlockfileWriter};
 use chroma_error::{ChromaError, ErrorCodes};
 use futures::StreamExt;
@@ -10,122 +11,6 @@ use tantivy::tokenizer::TokenStream;
 use tantivy::tokenizer::Tokenizer;
 use thiserror::Error;
 use uuid::Uuid;
-
-pub type TokenInstance = u128;
-
-// Unicode characters only use 21 bit, so we can encode a trigram in 21 * 3 = 63 bits (a u64).
-#[inline(always)]
-pub(super) fn pack_trigram(s: &str) -> u64 {
-    let mut u = 0u64;
-    for (i, c) in s.chars().take(3).enumerate() {
-        let shift = (2 - i) * 21;
-        u |= (c as u64) << shift;
-    }
-    u
-}
-
-unsafe fn encode_utf8_unchecked(c: u32, buf: &mut [u8]) -> usize {
-    if c == 0 {
-        0
-    } else if c < 0x80 {
-        buf[0] = c as u8;
-        1
-    } else if c < 0x800 {
-        buf[0] = (0xC0 | (c >> 6)) as u8;
-        buf[1] = (0x80 | (c & 0x3F)) as u8;
-        2
-    } else if c < 0x10000 {
-        buf[0] = (0xE0 | (c >> 12)) as u8;
-        buf[1] = (0x80 | ((c >> 6) & 0x3F)) as u8;
-        buf[2] = (0x80 | (c & 0x3F)) as u8;
-        3
-    } else {
-        buf[0] = (0xF0 | (c >> 18)) as u8;
-        buf[1] = (0x80 | ((c >> 12) & 0x3F)) as u8;
-        buf[2] = (0x80 | ((c >> 6) & 0x3F)) as u8;
-        buf[3] = (0x80 | (c & 0x3F)) as u8;
-        4
-    }
-}
-
-#[inline(always)]
-pub(super) fn unpack_trigram(u: u64) -> String {
-    let c0 = ((u >> 42) & 0x1F_FFFF) as u32;
-    let c1 = ((u >> 21) & 0x1F_FFFF) as u32;
-    let c2 = (u & 0x1F_FFFF) as u32;
-
-    // Preallocate the maximum possible size (3 chars * 4 bytes each)
-    let mut s = String::with_capacity(12);
-
-    unsafe {
-        // Directly get a mutable reference to the internal buffer
-        let v = s.as_mut_vec();
-        let len0 = v.len();
-
-        // Ensure the buffer has enough capacity
-        v.set_len(len0 + 12);
-
-        // Encode the codepoints directly into the buffer
-        let bytes_written_c0 = encode_utf8_unchecked(c0, &mut v[len0..]);
-        let bytes_written_c1 = encode_utf8_unchecked(c1, &mut v[len0 + bytes_written_c0..]);
-        let bytes_written_c2 =
-            encode_utf8_unchecked(c2, &mut v[len0 + bytes_written_c0 + bytes_written_c1..]);
-
-        // Set the correct length after writing
-        let total_bytes = bytes_written_c0 + bytes_written_c1 + bytes_written_c2;
-        v.set_len(len0 + total_bytes);
-    }
-
-    s
-}
-
-pub trait TokenContainer {
-    fn encode(token: &str, document_id: u32, offset: Option<u32>) -> Self;
-    fn get_token(&self) -> String;
-    fn get_encoded_token(&self) -> u64;
-    fn get_document_id(&self) -> u32;
-    fn get_encoded_token_and_document_id(&self) -> u128;
-    fn get_offset(&self) -> Option<u32>;
-}
-
-impl TokenContainer for TokenInstance {
-    #[inline(always)]
-    fn encode(token: &str, document_id: u32, offset: Option<u32>) -> Self {
-        (pack_trigram(token) as u128) << 64
-            | (document_id as u128) << 32
-            | offset.map(|o| o | (1 << 31)).unwrap_or(0) as u128
-    }
-
-    #[inline(always)]
-    fn get_encoded_token_and_document_id(&self) -> u128 {
-        *self >> 32
-    }
-
-    #[inline(always)]
-    fn get_encoded_token(&self) -> u64 {
-        (*self >> 64) as u64
-    }
-
-    #[inline(always)]
-    fn get_token(&self) -> String {
-        unpack_trigram((*self >> 64) as u64)
-    }
-
-    #[inline(always)]
-    fn get_document_id(&self) -> u32 {
-        (*self >> 32) as u32
-    }
-
-    #[inline(always)]
-    fn get_offset(&self) -> Option<u32> {
-        let offset = *self as u32;
-        if offset & (1 << 31) != 0 {
-            return Some(offset & !(1 << 31));
-        }
-
-        None
-    }
-}
 
 #[derive(Error, Debug)]
 pub enum FullTextIndexError {
@@ -162,7 +47,7 @@ pub enum DocumentMutation<'a> {
 
 #[derive(Clone)]
 pub struct FullTextIndexWriter {
-    tokenizer: NgramTokenizer, // todo
+    tokenizer: NgramTokenizer,
     token_instances: Arc<Mutex<Vec<TokenInstance>>>,
     posting_lists_blockfile_writer: BlockfileWriter,
 }
@@ -275,13 +160,13 @@ impl FullTextIndexWriter {
         token_instances.sort_unstable(); // todo: should this happen in separate thread?
 
         for encoded_instance in token_instances {
-            match encoded_instance.get_offset() {
+            match encoded_instance.get_position() {
                 Some(offset) => {
-                    let this_key = encoded_instance.get_encoded_token_and_document_id();
+                    let this_key = encoded_instance.omit_position();
                     if last_key != this_key {
                         if last_key != TokenInstance::MAX {
-                            let token = (last_key << 32).get_token();
-                            let document_id = (last_key << 32).get_document_id();
+                            let token = last_key.get_token();
+                            let document_id = last_key.get_offset_id();
                             self.posting_lists_blockfile_writer
                                 .set(&token, document_id, posting_list.clone())
                                 .await
@@ -298,7 +183,7 @@ impl FullTextIndexWriter {
                     self.posting_lists_blockfile_writer
                         .delete::<u32, Vec<u32>>(
                             &encoded_instance.get_token(),
-                            encoded_instance.get_document_id(), // todo: rename
+                            encoded_instance.get_offset_id(), // todo: rename
                         )
                         .await
                         .unwrap();
@@ -307,8 +192,8 @@ impl FullTextIndexWriter {
         }
 
         if last_key != TokenInstance::MAX {
-            let token = (last_key << 32).get_token();
-            let document_id = (last_key << 32).get_document_id();
+            let token = last_key.get_token();
+            let document_id = last_key.get_offset_id();
             self.posting_lists_blockfile_writer
                 .set(&token, document_id, posting_list.clone())
                 .await
