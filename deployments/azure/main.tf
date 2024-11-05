b2a87c27a3b22856d9449426a82a654ff9315e92
itaismith <42597089+itaismith@users.noreply.github.com>
2024-11-05 00:09:31 -0800

    [RELEASE] 0.5.18 (#3063)
    Release 0.5.18

--- deployments/azure/main.tf
+++ deployments/azure/main.tf
@@ -17,7 +17,7 @@ variable "machine_type" {
 
 variable "chroma_version" {
   description = "Chroma version to install"
-  default     = "0.5.17"
+  default     = "0.5.18"
 }
 
 variable "chroma_server_auth_credentials" {
