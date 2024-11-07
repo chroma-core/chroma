7870efc272be8d17265ccfff06a698a7c8345ef3
Ellis Brown <ellis.l.brown.ii@gmail.com>
2024-11-06 22:54:06 -0500

    openclip device bugfix

--- chromadb/utils/embedding_functions/open_clip_embedding_function.py
+++ chromadb/utils/embedding_functions/open_clip_embedding_function.py
@@ -49,21 +49,22 @@ class OpenCLIPEmbeddingFunction(EmbeddingFunction[Union[Documents, Images]]):
         )
         self._model = model
         self._model.to(device)
+        self._device = device
         self._preprocess = preprocess
         self._tokenizer = open_clip.get_tokenizer(model_name=model_name)
 
     def _encode_image(self, image: Image) -> Embedding:
         pil_image = self._PILImage.fromarray(image)
         with self._torch.no_grad():
-            image_features = self._model.encode_image(
-                self._preprocess(pil_image).unsqueeze(0)
-            )
+            preproc_image = self._preprocess(pil_image).unsqueeze(0).to(self._device)
+            image_features = self._model.encode_image(preproc_image)
             image_features /= image_features.norm(dim=-1, keepdim=True)
             return cast(Embedding, image_features.squeeze().cpu().numpy())
 
     def _encode_text(self, text: Document) -> Embedding:
         with self._torch.no_grad():
-            text_features = self._model.encode_text(self._tokenizer(text))
+            tok_text = self._tokenizer(text).to(self._device)
+            text_features = self._model.encode_text(tok_text)
             text_features /= text_features.norm(dim=-1, keepdim=True)
             return cast(Embedding, text_features.squeeze().cpu().numpy())
 
