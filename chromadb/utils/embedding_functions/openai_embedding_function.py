c71a0ad1f8283129e6c93d85202765cf1a53475c
KiyotoWatanabe <kiyoto.watanabe@divx.co.jp>
2024-11-07 15:49:40 +0900

    Revert "fix: Remove unused "type: ignore" comments"
    This reverts commit 1c378fa69918168cf20d89670fa0335233893cd4.

--- chromadb/utils/embedding_functions/openai_embedding_function.py
+++ chromadb/utils/embedding_functions/openai_embedding_function.py
@@ -116,7 +116,9 @@ class OpenAIEmbeddingFunction(EmbeddingFunction[Documents]):
             ).data
 
             # Sort resulting embeddings by index
-            sorted_embeddings = sorted(embeddings, key=lambda e: e.index)
+            sorted_embeddings = sorted(
+                embeddings, key=lambda e: e.index  # type: ignore
+            )
 
             # Return just the embeddings
             return cast(Embeddings, [result.embedding for result in sorted_embeddings])
@@ -131,7 +133,9 @@ class OpenAIEmbeddingFunction(EmbeddingFunction[Documents]):
                 ]
 
             # Sort resulting embeddings by index
-            sorted_embeddings = sorted(embeddings, key=lambda e: e["index"])
+            sorted_embeddings = sorted(
+                embeddings, key=lambda e: e["index"]  # type: ignore
+            )
 
             # Return just the embeddings
             return cast(
