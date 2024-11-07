afa22e8614dc8f0df3fc7346d068ca7122d522a6
Drew Kim <drewkim117@gmail.com>
2024-11-07 10:40:04 -0800

    Update SimpleQuotaEnforcer

--- chromadb/quota/simple_quota_enforcer/__init__.py
+++ chromadb/quota/simple_quota_enforcer/__init__.py
@@ -1,7 +1,17 @@
 from overrides import override
-from typing import Any, Callable, TypeVar, Dict
+from typing import Any, Callable, TypeVar, Dict, Optional
 
-from chromadb.quota import QuotaEnforcer
+from chromadb.api.types import (
+    Embeddings,
+    Metadatas,
+    Documents,
+    URIs,
+    IDs,
+    CollectionMetadata,
+    Where,
+    WhereDocument,
+)
+from chromadb.quota import QuotaEnforcer, Action
 from chromadb.config import System
 
 T = TypeVar("T", bound=Callable[..., Any])
@@ -20,5 +30,23 @@ class SimpleQuotaEnforcer(QuotaEnforcer):
         pass
 
     @override
-    def enforce(self, action: str, **kwargs: Dict[str, Any]) -> None:
+    def enforce(
+        self,
+        action: Action,
+        tenant: str,
+        metadatas: Optional[Metadatas] = None,
+        documents: Optional[Documents] = None,
+        embeddings: Optional[Embeddings] = None,
+        uris: Optional[URIs] = None,
+        ids: Optional[IDs] = None,
+        name: Optional[str] = None,
+        new_name: Optional[str] = None,
+        metadata: Optional[CollectionMetadata] = None,
+        new_metadata: Optional[CollectionMetadata] = None,
+        limit: Optional[int] = None,
+        where: Optional[Where] = None,
+        where_document: Optional[WhereDocument] = None,
+        n_results: Optional[int] = None,
+        query_embeddings: Optional[Embeddings] = None,
+    ) -> None:
         pass
