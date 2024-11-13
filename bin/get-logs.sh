baedb3d015ac7c2ab6915c35417e70dbe98fcd78
Sicheng Pan <sicheng@trychroma.com>
2024-11-13 12:39:38 -0800

    Update get log script

--- bin/get-logs.sh
+++ bin/get-logs.sh
@@ -1,29 +1,11 @@
 #!/usr/bin/env bash
 set -e
 
-# Hardcoded namespace
-NAMESPACE=chroma
-echo "Namespace: $NAMESPACE"
-
-# Check if the test-name and version-number are provided as arguments
-if [ -z "$1" ] || [ -z "$2" ]; then
-  echo "Usage: $0 <test-name> <version-number>"
-  exit 1  # Exit with code 1 indicating an error
-fi
-
-TEST_FILE_PATH=$1
-TEST_VERSION=$2
-
-# Extract the test file name from the path and remove the ".py" extension
-TEST_NAME=$(basename "$TEST_FILE_PATH" .py)
-echo "Test name: $TEST_NAME"
-echo "Test version: $TEST_VERSION"
+NAMESPACE="chroma"
 
 # Create a directory with the test name to store logs
-LOGS_DIR="./logs/${TEST_NAME}_${TEST_VERSION}"
-echo "Logs directory: $LOGS_DIR"
+LOGS_DIR="./logs"
 mkdir -p "$LOGS_DIR"
-echo "Created logs dir: $LOGS_DIR"
 
 # Get the list of all pods in the namespace
 PODS=$(kubectl get pods -n $NAMESPACE -o jsonpath='{.items[*].metadata.name}')
@@ -33,14 +15,5 @@ echo "Got all the pods: $PODS"
 for POD in $PODS; do
   echo "Getting logs for pod: $POD"
   # Save the logs to a file named after the pod and test name
-  kubectl logs $POD -n $NAMESPACE --since=0s > "${LOGS_DIR}/${POD}_logs.txt"
+  kubectl logs $POD -n $NAMESPACE --since=0s > "${LOGS_DIR}/${POD}.log"
 done
-
-# Zip all log files
-zip -r "${TEST_NAME}_${TEST_VERSION}_logs.zip" "$LOGS_DIR"
-
-# Print confirmation message
-echo "Logs have been zipped to ${TEST_NAME}_${TEST_VERSION}_logs.zip"
-
-# Output the path to the zip file
-echo "logs_zip_path=$(pwd)/${TEST_NAME}_${TEST_VERSION}_logs.zip" >> $GITHUB_OUTPUT
