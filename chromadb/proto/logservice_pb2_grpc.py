b55c26a6cb15dad4667d49e3d3359034bdef792a
Rohit P <rohitcp15@gmail.com>
2024-11-13 18:55:13 -0800

    [BUG] Reverse all proto version changes. (#3135)
    ## Description of changes
    
    *Summarize the changes made by this PR.*
     - Improvements & Bug fixes
    	 - Fix staging protobuf issue.
    	 - Reverts all proto version changes. And re-generated protos.
    
    ## Test plan
    *How are these changes tested?*
    
    - [ ] Tests pass locally with `pytest` for python, `yarn test` for js,
    `cargo test` for rust
    
    ## Documentation Changes
    *Are all docstrings for user-facing APIs updated if required? Do we need
    to make documentation changes in the [docs
    repository](https://github.com/chroma-core/docs)?*
    n/a

--- chromadb/proto/logservice_pb2_grpc.py
+++ chromadb/proto/logservice_pb2_grpc.py
@@ -5,8 +5,10 @@ import warnings
 
 from chromadb.proto import logservice_pb2 as chromadb_dot_proto_dot_logservice__pb2
 
-GRPC_GENERATED_VERSION = '1.66.1'
+GRPC_GENERATED_VERSION = '1.63.0'
 GRPC_VERSION = grpc.__version__
+EXPECTED_ERROR_RELEASE = '1.65.0'
+SCHEDULED_RELEASE_DATE = 'June 25, 2024'
 _version_not_supported = False
 
 try:
@@ -16,12 +18,10 @@ except ImportError:
     _version_not_supported = True
 
 if _version_not_supported:
-    raise RuntimeError(
-        f'The grpc package installed is at version {GRPC_VERSION},'
-        + f' but the generated code in chromadb/proto/logservice_pb2_grpc.py depends on'
-        + f' grpcio>={GRPC_GENERATED_VERSION}.'
-        + f' Please upgrade your grpc module to grpcio>={GRPC_GENERATED_VERSION}'
-        + f' or downgrade your generated code using grpcio-tools<={GRPC_VERSION}.'
+    warnings.warn(
+        f' This warning will become an error in {EXPECTED_ERROR_RELEASE},'
+        + f' scheduled for release on {SCHEDULED_RELEASE_DATE}.',
+        RuntimeWarning
     )
 
 
