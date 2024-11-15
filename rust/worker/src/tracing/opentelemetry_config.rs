07f9e1fdd8300d1cdc65c7b6187f46e0edb713db
Robert Escriva <robert@trychroma.com>
2024-11-15 09:50:16 -0800

    [CHORE]  Set service.name correctly for metrics.
    Confirmed in tilt, not just by looking at code.
    
    We see, e.g. the following metricsz:
    
        default_get_latency_bucket{job="compaction-service",le="250"} 529

--- rust/worker/src/tracing/opentelemetry_config.rs
+++ rust/worker/src/tracing/opentelemetry_config.rs
@@ -163,19 +163,14 @@ pub(crate) fn init_otel_tracing(service_name: &String, otel_endpoint: &String) {
 
         prev_hook(panic_info);
     }));
-    let mut metadata = tonic::metadata::MetadataMap::new();
-    metadata.insert(
-        "service.name",
-        // SAFETY(rescrv):  This will always be ascii in practice.  If it fails we will have a
-        // clear path to fixing it.
-        tonic::metadata::AsciiMetadataValue::from_str(service_name).unwrap(),
-    );
     let exporter = opentelemetry_otlp::new_exporter()
         .tonic()
-        .with_metadata(metadata)
         .with_endpoint(otel_endpoint);
     let provider = opentelemetry_otlp::new_pipeline()
         .metrics(opentelemetry_sdk::runtime::Tokio)
+        .with_resource(opentelemetry_sdk::resource::Resource::new(vec![
+            opentelemetry::KeyValue::new("service.name", service_name.clone()),
+        ]))
         .with_exporter(exporter)
         .build()
         .expect("Failed to build metrics provider");
