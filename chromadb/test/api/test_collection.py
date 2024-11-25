28c000ea353d967ae8ac20f2d3ed1e279c5a23c8
hammadb <hammad@trychroma.com>
2024-11-25 13:19:05 -0800

    add test

--- chromadb/test/api/test_collection.py
+++ chromadb/test/api/test_collection.py
@@ -1,10 +1,13 @@
 from chromadb.api import ClientAPI
+import numpy as np
+
+from chromadb.errors import InvalidDimensionException
 
 
 def test_duplicate_collection_create(
     client: ClientAPI,
 ) -> None:
-    collection = client.create_collection(
+    _ = client.create_collection(
         name="test",
         metadata={"hnsw:construction_ef": 128, "hnsw:search_ef": 128, "hnsw:M": 128},
     )
@@ -28,10 +31,34 @@ def test_not_existing_collection_delete(
     client: ClientAPI,
 ) -> None:
     try:
-        collection = client.delete_collection(
+        _ = client.delete_collection(
             name="test101",
         )
         assert False, "Expected exception"
     except Exception as e:
         print("Collection deletion failed as expected with error ", e)
         assert "does not exist" in e.args[0]
+
+
+def test_collection_dimension_mismatch(
+    client: ClientAPI,
+) -> None:
+    collection = client.create_collection(
+        name="test",
+    )
+    D = 768
+    N = 5
+    embeddings = np.random.random(size=(N, D))
+    ids = [str(i) for i in range(N)]
+
+    collection.add(ids=ids, embeddings=embeddings)  # type: ignore[arg-type]
+
+    WRONG_D = 512
+    wrong_embeddings = np.random.random(size=(N, WRONG_D))
+    try:
+        collection.add(ids=ids, embeddings=wrong_embeddings)  # type: ignore[arg-type]
+        assert False, "Expected exception"
+    except InvalidDimensionException:
+        print("Dimension mismatch failed as expected")
+    except Exception as e:
+        assert False, f"Unexpected exception {e}"
