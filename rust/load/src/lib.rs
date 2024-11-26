44ff2d3471268c49174d0c5d61854dd68318c473
Robert Escriva <robert@trychroma.com>
2024-11-26 12:50:55 -0800

    recursively resolve by name

--- rust/load/src/lib.rs
+++ rust/load/src/lib.rs
@@ -213,6 +213,11 @@ impl Workload {
                 return Err(Error::InvalidRequest(format!("workload not found: {name}")));
             }
         }
+        if let Workload::Hybrid(hybrid) = self {
+            for (_, workload) in hybrid {
+                workload.resolve_by_name(workloads)?;
+            }
+        }
         Ok(())
     }
 
