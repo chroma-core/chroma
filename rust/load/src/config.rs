3127046dc5cb683bb0c7849f374a7c8734bc7c8c
Hammad Bashir <HammadB@users.noreply.github.com>
2024-12-04 00:42:44 -0800

    [BUG] Default chroma load config path incorrect (#3241)
    ## Description of changes
    
    *Summarize the changes made by this PR.*
     - Improvements & Bug fixes
    - The default chroma load config path in the code and dockerfile was
    incorrect. If you ran the dockerfile you'd get this
    [output](https://app.warp.dev/block/7QGqrUQaQdzGcBq7pyNynH). Now it runs
    correctly.
       - Renamed the file to be more specific
    - Cleaned up unused copied code that is just confusing and distracting.
     - New functionality
       - None
    
    ## Test plan
    *How are these changes tested?*
    - [x] Tests pass locally with `pytest` for python, `yarn test` for js,
    `cargo test` for rust
    
    ## Documentation Changes
    None

--- rust/load/src/config.rs
+++ rust/load/src/config.rs
@@ -1,7 +1,7 @@
 use figment::providers::{Env, Format, Yaml};
 use serde::Deserialize;
 
-const DEFAULT_CONFIG_PATH: &str = "./chroma_config.yaml";
+const DEFAULT_CONFIG_PATH: &str = "./chroma_load_config.yaml";
 
 #[derive(Deserialize)]
 /// Root config for chroma-load service.  Can be part of a larger config file.
@@ -18,19 +18,12 @@ impl RootConfig {
     pub fn load_from_path(path: &str) -> Self {
         // Unfortunately, figment doesn't support environment variables with underscores. So we have to map and replace them.
         // Excluding our own environment variables, which are prefixed with CHROMA_.
-        let mut f = figment::Figment::from(Env::prefixed("CHROMA_").map(|k| match k {
-            k if k == "my_member_id" => k.into(),
-            k => k.as_str().replace("__", ".").into(),
-        }));
+        let mut f = figment::Figment::from(
+            Env::prefixed("CHROMA_").map(|k| return k.as_str().replace("__", ".").into()),
+        );
         if std::path::Path::new(path).exists() {
             f = figment::Figment::from(Yaml::file(path)).merge(f);
         }
-        // Apply defaults - this seems to be the best way to do it.
-        // https://github.com/SergioBenitez/Figment/issues/77#issuecomment-1642490298
-        // f = f.join(Serialized::default(
-        //     "worker.num_indexing_threads",
-        //     num_cpus::get(),
-        // ));
         let res = f.extract();
         match res {
             Ok(config) => config,
