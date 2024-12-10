92870049f880c787c2b8184fa36b1bb20c5133fa
Sicheng Pan <sicheng@trychroma.com>
2024-12-10 11:28:30 -0800

    [CLN] Cleanup query node after pushdown

--- rust/worker/src/execution/operators/mod.rs
+++ rust/worker/src/execution/operators/mod.rs
@@ -1,13 +1,7 @@
-pub(super) mod brute_force_knn;
 pub(super) mod count_records;
 pub(super) mod flush_s3;
-pub(super) mod get_vectors_operator;
-pub(super) mod hnsw_knn;
-pub(super) mod merge_knn_results;
-pub(super) mod normalize_vectors;
 pub(super) mod partition;
 pub(super) mod pull_log;
-pub(super) mod record_segment_prefetch;
 pub(super) mod register;
 pub mod spann_bf_pl;
 pub(super) mod spann_centers_search;
--- rust/worker/src/execution/orchestration/mod.rs
+++ rust/worker/src/execution/orchestration/mod.rs
@@ -1,9 +1,6 @@
 mod common;
 mod compact;
 mod count;
-mod get_vectors;
-#[allow(dead_code)]
-mod hnsw;
 mod spann_knn;
 pub(crate) use compact::*;
 pub(crate) use count::*;
