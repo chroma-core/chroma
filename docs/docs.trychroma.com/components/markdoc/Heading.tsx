dc97d4a9a886c585eef3429e03d6ef8758e67fcc
Itai Smith <itaismith@gmail.com>
2024-12-11 10:41:48 -0800

    [DOC] Fix vercel build fail

--- docs/docs.trychroma.com/components/markdoc/Heading.tsx
+++ docs/docs.trychroma.com/components/markdoc/Heading.tsx
@@ -1,5 +1,5 @@
-import * as React from 'react';
-import { useRouter } from 'next/router';
+import * as React from "react";
+import { useRouter } from "next/router";
 
 // Define the props type
 interface HeadingProps {
@@ -9,55 +9,59 @@ interface HeadingProps {
   className?: string;
 }
 
-export function Heading({ id = '', level = 1, children, className }: HeadingProps): JSX.Element {
+export function Heading({
+  id = "",
+  level = 1,
+  children,
+  className,
+}: HeadingProps): JSX.Element {
   const router = useRouter();
   const Component = `h${level}` as keyof JSX.IntrinsicElements; // This type assertion helps to ensure that Component is a valid JSX element tag
 
-  const isDocs = router.pathname.startsWith('/docs');
+  const isDocs = router.pathname.startsWith("/docs");
 
   if (level === 1) {
-    className = 'text-3xl mb-6 mt-8 font-semibold';
+    className = "text-3xl mb-6 mt-8 font-semibold";
   }
   if (level === 2) {
-    className = 'text-2xl mb-4 mt-6 font-semibold';
+    className = "text-2xl mb-4 mt-6 font-semibold";
   }
   if (level === 3) {
-    className = 'text-xl mb-4 mt-4 font-semibold';
+    className = "text-xl mb-4 mt-4 font-semibold";
   }
   if (level === 4) {
-    className = 'text-lg mb-4 mt-4 font-semibold';
+    className = "text-lg mb-4 mt-4 font-semibold";
   }
   if (level === 5) {
-    className = 'text-md mb-4 mt-4 font-semibold';
+    className = "text-md mb-4 mt-4 font-semibold";
   }
   if (level === 6) {
-    className = 'text-sm mb-4 mt-4 font-semibold';
+    className = "text-sm mb-4 mt-4 font-semibold";
   }
 
-
   const link = (
-    <Component className={['heading', className].filter(Boolean).join(' ')}>
+    <Component className={["heading", className].filter(Boolean).join(" ")}>
       <div id={id} />
       {children}
       <a
-      onClick={(e) => {
-        e.preventDefault();
-        navigator.clipboard.writeText(`${window.location.origin}${router.pathname}#${id}`);
-        // replacestate with the current url
-        window.history.replaceState(null, '', `${window.location.origin}${router.pathname}#${id}`);
-      }}
-      className="text-gray-500 hover:text-gray-700 ml-2 cursor-pointer href-anchor"
+        href="#"
+        onClick={(e: React.MouseEvent<HTMLAnchorElement>) => {
+          e.preventDefault();
+          navigator.clipboard.writeText(
+            `${window.location.origin}${router.pathname}#${id}`,
+          );
+          window.history.replaceState(
+            null,
+            "",
+            `${window.location.origin}${router.pathname}#${id}`,
+          );
+        }}
+        className="text-gray-500 hover:text-gray-700 ml-2 cursor-pointer href-anchor"
       >
         #
       </a>
     </Component>
   );
 
-  return isDocs && level !== 1 ? (
-    <a href={`#${id}`}>
-      {link}
-    </a>
-  ) : (
-    link
-  );
+  return isDocs && level !== 1 ? <a href={`#${id}`}>{link}</a> : link;
 }
