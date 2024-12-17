91f97d17f5589d139e098fd07c6a3033e7b782d0
Itai Smith <itaismith@gmail.com>
2024-12-17 15:11:34 -0800

    [RELEASE] 0.6.0

--- deployments/gcp/main.tf
+++ deployments/gcp/main.tf
@@ -23,7 +23,7 @@ variable "machine_type" {
 
 variable "chroma_version" {
   description = "Chroma version to install"
-  default     = "0.5.23"
+  default     = "0.6.0"
 }
 
 variable "chroma_server_auth_credentials" {
