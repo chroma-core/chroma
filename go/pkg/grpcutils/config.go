e4be53ab3b72fe93ff2570e655ffe9899ce944aa
Robert Escriva <robert@trychroma.com>
2025-01-09 12:21:05 -0800

    [ENH]  Add flags to the go binary for concurrency and stream processing. (#3449)
    By default, most grpc implementations allow up to 100 concurrent
    streams.  We have 200 concurrent requests, basically guaranteeing
    queueing.  Expose the two gRPC options in Go that will allow us to tune.

--- go/pkg/grpcutils/config.go
+++ go/pkg/grpcutils/config.go
@@ -4,6 +4,9 @@ type GrpcConfig struct {
 	// BindAddress is the address to bind the GRPC server to.
 	BindAddress string
 
+	MaxConcurrentStreams uint32
+	NumStreamWorkers uint32
+
 	// GRPC mTLS config
 	CertPath string
 	KeyPath  string
