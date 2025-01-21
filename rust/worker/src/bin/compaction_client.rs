a8276928ca77e21658e5853af4b05b22787ff10c
Sicheng Pan <sicheng@trychroma.com>
2025-01-21 13:47:27 -0800

    Use default allocator

--- rust/worker/src/bin/compaction_client.rs
+++ rust/worker/src/bin/compaction_client.rs
@@ -1,10 +1,3 @@
-#[cfg(not(target_env = "msvc"))]
-use tikv_jemallocator::Jemalloc;
-
-#[cfg(not(target_env = "msvc"))]
-#[global_allocator]
-static GLOBAL: Jemalloc = Jemalloc;
-
 #[tokio::main]
 async fn main() {
     worker::compaction_client_entrypoint().await;
