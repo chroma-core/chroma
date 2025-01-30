aa49aaceeca3e3b4d391fd2b79d336a7239125d8
Robert Escriva <robert@trychroma.com>
2025-01-30 13:21:30 -0800

    [BUG]  Fix Figment-based tests.
    It is unsafe to modify env-vars while a program is running.  The Jail
    macro provided by figment introduces race conditions as a result.  Move
    every test dealing with the env to an isolated integration test with one
    process per test to isolate them.

--- rust/worker/src/compactor/config.rs
+++ rust/worker/src/compactor/config.rs
@@ -1,12 +1,12 @@
 use serde::Deserialize;
 
 #[derive(Deserialize)]
-pub(crate) struct CompactorConfig {
-    pub(crate) compaction_manager_queue_size: usize,
-    pub(crate) max_concurrent_jobs: usize,
-    pub(crate) compaction_interval_sec: u64,
-    pub(crate) min_compaction_size: usize,
-    pub(crate) max_compaction_size: usize,
-    pub(crate) max_partition_size: usize,
-    pub(crate) disabled_collections: Vec<String>,
+pub struct CompactorConfig {
+    pub compaction_manager_queue_size: usize,
+    pub max_concurrent_jobs: usize,
+    pub compaction_interval_sec: u64,
+    pub min_compaction_size: usize,
+    pub max_compaction_size: usize,
+    pub max_partition_size: usize,
+    pub disabled_collections: Vec<String>,
 }
