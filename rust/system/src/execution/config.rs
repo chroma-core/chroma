13d33b40d586689b3d492a78020bc8c12b012678
hammadb <hammad@trychroma.com>
2025-01-30 14:58:31 -0800

    [BUG] Fix missing config default

--- rust/system/src/execution/config.rs
+++ rust/system/src/execution/config.rs
@@ -3,7 +3,12 @@ use serde::Deserialize;
 #[derive(Clone, Deserialize, Debug)]
 pub struct DispatcherConfig {
     pub num_worker_threads: usize,
+    #[serde(default = "default_task_queue_limit")]
     pub task_queue_limit: usize,
     pub dispatcher_queue_size: usize,
     pub worker_queue_size: usize,
+}
+
+fn default_task_queue_limit() -> usize {
+    1000
 }
