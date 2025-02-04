30c4a1111fb700091233a59b8e9da98241b0500c
Sicheng Pan <sicheng@trychroma.com>
2025-02-04 11:21:26 -0800

    Fix sql query

--- rust/segment/src/sqlite_metadata.rs
+++ rust/segment/src/sqlite_metadata.rs
@@ -11,7 +11,7 @@ use chroma_types::{
     MetadataExpression, MetadataSetValue, MetadataValue, PrimitiveOperator, SetOperator, Where,
     CHROMA_DOCUMENT_KEY,
 };
-use sea_query::{Cond, Expr, Func, IntoCondition, Query, SqliteQueryBuilder};
+use sea_query::{Expr, Func, Query, SimpleExpr, SqliteQueryBuilder};
 use sea_query_binder::SqlxBinder;
 use sqlx::Row;
 use thiserror::Error;
@@ -22,12 +22,20 @@ pub enum SqliteMetadataError {
     Sqlite(#[from] sqlx::Error),
 }
 
-trait IntoSqliteCondition {
-    fn eval(&self) -> Cond;
+trait IntoSqliteExpr {
+    /// Evaluate to a binary integer (0/1) indicating boolean value
+    /// We cannot directly use a boolean value because `Any` and `All` aggregation does not exist
+    /// We need to use `Min` and `Max` as a workaround
+    /// In SQLite, boolean can be implicitly treated as binary integer
+    fn eval(&self) -> SimpleExpr;
+
+    fn one() -> SimpleExpr {
+        Expr::value(1)
+    }
 }
 
-impl IntoSqliteCondition for Where {
-    fn eval(&self) -> Cond {
+impl IntoSqliteExpr for Where {
+    fn eval(&self) -> SimpleExpr {
         match self {
             Where::Composite(expr) => expr.eval(),
             Where::Document(expr) => expr.eval(),
@@ -36,21 +44,24 @@ impl IntoSqliteCondition for Where {
     }
 }
 
-impl IntoSqliteCondition for CompositeExpression {
-    fn eval(&self) -> Cond {
-        let mut binder = match self.operator {
-            BooleanOperator::And => Cond::all(),
-            BooleanOperator::Or => Cond::any(),
-        };
+impl IntoSqliteExpr for CompositeExpression {
+    fn eval(&self) -> SimpleExpr {
+        let mut expr = Self::one();
         for child in &self.children {
-            binder = binder.add(child.eval())
+            expr = expr.mul(match self.operator {
+                BooleanOperator::And => child.eval(),
+                BooleanOperator::Or => Self::one().sub(child.eval()),
+            })
         }
-        binder
+        match self.operator {
+            BooleanOperator::And => expr,
+            BooleanOperator::Or => Self::one().sub(expr),
+        }
     }
 }
 
-impl IntoSqliteCondition for DocumentExpression {
-    fn eval(&self) -> Cond {
+impl IntoSqliteExpr for DocumentExpression {
+    fn eval(&self) -> SimpleExpr {
         let doc_col = Expr::col((
             EmbeddingFulltextSearch::Table,
             EmbeddingFulltextSearch::StringValue,
@@ -62,15 +73,14 @@ impl IntoSqliteCondition for DocumentExpression {
                 .not_like(format!("%{}%", self.text))
                 .or(doc_col.is_null()),
         }
-        .into_condition()
     }
 }
 
-impl IntoSqliteCondition for MetadataExpression {
-    fn eval(&self) -> Cond {
+impl IntoSqliteExpr for MetadataExpression {
+    fn eval(&self) -> SimpleExpr {
         let key_cond = Expr::col((EmbeddingMetadata::Table, EmbeddingMetadata::Key))
             .eq(Expr::val(self.key.to_string()));
-        let val_cond = match &self.comparison {
+        match &self.comparison {
             MetadataComparison::Primitive(op, val) => {
                 let (col, sval) = match val {
                     MetadataValue::Bool(b) => (EmbeddingMetadata::BoolValue, Expr::val(*b)),
@@ -80,12 +90,18 @@ impl IntoSqliteCondition for MetadataExpression {
                 };
                 let scol = Expr::col((EmbeddingMetadata::Table, col));
                 match op {
-                    PrimitiveOperator::Equal => scol.eq(sval),
-                    PrimitiveOperator::NotEqual => scol.clone().ne(sval).or(scol.is_null()),
-                    PrimitiveOperator::GreaterThan => scol.gt(sval),
-                    PrimitiveOperator::GreaterThanOrEqual => scol.gte(sval),
-                    PrimitiveOperator::LessThan => scol.lt(sval),
-                    PrimitiveOperator::LessThanOrEqual => scol.lte(sval),
+                    PrimitiveOperator::Equal => Expr::expr(key_cond.and(scol.eq(sval))).max(),
+                    PrimitiveOperator::NotEqual => {
+                        Expr::expr(key_cond.and(scol.eq(sval)).not()).min()
+                    }
+                    PrimitiveOperator::GreaterThan => Expr::expr(key_cond.and(scol.gt(sval))).max(),
+                    PrimitiveOperator::GreaterThanOrEqual => {
+                        Expr::expr(key_cond.and(scol.gte(sval))).max()
+                    }
+                    PrimitiveOperator::LessThan => Expr::expr(key_cond.and(scol.lt(sval))).max(),
+                    PrimitiveOperator::LessThanOrEqual => {
+                        Expr::expr(key_cond.and(scol.lte(sval))).max()
+                    }
                 }
             }
             MetadataComparison::Set(op, vals) => {
@@ -109,12 +125,11 @@ impl IntoSqliteCondition for MetadataExpression {
                 };
                 let scol = Expr::col((EmbeddingMetadata::Table, col));
                 match op {
-                    SetOperator::In => scol.is_in(svals),
-                    SetOperator::NotIn => scol.clone().is_in(svals).or(scol.is_null()),
+                    SetOperator::In => Expr::expr(scol.is_in(svals)).max(),
+                    SetOperator::NotIn => Expr::expr(scol.is_in(svals).not()).min(),
                 }
             }
-        };
-        key_cond.and(val_cond).into_condition()
+        }
     }
 }
 
@@ -163,28 +178,58 @@ impl SqliteMetadataReader {
             },
         }: Get,
     ) -> Result<GetResult, SqliteMetadataError> {
-        let mut query = Query::select();
-        query.column((Embeddings::Table, Embeddings::EmbeddingId));
-        if document || metadata {
-            query.columns(
-                [
-                    EmbeddingMetadata::Key,
-                    EmbeddingMetadata::StringValue,
-                    EmbeddingMetadata::IntValue,
-                    EmbeddingMetadata::FloatValue,
-                    EmbeddingMetadata::BoolValue,
-                ]
-                .map(|c| (EmbeddingMetadata::Table, c)),
-            );
-        }
-
-        query.from(Embeddings::Table).and_where(
+        let mut filter_limit_query = Query::select();
+        filter_limit_query.column((Embeddings::Table, Embeddings::Id));
+        filter_limit_query.from(Embeddings::Table).and_where(
             Expr::col((Embeddings::Table, Embeddings::SegmentId))
                 .eq(collection_and_segments.metadata_segment.id.to_string()),
         );
 
-        if document || metadata || where_clause.is_some() {
-            query
+        if let Some(ids) = &query_ids {
+            filter_limit_query
+                .cond_where(Expr::col((Embeddings::Table, Embeddings::EmbeddingId)).is_in(ids));
+        }
+
+        if let Some(whr) = &where_clause {
+            filter_limit_query
+                .left_join(
+                    EmbeddingMetadata::Table,
+                    Expr::col((Embeddings::Table, Embeddings::Id))
+                        .equals((EmbeddingMetadata::Table, EmbeddingMetadata::Id)),
+                )
+                .left_join(
+                    EmbeddingFulltextSearch::Table,
+                    Expr::col((Embeddings::Table, Embeddings::Id)).equals((
+                        EmbeddingFulltextSearch::Table,
+                        EmbeddingFulltextSearch::Rowid,
+                    )),
+                )
+                .add_group_by([
+                    Expr::col((Embeddings::Table, Embeddings::Id)).into(),
+                    Expr::col((
+                        EmbeddingFulltextSearch::Table,
+                        EmbeddingFulltextSearch::StringValue,
+                    ))
+                    .into(),
+                ])
+                .cond_having(whr.eval());
+        }
+
+        filter_limit_query
+            .order_by((Embeddings::Table, Embeddings::Id), sea_query::Order::Asc)
+            .offset(skip as u64)
+            .limit(fetch.unwrap_or(u32::MAX) as u64);
+
+        let mut projection_query = Query::select();
+        projection_query
+            .column((Embeddings::Table, Embeddings::EmbeddingId))
+            .from(Embeddings::Table)
+            .and_where(
+                Expr::col((Embeddings::Table, Embeddings::Id)).in_subquery(filter_limit_query),
+            );
+
+        if document || metadata {
+            projection_query
                 .left_join(
                     EmbeddingMetadata::Table,
                     Expr::col((Embeddings::Table, Embeddings::Id))
@@ -196,26 +241,23 @@ impl SqliteMetadataReader {
                         EmbeddingFulltextSearch::Table,
                         EmbeddingFulltextSearch::Rowid,
                     ))),
+                )
+                .columns(
+                    [
+                        EmbeddingMetadata::Key,
+                        EmbeddingMetadata::StringValue,
+                        EmbeddingMetadata::IntValue,
+                        EmbeddingMetadata::FloatValue,
+                        EmbeddingMetadata::BoolValue,
+                    ]
+                    .map(|c| (EmbeddingMetadata::Table, c)),
                 );
         }
 
-        if let Some(ids) = &query_ids {
-            query.cond_where(Expr::col((Embeddings::Table, Embeddings::EmbeddingId)).is_in(ids));
-        }
+        let (sql, values) = projection_query.build_sqlx(SqliteQueryBuilder);
+        println!("{sql}");
+        println!("{values:?}");
 
-        if let Some(whr) = &where_clause {
-            query.cond_where(whr.eval());
-        }
-
-        query
-            .order_by((Embeddings::Table, Embeddings::Id), sea_query::Order::Asc)
-            .offset(skip as u64);
-
-        if let Some(limit) = fetch {
-            query.limit(limit as u64);
-        }
-
-        let (sql, values) = query.build_sqlx(SqliteQueryBuilder);
         let rows = sqlx::query_with(&sql, values)
             .fetch_all(self.db.get_conn())
             .await?;
@@ -241,16 +283,13 @@ impl SqliteMetadataReader {
                     }
 
                     if let Some(metadata) = record.metadata.as_mut() {
-                        if let Ok(s) = row.try_get(2) {
+                        if let Ok(Some(s)) = row.try_get(2) {
                             metadata.insert(key.clone(), MetadataValue::Str(s));
-                        }
-                        if let Ok(i) = row.try_get(3) {
+                        } else if let Ok(Some(i)) = row.try_get(3) {
                             metadata.insert(key.clone(), MetadataValue::Int(i));
-                        }
-                        if let Ok(f) = row.try_get(4) {
+                        } else if let Ok(Some(f)) = row.try_get(4) {
                             metadata.insert(key.clone(), MetadataValue::Float(f));
-                        }
-                        if let Ok(b) = row.try_get(5) {
+                        } else if let Ok(Some(b)) = row.try_get(5) {
                             metadata.insert(key, MetadataValue::Bool(b));
                         }
                     }
@@ -266,12 +305,18 @@ impl SqliteMetadataReader {
 
 #[cfg(test)]
 mod tests {
-    use chroma_sqlite::db::test_utils::get_new_sqlite_db;
+    use std::str::FromStr;
+
+    use chroma_sqlite::{
+        config::{MigrationHash, MigrationMode, SqliteDBConfig},
+        db::{test_utils::get_new_sqlite_db, SqliteDb},
+    };
     use chroma_types::{
         operator::{Filter, Limit, Projection, Scan},
         plan::{Count, Get},
-        BooleanOperator, CompositeExpression, DocumentExpression, MetadataComparison,
-        MetadataExpression, MetadataValue, PrimitiveOperator, Where,
+        BooleanOperator, CollectionAndSegments, CompositeExpression, DocumentExpression,
+        MetadataComparison, MetadataExpression, MetadataValue, PrimitiveOperator, SegmentUuid,
+        Where,
     };
 
     use crate::test::TestSegment;
@@ -299,21 +344,21 @@ mod tests {
             db: get_new_sqlite_db().await,
         };
 
-        metadata_reader
+        let result = metadata_reader
             .get(Get {
                 scan: Scan {
-                    collection_and_segments: TestSegment::default().into(),
+                    collection_and_segments,
                 },
                 filter: Filter {
-                    query_ids: Some(vec!["Cat".into(), "Dog".into()]),
+                    query_ids: None,
                     where_clause: Some(Where::Composite(CompositeExpression {
                         operator: BooleanOperator::Or,
                         children: vec![
                             Where::Metadata(MetadataExpression {
                                 key: "age".into(),
                                 comparison: MetadataComparison::Primitive(
-                                    PrimitiveOperator::NotEqual,
-                                    MetadataValue::Int(1),
+                                    PrimitiveOperator::GreaterThan,
+                                    MetadataValue::Int(0),
                                 ),
                             }),
                             Where::Document(DocumentExpression {
@@ -324,16 +369,16 @@ mod tests {
                     })),
                 },
                 limit: Limit {
-                    skip: 2,
-                    fetch: Some(6),
+                    skip: 0,
+                    fetch: None,
                 },
                 proj: Projection {
                     document: true,
                     embedding: false,
-                    metadata: false,
+                    metadata: true,
                 },
             })
             .await
-            .expect("Count should not fail");
+            .expect("Get should not fail");
     }
 }
--- rust/sqlite/src/lib.rs
+++ rust/sqlite/src/lib.rs
@@ -1,4 +1,4 @@
-mod config;
+pub mod config;
 pub mod db;
 mod migrations;
 pub mod table;
