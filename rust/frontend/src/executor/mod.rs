785140b722a920d688250e9c6af65daba95c5285
Macronova <60079945+Sicheng-Pan@users.noreply.github.com>
2025-02-05 06:05:44 +0800

    [ENH] Implement local executor (#3678)
    ## Description of changes
    
    *Summarize the changes made by this PR.*
     - Improvements & Bug fixes
       - N/A
     - New functionality
       - Implement local executor
    
    ## Test plan
    *How are these changes tested?*
    
    - [ ] Tests pass locally with `pytest` for python, `yarn test` for js, `cargo test` for rust
    
    ## Documentation Changes
    *Are all docstrings for user-facing APIs updated if required? Do we need to make documentation changes in the [docs repository](https://github.com/chroma-core/docs)?*

--- rust/frontend/src/executor/mod.rs
+++ rust/frontend/src/executor/mod.rs
@@ -4,32 +4,38 @@ use chroma_types::{
     ExecutorError,
 };
 use distributed::DistributedExecutor;
+use local::LocalExecutor;
 
 //////////////////////// Exposed Modules ////////////////////////
 pub(super) mod client_manager;
 pub(crate) mod config;
 mod distributed;
+mod local;
 
 //////////////////////// Main Types ////////////////////////
 #[derive(Clone, Debug)]
 pub(crate) enum Executor {
     Distributed(DistributedExecutor),
+    Local(LocalExecutor),
 }
 
 impl Executor {
     pub async fn count(&mut self, plan: Count) -> Result<CountResult, ExecutorError> {
         match self {
             Executor::Distributed(distributed_executor) => distributed_executor.count(plan).await,
+            Executor::Local(local_executor) => local_executor.count(plan).await,
         }
     }
     pub async fn get(&mut self, plan: Get) -> Result<GetResult, ExecutorError> {
         match self {
             Executor::Distributed(distributed_executor) => distributed_executor.get(plan).await,
+            Executor::Local(local_executor) => local_executor.get(plan).await,
         }
     }
     pub async fn knn(&mut self, plan: Knn) -> Result<KnnBatchResult, ExecutorError> {
         match self {
             Executor::Distributed(distributed_executor) => distributed_executor.knn(plan).await,
+            Executor::Local(local_executor) => local_executor.knn(plan).await,
         }
     }
     pub async fn is_ready(&self) -> bool {
