084427f5efb86fcddf97c9289d9538dd82738917
itaismith <42597089+itaismith@users.noreply.github.com>
2025-02-07 12:58:17 -0800

    [DOC] Adding updates section (#3505)
    ## Description of changes
    
    Adding an updates section for migration announcements and
    troubleshooting/known issues.

--- docs/docs.trychroma.com/app/[...slug]/layout.tsx
+++ docs/docs.trychroma.com/app/[...slug]/layout.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Sidebar from "@/components/sidebar/sidebar";
+import UpdatesNavbar from "@/components/header/updates-navbar";
 
 interface LayoutProps {
   children: React.ReactNode;
@@ -10,7 +11,7 @@ const PageLayout: React.FC<LayoutProps> = ({ children, params }) => {
   const { slug } = params;
 
   return (
-    <div className="flex flex-grow overflow-hidden">
+    <div className={`flex flex-grow overflow-hidden `}>
       <Sidebar path={slug} />
       <div className="flex-grow overflow-y-auto">{children}</div>
     </div>
