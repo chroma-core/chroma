ef2829384f6a8ddbfede668650c4f3ed4f0b51fd
Hammad Bashir <HammadB@users.noreply.github.com>
2025-02-10 15:59:37 -0800

    [CLN] Deprecate v1 api (#3751)
    ## Description of changes
    
    *Summarize the changes made by this PR.*
     - Improvements & Bug fixes
       - Return 410 GONE on v1 apis for GET/PUT/PATCH/DELETE/HEAD/OPTIONS
       - Misc cleanup of todos/ adding todos
       - Remove / root handling
       - 
     - New functionality
       - None
    
    ## Test plan
    *How are these changes tested?*
    - [x] Tests pass locally with `pytest` for python, `yarn test` for js,
    `cargo test` for rust
    
    ## Documentation Changes
    None

--- rust/frontend/src/bin/frontend_service.rs
+++ rust/frontend/src/bin/frontend_service.rs
@@ -2,6 +2,5 @@ use chroma_frontend::frontend_service_entrypoint;
 
 #[tokio::main]
 async fn main() {
-    // TODO(hammadb): Implement the frontend service entrypoint
     frontend_service_entrypoint().await;
 }
