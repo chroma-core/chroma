535bb5bf1bdd98f81450a6402401f6721ccc2b0c
Robert Escriva <robert@trychroma.com>
2025-02-10 13:56:38 -0800

    [ENH]  Add frontend auth support to rust frontend.
    This currently only supplies a NOP/() implementation.  Will implement
    the other server-side providers as required.

--- rust/frontend/src/bin/frontend_service.rs
+++ rust/frontend/src/bin/frontend_service.rs
@@ -2,5 +2,5 @@ use chroma_frontend::frontend_service_entrypoint;
 
 #[tokio::main]
 async fn main() {
-    frontend_service_entrypoint().await;
+    frontend_service_entrypoint(Arc::new(()) as _).await;
 }
