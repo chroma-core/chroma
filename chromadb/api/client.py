bbc7f13fd16f8e5c8ccea2636822d1e137bd8baf
macronova <trivial@invariantspace.com>
2025-02-12 14:05:04 +0800

    Reset start/stop impl

--- chromadb/api/client.py
+++ chromadb/api/client.py
@@ -61,7 +61,6 @@ class Client(SharedSystemClient, ClientAPI):
 
         # Get the root system component we want to interact with
         self._server = self._system.instance(ServerAPI)
-        self._server.start()
 
         user_identity = self.get_user_identity()
 
