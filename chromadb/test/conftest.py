d7455cbfd7e2d9d6134ae0b7257a231a76a434fb
Sicheng Pan <sicheng@trychroma.com>
2025-02-13 12:07:32 -0800

    [TST] Enable legacy tests for ephemeral Rust impl

--- chromadb/test/conftest.py
+++ chromadb/test/conftest.py
@@ -571,12 +571,10 @@ def sqlite_fixture() -> Generator[System, None, None]:
 
 @pytest.fixture
 def sqlite() -> Generator[System, None, None]:
-    # TODO: Enable Rust ephemeral client for testing
-    # if "CHROMA_RUST_BINDINGS_TEST_ONLY" in os.environ:
-    #     yield from rust_system()
-    # else:
-    #     yield from sqlite_fixture()
-    yield from sqlite_fixture()
+    if "CHROMA_RUST_BINDINGS_TEST_ONLY" in os.environ:
+        yield from rust_system(persistent=False)
+    else:
+        yield from sqlite_fixture()
 
 
 def sqlite_persistent_fixture() -> Generator[System, None, None]:
