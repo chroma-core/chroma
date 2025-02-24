8009b8d7da6998ebae1e4d5738a1bdf87fd5598d
moltrus <moltrusweb@gmail.com>
2025-02-24 15:25:53 +0530

    [ENH] Update Jina Embedding Function to support v3 with new parameters (#3095)

--- chromadb/utils/embedding_functions/jina_embedding_function.py
+++ chromadb/utils/embedding_functions/jina_embedding_function.py
@@ -11,18 +11,26 @@ logger = logging.getLogger(__name__)
 class JinaEmbeddingFunction(EmbeddingFunction[Documents]):
     """
     This class is used to get embeddings for a list of texts using the Jina AI API.
-    It requires an API key and a model name. The default model name is "jina-embeddings-v2-base-en".
+    It requires an API key and a model name. The default model name is "jina-embeddings-v3".
     """
 
-    def __init__(self, api_key: str, model_name: str = "jina-embeddings-v2-base-en"):
+    def __init__(self, api_key: str, model_name: str = "jina-embeddings-v3", task: str = "text-matching", late_chunking: bool = False, dimensions: int = 1024, embedding_type: str = "float"):
         """
         Initialize the JinaEmbeddingFunction.
 
         Args:
             api_key (str): Your API key for the Jina AI API.
-            model_name (str, optional): The name of the model to use for text embeddings. Defaults to "jina-embeddings-v2-base-en".
+            model_name (str, optional): The name of the model to use for text embeddings. Defaults to "jina-embeddings-v3".
+            task (str,  optional): The model will generate optimized embeddings for that task. Defaults to "text-matching".
+            late_chunking (bool,  optional): The model will generate contextual chunk embeddings. Defaults to "False"
+            dimensions (int,  optional): Number of dimensions. Defaults to "1024".
+            embedding_type (str, optional): Type of embedding to be returned. Defaults to "float".
         """
         self._model_name = model_name
+        self._task = task
+        self._late_chunking = late_chunking
+        self._dimensions = dimensions
+        self._embedding_type = embedding_type
         self._api_url = "https://api.jina.ai/v1/embeddings"
         self._session = httpx.Client()
         self._session.headers.update(
@@ -46,7 +54,14 @@ class JinaEmbeddingFunction(EmbeddingFunction[Documents]):
         """
         # Call Jina AI Embedding API
         resp = self._session.post(
-            self._api_url, json={"input": input, "model": self._model_name}
+            self._api_url, json={
+                "input": input, 
+                "model": self._model_name, 
+                "task": self._task, 
+                "late_chunking": self._late_chunking,
+                "dimensions": self._dimensions, 
+                "embedding_type":self._embedding_type
+            }
         ).json()
         if "data" not in resp:
             raise RuntimeError(resp["detail"])
