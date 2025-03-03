6f4ede4e747ea75296e38d59286c3eb34e315c25
Max Isom <hi@maxisom.me>
2025-03-03 13:19:24 -0800

    [ENH]: use separate exports for JS EFs

--- clients/js/src/index.ts
+++ clients/js/src/index.ts
@@ -2,17 +2,10 @@ export { ChromaClient } from "./ChromaClient";
 export { AdminClient } from "./AdminClient";
 export { CloudClient } from "./CloudClient";
 export { Collection } from "./Collection";
-export { IEmbeddingFunction } from "./embeddings/IEmbeddingFunction";
-export { OpenAIEmbeddingFunction } from "./embeddings/OpenAIEmbeddingFunction";
-export { CohereEmbeddingFunction } from "./embeddings/CohereEmbeddingFunction";
-export { TransformersEmbeddingFunction } from "./embeddings/TransformersEmbeddingFunction";
-export { DefaultEmbeddingFunction } from "./embeddings/DefaultEmbeddingFunction";
-export { HuggingFaceEmbeddingServerFunction } from "./embeddings/HuggingFaceEmbeddingServerFunction";
-export { JinaEmbeddingFunction } from "./embeddings/JinaEmbeddingFunction";
-export { GoogleGenerativeAiEmbeddingFunction } from "./embeddings/GoogleGeminiEmbeddingFunction";
-export { OllamaEmbeddingFunction } from "./embeddings/OllamaEmbeddingFunction";
+export type { IEmbeddingFunction } from "./embeddings/IEmbeddingFunction";
+export * from "./embeddings/DefaultEmbeddingFunction";
 
-export {
+export type {
   IncludeEnum,
   GetParams,
   CollectionMetadata,
--- clients/js/tsup.config.ts
+++ clients/js/tsup.config.ts
@@ -5,6 +5,16 @@ export default defineConfig((options: Options) => {
   const commonOptions: Partial<Options> = {
     entry: {
       chromadb: "src/index.ts",
+      "embeddings/cohere": "src/embeddings/CohereEmbeddingFunction.ts",
+      "embeddings/google": "src/embeddings/GoogleGeminiEmbeddingFunction.ts",
+      "embeddings/huggingface":
+        "src/embeddings/HuggingFaceEmbeddingServerFunction.ts",
+      "embeddings/jina": "src/embeddings/JinaEmbeddingFunction.ts",
+      "embeddings/ollama": "src/embeddings/OllamaEmbeddingFunction.ts",
+      "embeddings/openai": "src/embeddings/OpenAIEmbeddingFunction.ts",
+      "embeddings/transformers":
+        "src/embeddings/TransformersEmbeddingFunction.ts",
+      "embeddings/voyageai": "src/embeddings/VoyageAIEmbeddingFunction.ts",
     },
     sourcemap: true,
     dts: true,
