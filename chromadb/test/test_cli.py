e7cf77cb08242761e70b389a5ac4e06fb2687727
Itai Smith <itaismith@gmail.com>
2025-03-02 21:41:52 -0800

    Add cli to path on windows

--- chromadb/test/test_cli.py
+++ chromadb/test/test_cli.py
@@ -1,7 +1,28 @@
+import ctypes
+import os
+import platform
 import subprocess
 
 
+def is_admin():
+    try:
+        return ctypes.windll.shell32.IsUserAnAdmin() != 0
+    except Exception:
+        return False
+
+
+def windows_setup():
+    if is_admin():
+        expected_install_dir = os.path.join(os.environ.get("ProgramFiles", "C:\\Program Files"), "Chroma")
+    else:
+        expected_install_dir = os.path.join(os.environ["USERPROFILE"], "bin")
+    os.environ["PATH"] = expected_install_dir + ";" + os.environ["PATH"]
+
+
 def test_cli_wrapper():
+    if platform.system() == "Windows":
+        windows_setup()
+
     result = subprocess.run(
         ["python", "-m", "chromadb.cli.cli", "--help"],
         capture_output=True,
