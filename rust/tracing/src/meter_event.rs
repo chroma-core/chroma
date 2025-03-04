b57482f5ada2ad3b35692c34b578228d35e1293d
Sicheng Pan <sicheng@trychroma.com>
2025-03-03 16:15:30 -0800

    Define additional handler trait methods

--- rust/tracing/src/meter_event.rs
+++ rust/tracing/src/meter_event.rs
@@ -80,16 +80,14 @@ impl MeterEvent {
     }
 }
 
-pub static METER_EVENT_SENDER: OnceLock<UnboundedSender<MeterEvent>> = OnceLock::new();
+pub static METER_EVENT_SENDER: OnceLock<UnboundedSender<Option<MeterEvent>>> = OnceLock::new();
 
 impl MeterEvent {
     pub async fn submit(self) {
         if let Some(handler) = METER_EVENT_SENDER.get() {
-            if let Err(err) = handler.send(self) {
+            if let Err(err) = handler.send(Some(self)) {
                 tracing::error!("Unable to send meter event: {err}")
             }
-        } else {
-            tracing::error!("Meter event handler is unintialized")
         }
     }
 }
@@ -97,11 +95,14 @@ impl MeterEvent {
 #[async_trait]
 pub trait MeterEventHandler {
     async fn handle(&mut self, _event: MeterEvent) {}
-    async fn listen(&mut self, mut rx: UnboundedReceiver<MeterEvent>) {
-        while let Some(event) = rx.recv().await {
+    async fn listen(&mut self, mut rx: UnboundedReceiver<Option<MeterEvent>>) {
+        while let Some(event) = rx.recv().await.flatten() {
             self.handle(event).await
         }
+        self.on_stop().await;
     }
+    async fn on_start(&mut self) {}
+    async fn on_stop(&mut self) {}
 }
 
 #[async_trait]
@@ -109,11 +110,14 @@ impl MeterEventHandler for () {}
 
 pub fn init_meter_event_handler(mut handler: impl MeterEventHandler + Send + Sync + 'static) {
     let (tx, rx) = unbounded_channel();
-    let runtime_handle = Handle::current();
-    runtime_handle.spawn(async move { handler.listen(rx).await });
     if METER_EVENT_SENDER.set(tx).is_err() {
         tracing::error!("Meter event handler is already initialized")
     }
+    let runtime_handle = Handle::current();
+    runtime_handle.spawn(async move {
+        handler.on_start().await;
+        handler.listen(rx).await;
+    });
 }
 
 #[cfg(test)]
