4f5ec2c8e80ac7d93d0825347c0de87c81a14f17
macronova <trivial@invariantspace.com>
2025-03-05 10:33:09 +0800

    [CLN] Remove meter arg from frontend entrypoint

--- rust/frontend/src/bin/frontend_service.rs
+++ rust/frontend/src/bin/frontend_service.rs
@@ -4,5 +4,5 @@ use chroma_frontend::frontend_service_entrypoint;
 
 #[tokio::main]
 async fn main() {
-    frontend_service_entrypoint(Arc::new(()) as _, Arc::new(()) as _, ()).await;
+    frontend_service_entrypoint(Arc::new(()) as _, Arc::new(()) as _).await;
 }
