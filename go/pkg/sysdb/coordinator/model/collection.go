61b4fae5d0df1be2505316b9418af9168ea5b927
Drew Kim <drewkim117@gmail.com>
2025-03-04 20:16:41 -0800

    comment

--- go/pkg/sysdb/coordinator/model/collection.go
+++ go/pkg/sysdb/coordinator/model/collection.go
@@ -17,7 +17,7 @@ type Collection struct {
 	Version                    int32
 	UpdatedAt                  types.Timestamp
 	TotalRecordsPostCompaction uint64
-	SizeBytesPostCompaction    uint64
+	SizeBytesPostCompaction    uint64 // Note: This represents the size of the records off the log
 	LastCompactionTimeSecs     uint64
 }
 
