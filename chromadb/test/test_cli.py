0622486cd75dbf84375f582016c56f27a299710a
itaismith <42597089+itaismith@users.noreply.github.com>
2025-03-10 16:05:51 -0700

    Revert "[ENH] Bundle CLI in python chromadb" (#3951)
    Reverts chroma-core/chroma#3940

--- chromadb/test/test_cli.py
+++ chromadb/test/test_cli.py
@@ -1,35 +1,38 @@
 import multiprocessing
 import multiprocessing.context
-import sys
-import time
 from multiprocessing.synchronize import Event
 
-import chromadb
+from typer.testing import CliRunner
+
 from chromadb.api.client import Client
 from chromadb.api.models.Collection import Collection
+from chromadb.cli.cli import app
 from chromadb.cli.utils import set_log_file_path
 from chromadb.config import Settings, System
 from chromadb.db.base import get_sql
 from chromadb.db.impl.sqlite import SqliteDB
-from chromadb.cli import cli
 from pypika import Table
 import numpy as np
 
 from chromadb.test.property import invariants
 
-def start_app() -> None:
-    sys.argv = ["chroma", "run"]
-    cli.main()
+runner = CliRunner()
+
 
 def test_app() -> None:
-    server_process = multiprocessing.Process(target=start_app)
-    server_process.start()
-    time.sleep(5)
-    client = chromadb.HttpClient()
-    heartbeat = client.heartbeat()
-    server_process.terminate()
-    server_process.join()
-    assert heartbeat > 0
+    result = runner.invoke(
+        app,
+        [
+            "run",
+            "--path",
+            "chroma_test_data",
+            "--port",
+            "8001",
+            "--test",
+        ],
+    )
+    assert "chroma_test_data" in result.stdout
+    assert "8001" in result.stdout
 
 
 def test_utils_set_log_file_path() -> None:
@@ -67,8 +70,12 @@ def test_vacuum(sqlite_persistent: System) -> None:
         cur.execute(sql, params)
         assert cur.fetchall() == []
 
-    sys.argv = ["chroma", "vacuum", "--path", system.settings.persist_directory, "--force"]
-    cli.main()
+    result = runner.invoke(
+        app,
+        ["utils", "vacuum", "--path", system.settings.persist_directory],
+        input="y\n",
+    )
+    assert result.exit_code == 0
 
     # Maintenance log should have a vacuum entry
     with sqlite.tx() as cur:
@@ -106,7 +113,7 @@ def simulate_transactional_write(
     system.stop()
 
 
-def test_vacuum_errors_if_locked(sqlite_persistent: System, capfd) -> None:
+def test_vacuum_errors_if_locked(sqlite_persistent: System) -> None:
     """Vacuum command should fail with details if there is a long-lived lock on the database."""
     ctx = multiprocessing.get_context("spawn")
     ready_event = ctx.Event()
@@ -119,10 +126,19 @@ def test_vacuum_errors_if_locked(sqlite_persistent: System, capfd) -> None:
     ready_event.wait()
 
     try:
-        sys.argv = ["chroma", "vacuum", "--path", sqlite_persistent.settings.persist_directory, "--force"]
-        cli.main()
-        captured = capfd.readouterr()
-        assert "Failed to vacuum Chroma" in captured.err.strip()
+        result = runner.invoke(
+            app,
+            [
+                "utils",
+                "vacuum",
+                "--path",
+                sqlite_persistent.settings.persist_directory,
+                "--force",
+            ],
+            env={"CHROMA_API_IMPL": "chromadb.api.segment.SegmentAPI"},
+        )
+        assert result.exit_code == 1
+        assert "database is locked" in result.stdout
     finally:
         shutdown_event.set()
         process.join()
