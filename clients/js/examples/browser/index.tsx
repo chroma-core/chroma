866d2af6b8f896638d71f6dbd842561b5cee8a6d
Philip I. Thomas <philip@trychroma.com>
2025-03-10 08:53:42 -0700

    js client: deprecate browser examples (no longer supported)

--- clients/js/examples/browser/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { App } from "./app";
-
-// Display which package we're using
-const packageType = import.meta.env.VITE_PACKAGE || 'default (bundled)';
-console.log(`Using ChromaDB package: ${packageType}`);
-
-ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
