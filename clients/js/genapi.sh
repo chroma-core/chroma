5c39edf87c1790c6cb8348b5d8825d334695aba4
Philip I. Thomas <philip@trychroma.com>
2025-03-12 09:02:10 -0700

    update api generation

--- clients/js/genapi.sh
+++ clients/js/genapi.sh
@@ -1,6 +1,18 @@
 #!/usr/bin/env sh
 
-curl -s https://api.trychroma.com:8000/openapi.json | jq > openapi.json
+curl -s http://localhost:8000/openapi.json | jq > openapi.json
+
+# Removes `-v2` suffix so that API client library exposes latest API version without version suffix
+# Fixes duplicate version suffixes in operationIds like "_v1-v1" and remove "-v2" suffixes
+jq '
+  walk(
+    if type == "object" and has("operationId") then
+      .operationId = (.operationId | gsub("_v1-v1$"; "_v1") | gsub("-v2$"; ""))
+    else
+      .
+    end
+  )
+' openapi.json > openapi.processed.json && mv openapi.processed.json openapi.json
 
 openapi-generator-plus -c config.yml
 
