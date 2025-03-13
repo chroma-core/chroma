a6d193ff94f063db196df68bd35a4b671ef62deb
Itai Smith <itaismith@gmail.com>
2025-03-13 09:28:10 -0700

    Add more run args

--- rust/cli/src/commands/run.rs
+++ rust/cli/src/commands/run.rs
@@ -6,17 +6,56 @@ use std::sync::Arc;
 
 #[derive(Parser, Debug)]
 pub struct RunArgs {
-    #[clap(name = "config", default_value = None)]
-    config: Option<String>,
+    #[clap(
+        index = 1,
+        long = "config-path",
+        conflicts_with_all = &["path", "host", "port"],
+        help = "The path to the Chroma config file"
+    )]
+    config_path: Option<String>,
+
+    #[clap(
+        long,
+        conflicts_with = "config-path",
+        help = "The persistence path to your Chroma DB"
+    )]
+    path: Option<String>,
+
+    #[clap(
+        long,
+        default_value = "localhost",
+        conflicts_with = "config-path",
+        help = "The host to listen to. Default: localhost"
+    )]
+    host: Option<String>,
+
+    #[clap(
+        long,
+        conflicts_with = "config-path",
+        help = "The port to run the server on"
+    )]
+    port: Option<u16>,
 }
 
 pub fn run(args: RunArgs) {
     println!("{}", LOGO);
 
-    let config = match &args.config {
-        Some(path) => FrontendServerConfig::load_from_path(path),
+    let mut config = match &args.config_path {
+        Some(config_path) => FrontendServerConfig::load_from_path(config_path),
         None => FrontendServerConfig::single_node_default(),
     };
+
+    if let Some(path) = args.path {
+        config.persist_path = Some(path);
+    }
+
+    if let Some(port) = args.port {
+        config.port = port;
+    }
+
+    if let Some(host) = args.host {
+        config.listen_address = host;
+    }
 
     let runtime = tokio::runtime::Runtime::new().expect("Failed to start Chroma");
     runtime.block_on(async {
