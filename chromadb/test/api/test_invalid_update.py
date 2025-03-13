2ccaafdbc43a8575a9e9213ce0c54a4c03dcde09
Max Isom <codetheweb@users.noreply.github.com>
2025-03-13 10:49:54 -0700

    [BUG]: correctly handle replays on local HNSW writer (#3936)
    ## Description of changes
    
    Found while working on the collection method proptest for Rust.
    
    ## Test plan
    *How are these changes tested?*
    
    - [x] Tests pass locally with `pytest` for python, `yarn test` for js,
    `cargo test` for rust
    
    Added test fails on main for the Rust bindings.
    
    ## Documentation Changes
    *Are all docstrings for user-facing APIs updated if required? Do we need
    to make documentation changes in the [docs
    repository](https://github.com/chroma-core/docs)?*
    
    n/a

---
+++ chromadb/test/api/test_invalid_update.py
@@ -0,0 +1,16 @@
+import numpy as np
+from chromadb.api import ClientAPI
+from chromadb.api.types import IncludeEnum
+
+
+def test_invalid_update(client: ClientAPI) -> None:
+    collection = client.create_collection("test")
+
+    # Update is invalid because ID does not exist
+    collection.update(ids=["foo"], embeddings=[[0.0, 0.0, 0.0]])
+
+    collection.add(ids=["foo"], embeddings=[[1.0, 1.0, 1.0]])
+    result = collection.get(ids=["foo"], include=[IncludeEnum.embeddings])
+    # Embeddings should be the same as what was provided to .add()
+    assert result["embeddings"] is not None
+    assert np.allclose(result["embeddings"][0], np.array([1.0, 1.0, 1.0]))
