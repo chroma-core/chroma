d27976aa3f3faefdbbf7373c120125f0a5feeb94
Itai Smith <itaismith@gmail.com>
2025-03-15 01:06:42 -0700

    Add update to python CLI

--- chromadb/cli/cli.py
+++ chromadb/cli/cli.py
@@ -1,17 +1,20 @@
+import re
 from typing import Optional
 
 import chromadb_rust_bindings
+import requests
+from packaging.version import parse
 from rich.console import Console
 from rich.progress import Progress, SpinnerColumn, TextColumn
 import typer.rich_utils
 from typing_extensions import Annotated
 import typer
-import uvicorn
 import os
 import webbrowser
 
+import chromadb
 from chromadb.api.client import Client
-from chromadb.cli.utils import get_directory_size, set_log_file_path, sizeof_fmt
+from chromadb.cli.utils import get_directory_size, sizeof_fmt
 from chromadb.config import Settings, System
 from chromadb.db.impl.sqlite import SqliteDB
 from chromadb.ingest.impl.utils import trigger_vector_segments_max_seq_id_migration
@@ -154,6 +157,30 @@ def docs() -> None:
 
     webbrowser.open("https://docs.trychroma.com")
 
+
+def update():
+    try:
+        url = f"https://api.github.com/repos/chroma-core/chroma/releases"
+        response = requests.get(url)
+        response.raise_for_status()
+        releases = response.json()
+
+        version_pattern = re.compile(r'^\d+\.\d+\.\d+$')
+        numeric_releases = [r["tag_name"] for r in releases if version_pattern.fullmatch(r["tag_name"])]
+
+        if not numeric_releases:
+            print("Couldn't fetch the latest Chroma version")
+            return
+
+        latest = max(numeric_releases, key=parse)
+        if latest == chromadb.__version__:
+            print("Your Chroma version is up-to-date")
+            return
+
+        print(f"A new version of Chroma is available!\nIf you're using pip, run 'pip install --upgrade chromadb' to upgrade to version {latest}")
+
+    except Exception as e:
+        print("Couldn't fetch the latest Chroma version")
 
 if __name__ == "__main__":
     app()
