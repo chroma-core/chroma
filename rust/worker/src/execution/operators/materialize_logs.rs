2679cd939d50f85fcc076d1df18873ce8a95f2f2
Sicheng Pan <sicheng@trychroma.com>
2025-03-18 17:00:55 -0700

    Calculate logical size delta

--- rust/worker/src/execution/operators/materialize_logs.rs
+++ rust/worker/src/execution/operators/materialize_logs.rs
@@ -4,8 +4,9 @@ use chroma_error::ChromaError;
 use chroma_segment::blockfile_record::{RecordSegmentReader, RecordSegmentReaderCreationError};
 use chroma_segment::types::{materialize_logs, LogMaterializerError, MaterializeLogsResult};
 use chroma_system::Operator;
-use chroma_types::{Chunk, LogRecord, Segment};
+use chroma_types::{chroma_proto, Chunk, DataRecord, LogRecord, Segment};
 use futures::TryFutureExt;
+use prost::Message;
 use std::sync::atomic::AtomicU32;
 use std::sync::Arc;
 use thiserror::Error;
@@ -60,11 +61,17 @@ impl MaterializeLogInput {
     }
 }
 
+#[derive(Debug)]
+pub struct MaterializeLogOutput {
+    pub result: MaterializeLogsResult,
+    pub logical_size_delta: i64,
+}
+
 #[async_trait]
-impl Operator<MaterializeLogInput, MaterializeLogsResult> for MaterializeLogOperator {
+impl Operator<MaterializeLogInput, MaterializeLogOutput> for MaterializeLogOperator {
     type Error = MaterializeLogOperatorError;
 
-    async fn run(&self, input: &MaterializeLogInput) -> Result<MaterializeLogsResult, Self::Error> {
+    async fn run(&self, input: &MaterializeLogInput) -> Result<MaterializeLogOutput, Self::Error> {
         tracing::debug!("Materializing {} log entries", input.logs.total_len());
 
         let record_segment_reader =
@@ -85,12 +92,42 @@ impl Operator<MaterializeLogInput, MaterializeLogsResult> for MaterializeLogOper
                 }
             };
 
-        materialize_logs(
+        let result = materialize_logs(
             &record_segment_reader,
             input.logs.clone(),
             Some(input.offset_id.clone()),
         )
         .map_err(MaterializeLogOperatorError::LogMaterializationFailed)
-        .await
+        .await?;
+
+        let mut logical_size_delta = 0;
+        for record in &result {
+            let hydrated = record.hydrate(record_segment_reader.as_ref()).await?;
+            let old_size = hydrated
+                .get_data_record()
+                .map(DataRecord::get_size)
+                .unwrap_or_default() as i64;
+            let merged_metadata = hydrated.merged_metadata();
+            // NOTE: The size calculation should mirror DataRecord::get_size
+            let new_size = (hydrated.get_user_id().len()
+                + size_of_val(hydrated.merged_embeddings_ref())
+                + if merged_metadata.is_empty() {
+                    0
+                } else {
+                    chroma_proto::UpdateMetadata::from(merged_metadata)
+                        .encode_to_vec()
+                        .len()
+                }
+                + hydrated
+                    .merged_document_ref()
+                    .map(|doc| doc.len())
+                    .unwrap_or_default()) as i64;
+            logical_size_delta += new_size - old_size;
+        }
+
+        Ok(MaterializeLogOutput {
+            result,
+            logical_size_delta,
+        })
     }
 }
