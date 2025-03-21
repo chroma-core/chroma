14bb381727d2558aa8636a7a30a056cbd79bb9b9
Sicheng Pan <sicheng@trychroma.com>
2025-03-21 13:02:58 -0700

    Disable full text bench

--- rust/worker/src/compactor/compaction_manager.rs
+++ rust/worker/src/compactor/compaction_manager.rs
@@ -124,6 +124,7 @@ impl CompactionManager {
 
         match self.system {
             Some(ref system) => {
+                tracing::trace!("[COMPACTION-INFO] Jon: {compaction_job:?}");
                 let orchestrator = CompactOrchestrator::new(
                     compaction_job.clone(),
                     compaction_job.collection_id,
