d27795f4ef3a4c6b2d8a5aa93feb2adb105bc864
Jai Radhakrishnan <jairad26@gmail.com>
2025-03-24 15:26:30 -0700

    .

--- chromadb/api/client.py
+++ chromadb/api/client.py
@@ -159,7 +159,6 @@ class Client(SharedSystemClient, ClientAPI):
             get_or_create=get_or_create,
             configuration=configuration,
         )
-        print(f"configuration_json: {model.configuration_json}")
         return Collection(
             client=self._server,
             model=model,
