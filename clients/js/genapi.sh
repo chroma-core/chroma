9e09ae4209c110296f996f8426117f86728de15e
Jai Radhakrishnan <jairad26@gmail.com>
2025-03-25 10:57:36 -0700

    .

--- clients/js/genapi.sh
+++ clients/js/genapi.sh
@@ -19,14 +19,14 @@ fi
 openapi-generator-plus -c config.yml
 
 if [[ "$OSTYPE" == "darwin"* ]]; then
-  sed -i '' -e '/import "whatwg-fetch";/d' -e 's/window.fetch/fetch/g' src/generated/runtime.ts
+  sed -i '' -e '/import "whatwg-fetch";/d' -e 's/window.fetch/fetch/g' packages/chromadb-core/src/generated/runtime.ts
 else
-  sed -i -e '/import "whatwg-fetch";/d' -e 's/window.fetch/fetch/g' src/generated/runtime.ts
+  sed -i -e '/import "whatwg-fetch";/d' -e 's/window.fetch/fetch/g' packages/chromadb-core/src/generated/runtime.ts
 fi
 
 # Add isomorphic-fetch dependency to runtime.ts
 echo "import 'isomorphic-fetch';" > temp.txt
-cat src/generated/runtime.ts >> temp.txt
-mv temp.txt src/generated/runtime.ts
+cat packages/chromadb-core/src/generated/runtime.ts >> temp.txt
+mv temp.txt packages/chromadb-core/src/generated/runtime.ts
 
 rm openapi.json
