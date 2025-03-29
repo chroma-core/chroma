92d503780acea9ef00aa632a2e230c345e32e1c6
Robert Escriva <robert@trychroma.com>
2025-03-28 17:16:54 -0700

    constant

--- rust/worker/src/execution/orchestration/compact.rs
+++ rust/worker/src/execution/orchestration/compact.rs
@@ -750,7 +750,7 @@ impl Orchestrator for CompactOrchestrator {
             Box::new(FetchLogOperator {
                 log_client: self.log.clone(),
                 // TODO(rescrv): Plumb through to configure this.
-                batch_size: const { 4 * 1024 * 1024 / 16_000 },
+                batch_size: 500,
                 // Here we do not need to be inclusive since the compaction job
                 // offset is the one after the last compaction offset
                 start_log_offset_id: self.compaction_job.offset as u32,
