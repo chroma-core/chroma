89db85715c2f22ddc83436e77ac6409b79af002e
Philip I. Thomas <philip@trychroma.com>
2025-04-02 14:04:07 -0700

    fix: do not bundle packages that use sharp

--- clients/js/packages/chromadb/src/index.ts
+++ clients/js/packages/chromadb/src/index.ts
@@ -5,11 +5,8 @@
 // Import core components
 import * as core from '@internal/chromadb-core';
 
-// Force inclusion of all embedding packages in the bundle
 require('openai');
 require('@google/generative-ai');
-require('@xenova/transformers');
-require('chromadb-default-embed');
 require('cohere-ai');
 require('voyageai');
 require('ollama');
