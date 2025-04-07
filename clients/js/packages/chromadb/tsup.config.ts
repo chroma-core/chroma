1a44ad10213e676feb1582adde4eb79623c90c73
Itai Smith <itaismith@gmail.com>
2025-04-07 10:21:58 -0700

    checkpoint

--- clients/js/packages/chromadb/tsup.config.ts
+++ clients/js/packages/chromadb/tsup.config.ts
@@ -5,10 +5,19 @@ export default defineConfig((options: Options) => {
   const commonOptions: Partial<Options> = {
     entry: {
       chromadb: "src/index.ts",
+      cli: "src/cli.ts"
     },
     sourcemap: true,
     dts: true,
     target: "es2020",
+    external: [
+      'chromadb-js-bindings-darwin-arm64',
+      'chromadb-js-bindings-darwin-x64',
+      'chromadb-js-bindings-linux-arm64-gnu',
+      'chromadb-js-bindings-linux-x64-gnu',
+      'chromadb-js-bindings-win32-arm64-msvc',
+      'chromadb-js-bindings-win32-x64-msvc',
+    ],
     // Include core package and all embedding packages in the bundle for the thick client
     noExternal: [
       '@internal/chromadb-core',
