4548dbbf64e46333867776226dcdab4394d7735c
Macronova <60079945+Sicheng-Pan@users.noreply.github.com>
2025-04-11 12:24:32 -0700

    [CLN] Deprecate delete list file in GC (#4266)
    ## Description of changes
    
    *Summarize the changes made by this PR.*
     - Improvements & Bug fixes
       - The delete list is no longer used during GC and should be removed
     - New functionality
       - ...
    
    ## Test plan
    *How are these changes tested?*
    
    - [ ] Tests pass locally with `pytest` for python, `yarn test` for js, `cargo test` for rust
    
    ## Documentation Changes
    *Are all docstrings for user-facing APIs updated if required? Do we need to make documentation changes in the [docs repository](https://github.com/chroma-core/docs)?*

--- rust/garbage_collector/src/types.rs
+++ rust/garbage_collector/src/types.rs
@@ -1,7 +1,5 @@
 // GC will use it to rename a S3 file to a new name.
 pub(crate) const RENAMED_FILE_PREFIX: &str = "gc/renamed/";
-// GC will use it to list files that would be deleted.
-pub(crate) const DELETE_LIST_FILE_PREFIX: &str = "gc/delete-list/";
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, serde::Deserialize, Default)]
 #[serde(rename_all = "lowercase")]
