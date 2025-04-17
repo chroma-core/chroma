5f8a9c370da32556b065fe9342ffd4b13ace7671
Really Him <hesereallyhim@proton.me>
2025-04-17 14:52:44 -0400

    fix: double the padding relative to header level

--- docs/docs.trychroma.com/components/table-of-contents.tsx
+++ docs/docs.trychroma.com/components/table-of-contents.tsx
@@ -5,7 +5,7 @@ import { useEffect, useState } from "react";
 
 // @ts-expect-error - ignore
 export function TableOfContents({ toc }) {
-  
+
     return (
       <div className="sticky top-0 w-[300px] h-full py-5 overflow-y-auto mx-10 hidden xl:block">
         {/* Title Section */}
@@ -13,13 +13,13 @@ export function TableOfContents({ toc }) {
           <TextIcon className="h-5 w-5 pr-1" />
           &nbsp;On this page
         </div>
-  
+
         {/* Navigation Links */}
         <nav>
           {
           // @ts-expect-error - ignore
           toc.map((item) => (
-            <div key={item.id} className={`mt-1 pl-${item.level * 1}`}>
+            <div key={item.id} className={`mt-1 pl-${(item.level - 1)* 2}`}>
               <a
                 href={`#${item.id}`}
                 className={`text-gray-700 dark:text-gray-200 text-sm font-normal no-underline transition-all hover:text-blue-500
