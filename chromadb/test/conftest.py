ffa15143db01e619ffe1cf52c2e4c88789b82c2b
Robert Escriva <robert@rescrv.net>
2025-04-18 11:48:12 -0700

    normal prop testing

--- chromadb/test/conftest.py
+++ chromadb/test/conftest.py
@@ -47,7 +47,7 @@ from chromadb.api.types import Documents, Embeddings
 logger = logging.getLogger(__name__)
 
 VALID_PRESETS = ["fast", "normal", "slow"]
-CURRENT_PRESET = os.getenv("PROPERTY_TESTING_PRESET", "fast")
+CURRENT_PRESET = "normal" # os.getenv("PROPERTY_TESTING_PRESET", "fast")
 
 if CURRENT_PRESET not in VALID_PRESETS:
     raise ValueError(
