cf6b705205e840425b53fda9dbeb6b9275497976
Sanket Kedia <sanket@trychroma.com>
2025-04-18 13:09:26 -0700

    Fix uninted reader

--- rust/worker/src/execution/orchestration/spann_knn.rs
+++ rust/worker/src/execution/orchestration/spann_knn.rs
@@ -1,6 +1,9 @@
 use async_trait::async_trait;
 use chroma_distance::{normalize, DistanceFunction};
-use chroma_segment::{distributed_spann::SpannSegmentReader, spann_provider::SpannProvider};
+use chroma_segment::{
+    distributed_spann::{SpannSegmentReader, SpannSegmentReaderError},
+    spann_provider::SpannProvider,
+};
 use chroma_system::{
     wrap, ComponentContext, ComponentHandle, Dispatcher, Handler, Orchestrator, TaskMessage,
     TaskResult,
@@ -131,25 +134,6 @@ impl SpannKnnOrchestrator {
             self.send(task, ctx).await;
         }
     }
-
-    async fn set_spann_reader(&mut self, ctx: &ComponentContext<Self>) {
-        let reader_res = SpannSegmentReader::from_segment(
-            &self.collection,
-            &self.knn_filter_output.vector_segment,
-            &self.spann_provider.blockfile_provider,
-            &self.spann_provider.hnsw_provider,
-            self.knn_filter_output.dimension,
-        )
-        .await;
-        let reader = match self.ok_or_terminate(reader_res, ctx) {
-            Some(reader) => reader,
-            None => {
-                tracing::error!("Failed to create SpannSegmentReader");
-                return;
-            }
-        };
-        self.spann_reader = Some(reader);
-    }
 }
 
 #[async_trait]
@@ -176,16 +160,41 @@ impl Orchestrator for SpannKnnOrchestrator {
             ctx.receiver(),
         );
         tasks.push(knn_log_task);
-        self.set_spann_reader(ctx).await;
-        let head_search_task = wrap(
-            Box::new(self.head_search.clone()),
-            SpannCentersSearchInput {
-                reader: self.spann_reader.clone(),
-                normalized_query: self.normalized_query_emb.clone(),
+        let reader_res = SpannSegmentReader::from_segment(
+            &self.collection,
+            &self.knn_filter_output.vector_segment,
+            &self.spann_provider.blockfile_provider,
+            &self.spann_provider.hnsw_provider,
+            self.knn_filter_output.dimension,
+        )
+        .await;
+        match reader_res {
+            Ok(reader) => {
+                self.spann_reader = Some(reader.clone());
+                // Spawn the centers search task if reader is found.
+                let head_search_task = wrap(
+                    Box::new(self.head_search.clone()),
+                    SpannCentersSearchInput {
+                        reader: Some(reader),
+                        normalized_query: self.normalized_query_emb.clone(),
+                    },
+                    ctx.receiver(),
+                );
+                tasks.push(head_search_task);
+            }
+            Err(e) => match e {
+                // Segment uninited means no compaction yet.
+                SpannSegmentReaderError::UninitializedSegment => {
+                    // If the segment is uninitialized, we can skip the head search.
+                    self.spann_reader = None;
+                    self.heads_searched = true;
+                }
+                _ => {
+                    let _: Option<()> = self
+                        .ok_or_terminate(Err(KnnError::SpannSegmentReaderCreationError(e)), ctx);
+                }
             },
-            ctx.receiver(),
-        );
-        tasks.push(head_search_task);
+        }
 
         let prefetch_task = wrap(
             Box::new(PrefetchSegmentOperator::new()),
@@ -273,8 +282,8 @@ impl Handler<TaskResult<SpannCentersSearchOutput, SpannCentersSearchError>>
             None => return,
         };
         // Set state that is used for tracking when we are ready for merging.
-        self.heads_searched = true;
         self.num_outstanding_bf_pl = output.center_ids.len();
+        self.heads_searched = true;
         // Spawn fetch posting list tasks for the centers.
         for head_id in output.center_ids {
             // Invoke Head search operator.
