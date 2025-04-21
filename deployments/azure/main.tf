44ad2ae539f2fc92ddbe616908e982a6f4cc96aa
Itai Smith <itaismith@gmail.com>
2025-04-21 16:52:35 -0700

    [RELEASE] 1.0.6

--- deployments/azure/main.tf
+++ deployments/azure/main.tf
@@ -17,7 +17,7 @@ variable "machine_type" {
 
 variable "chroma_version" {
   description = "Chroma version to install"
-  default     = "1.0.5"
+  default     = "1.0.6"
 }
 
 variable "chroma_server_auth_credentials" {
