402a45812c2b66316f43c40c3211c0cb93903150
Jai Radhakrishnan <jairad26@gmail.com>
2025-05-02 15:34:19 -0700

    [ENH] replace group_by and having with distinct and where for local chroma

--- rust/segment/src/sqlite_metadata.rs
+++ rust/segment/src/sqlite_metadata.rs
@@ -382,9 +382,9 @@ trait IntoSqliteExpr {
     /// In SQLite, boolean can be implicitly treated as binary integer
     fn eval(&self) -> SimpleExpr;
 
-    fn one() -> SimpleExpr {
-        Expr::value(1)
-    }
+    // fn one() -> SimpleExpr {
+    //     Expr::value(1)
+    // }
 }
 
 impl IntoSqliteExpr for Where {
@@ -460,16 +460,25 @@ impl IntoSqliteExpr for Where {
 
 impl IntoSqliteExpr for CompositeExpression {
     fn eval(&self) -> SimpleExpr {
-        let mut expr = Self::one();
-        for child in &self.children {
-            expr = expr.mul(match self.operator {
-                BooleanOperator::And => child.eval(),
-                BooleanOperator::Or => Self::one().sub(child.eval()),
-            })
-        }
         match self.operator {
-            BooleanOperator::And => expr,
-            BooleanOperator::Or => Self::one().sub(expr),
+            BooleanOperator::And => {
+                // Start with a literal true expression
+                let mut expr = SimpleExpr::Value(sea_query::Value::Bool(Some(true)));
+                for child in &self.children {
+                    // And with each child evaluation
+                    expr = Expr::expr(expr).and(child.eval());
+                }
+                expr
+            }
+            BooleanOperator::Or => {
+                // Start with a literal false expression
+                let mut expr = SimpleExpr::Value(sea_query::Value::Bool(Some(false)));
+                for child in &self.children {
+                    // Or with each child evaluation
+                    expr = Expr::expr(expr).or(child.eval());
+                }
+                expr
+            }
         }
     }
 }
@@ -508,22 +517,76 @@ impl IntoSqliteExpr for MetadataExpression {
                 let scol = Expr::col((EmbeddingMetadata::Table, col));
                 match op {
                     PrimitiveOperator::Equal => {
-                        Expr::expr(key_cond.and(scol.eq(sval).is(true))).max()
+                        // Create a subquery that finds IDs with matching metadata
+                        let subq = Query::select()
+                            .column(EmbeddingMetadata::Id)
+                            .from(EmbeddingMetadata::Table)
+                            .and_where(key_cond.clone())
+                            .and_where(scol.eq(sval))
+                            .to_owned();
+
+                        // Return condition that embedding ID is IN this subquery
+                        Expr::col((Embeddings::Table, Embeddings::Id)).in_subquery(subq)
                     }
                     PrimitiveOperator::NotEqual => {
-                        Expr::expr(key_cond.and(scol.eq(sval).is(true)).not()).min()
+                        // Create a subquery that finds IDs with matching metadata
+                        let subq = Query::select()
+                            .column(EmbeddingMetadata::Id)
+                            .from(EmbeddingMetadata::Table)
+                            .and_where(key_cond.clone())
+                            .and_where(scol.eq(sval))
+                            .to_owned();
+
+                        // Return condition that embedding ID is NOT IN this subquery
+                        Expr::col((Embeddings::Table, Embeddings::Id)).not_in_subquery(subq)
                     }
                     PrimitiveOperator::GreaterThan => {
-                        Expr::expr(key_cond.and(scol.gt(sval).is(true))).max()
+                        // Create a subquery that finds IDs with matching metadata
+                        let subq = Query::select()
+                            .column(EmbeddingMetadata::Id)
+                            .from(EmbeddingMetadata::Table)
+                            .and_where(key_cond.clone())
+                            .and_where(scol.gt(sval))
+                            .to_owned();
+
+                        // Return condition that embedding ID is IN this subquery
+                        Expr::col((Embeddings::Table, Embeddings::Id)).in_subquery(subq)
                     }
                     PrimitiveOperator::GreaterThanOrEqual => {
-                        Expr::expr(key_cond.and(scol.gte(sval).is(true))).max()
+                        // Create a subquery that finds IDs with matching metadata
+                        let subq = Query::select()
+                            .column(EmbeddingMetadata::Id)
+                            .from(EmbeddingMetadata::Table)
+                            .and_where(key_cond.clone())
+                            .and_where(scol.gte(sval))
+                            .to_owned();
+
+                        // Return condition that embedding ID is IN this subquery
+                        Expr::col((Embeddings::Table, Embeddings::Id)).in_subquery(subq)
                     }
                     PrimitiveOperator::LessThan => {
-                        Expr::expr(key_cond.and(scol.lt(sval).is(true))).max()
+                        // Create a subquery that finds IDs with matching metadata
+                        let subq = Query::select()
+                            .column(EmbeddingMetadata::Id)
+                            .from(EmbeddingMetadata::Table)
+                            .and_where(key_cond.clone())
+                            .and_where(scol.lt(sval))
+                            .to_owned();
+
+                        // Return condition that embedding ID is IN this subquery
+                        Expr::col((Embeddings::Table, Embeddings::Id)).in_subquery(subq)
                     }
                     PrimitiveOperator::LessThanOrEqual => {
-                        Expr::expr(key_cond.and(scol.lte(sval).is(true))).max()
+                        // Create a subquery that finds IDs with matching metadata
+                        let subq = Query::select()
+                            .column(EmbeddingMetadata::Id)
+                            .from(EmbeddingMetadata::Table)
+                            .and_where(key_cond.clone())
+                            .and_where(scol.lte(sval))
+                            .to_owned();
+
+                        // Return condition that embedding ID is IN this subquery
+                        Expr::col((Embeddings::Table, Embeddings::Id)).in_subquery(subq)
                     }
                 }
             }
@@ -547,10 +610,27 @@ impl IntoSqliteExpr for MetadataExpression {
                     ),
                 };
                 let scol = Expr::col((EmbeddingMetadata::Table, col));
-                let val_in = key_cond.and(scol.is_in(svals).is(true));
                 match op {
-                    SetOperator::In => Expr::expr(val_in).max(),
-                    SetOperator::NotIn => Expr::expr(val_in.not()).min(),
+                    SetOperator::In => {
+                        let subq = Query::select()
+                            .column(EmbeddingMetadata::Id)
+                            .from(EmbeddingMetadata::Table)
+                            .and_where(key_cond.clone())
+                            .and_where(scol.is_in(svals))
+                            .to_owned();
+
+                        Expr::col((Embeddings::Table, Embeddings::Id)).in_subquery(subq)
+                    }
+                    SetOperator::NotIn => {
+                        let subq = Query::select()
+                            .column(EmbeddingMetadata::Id)
+                            .from(EmbeddingMetadata::Table)
+                            .and_where(key_cond.clone())
+                            .and_where(scol.is_in(svals))
+                            .to_owned();
+
+                        Expr::col((Embeddings::Table, Embeddings::Id)).not_in_subquery(subq)
+                    }
                 }
             }
         }
@@ -658,15 +738,8 @@ impl SqliteMetadataReader {
                         EmbeddingFulltextSearch::Rowid,
                     )),
                 )
-                .add_group_by([
-                    Expr::col((Embeddings::Table, Embeddings::Id)).into(),
-                    Expr::col((
-                        EmbeddingFulltextSearch::Table,
-                        EmbeddingFulltextSearch::StringValue,
-                    ))
-                    .into(),
-                ])
-                .cond_having(whr.eval());
+                .distinct()
+                .cond_where(whr.eval());
         }
 
         filter_limit_query
