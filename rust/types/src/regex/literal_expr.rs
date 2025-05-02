a8713426f8a21b6ebe8f40f9da63b7e8ad5b94bc
sicheng <sicheng@trychroma.com>
2025-05-01 17:30:20 -0700

    Skip flatten since hir should guarantee no nested concat

--- rust/types/src/regex/literal_expr.rs
+++ rust/types/src/regex/literal_expr.rs
@@ -23,21 +23,15 @@ impl From<Hir> for LiteralExpr {
             Hir::Class(class_unicode) => Self::Literal(vec![Literal::Class(class_unicode)]),
             Hir::Repetition { min, max: _, sub } => Hir::Concat(vec![*sub; min as usize]).into(),
             Hir::Concat(hirs) => {
-                let exprs = hirs
-                    .into_iter()
-                    .flat_map(|hir| match LiteralExpr::from(hir) {
-                        LiteralExpr::Concat(literal_exprs) => literal_exprs.into_iter(),
-                        expr => vec![expr].into_iter(),
-                    })
-                    .fold(Vec::new(), |mut exprs, expr| {
-                        match (exprs.last_mut(), expr) {
-                            (Some(Self::Literal(literal)), Self::Literal(extra_literal)) => {
-                                literal.extend(extra_literal)
-                            }
-                            (_, expr) => exprs.push(expr),
+                let exprs = hirs.into_iter().fold(Vec::new(), |mut exprs, expr| {
+                    match (exprs.last_mut(), expr) {
+                        (Some(Self::Literal(literal)), Self::Literal(extra_literal)) => {
+                            literal.extend(extra_literal)
                         }
-                        exprs
-                    });
+                        (_, expr) => exprs.push(expr),
+                    }
+                    exprs
+                });
                 Self::Concat(exprs)
             }
             Hir::Alternation(hirs) => Self::Alternation(hirs.into_iter().map(Into::into).collect()),
