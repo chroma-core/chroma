a3e7625274fc2253b22f7bbeac0a498dea73778f
Nick Arner <nicholasarner@gmail.com>
2025-05-12 15:59:01 -0700

    updated with build error notes for local development

--- rust/swift_bindings/src/lib.rs
+++ rust/swift_bindings/src/lib.rs
@@ -1,8 +1,50 @@
 // lib.rs ---------------------------------------------------------------
 use thiserror::Error;
-use anyhow::Context;                      // for .context(...)
+use anyhow::Context;           
+
+//published crate
 use chromadb::client::ChromaClient;
 use chromadb::collection::{ChromaCollection, CollectionEntries};
+
+
+// for local development...doesn't work yet
+// running `./build_swift_framework.sh` results in this error: 
+
+/* 
+error[E0034]: multiple applicable items in scope
+   --> /Users/nicholasarner/.cargo/registry/src/index.crates.io-6f17d22bba15001f/arrow-arith-52.2.0/src/temporal.rs:90:36
+    |
+90  |         DatePart::Quarter => |d| d.quarter() as i32,
+    |                                    ^^^^^^^ multiple `quarter` found
+    |
+note: candidate #1 is defined in the trait `ChronoDateExt`
+   --> /Users/nicholasarner/.cargo/registry/src/index.crates.io-6f17d22bba15001f/arrow-arith-52.2.0/src/temporal.rs:401:5
+    |
+401 |     fn quarter(&self) -> u32;
+    |     ^^^^^^^^^^^^^^^^^^^^^^^^^
+note: candidate #2 is defined in the trait `Datelike`
+   --> /Users/nicholasarner/.cargo/registry/src/index.crates.io-6f17d22bba15001f/chrono-0.4.41/src/traits.rs:47:5
+    |
+47  |     fn quarter(&self) -> u32 {
+    |     ^^^^^^^^^^^^^^^^^^^^^^^^
+help: disambiguate the method for candidate #1
+    |
+90  |         DatePart::Quarter => |d| ChronoDateExt::quarter(&d) as i32,
+    |                                  ~~~~~~~~~~~~~~~~~~~~~~~~~~
+help: disambiguate the method for candidate #2
+    |
+90  |         DatePart::Quarter => |d| Datelike::quarter(&d) as i32,
+    |                                  ~~~~~~~~~~~~~~~~~~~~~
+    
+For more information about this error, try `rustc --explain E0034`.
+error: could not compile `arrow-arith` (lib) due to 1 previous error
+warning: build failed, waiting for other jobs to finish...
+*/
+
+//use chroma::client::ChromaClient;
+//use chroma::collection::{ChromaCollection, CollectionEntries};
+
+
 
 // ----------------------------------------------------------------------
 //  UniFFI scaffolding
