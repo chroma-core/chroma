5ee695a8f180cbf8221e8464df00eccf356e49dd
Robert Escriva <robert@rescrv.net>
2025-05-15 08:44:13 -0700

    Update collection, not Insert collection

--- go/pkg/log/store/db/queries.sql.go
+++ go/pkg/log/store/db/queries.sql.go
@@ -276,7 +276,7 @@ func (q *Queries) PurgeRecords(ctx context.Context) error {
 }
 
 const sealLog = `-- name: SealLog :one
-INSERT INTO collection (id, is_sealed) values($1, true) returning id, record_compaction_offset_position, record_enumeration_offset_position, is_sealed
+UPDATE COLLECTION collection SET id = $1, is_sealed = true returning id, record_compaction_offset_position, record_enumeration_offset_position, is_sealed
 `
 
 func (q *Queries) SealLog(ctx context.Context, id string) (Collection, error) {
