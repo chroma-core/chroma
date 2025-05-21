d48cf0e2fd035784ce2aa0f759843b0ab0eac351
Robert Escriva <robert@trychroma.com>
2025-05-20 18:17:12 -0700

    [ENH]  Add a lower bound to the scout logs response. (#4566)
    ## Description of changes
    
    This allows one scout-logs request to know everything that's on the log.
    It's necessary for the range to be everything pull-logs can access.
    
    ## Test plan
    
    - [X] Tests pass locally with `pytest` for python, `yarn test` for js,
    `cargo test` for rust
    
    ## Documentation Changes
    
    N/A

--- go/pkg/log/server/server.go
+++ go/pkg/log/server/server.go
@@ -54,16 +54,18 @@ func (s *logServer) ScoutLogs(ctx context.Context, req *logservicepb.ScoutLogsRe
 	if err != nil {
 		return
 	}
+	var start int64
 	var limit int64
-	_, limit, err = s.lr.GetBoundsForCollection(ctx, collectionID.String())
+	start, limit, err = s.lr.GetBoundsForCollection(ctx, collectionID.String())
 	if err != nil {
 		return
 	}
 	// +1 to convert from the (] bound to a [) bound.
 	res = &logservicepb.ScoutLogsResponse{
+		FirstUncompactedRecordOffset: int64(start + 1),
 		FirstUninsertedRecordOffset: int64(limit + 1),
 	}
-	trace_log.Info("Scouted Logs", zap.Int64("limit", int64(limit + 1)), zap.String("collectionId", req.CollectionId))
+	trace_log.Info("Scouted Logs", zap.Int64("start", int64(start + 1)), zap.Int64("limit", int64(limit + 1)), zap.String("collectionId", req.CollectionId))
 	return
 }
 
