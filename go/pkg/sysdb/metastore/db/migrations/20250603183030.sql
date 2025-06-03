8e1b1a6d0d669ff16f8daf7afb7dc1771f38cce5
Drew Kim <drewkim117@gmail.com>
2025-06-03 11:39:11 -0700

    update index name

--- go/pkg/sysdb/metastore/db/migrations/20250603183030.sql
+++
@@ -1,4 +0,0 @@
--- Modify "tenants" table
-ALTER TABLE "public"."tenants" ADD COLUMN "static_name" text NULL;
--- Create index "idx_static_name_not_null" to table: "tenants"
-CREATE UNIQUE INDEX "idx_static_name_not_null" ON "public"."tenants" ("static_name") WHERE (static_name IS NOT NULL);
