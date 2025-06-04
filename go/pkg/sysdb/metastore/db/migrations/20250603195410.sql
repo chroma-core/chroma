ccf0cb0398f56ff3de7c29e171ea6f231480a2ca
Drew Kim <drewkim117@gmail.com>
2025-06-03 21:10:15 -0700

    sqlite migration

--- go/pkg/sysdb/metastore/db/migrations/20250603195410.sql
+++ go/pkg/sysdb/metastore/db/migrations/20250603195410.sql
@@ -1,4 +1,4 @@
 -- Modify "tenants" table
-ALTER TABLE "public"."tenants" ADD COLUMN "static_name" text NULL;
--- Create index "idx_static_name_unique" to table: "tenants"
-CREATE UNIQUE INDEX "idx_static_name_unique" ON "public"."tenants" ("static_name") WHERE (static_name IS NOT NULL);
+ALTER TABLE "public"."tenants" ADD COLUMN "resource_name" text NULL;
+-- Create index "idx_resource_name_unique" to table: "tenants"
+CREATE UNIQUE INDEX "idx_resource_name_unique" ON "public"."tenants" ("resource_name") WHERE (resource_name IS NOT NULL);
