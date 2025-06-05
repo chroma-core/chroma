f2db6192c9806181767a7c2ef6b3666e0a9d0ea1
Max Isom <hi@maxisom.me>
2025-06-05 11:47:57 -0700

    run sequentially

--- go/script/migrate_up_test.sh
+++ go/script/migrate_up_test.sh
@@ -1,15 +1,5 @@
-LOCK_FILE="/tmp/atlas_migrate.lock"
-
-exec 200>"$LOCK_FILE"
-if ! flock -n 200; then
-  echo "Waiting for another atlas instance to complete..."
-  flock 200
-fi
-
 atlas schema apply \
   -u "$1" \
   --to file://pkg/log/store/schema/ \
 --dev-url "docker://postgres/15/dev" \
 --auto-approve
-
-flock -u 200
