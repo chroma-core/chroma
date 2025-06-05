b095a60e1cabc11ef8acf7df62f3c09b34c54e13
Drew Kim <drewkim117@gmail.com>
2025-06-05 11:26:30 -0700

    test

--- rust/sysdb/src/sqlite.rs
+++ rust/sysdb/src/sqlite.rs
@@ -1148,7 +1148,7 @@ mod tests {
         // Get tenant
         let tenant = sysdb.get_tenant("new_tenant").await.unwrap();
         assert_eq!(tenant.name, "new_tenant");
-        assert_eq!(tenant.resource_name, Some("new_resource_name".to_string()));
+        assert_eq!(tenant.resource_name, None);
     }
 
     #[tokio::test]
