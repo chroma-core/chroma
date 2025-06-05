38ffeb9503a8c15396dc01b982cfc97fa96b70ce
Drew Kim <drewkim117@gmail.com>
2025-06-05 10:32:58 -0700

    rm migration

--- chromadb/migrations/sysdb/00010-tenant-resource-name.sqlite.sql
+++
@@ -1,3 +0,0 @@
--- This adds resource_name to tenants and makes it unique
-ALTER TABLE tenants ADD COLUMN resource_name text NULL;
-CREATE UNIQUE INDEX idx_resource_name_unique ON tenants (resource_name) WHERE (resource_name IS NOT NULL);
