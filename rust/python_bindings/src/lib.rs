7f6fe900607ac5152ee596e0ea8826ff287dc541
Max Isom <hi@maxisom.me>
2025-06-13 13:04:44 -0700

    fix logging

--- rust/python_bindings/src/lib.rs
+++ rust/python_bindings/src/lib.rs
@@ -1,6 +1,9 @@
 #![recursion_limit = "256"]
 mod bindings;
 mod errors;
+use std::fs::File;
+
+use simplelog::{CombinedLogger, Config, LevelFilter, WriteLogger};
 
 use crate::bindings::cli;
 use bindings::{Bindings, PythonBindingsConfig};
@@ -11,7 +14,21 @@ use chroma_sqlite::config::{MigrationHash, MigrationMode, SqliteDBConfig};
 
 #[pymodule]
 fn chromadb_rust_bindings(m: &Bound<'_, PyModule>) -> PyResult<()> {
-    // pyo3_log::Logger::new(m.py(), pyo3_log::Caching::LoggersAndLevels)?
+    // Get file name from PYTEST_XDIST_WORKER env var
+    let worker_id = std::env::var("PYTEST_XDIST_WORKER").unwrap_or_else(|_| "unknown".to_string());
+    let log_file_name = format!("chroma_rust_bindings_{}.log", worker_id);
+
+    CombinedLogger::init(vec![
+        // Only write to a file called app.log
+        WriteLogger::new(
+            LevelFilter::Info, // global filter
+            Config::default(), // timestamp, level, target, etc.
+            File::create(log_file_name).expect("Failed to create log file"),
+        ),
+    ])
+    .unwrap();
+
+    // pyo3_log::Logger::new(m.py(), pyo3_log::Caching::Nothing)?
     //     .filter(log::LevelFilter::Info)
     //     .install()
     //     .expect("Someone installed a logger before us :-(");
