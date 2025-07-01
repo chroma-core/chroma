07aef9229204e562490f6228e8f5da34484a7db5
Cooper Gamble <gamble.cooper23@gmail.com>
2025-07-01 10:23:00 -0700

    make meteringatomicu64 public

--- rust/metering/src/lib.rs
+++ rust/metering/src/lib.rs
@@ -2,9 +2,12 @@ mod core;
 mod receiver;
 mod types;
 
-pub use core::{
-    close, create, get_current, with_current, CollectionForkContext, CollectionReadContext,
-    CollectionWriteContext, Enterable, FtsQueryLength, LatestCollectionLogicalSizeBytes,
-    LogSizeBytes, MetadataPredicateCount, MeterEvent, MeteredFutureExt, PulledLogSizeBytes,
-    QueryEmbeddingCount, ReadAction, ReturnBytes, WriteAction,
+pub use {
+    core::{
+        close, create, get_current, with_current, CollectionForkContext, CollectionReadContext,
+        CollectionWriteContext, Enterable, FtsQueryLength, LatestCollectionLogicalSizeBytes,
+        LogSizeBytes, MetadataPredicateCount, MeterEvent, MeteredFutureExt, PulledLogSizeBytes,
+        QueryEmbeddingCount, ReadAction, ReturnBytes, WriteAction,
+    },
+    types::MeteringAtomicU64,
 };
