924cf4902e2f85e2d125aceaac5f8ca0050938d3
Itai Smith <itaismith@gmail.com>
2025-08-07 12:56:45 -0700

    [RELEASE] CLI 1.1.5, Python 1.0.16, JS 3.0.11

--- deployments/gcp/main.tf
+++ deployments/gcp/main.tf
@@ -23,7 +23,7 @@ variable "machine_type" {
 
 variable "chroma_version" {
   description = "Chroma version to install"
-  default     = "1.0.15"
+  default     = "1.0.16"
 }
 
 variable "chroma_server_auth_credentials" {
