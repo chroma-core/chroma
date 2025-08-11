2602d7d44bb9cb2648046602267a9273a2138cf8
Daniel Sparing <daniel@sparing.nl>
2025-08-11 18:38:04 +0200

    fix grammar in:
    "does not exists"

--- rust/types/src/api_types.rs
+++ rust/types/src/api_types.rs
@@ -69,7 +69,7 @@ pub enum GetCollectionWithSegmentsError {
     GetSegmentsError(#[from] GetSegmentsError),
     #[error("Grpc error: {0}")]
     Grpc(#[from] Status),
-    #[error("Collection [{0}] does not exists.")]
+    #[error("Collection [{0}] does not exist")]
     NotFound(String),
     #[error(transparent)]
     Internal(#[from] Box<dyn ChromaError>),
@@ -628,7 +628,7 @@ pub type GetCollectionResponse = Collection;
 pub enum GetCollectionError {
     #[error(transparent)]
     Internal(#[from] Box<dyn ChromaError>),
-    #[error("Collection [{0}] does not exists")]
+    #[error("Collection [{0}] does not exist")]
     NotFound(String),
 }
 
@@ -802,7 +802,7 @@ pub struct UpdateCollectionResponse {}
 
 #[derive(Error, Debug)]
 pub enum UpdateCollectionError {
-    #[error("Collection [{0}] does not exists")]
+    #[error("Collection [{0}] does not exist")]
     NotFound(String),
     #[error("Metadata reset unsupported")]
     MetadataResetUnsupported,
@@ -858,7 +858,7 @@ pub struct DeleteCollectionResponse {}
 
 #[derive(Error, Debug)]
 pub enum DeleteCollectionError {
-    #[error("Collection [{0}] does not exists")]
+    #[error("Collection [{0}] does not exist")]
     NotFound(String),
     #[error(transparent)]
     Validation(#[from] ChromaValidationError),
@@ -928,7 +928,7 @@ pub enum ForkCollectionError {
     Local,
     #[error(transparent)]
     Internal(#[from] Box<dyn ChromaError>),
-    #[error("Collection [{0}] does not exists")]
+    #[error("Collection [{0}] does not exist")]
     NotFound(String),
     #[error("Failed to convert proto segment")]
     SegmentConversionError(#[from] SegmentConversionError),
@@ -977,7 +977,7 @@ impl ChromaError for CountForksError {
 pub enum GetCollectionSizeError {
     #[error(transparent)]
     Internal(#[from] Box<dyn ChromaError>),
-    #[error("Collection [{0}] does not exists")]
+    #[error("Collection [{0}] does not exist")]
     NotFound(String),
 }
 
@@ -994,7 +994,7 @@ impl ChromaError for GetCollectionSizeError {
 pub enum ListCollectionVersionsError {
     #[error(transparent)]
     Internal(#[from] Box<dyn ChromaError>),
-    #[error("Collection [{0}] does not exists")]
+    #[error("Collection [{0}] does not exist")]
     NotFound(String),
 }
 
