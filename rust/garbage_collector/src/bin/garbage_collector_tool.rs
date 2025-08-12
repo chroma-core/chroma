444a69ecbe9fba11aa77d3ed0c401eacc8887862
Max Isom <hi@maxisom.me>
2025-08-12 12:54:48 -0700

    add download command

--- rust/garbage_collector/src/bin/garbage_collector_tool.rs
+++ rust/garbage_collector/src/bin/garbage_collector_tool.rs
@@ -5,13 +5,25 @@ use chroma_storage::Storage;
 use chroma_sysdb::{GetCollectionsOptions, SysDb};
 use chroma_system::Dispatcher;
 use chroma_system::Orchestrator;
+use chroma_types::chroma_proto::CollectionVersionFile;
+use chroma_types::CollectionUuid;
+use chroma_types::Segment;
 use chrono::DateTime;
 use chrono::Utc;
 use clap::Parser;
+use futures::StreamExt;
 use garbage_collector_library::{
     config::GarbageCollectorConfig,
     garbage_collector_orchestrator_v2::GarbageCollectorOrchestrator, types::CleanupMode,
 };
+use indicatif::ProgressBar;
+use indicatif::ProgressStyle;
+use prost::Message;
+use std::collections::HashSet;
+use std::io::Write;
+use std::path::PathBuf;
+use std::str::FromStr;
+use std::sync::Arc;
 
 #[derive(Debug, clap::ValueEnum, Clone)]
 enum CliCleanupMode {
@@ -34,6 +46,43 @@ impl From<CliCleanupMode> for CleanupMode {
     }
 }
 
+#[derive(Debug, clap::Args)]
+#[group(required = true, multiple = false)]
+struct CollectionIdsSource {
+    #[clap(long)]
+    collection_id: Option<String>,
+    #[clap(long)]
+    collection_ids: Option<Vec<String>>,
+    #[clap(long)]
+    read_collection_ids_stdin: Option<bool>,
+}
+
+impl CollectionIdsSource {
+    fn get_collection_ids(&self) -> HashSet<CollectionUuid> {
+        if self.read_collection_ids_stdin.unwrap_or(false) {
+            let mut collection_ids = String::new();
+            std::io::stdin()
+                .read_line(&mut collection_ids)
+                .expect("Failed to read from stdin");
+            collection_ids
+                .trim()
+                .split(',')
+                .map(|id| {
+                    CollectionUuid::from_str(id.trim()).expect("Invalid collection ID format")
+                })
+                .collect()
+        } else if let Some(id) = &self.collection_id {
+            HashSet::from([CollectionUuid::from_str(id).expect("Invalid collection ID format")])
+        } else if let Some(ids) = &self.collection_ids {
+            ids.iter()
+                .map(|id| CollectionUuid::from_str(id).expect("Invalid collection ID format"))
+                .collect()
+        } else {
+            unreachable!()
+        }
+    }
+}
+
 #[derive(Debug, Parser)]
 enum GarbageCollectorCommand {
     /// Manually run garbage collection on a specific collection
@@ -51,6 +100,17 @@ enum GarbageCollectorCommand {
         #[arg(long)]
         collection_soft_delete_absolute_cutoff_time: DateTime<Utc>,
     },
+
+    DownloadCollection {
+        #[clap(flatten)]
+        collection_id_source: CollectionIdsSource,
+        #[arg(long)]
+        output_directory: PathBuf,
+        #[arg(long, default_value = "false")]
+        include_blocks: bool,
+        #[arg(short, long, default_value = "10")]
+        download_concurrency: usize,
+    },
 }
 
 #[derive(Debug, Parser)]
@@ -61,6 +121,24 @@ struct Args {
 
     #[command(subcommand)]
     command: GarbageCollectorCommand,
+}
+
+async fn download_file(
+    storage_client: &Storage,
+    storage_key: &str,
+    output_directory: &PathBuf,
+) -> Result<Arc<Vec<u8>>, Box<dyn std::error::Error>> {
+    let output_path = output_directory.join(storage_key);
+
+    if output_path.exists() {
+        let data = std::fs::read(output_path)?;
+        return Ok(Arc::new(data));
+    }
+
+    let data = storage_client.get(storage_key, Default::default()).await?;
+    std::fs::create_dir_all(output_path.parent().unwrap())?;
+    std::fs::write(output_path, data.as_ref())?;
+    Ok(data)
 }
 
 #[tokio::main]
@@ -151,6 +229,156 @@ async fn main() {
             );
 
             orchestrator.run(system.clone()).await.unwrap();
+        }
+
+        GarbageCollectorCommand::DownloadCollection {
+            collection_id_source,
+            output_directory,
+            include_blocks,
+            download_concurrency,
+        } => {
+            if include_blocks {
+                unimplemented!("Downloading collections with blocks is not yet implemented");
+            }
+
+            let registry = chroma_config::registry::Registry::new();
+            let config = GarbageCollectorConfig::load_from_path(&args.config_path);
+
+            let storage_client = Storage::try_from_config(&config.storage_config, &registry)
+                .await
+                .expect("Failed to create storage client");
+
+            let mut sysdb_client = SysDb::try_from_config(
+                &chroma_sysdb::SysDbConfig::Grpc(config.sysdb_config),
+                &registry,
+            )
+            .await
+            .expect("Failed to create sysdb client");
+
+            let collection_ids = collection_id_source.get_collection_ids();
+
+            let collections = sysdb_client
+                .get_collections(GetCollectionsOptions {
+                    collection_ids: Some(collection_ids.iter().cloned().collect()),
+                    ..Default::default()
+                })
+                .await
+                .expect("Failed to get collections");
+
+            if collections.len() != collection_ids.len() {
+                tracing::error!(
+                    "Expected {} collections, but found {}",
+                    collection_ids.len(),
+                    collections.len()
+                );
+                return;
+            }
+
+            let object_store_output_directory = output_directory.join("object_store");
+            let collections_jsonl_path = output_directory.join("collections.jsonl");
+
+            std::fs::create_dir_all(&collections_jsonl_path.parent().unwrap())
+                .expect("Failed to create output directory");
+
+            let mut collections_jsonl_file = std::fs::File::create(&collections_jsonl_path)
+                .expect("Failed to create JSONL file");
+
+            for collection in &collections {
+                let collection_json = serde_json::to_string(collection)
+                    .expect("Failed to serialize collection to JSON");
+                writeln!(collections_jsonl_file, "{}", collection_json)
+                    .expect("Failed to write to JSONL file");
+            }
+
+            let bar = ProgressBar::new(collections.len() as u64);
+            bar.set_style(
+                ProgressStyle::default_spinner()
+                    .template("{msg} {spinner} {bar:40.cyan/blue} {pos}/{len} {eta}")
+                    .unwrap(),
+            );
+            bar.enable_steady_tick(std::time::Duration::from_millis(100));
+            bar.set_message("Downloading version and lineage files...");
+
+            let sparse_indices_to_fetch = futures::stream::iter(collections)
+                .map(|collection| {
+                    let storage_client = storage_client.clone();
+                    let object_store_output_directory = &object_store_output_directory;
+                    let bar = bar.clone();
+                    async move {
+                        let version_file = download_file(
+                            &storage_client,
+                            collection.version_file_path.as_ref().unwrap(),
+                            &object_store_output_directory,
+                        )
+                        .await
+                        .expect("Failed to download version file");
+
+                        let decoded_version_file =
+                            CollectionVersionFile::decode(version_file.as_slice()).unwrap();
+
+                        let mut sparse_indices_to_fetch = HashSet::new();
+
+                        for version in decoded_version_file.version_history.unwrap().versions {
+                            for segment in version.segment_info.unwrap().segment_compaction_info {
+                                for (_, value) in segment
+                                    .file_paths
+                                    .into_iter()
+                                    .filter(|(k, _)| !k.contains("hnsw"))
+                                {
+                                    for path in value.paths {
+                                        let (prefix, id) =
+                                            Segment::extract_prefix_and_id(&path).unwrap();
+                                        sparse_indices_to_fetch
+                                            .insert(RootManager::get_storage_key(prefix, &id));
+                                    }
+                                }
+                            }
+                        }
+
+                        if let Some(lineage_file_path) = &collection.lineage_file_path {
+                            download_file(
+                                &storage_client,
+                                lineage_file_path,
+                                &object_store_output_directory,
+                            )
+                            .await
+                            .expect("Failed to download lineage file");
+                        }
+
+                        bar.inc(1);
+                        tokio::time::sleep(std::time::Duration::from_millis(100)).await;
+
+                        futures::stream::iter(sparse_indices_to_fetch)
+                    }
+                })
+                .buffer_unordered(download_concurrency)
+                .flatten()
+                .collect::<HashSet<String>>()
+                .await;
+
+            bar.reset();
+            bar.set_length(sparse_indices_to_fetch.len() as u64);
+            bar.set_message("Fetching sparse indices...");
+
+            futures::stream::iter(sparse_indices_to_fetch)
+                .map(|path| {
+                    let storage_client = storage_client.clone();
+                    let object_store_output_directory = &object_store_output_directory;
+                    let bar = bar.clone();
+                    async move {
+                        download_file(&storage_client, &path, &object_store_output_directory)
+                            .await
+                            .expect("Failed to download sparse index file");
+
+                        bar.inc(1);
+                        tokio::time::sleep(std::time::Duration::from_millis(100)).await;
+                    }
+                })
+                .buffer_unordered(download_concurrency)
+                .collect::<()>()
+                .await;
+
+            bar.finish();
         }
     }
 }
