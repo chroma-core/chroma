2dce764bcb300256a26ff8f5841fa91ea8c47cb2
grf53 <grf0503@gmail.com>
2025-08-25 22:09:56 +0900

    [BUG] AttributeError on "FastAPI.get"

--- chromadb/server/fastapi/__init__.py
+++ chromadb/server/fastapi/__init__.py
@@ -1123,8 +1123,7 @@ class FastAPI(Server):
 
         if get_result["embeddings"] is not None:
             get_result["embeddings"] = [
-                cast(Embedding, embedding).tolist()
-                for embedding in get_result["embeddings"]
+                cast(Embedding, embedding) for embedding in get_result["embeddings"]
             ]
 
         return get_result
