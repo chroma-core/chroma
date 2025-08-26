89ec6cd0f5bfc4569318313477fbc5b0927e3481
Robert Escriva <robert@trychroma.com>
2025-08-26 13:25:45 -0700

    [ENH]  Make a describe-all.txt that describes the cluster state after testing.
    I suspect that our test add failures are because of memory pressure
    causing a pod to crash.  I suspect this because I've seen failures to
    connect to etcd and the kind cluster, which shouldn't happen once tilt
    believes itself to be up and alive.
    
    This PR dumps kubectl describe -A all to get stuff like pod restarts
    into the test data that we log per test run.

--- bin/get-logs.sh
+++ bin/get-logs.sh
@@ -17,6 +17,9 @@ TEMP_DIR=$(mktemp -d)
 mkdir "$TEMP_DIR/logs"
 mkdir "$TEMP_DIR/traces"
 
+# Create a description of the k8s cluster
+kubectl describe -A all > "${TEMP_DIR}/logs/describe-all.txt" || true
+
 # Get the list of all pods in the namespace
 PODS=$(kubectl get pods -n $NAMESPACE -o jsonpath='{.items[*].metadata.name}')
 echo "Got all the pods: $PODS"
