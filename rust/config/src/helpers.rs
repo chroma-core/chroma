eb3b2f4c0381a5bfe63a33760725cd521a6ce70e
Cooper Gamble <gamble.cooper23@gmail.com>
2025-09-03 13:31:41 -0700

    update

--- rust/config/src/helpers.rs
+++ rust/config/src/helpers.rs
@@ -21,6 +21,6 @@ pub fn deserialize_duration_from_milliseconds<'de, D>(d: D) -> Result<Duration, 
 where
     D: serde::Deserializer<'de>,
 {
-    let secs: u64 = serde::Deserialize::deserialize(d)?;
-    Ok(Duration::from_millis(secs))
+    let millis: u64 = serde::Deserialize::deserialize(d)?;
+    Ok(Duration::from_millis(millis))
 }
