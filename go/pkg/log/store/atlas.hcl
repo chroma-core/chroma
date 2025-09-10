eb3fa7a555a442dde66d777b855d76243edbe0de
Robert Escriva <robert@trychroma.com>
2025-09-10 10:40:11 -0700

    [CLN]  Remove the go log service code and Docker.

--- go/pkg/log/store/atlas.hcl
+++
@@ -1,6 +0,0 @@
-env "prod" {
-  url = getenv("CHROMA_DB_LOG_URL")
-  migration {
-    dir = "file://migrations"
-  }
-}
--- go/pkg/log/store/schema/record_log.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE record_log (
-                        "offset" BIGINT NOT NULL,
-                        collection_id text NOT NULL,
-                        timestamp BIGINT NOT NULL,
-                        record bytea NOT NULL,
-                        PRIMARY KEY(collection_id, "offset")
-);
