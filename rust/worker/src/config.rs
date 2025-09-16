0c74ccf2f171d16d220de5baeca1489805f071b7
Macronova <60079945+Sicheng-Pan@users.noreply.github.com>
2025-09-16 12:33:47 -0700

    [ENH] Tenant override for bm25 (#5483)
    ## Description of changes
    
    _Summarize the changes made by this PR._
    
    - Improvements & Bug fixes
      - N/A
    - New functionality
    - Adds a temporary config to always enable bm25 for certain tenants.
    This should be removed once we have collection schema ready.
    
    ## Test plan
    
    _How are these changes tested?_
    
    - [ ] Tests pass locally with `pytest` for python, `yarn test` for js,
    `cargo test` for rust
    
    ## Migration plan
    
    _Are there any migrations, or any forwards/backwards compatibility
    changes needed in order to make sure this change deploys reliably?_
    
    ## Observability plan
    
    _What is the plan to instrument and monitor this change?_
    
    ## Documentation Changes
    
    _Are all docstrings for user-facing APIs updated if required? Do we need
    to make documentation changes in the [docs
    section](https://github.com/chroma-core/chroma/tree/main/docs/docs.trychroma.com)?_

--- rust/worker/src/config.rs
+++ rust/worker/src/config.rs
@@ -5,7 +5,7 @@ use chroma_sysdb::SysDbConfig;
 use chroma_tracing::{OtelFilter, OtelFilterLevel};
 use figment::providers::{Env, Format, Yaml};
 use serde::{Deserialize, Serialize};
-use std::time::Duration;
+use std::{collections::HashSet, time::Duration};
 
 const DEFAULT_CONFIG_PATH: &str = "./chroma_config.yaml";
 
@@ -151,6 +151,10 @@ pub struct QueryServiceConfig {
         default = "QueryServiceConfig::default_grpc_shutdown_grace_period"
     )]
     pub grpc_shutdown_grace_period: Duration,
+    // TODO: This is a temporary config to enable bm25 for certain tenants.
+    // This should be removed once we have collection schema ready.
+    #[serde(default)]
+    pub bm25_tenant: HashSet<String>,
 }
 
 impl QueryServiceConfig {
