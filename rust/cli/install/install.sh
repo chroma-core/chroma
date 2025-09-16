27cd7ac9024f808cabe9f6b035a029d8a056d3db
Jai Radhakrishnan <jairad26@gmail.com>
2025-09-16 11:33:13 -0700

    [RELEASE] Python 1.1.0, CLI 1.1.10, JS 3.0.16

--- rust/cli/install/install.sh
+++ rust/cli/install/install.sh
@@ -7,7 +7,7 @@ set -e
 #   curl -sSL https://raw.githubusercontent.com/chroma-core/chroma/main/rust/cli/install/install.sh | bash
 # ----------------------------------------------
 REPO="chroma-core/chroma"
-RELEASE="cli-1.1.9"
+RELEASE="cli-1.1.10"
 
 OS=$(uname -s)
 ARCH=$(uname -m)
