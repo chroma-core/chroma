2424f86846ed7a7abb3b31bff8ca8d0931fbd3bf
Cooper Gamble <cooper@trychroma.com>
2025-09-24 10:12:20 -0700

    [CHORE] move middleware helper behind a feature in chroma-tracing (#5515)
    ## Description of changes
    
    Move the middleware helper behind a `middleware` feature in
    `chroma-tracing`.
    
    ## Test plan
    
    - [x] Tests pass locally with `pytest` for python, `yarn test` for js,
    `cargo test` for rust

--- rust/tracing/src/lib.rs
+++ rust/tracing/src/lib.rs
@@ -3,6 +3,7 @@ pub mod grpc_client_trace_layer;
 #[cfg(feature = "grpc")]
 pub mod grpc_server_trace_layer;
 pub mod init_tracer;
+#[cfg(feature = "middleware")]
 mod tower_tracing;
 pub mod util;
 
@@ -14,4 +15,6 @@ pub use init_tracer::{
     init_global_filter_layer, init_otel_layer, init_otel_tracing, init_panic_tracing_hook,
     init_stdout_layer, init_tracing, OtelFilter, OtelFilterLevel,
 };
+
+#[cfg(feature = "middleware")]
 pub use tower_tracing::add_tracing_middleware;
