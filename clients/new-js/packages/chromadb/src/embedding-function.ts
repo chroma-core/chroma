8c386aaff23cfb6b833fca14b8c1804219bb6c5f
Sanket Kedia <kediasanket11121993@gmail.com>
2025-10-17 15:53:19 -0700

    [BUG]: Fix JS client (#5634)
    ## Description of changes
    
    _Summarize the changes made by this PR._
    
    - Improvements & Bug fixes
      - Fix js client tests
    - New functionality
      - ...
    
    ## Test plan
    
    _How are these changes tested?_
    - [x] Tests pass locally with `pytest` for python, `yarn test` for js,
    `cargo test` for rust
    
    ## Migration plan
    None
    
    ## Observability plan
    None
    
    ## Documentation Changes
    None

--- clients/new-js/packages/chromadb/src/embedding-function.ts
+++ clients/new-js/packages/chromadb/src/embedding-function.ts
@@ -251,6 +251,13 @@ export const getSparseEmbeddingFunction = async (
     return undefined;
   }
 
+  if (efConfig.type === "unknown") {
+    console.warn(
+      `Unknown embedding function configuration for collection ${collectionName}. 'add' and 'query' will fail unless you provide them embeddings directly.`,
+    );
+    return undefined;
+  }
+
   const name = efConfig.name;
 
   const sparseEmbeddingFunction = knownSparseEmbeddingFunctions.get(name);
