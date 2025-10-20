0247b6152b578e26123518160a0fcf116fc09cb7
Robert Escriva <robert@trychroma.com>
2025-10-19 23:05:23 -0700

    [BUG]  Remove chroma load because it pulls in SSL. (#5664)
    ## Description of changes
    
    chroma-load uses a third-party rust client.  It pulls in ssl.  As we
    have problems in CI due to SSL and our own client is held up by it,
    remove chroma load until we can port to it.
    
    ## Test plan
    
    CI
    
    ## Migration plan
    
    N/A
    
    ## Observability plan
    
    N/A
    
    ## Documentation Changes
    
    N/A

--- rust/load/src/bin/chroma-load.rs
+++ rust/load/src/bin/chroma-load.rs
@@ -1,6 +1,6 @@
-//! The main chroma-load service.
-
-#[tokio::main]
-async fn main() {
-    chroma_load::entrypoint().await;
+fn main() {
+    println!("moo!");
+    // Stop once per day.
+    std::thread::sleep(std::time::Duration::from_secs(86_400));
+    println!("going down!");
 }
