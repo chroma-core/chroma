ef2ecc8540245399f8068d99d82d93f1f773af0e
Robert Escriva <robert@trychroma.com>
2025-10-20 18:22:04 -0700

    [ENH]  Export types from `chroma` crate (#5676)
    ## Description of changes
    
    For ergonomics, export every type referenced by the chroma::collection
    crate from chroma::types::*.
    
    ## Test plan
    
    CI
    
    ## Migration plan
    
    N/A
    
    ## Observability plan
    
    N/A
    
    ## Documentation Changes
    
    N/A

--- rust/chroma/src/types.rs
+++ rust/chroma/src/types.rs
@@ -1,1 +1,9 @@
 pub use chroma_api_types::{GetUserIdentityResponse, HeartbeatResponse};
+
+pub use chroma_types::{
+    plan::SearchPayload, AddCollectionRecordsRequest, AddCollectionRecordsResponse, Collection,
+    DeleteCollectionRecordsRequest, DeleteCollectionRecordsResponse, ForkCollectionRequest,
+    GetRequest, GetResponse, IncludeList, InternalSchema, Metadata, QueryRequest, QueryResponse,
+    SearchRequest, SearchResponse, UpdateCollectionRecordsRequest, UpdateCollectionRecordsResponse,
+    UpdateMetadata, UpsertCollectionRecordsRequest, UpsertCollectionRecordsResponse, Where,
+};
