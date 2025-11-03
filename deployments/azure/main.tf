66fa6055af7a4cc7e7b472a7c95e447883b1be06
Itai Smith <itaismith@gmail.com>
2025-11-03 10:24:17 -0800

    [RELEASE] Python 1.3.1, JS 3.1.1, CLI 1.2.1

--- deployments/azure/main.tf
+++ deployments/azure/main.tf
@@ -17,7 +17,7 @@ variable "machine_type" {
 
 variable "chroma_version" {
   description = "Chroma version to install"
-  default     = "1.3.0"
+  default     = "1.3.1"
 }
 
 variable "chroma_server_auth_credentials" {
