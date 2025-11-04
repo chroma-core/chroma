605fef4c4b1e4e6368096bde60580fa2b01d73ef
Jai Radhakrishnan <55522316+jairad26@users.noreply.github.com>
2025-11-03 18:29:06 -0800

    [RELEASE] Python 1.3.2 (#5801)
    ## Description of changes
    
    _Summarize the changes made by this PR._
    
    - Improvements & Bug fixes
      - Release python version 1.3.2
    - New functionality
      - ...
    
    ## Test plan
    
    _How are these changes tested?_
    
    - [ ] Tests pass locally with `pytest` for python, `yarn test` for js,
    `cargo test` for rust
    
    ## Migration plan
    
    _Are there any migrations, or any forwards/backwards compatibility
    changes needed in order to make sure this change deploys reliably?_
    
    ## Observability plan
    
    _What is the plan to instrument and monitor this change?_
    
    ## Documentation Changes
    
    _Are all docstrings for user-facing APIs updated if required? Do we need
    to make documentation changes in the [docs
    section](https://github.com/chroma-core/chroma/tree/main/docs/docs.trychroma.com)?_

--- deployments/gcp/main.tf
+++ deployments/gcp/main.tf
@@ -23,7 +23,7 @@ variable "machine_type" {
 
 variable "chroma_version" {
   description = "Chroma version to install"
-  default     = "1.3.1"
+  default     = "1.3.2"
 }
 
 variable "chroma_server_auth_credentials" {
