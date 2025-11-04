d406e19e04ca1d7317967c7b70d92882b6811262
Jai Radhakrishnan <jairad26@gmail.com>
2025-11-04 14:37:18 -0800

    [RELEASE] Python 1.3.3

--- deployments/gcp/main.tf
+++ deployments/gcp/main.tf
@@ -23,7 +23,7 @@ variable "machine_type" {
 
 variable "chroma_version" {
   description = "Chroma version to install"
-  default     = "1.3.2"
+  default     = "1.3.3"
 }
 
 variable "chroma_server_auth_credentials" {
