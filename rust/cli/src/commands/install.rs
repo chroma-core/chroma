87b0b9c244578fda8567ce320fab03945d99fdd6
Itai Smith <itaismith@gmail.com>
2025-12-04 10:58:09 -0800

    [ENV] Support cloud env variables for sample apps

--- rust/cli/src/commands/install.rs
+++ rust/cli/src/commands/install.rs
@@ -1,6 +1,6 @@
 use crate::ui_utils::read_secret;
 use crate::utils::UtilsError::UserInputFailed;
-use crate::utils::{read_config, write_config, CliConfig, CliError, SELECTION_LIMIT};
+use crate::utils::{get_current_profile, read_config, write_config, CliConfig, CliError, Profile, SELECTION_LIMIT};
 use clap::Parser;
 use colored::Colorize;
 use dialoguer::theme::ColorfulTheme;
@@ -44,8 +44,12 @@ pub enum InstallError {
 pub struct InstallArgs {
     #[clap(index = 1, help = "The name of the sample app to install")]
     name: Option<String>,
-    #[clap(long, conflicts_with_all = ["name"])]
+    #[clap(long, conflicts_with_all = ["name", "local"])]
     list: bool,
+    #[clap(long)]
+    local: bool,
+    #[clap(long)]
+    db: String,
     #[clap(long, hide = true)]
     dev: Option<String>,
 }
@@ -98,7 +102,16 @@ impl SampleAppEnvVariables {
                 "http://localhost:8000".to_string(),
             ),
             ("CHROMA_TENANT".to_string(), "default_tenant".to_string()),
-            ("CHROMA_DB_NAME".to_string(), "default_database".to_string()),
+            ("CHROMA_DATABASE".to_string(), "default_database".to_string()),
+        ]);
+        SampleAppEnvVariables(map)
+    }
+    pub fn cloud(profile: Profile, db_name: String) -> Self {
+        let map = HashMap::from([
+            ("CHROMA_HOST".to_string(), "https://api.trychroma.com".to_string()),
+            ("CHROMA_TENANT".to_string(), profile.tenant_id),
+            ("CHROMA_DATABASE".to_string(), db_name),
+            ("CHROMA_API_KEY".to_string(), profile.api_key),
         ]);
         SampleAppEnvVariables(map)
     }
@@ -437,8 +450,14 @@ fn write_env_file(
     Ok(())
 }
 
-fn get_app_env_variables(app_config: &SampleAppConfig) -> Result<SampleAppEnvVariables, CliError> {
-    let mut env_variables = SampleAppEnvVariables::local();
+fn get_app_env_variables(app_config: &SampleAppConfig, local: bool) -> Result<SampleAppEnvVariables, CliError> {
+    let mut env_variables = match local {
+        false => {
+            let (_, current_profile) = get_current_profile()?;
+            SampleAppEnvVariables::cloud(current_profile, "".to_string())
+        },
+        true => SampleAppEnvVariables::local()
+    };
 
     app_config
         .required_env_variables
@@ -512,7 +531,7 @@ async fn install_sample_app(args: InstallArgs) -> Result<(), CliError> {
     let app_config =
         read_app_config(app_name.as_str()).map_err(|_| InstallError::AppConfigReadFailed)?;
 
-    let env_variables = get_app_env_variables(&app_config)?;
+    let env_variables = get_app_env_variables(&app_config, args.local)?;
     write_env_file(env_variables, format!("./{}/.env", app_name))
         .map_err(|_| InstallError::EnvFileWriteFailed)?;
 
