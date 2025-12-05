118ffafd8df8b95cbfd7dfdc121c7df745a0ab45
Kyle D <kyle@trychroma.com>
2025-12-05 12:33:43 -0800

    [DOC] Fix doc 404 and 500 and other stuff

--- docs/docs.trychroma.com/middleware.ts
+++ docs/docs.trychroma.com/middleware.ts
@@ -78,7 +78,7 @@ const legacyPathsMapping: Record<string, string> = {
 };
 
 export const middleware = (request: NextRequest) => {
-  const path = request.nextUrl.pathname;
+  const path = request.nextUrl.pathname.toLowerCase();
 
   if (path in legacyPathsMapping) {
     const currentPath = legacyPathsMapping[path];
