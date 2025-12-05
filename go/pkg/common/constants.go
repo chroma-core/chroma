dbbe74c0cb5f9393c97679b6cb88411f3c1d965f
Tanuj Nayak <tanujnay112@live.com>
2025-12-04 17:56:45 -0800

    feat: add output collection metadata and prevent attaching to output collections
    - Add chroma:attached_function_id metadata when creating output collections
    - Add check in AttachFunction to prevent attaching functions to output collections
    - Add ErrCannotAttachToOutputCollection error

--- go/pkg/common/constants.go
+++ go/pkg/common/constants.go
@@ -3,4 +3,8 @@ package common
 const (
 	DefaultTenant   = "default_tenant"
 	DefaultDatabase = "default_database"
+
+	// SourceAttachedFunctionIDKey is the metadata key used to mark output collections
+	// and link them to their attached function.
+	SourceAttachedFunctionIDKey = "chroma:source_attached_function_id"
 )
