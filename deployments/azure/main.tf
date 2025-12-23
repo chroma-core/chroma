d934f8370d13a6be9a5d3c2e2cce0bd0a6cafc0a
Itai Smith <itaismith@gmail.com>
2025-12-23 14:41:13 -0800

    Update CLI versions

--- deployments/azure/main.tf
+++ deployments/azure/main.tf
@@ -17,7 +17,7 @@ variable "machine_type" {
 
 variable "chroma_version" {
   description = "Chroma version to install"
-  default     = "1.3.7"
+  default     = "1.4.0"
 }
 
 variable "chroma_server_auth_credentials" {
