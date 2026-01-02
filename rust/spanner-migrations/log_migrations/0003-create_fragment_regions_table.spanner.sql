62532d65a7d1303d693d26be008ca30dc2fb0ee2
Robert Escriva <robert@trychroma.com>
2026-01-02 15:18:44 -0800

    interleave in parent

--- rust/spanner-migrations/log_migrations/0003-create_fragment_regions_table.spanner.sql
+++ rust/spanner-migrations/log_migrations/0003-create_fragment_regions_table.spanner.sql
@@ -2,4 +2,5 @@ CREATE TABLE IF NOT EXISTS fragment_regions (
     log_id STRING(36) NOT NULL,
     ident STRING(36) NOT NULL,
     region STRING(32) NOT NULL
-) PRIMARY KEY (log_id, ident, region);
+) PRIMARY KEY (log_id, ident, region),
+    INTERLEAVE IN PARENT fragments ON DELETE NO ACTION;
