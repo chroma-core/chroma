c2692a2abea29b107f6ed9702692b0bb9f0f0ee3
Sanket Kedia <sanket@trychroma.com>
2026-01-07 12:19:07 -0800

    [ENH]: Spanner collection and segments Schemas

---
+++ rust/spanner-migrations/migrations/0004-create_collections.spanner.sql
@@ -0,0 +1,14 @@
+-- Create collections table
+
+CREATE TABLE IF NOT EXISTS collections (
+    id STRING(MAX) NOT NULL,
+    name STRING(MAX) NOT NULL,
+    dimension INT64,
+    database_id STRING(MAX) NOT NULL,
+    database_name STRING(MAX) NOT NULL,
+    tenant_id STRING(MAX) NOT NULL,
+    is_deleted BOOL DEFAULT (false),
+    created_at TIMESTAMP NOT NULL OPTIONS (allow_commit_timestamp=true),
+    updated_at TIMESTAMP NOT NULL OPTIONS (allow_commit_timestamp=true),
+) PRIMARY KEY (id)
+
