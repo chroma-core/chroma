0e1d6bd782050df14deedd8ec2f561a6c05c0fa0
Sanket Kedia <sanket@trychroma.com>
2026-01-07 13:34:28 -0800

    fix test

--- rust/spanner-migrations/migrations/0004-create_collections.spanner.sql
+++ rust/spanner-migrations/migrations/0004-create_collections.spanner.sql
@@ -10,5 +10,5 @@ CREATE TABLE IF NOT EXISTS collections (
     is_deleted BOOL DEFAULT (false),
     created_at TIMESTAMP NOT NULL OPTIONS (allow_commit_timestamp=true),
     updated_at TIMESTAMP NOT NULL OPTIONS (allow_commit_timestamp=true)
-) PRIMARY KEY (id)
+) PRIMARY KEY (collection_id)
 
