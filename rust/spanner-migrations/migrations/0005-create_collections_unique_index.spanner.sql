a57778306588a3a6756c5fbe3a7c2f691cfccf75
Sanket Kedia <sanket@trychroma.com>
2026-01-09 13:14:56 -0800

    review comments

--- rust/spanner-migrations/migrations/0005-create_collections_unique_index.spanner.sql
+++ rust/spanner-migrations/migrations/0005-create_collections_unique_index.spanner.sql
@@ -1,4 +1,3 @@
 -- Create unique index on collections (database_id, name) to ensure collection names are unique within a database
-
-CREATE UNIQUE INDEX idx_collections_database_id_name ON collections (name, database_id);
+CREATE UNIQUE INDEX collection_unique_idx ON collections (database_id, name);
 
