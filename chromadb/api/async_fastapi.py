BlackRoad-AI/blackroad-chroma

[ENH]: Python client changes for indexing status

chromadb/api/async_fastapi.py
@@ -32,6 +32,7 @@ from chromadb.api.types import (
     Embeddings,
     IDs,
     Include,
+    IndexingStatus,
     Schema,
     Metadatas,
     URIs,
@@ -413,6 +414,27 @@ class AsyncFastAPI(BaseHTTPClient, AsyncServerAPI):
         )
         model = CollectionModel.from_json(resp_json)
         return model
+
+    @trace_method(
+        "AsyncFastAPI._get_indexing_status", OpenTelemetryGranularity.OPERATION
+    )
+    @override
+    async def _get_indexing_status(
+        self,
+        collection_id: UUID,
+        tenant: str = DEFAULT_TENANT,
+        database: str = DEFAULT_DATABASE,
+    ) -> IndexingStatus:
+        resp_json = await self._make_request(
+            "get",
+            f"/tenants/{tenant}/databases/{database}/collections/{collection_id}/indexing_status",
+        )
+        return IndexingStatus(
+            num_indexed_ops=resp_json["num_indexed_ops"],
+            num_unindexed_ops=resp_json["num_unindexed_ops"],
+            total_ops=resp_json["total_ops"],
+            indexing_progress=resp_json["op_indexing_progress"],
+        )
 
     @trace_method("AsyncFastAPI._search", OpenTelemetryGranularity.OPERATION)
     @override

chromadb/api/fastapi.py
@@ -26,6 +26,7 @@ from chromadb.api.types import (
     Embeddings,
     IDs,
     Include,
+    IndexingStatus,
     Schema,
     Metadatas,
     URIs,
@@ -378,6 +379,25 @@ class FastAPI(BaseHTTPClient, ServerAPI):
         )
         model = CollectionModel.from_json(resp_json)
         return model
+
+    @trace_method("FastAPI._get_indexing_status", OpenTelemetryGranularity.OPERATION)
+    @override
+    def _get_indexing_status(
+        self,
+        collection_id: UUID,
+        tenant: str = DEFAULT_TENANT,
+        database: str = DEFAULT_DATABASE,
+    ) -> IndexingStatus:
+        resp_json = self._make_request(
+            "get",
+            f"/tenants/{tenant}/databases/{database}/collections/{collection_id}/indexing_status",
+        )
+        return IndexingStatus(
+            num_indexed_ops=resp_json["num_indexed_ops"],
+            num_unindexed_ops=resp_json["num_unindexed_ops"],
+            total_ops=resp_json["total_ops"],
+            indexing_progress=resp_json["op_indexing_progress"],
+        )
 
     @trace_method("FastAPI._search", OpenTelemetryGranularity.OPERATION)
     @override

chromadb/api/models/AsyncCollection.py
@@ -6,6 +6,7 @@ from chromadb.api.types import (
     Embedding,
     PyEmbedding,
     Include,
+    IndexingStatus,
     Metadata,
     Document,
     Image,
@@ -91,6 +92,22 @@ class AsyncCollection(CollectionCommon["AsyncServerAPI"]):
 
         """
         return await self._client._count(
+            collection_id=self.id,
+            tenant=self.tenant,
+            database=self.database,
+        )
+
+    async def get_indexing_status(self) -> IndexingStatus:
+        """Get the indexing status of this collection.
+
+        Returns:
+            IndexingStatus: A dict containing:
+                - num_indexed_ops: Number of user operations that have been indexed
+                - num_unindexed_ops: Number of user operations pending indexing
+                - total_ops: Total number of user operations in collection
+                - indexing_progress: Proportion of user operations that have been indexed as a float between 0 and 1
+        """
+        return await self._client._get_indexing_status(
             collection_id=self.id,
             tenant=self.tenant,
             database=self.database,

chromadb/api/models/Collection.py
@@ -7,6 +7,7 @@ from chromadb.api.types import (
     Embedding,
     PyEmbedding,
     Include,
+    IndexingStatus,
     Metadata,
     Document,
     Image,
@@ -45,6 +46,22 @@ class Collection(CollectionCommon["ServerAPI"]):
 
         """
         return self._client._count(
+            collection_id=self.id,
+            tenant=self.tenant,
+            database=self.database,
+        )
+
+    def get_indexing_status(self) -> IndexingStatus:
+        """Get the indexing status of this collection.
+
+        Returns:
+            IndexingStatus: A dict containing:
+                - num_indexed_ops: Number of user operations that have been indexed
+                - num_unindexed_ops: Number of user operations pending indexing
+                - total_ops: Total number of user operations in collection
+                - indexing_progress: Proportion of user operations that have been indexed as a float between 0 and 1
+        """
+        return self._client._get_indexing_status(
             collection_id=self.id,
             tenant=self.tenant,
             database=self.database,

chroma-core/chroma

chromadb/api/async_fastapi.py
@@ -32,6 +32,7 @@ from chromadb.api.types import (
     Embeddings,
     IDs,
     Include,
+    IndexingStatus,
     Schema,
     Metadatas,
     URIs,
@@ -413,6 +414,27 @@ class AsyncFastAPI(BaseHTTPClient, AsyncServerAPI):
         )
         model = CollectionModel.from_json(resp_json)
         return model
+
+    @trace_method(
+        "AsyncFastAPI._get_indexing_status", OpenTelemetryGranularity.OPERATION
+    )
+    @override
+    async def _get_indexing_status(
+        self,
+        collection_id: UUID,
+        tenant: str = DEFAULT_TENANT,
+        database: str = DEFAULT_DATABASE,
+    ) -> IndexingStatus:
+        resp_json = await self._make_request(
+            "get",
+            f"/tenants/{tenant}/databases/{database}/collections/{collection_id}/indexing_status",
+        )
+        return IndexingStatus(
+            num_indexed_ops=resp_json["num_indexed_ops"],
+            num_unindexed_ops=resp_json["num_unindexed_ops"],
+            total_ops=resp_json["total_ops"],
+            op_indexing_progress=resp_json["op_indexing_progress"],
+        )
 
     @trace_method("AsyncFastAPI._search", OpenTelemetryGranularity.OPERATION)
     @override

chromadb/api/fastapi.py
@@ -26,6 +26,7 @@ from chromadb.api.types import (
     Embeddings,
     IDs,
     Include,
+    IndexingStatus,
     Schema,
     Metadatas,
     URIs,
@@ -378,6 +379,25 @@ class FastAPI(BaseHTTPClient, ServerAPI):
         )
         model = CollectionModel.from_json(resp_json)
         return model
+
+    @trace_method("FastAPI._get_indexing_status", OpenTelemetryGranularity.OPERATION)
+    @override
+    def _get_indexing_status(
+        self,
+        collection_id: UUID,
+        tenant: str = DEFAULT_TENANT,
+        database: str = DEFAULT_DATABASE,
+    ) -> IndexingStatus:
+        resp_json = self._make_request(
+            "get",
+            f"/tenants/{tenant}/databases/{database}/collections/{collection_id}/indexing_status",
+        )
+        return IndexingStatus(
+            num_indexed_ops=resp_json["num_indexed_ops"],
+            num_unindexed_ops=resp_json["num_unindexed_ops"],
+            total_ops=resp_json["total_ops"],
+            op_indexing_progress=resp_json["op_indexing_progress"],
+        )
 
     @trace_method("FastAPI._search", OpenTelemetryGranularity.OPERATION)
     @override

chromadb/api/models/AsyncCollection.py
@@ -6,6 +6,7 @@ from chromadb.api.types import (
     Embedding,
     PyEmbedding,
     Include,
+    IndexingStatus,
     Metadata,
     Document,
     Image,
@@ -91,6 +92,22 @@ class AsyncCollection(CollectionCommon["AsyncServerAPI"]):
 
         """
         return await self._client._count(
+            collection_id=self.id,
+            tenant=self.tenant,
+            database=self.database,
+        )
+
+    async def get_indexing_status(self) -> IndexingStatus:
+        """Get the indexing status of this collection.
+
+        Returns:
+            IndexingStatus: An object containing:
+                - num_indexed_ops: Number of user operations that have been indexed
+                - num_unindexed_ops: Number of user operations pending indexing
+                - total_ops: Total number of user operations in collection
+                - op_indexing_progress: Proportion of user operations that have been indexed as a float between 0 and 1
+        """
+        return await self._client._get_indexing_status(
             collection_id=self.id,
             tenant=self.tenant,
             database=self.database,

chromadb/api/models/Collection.py
@@ -7,6 +7,7 @@ from chromadb.api.types import (
     Embedding,
     PyEmbedding,
     Include,
+    IndexingStatus,
     Metadata,
     Document,
     Image,
@@ -45,6 +46,22 @@ class Collection(CollectionCommon["ServerAPI"]):
 
         """
         return self._client._count(
+            collection_id=self.id,
+            tenant=self.tenant,
+            database=self.database,
+        )
+
+    def get_indexing_status(self) -> IndexingStatus:
+        """Get the indexing status of this collection.
+
+        Returns:
+            IndexingStatus: An object containing:
+                - num_indexed_ops: Number of user operations that have been indexed
+                - num_unindexed_ops: Number of user operations pending indexing
+                - total_ops: Total number of user operations in collection
+                - op_indexing_progress: Proportion of user operations that have been indexed as a float between 0 and 1
+        """
+        return self._client._get_indexing_status(
             collection_id=self.id,
             tenant=self.tenant,
             database=self.database,