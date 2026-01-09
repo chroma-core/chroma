b23cc3b2c79c94d5d3654566735b3f2ceed77577
Sanket Kedia <sanket@trychroma.com>
2026-01-09 13:21:08 -0800

    change index name

--- rust/spanner-migrations/migrations/0003-create_databases_unique_index.spanner.sql
+++ rust/spanner-migrations/migrations/0003-create_databases_unique_index.spanner.sql
@@ -1,5 +1,5 @@
 -- Create unique index on databases (name, tenant_id)
 -- This ensures that database names are unique within a tenant
 
-CREATE UNIQUE INDEX idx_tenantid_name ON databases (name, tenant_id);
+CREATE UNIQUE INDEX database_unique_idx ON databases (tenant_id, name);
 
