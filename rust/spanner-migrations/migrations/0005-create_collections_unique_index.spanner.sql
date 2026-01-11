7415a8390e0f1efefc61c22bef2c33f6012bbcfa
Sanket Kedia <sanket@trychroma.com>
2026-01-11 13:20:15 -0800

    remove redundant index

--- rust/spanner-migrations/migrations/0005-create_collections_unique_index.spanner.sql
+++
@@ -1,3 +0,0 @@
--- Create unique index on collections (database_id, name) to ensure collection names are unique within a database
-CREATE UNIQUE INDEX collection_unique_idx ON collections (database_id, name);
-
