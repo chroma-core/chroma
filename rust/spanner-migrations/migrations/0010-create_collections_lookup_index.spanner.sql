101647d5122b0d52880073d71fb35ff7be6b989b
Sanket Kedia <sanket@trychroma.com>
2026-01-13 15:40:26 -0800

    fix migration hash

--- rust/spanner-migrations/migrations/0010-create_collections_lookup_index.spanner.sql
+++
@@ -1,4 +0,0 @@
--- Unique index for looking up collections by name within a tenant/database
--- Ensures collection names are unique within a database
-CREATE UNIQUE INDEX collections_lookup_idx ON collections (tenant_id, database_name, name);
-
