a770f9e93fe1ceef496bc35d2e8d7478a9de15b3
Robert Escriva <robert@trychroma.com>
2026-01-22 14:30:42 -0800

    remove default database

--- rust/spanner-migrations/migrations/0013-insert_default_database.spanner.sql
+++
@@ -1,3 +0,0 @@
-DML:
-INSERT INTO databases (id, name, tenant_id, created_at, updated_at)
-VALUES ('00000000-0000-0000-0000-000000000000', 'default_database', 'default_tenant', PENDING_COMMIT_TIMESTAMP(), PENDING_COMMIT_TIMESTAMP());
