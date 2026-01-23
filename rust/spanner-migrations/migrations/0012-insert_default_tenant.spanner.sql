38f22dca2a0fe550704a091cc8063b3d3562851e
Robert Escriva <robert@trychroma.com>
2026-01-23 11:02:53 -0800

    fix migration

--- rust/spanner-migrations/migrations/0012-insert_default_tenant.spanner.sql
+++ rust/spanner-migrations/migrations/0012-insert_default_tenant.spanner.sql
@@ -1,4 +1,3 @@
 DML:
-INSERT INTO tenants (id, created_at, updated_at, last_compaction_time)
-SELECT 'default_tenant', PENDING_COMMIT_TIMESTAMP(), PENDING_COMMIT_TIMESTAMP(), TIMESTAMP '1970-01-01 00:00:00+00'
-WHERE NOT EXISTS (SELECT 1 FROM tenants WHERE id = 'default_tenant');
+INSERT OR IGNORE INTO tenants (id, created_at, updated_at, last_compaction_time)
+VALUES ('default_tenant', PENDING_COMMIT_TIMESTAMP(), PENDING_COMMIT_TIMESTAMP(), TIMESTAMP '1970-01-01 00:00:00+00');
