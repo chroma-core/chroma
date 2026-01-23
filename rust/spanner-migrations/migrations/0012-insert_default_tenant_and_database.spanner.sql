524708bfb07c99744cb63d71546556dda9c0a805
Robert Escriva <robert@trychroma.com>
2026-01-23 09:00:27 -0800

    propel1

--- rust/spanner-migrations/migrations/0012-insert_default_tenant_and_database.spanner.sql
+++ rust/spanner-migrations/migrations/0012-insert_default_tenant_and_database.spanner.sql
@@ -1,3 +1,4 @@
 DML:
 INSERT INTO tenants (id, created_at, updated_at, last_compaction_time)
-VALUES ('default_tenant', PENDING_COMMIT_TIMESTAMP(), PENDING_COMMIT_TIMESTAMP(), TIMESTAMP '1970-01-01 00:00:00+00');
+SELECT 'default_tenant', PENDING_COMMIT_TIMESTAMP(), PENDING_COMMIT_TIMESTAMP(), TIMESTAMP '1970-01-01 00:00:00+00'
+WHERE NOT EXISTS (SELECT 1 FROM tenants WHERE id = 'default_tenant');
