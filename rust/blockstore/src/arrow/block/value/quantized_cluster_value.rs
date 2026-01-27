d752911e523c1b23ed1cc7f77fb724e3353b0705
macronova <sicheng@trychroma.com>
2026-01-26 21:05:01 -0800

    Writable value

--- rust/blockstore/src/arrow/block/value/quantized_cluster_value.rs
+++ rust/blockstore/src/arrow/block/value/quantized_cluster_value.rs
@@ -1,14 +1,21 @@
 use std::sync::Arc;
 
 use arrow::{
-    array::{Array, ListArray, PrimitiveArray, StructArray},
-    datatypes::{ArrowPrimitiveType, Float32Type, UInt64Type, UInt8Type},
+    array::{
+        Array, ArrayRef, Float32Builder, ListArray, ListBuilder, PrimitiveArray, StructArray,
+        UInt64Builder, UInt8Builder,
+    },
+    datatypes::{ArrowPrimitiveType, DataType, Field, Fields, Float32Type, UInt64Type, UInt8Type},
 };
-use chroma_types::QuantizedCluster;
+use chroma_types::{QuantizedCluster, QuantizedClusterOwned};
 
-use crate::arrow::{
-    block::delta::BlockStorage,
-    types::{ArrowReadableValue, ArrowWriteableKey},
+use crate::{
+    arrow::{
+        block::delta::{BlockStorage, UnorderedBlockDelta},
+        types::{ArrowReadableValue, ArrowWriteableKey, ArrowWriteableValue},
+    },
+    key::KeyWrapper,
+    BlockfileWriterMutationOrdering,
 };
 
 const CENTER_COLUMN: usize = 0;
@@ -36,6 +43,148 @@ fn get_list_array(struct_array: &StructArray, column: usize) -> &ListArray {
         .as_any()
         .downcast_ref::<ListArray>()
         .expect("expected list array")
+}
+
+pub struct QuantizedClusterSizeTracker {
+    pub cluster_count: usize,
+    pub code_length: usize,
+    pub dimension: usize,
+    pub vector_count: usize,
+}
+
+pub struct QuantizedClusterArrowBuilder {
+    center: ListBuilder<Float32Builder>,
+    codes: ListBuilder<UInt8Builder>,
+    ids: ListBuilder<UInt64Builder>,
+    versions: ListBuilder<UInt64Builder>,
+}
+
+impl ArrowWriteableValue for QuantizedCluster<'_> {
+    type ReadableValue<'data> = QuantizedCluster<'data>;
+    type PreparedValue = QuantizedClusterOwned;
+    type SizeTracker = QuantizedClusterSizeTracker;
+    type ArrowBuilder = QuantizedClusterArrowBuilder;
+
+    fn offset_size(_: usize) -> usize {
+        unimplemented!("not used for custom delta storage")
+    }
+
+    fn validity_size(_: usize) -> usize {
+        unimplemented!("not used for custom delta storage")
+    }
+
+    fn add(_prefix: &str, _key: KeyWrapper, _value: Self, _delta: &BlockStorage) {
+        todo!("Implement after QuantizedClusterDelta")
+    }
+
+    fn delete(_prefix: &str, _key: KeyWrapper, _delta: &UnorderedBlockDelta) {
+        todo!("Implement after QuantizedClusterDelta")
+    }
+
+    fn get_delta_builder(_: BlockfileWriterMutationOrdering) -> BlockStorage {
+        todo!("Implement after QuantizedClusterDelta")
+    }
+
+    fn get_arrow_builder(tracker: Self::SizeTracker) -> Self::ArrowBuilder {
+        QuantizedClusterArrowBuilder {
+            center: ListBuilder::with_capacity(
+                Float32Builder::with_capacity(tracker.cluster_count * tracker.dimension),
+                tracker.cluster_count,
+            ),
+            codes: ListBuilder::with_capacity(
+                UInt8Builder::with_capacity(tracker.vector_count * tracker.code_length),
+                tracker.cluster_count,
+            ),
+            ids: ListBuilder::with_capacity(
+                UInt64Builder::with_capacity(tracker.vector_count),
+                tracker.cluster_count,
+            ),
+            versions: ListBuilder::with_capacity(
+                UInt64Builder::with_capacity(tracker.vector_count),
+                tracker.cluster_count,
+            ),
+        }
+    }
+
+    fn prepare(value: Self) -> Self::PreparedValue {
+        QuantizedClusterOwned::from(value)
+    }
+
+    fn append(value: Self::PreparedValue, builder: &mut Self::ArrowBuilder) {
+        builder.center.values().append_slice(&value.center);
+        builder.center.append(true);
+
+        builder.codes.values().append_slice(&value.codes);
+        builder.codes.append(true);
+
+        builder.ids.values().append_slice(&value.ids);
+        builder.ids.append(true);
+
+        builder.versions.values().append_slice(&value.versions);
+        builder.versions.append(true);
+    }
+
+    fn finish(
+        mut builder: Self::ArrowBuilder,
+        _size_tracker: &Self::SizeTracker,
+    ) -> (Field, Arc<dyn Array>) {
+        let center_field = Field::new(
+            "center",
+            DataType::List(Arc::new(Field::new("item", DataType::Float32, true))),
+            true,
+        );
+        let codes_field = Field::new(
+            "codes",
+            DataType::List(Arc::new(Field::new("item", DataType::UInt8, true))),
+            true,
+        );
+        let ids_field = Field::new(
+            "ids",
+            DataType::List(Arc::new(Field::new("item", DataType::UInt64, true))),
+            true,
+        );
+        let versions_field = Field::new(
+            "versions",
+            DataType::List(Arc::new(Field::new("item", DataType::UInt64, true))),
+            true,
+        );
+
+        let center_array = builder.center.finish();
+        let codes_array = builder.codes.finish();
+        let ids_array = builder.ids.finish();
+        let versions_array = builder.versions.finish();
+
+        let struct_array = StructArray::from(vec![
+            (
+                Arc::new(center_field.clone()),
+                Arc::new(center_array) as ArrayRef,
+            ),
+            (
+                Arc::new(codes_field.clone()),
+                Arc::new(codes_array) as ArrayRef,
+            ),
+            (Arc::new(ids_field.clone()), Arc::new(ids_array) as ArrayRef),
+            (
+                Arc::new(versions_field.clone()),
+                Arc::new(versions_array) as ArrayRef,
+            ),
+        ]);
+
+        let struct_fields =
+            Fields::from(vec![center_field, codes_field, ids_field, versions_field]);
+        let value_field = Field::new("value", DataType::Struct(struct_fields), true);
+        let value_arr = (&struct_array as &dyn Array).slice(0, struct_array.len());
+
+        (value_field, value_arr)
+    }
+
+    fn get_owned_value_from_delta(
+        _prefix: &str,
+        _key: KeyWrapper,
+        _delta: &UnorderedBlockDelta,
+    ) -> Option<Self::PreparedValue> {
+        todo!("Implement after QuantizedClusterDelta")
+    }
 }
 
 impl<'data> ArrowReadableValue<'data> for QuantizedCluster<'data> {
@@ -85,6 +234,6 @@ impl<'data> ArrowReadableValue<'data> for QuantizedCluster<'data> {
         _value: Self,
         _storage: &mut BlockStorage,
     ) {
-        todo!("Implement after ArrowWriteableValue")
+        todo!("Implement after QuantizedClusterDelta")
     }
 }
