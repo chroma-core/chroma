dafbf0fea9adac645a5075dcefe10e72686940ca
macronova <sicheng@trychroma.com>
2026-02-13 16:04:40 -0800

    Avoid data loading in CPU opeartor

--- rust/worker/src/execution/operators/quantized_spann_load_cluster.rs
+++ rust/worker/src/execution/operators/quantized_spann_load_cluster.rs
@@ -1,7 +1,11 @@
+use std::collections::HashMap;
+
 use async_trait::async_trait;
 use chroma_error::{ChromaError, ErrorCodes};
-use chroma_segment::quantized_spann::QuantizedSpannSegmentReader;
+use chroma_segment::quantized_spann::{QuantizedSpannSegmentError, QuantizedSpannSegmentReader};
 use chroma_system::{Operator, OperatorType};
+use chroma_types::QuantizedClusterOwned;
+use futures::future::try_join_all;
 use thiserror::Error;
 
 #[derive(Debug)]
@@ -11,19 +15,20 @@ pub struct QuantizedSpannLoadClusterInput {
 
 #[derive(Debug)]
 pub struct QuantizedSpannLoadClusterOutput {
-    pub cluster_id: u32,
+    pub cluster: QuantizedClusterOwned,
+    pub global_versions: HashMap<u32, u32>,
 }
 
 #[derive(Error, Debug)]
 pub enum QuantizedSpannLoadClusterError {
-    #[error("Error loading quantized spann cluster")]
-    LoadClusterError,
+    #[error("Error loading quantized spann cluster: {0}")]
+    LoadClusterError(#[from] QuantizedSpannSegmentError),
 }
 
 impl ChromaError for QuantizedSpannLoadClusterError {
     fn code(&self) -> ErrorCodes {
         match self {
-            Self::LoadClusterError => ErrorCodes::Internal,
+            Self::LoadClusterError(e) => e.code(),
         }
     }
 }
@@ -43,9 +48,21 @@ impl Operator<QuantizedSpannLoadClusterInput, QuantizedSpannLoadClusterOutput>
         &self,
         input: &QuantizedSpannLoadClusterInput,
     ) -> Result<QuantizedSpannLoadClusterOutput, QuantizedSpannLoadClusterError> {
-        self.reader.load_cluster(input.cluster_id).await;
+        let cluster = self.reader.get_cluster(input.cluster_id).await?;
+
+        let versions =
+            try_join_all(cluster.ids.iter().map(|&id| self.reader.get_version(id))).await?;
+
+        let global_versions = cluster
+            .ids
+            .iter()
+            .copied()
+            .zip(versions)
+            .collect::<HashMap<_, _>>();
+
         Ok(QuantizedSpannLoadClusterOutput {
-            cluster_id: input.cluster_id,
+            cluster,
+            global_versions,
         })
     }
 
