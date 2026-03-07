d52bc066bacdb74b116528ad0d6a8ad1b02c09db
itaismith <42597089+itaismith@users.noreply.github.com>
2026-03-06 22:06:35 -0800

    [ENH] Update Gemini EFs (#6577)
    ## Description of changes
    
    Updating Gemini EFs to support dim reduction

--- clients/new-js/packages/ai-embeddings/common/src/schema-utils.ts
+++ clients/new-js/packages/ai-embeddings/common/src/schema-utils.ts
@@ -3,6 +3,7 @@ import baseSchema from "../../../../../../schemas/embedding_functions/base_schem
 import chromaLangchainSchema from "../../../../../../schemas/embedding_functions/chroma_langchain.json";
 import cohereSchema from "../../../../../../schemas/embedding_functions/cohere.json";
 import defaultSchema from "../../../../../../schemas/embedding_functions/default.json";
+import googleGenaiSchema from "../../../../../../schemas/embedding_functions/google_genai.json";
 import googleGenerativeAiSchema from "../../../../../../schemas/embedding_functions/google_generative_ai.json";
 import googlePalmSchema from "../../../../../../schemas/embedding_functions/google_palm.json";
 import googleVertexSchema from "../../../../../../schemas/embedding_functions/google_vertex.json";
@@ -54,6 +55,7 @@ const schemaMap = {
 	"chroma-langchain": chromaLangchainSchema as Schema,
 	cohere: cohereSchema as Schema,
 	default: defaultSchema as Schema,
+	"google-gemini": googleGenaiSchema as Schema,
 	"google-generative-ai": googleGenerativeAiSchema as Schema,
 	"google-palm": googlePalmSchema as Schema,
 	"google-vertex": googleVertexSchema as Schema,
