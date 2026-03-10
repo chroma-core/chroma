eca66b7afd4b7aea227644f8c78030f16fc2837a
itaismith <42597089+itaismith@users.noreply.github.com>
2026-03-09 23:26:13 -0700

    [RELEASE] Python 1.5.5 (#6602)
    Re-release due to test PyPi race condition on uploading 1.5.4

--- deployments/azure/main.tf
+++ deployments/azure/main.tf
@@ -17,7 +17,7 @@ variable "machine_type" {
 
 variable "chroma_version" {
   description = "Chroma version to install"
-  default     = "1.5.4"
+  default     = "1.5.5"
 }
 
 variable "chroma_server_auth_credentials" {
