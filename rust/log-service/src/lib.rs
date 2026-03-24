ab4a15df0055b65b8c2fa6e3ce1115d55ecfad22
chroma-droid <tools.external+atlantis@trychroma.com>
2026-03-24 13:58:41 -0700

    [HOTFIX] applying PR #6718 to rc/2026-03-20 (#6722)
    This PR cherry-picks the commit f206bbf894bd8a9f3bcf4db7d7a01425c70640d7
    onto rc/2026-03-20. If there are unresolved conflicts, please resolve
    them manually.
    
    Co-authored-by: Robert Escriva <robert@trychroma.com>

--- rust/log-service/src/lib.rs
+++ rust/log-service/src/lib.rs
@@ -2068,15 +2068,17 @@ impl LogServer {
                 )));
             }
             Err(err) => {
+                tracing::error!(err = %err, "get_log_from_handle failure");
                 return Err(Status::unknown(err.to_string()));
             }
         };
         let mut messages = Vec::with_capacity(push_logs.records.len());
         for record in push_logs.records {
             let mut buf = vec![];
-            record
-                .encode(&mut buf)
-                .map_err(|err| Status::unknown(err.to_string()))?;
+            record.encode(&mut buf).map_err(|err| {
+                tracing::error!(err = %err, "proto encode failure");
+                Status::unknown(err.to_string())
+            })?;
             messages.push(buf);
         }
         let record_count = messages.len() as i32;
@@ -2089,7 +2091,10 @@ impl LogServer {
                     "batching",
                 ));
             }
-            Err(err) => return Err(Status::new(err.code().into(), err.to_string())),
+            Err(err) => {
+                tracing::error!(err = %err, "append_many failure");
+                return Err(Status::new(err.code().into(), err.to_string()));
+            }
         };
         if let Some(cache) = self.cache.as_ref() {
             let cache_key = cache_key_for_manifest_and_etag(collection_id);
