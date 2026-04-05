0a24242a92cc8e14285cec7c3b9632fae9dd779b
hammadb <hammad@trychroma.com>
2026-04-04 17:52:16 -0700

    [ENH] Add SparsePostingBlock

--- rust/blockstore/src/arrow/block/value/mod.rs
+++ rust/blockstore/src/arrow/block/value/mod.rs
@@ -3,6 +3,7 @@ pub(super) mod f32_value;
 pub(super) mod float32array_value;
 pub(super) mod quantized_cluster_value;
 pub(super) mod roaring_bitmap_value;
+pub(super) mod sparse_posting_block_value;
 pub(super) mod spann_posting_list_value;
 pub(super) mod str_value;
 pub(super) mod u32_value;
