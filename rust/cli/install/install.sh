00cb89f306e12d7efdefd7636ad05227ffc44deb
Itai Smith <itaismith@gmail.com>
2026-04-07 16:31:39 -0700

    [RELEASE] CLI 1.4.3 Python 1.5.7 JS 3.4.3

--- rust/cli/install/install.sh
+++ rust/cli/install/install.sh
@@ -7,7 +7,7 @@ set -e
 #   curl -sSL https://raw.githubusercontent.com/chroma-core/chroma/main/rust/cli/install/install.sh | bash
 # ----------------------------------------------
 REPO="chroma-core/chroma"
-RELEASE="cli-1.4.2"
+RELEASE="cli-1.4.3"
 
 OS=$(uname -s)
 ARCH=$(uname -m)
