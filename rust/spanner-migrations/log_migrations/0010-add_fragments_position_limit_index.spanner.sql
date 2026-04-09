abe6a70790802b276927017fddb913e9dcd83431
Robert Escriva <robert@trychroma.com>
2026-04-09 15:52:02 -0700

    [BUG]  Make the most recent log spanner-migration idempotent. (#6863)
    ## Description of changes
    
    A previous PR introduced an index in spanner, but it wasn't idempotently
    created.
    
    Idempotently create it.
    
    ## Test plan
    
    CI
        
    ## Migration plan
    
    It's in here.
    
    ## Observability plan
    
    Watch it.
    
    ## Documentation Changes
    
    N/A

--- rust/spanner-migrations/log_migrations/0010-add_fragments_position_limit_index.spanner.sql
+++ rust/spanner-migrations/log_migrations/0010-add_fragments_position_limit_index.spanner.sql
@@ -1,3 +1,3 @@
-CREATE INDEX fragments_by_position_limit
+CREATE INDEX IF NOT EXISTS fragments_by_position_limit
 ON fragments(log_id, position_limit)
 STORING (path, position_start, num_bytes, setsum);
