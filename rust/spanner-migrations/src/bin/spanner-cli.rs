4ff73f1235c335ccceea2ed3ff8e1d3897f50694
Robert Escriva <robert@trychroma.com>
2026-04-22 16:49:38 -0700

    cargo fmt

--- rust/spanner-migrations/src/bin/spanner-cli.rs
+++ rust/spanner-migrations/src/bin/spanner-cli.rs
@@ -2,22 +2,25 @@ use std::path::Path;
 
 const KNOWN_PATH: &str = "/opt/homebrew/share/google-cloud-sdk/bin/spanner-cli";
 
-
 fn main() {
     if Path::new(KNOWN_PATH).exists() {
         let mut pid = std::process::Command::new(KNOWN_PATH)
             .args(std::env::args_os().skip(1))
             .spawn()
             .expect("failed to spawn spanner-cli");
-        let exit = pid.wait().expect("could not wait for spanner-cli; zombies?");
+        let exit = pid
+            .wait()
+            .expect("could not wait for spanner-cli; zombies?");
         std::process::exit(exit.code().unwrap_or(if exit.success() { 0 } else { 1 }));
     }
-    eprintln!("spanner-cli not found at its known location.
+    eprintln!(
+        "spanner-cli not found at its known location.
 if you haven't run this tool before, the following should help; it's on you to have gcloud:
 
 ```console
 gcloud components install spanner-cli
 ```
-");
+"
+    );
     std::process::exit(127);
 }
