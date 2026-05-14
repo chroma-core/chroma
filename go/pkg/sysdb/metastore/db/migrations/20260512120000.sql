b8e03b23f04dfb5ab251401f1eca5bd6a5c5489d
hammadb <hammad@trychroma.com>
2026-05-13 17:33:39 -0700

    cln

--- go/pkg/sysdb/metastore/db/migrations/20260512120000.sql
+++ go/pkg/sysdb/metastore/db/migrations/20260512120000.sql
@@ -2,7 +2,7 @@
 INSERT INTO "public"."functions" ("id", "name", "is_incremental", "return_type", "is_async") VALUES (
   '9e3c7540-4ddd-40a2-bbff-ad9cb3f06efc',
   'http_generate',
-  false,
+  true,
   '{}',
   true,
 );
